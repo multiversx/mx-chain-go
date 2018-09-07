caa344c2e68a5963cbbf6143378995b465243b51
Sebastian Marian <sebastian.marian@oxsy.ro>
2018-09-07 11:50:57 +0300

    Test a simple P2P blockchain in Go (gossip blocks between peers)

--- p2p/testP2P.go
+++ p2p/testP2P.go
@@ -5,7 +5,8 @@ package main
 //a import "./ex03ONetTesting"
 //a import "./ex04TestDiscovery"
 //a import "./ex05ChatWithGossipPeerDiscovery"
-import "./ex06Floodsub"
+//a import "./ex06Floodsub"
+import "./ex07ChatWithGossip"
 
 func main() {
 	//comment and uncomment which test do you want to run
@@ -26,5 +27,8 @@ func main() {
 	//ex05ChatWithGossipPeerDiscovery.Main()
 
 	//example 6, test floodsub implementation
-	ex06Floodsub.Main()
+	//ex06Floodsub.Main()
+
+	//example 7, test peer chat and gossip
+	ex07ChatWithGossip.Main()
 }
