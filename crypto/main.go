7b6465714dbc3d3250df28912f30185707260a3f
mihai.stancu <stancu.mihai.dorian@gmail.com>
2018-09-13 02:24:52 +0200

    Refactoring

--- crypto/main.go
+++ crypto/main.go
@@ -6,15 +6,13 @@ import (
 	"elrond-go-sandbox/crypto/ed25519"
 	"elrond-go-sandbox/crypto/schnorr"
 	"fmt"
-	"gopkg.in/dedis/kyber.v2/group/edwards25519"
 )
 
 var group = ed25519.Group{}
-var curve = edwards25519.NewBlakeSHA256Ed25519()
 
 func main() {
-	privateKey := curve.Scalar().Pick(curve.RandomStream())
-	publicKey := curve.Point().Mul(privateKey, curve.Point().Base())
+	privateKey := group.RandomScalar()
+	publicKey := group.Mul(privateKey, group.G())
 
 	fmt.Printf("Generated private key: %s\n", privateKey)
 	fmt.Printf("Derived public key: %s\n\n", publicKey)
@@ -29,7 +27,8 @@ func main() {
 	fmt.Printf("Are the original and derived public keys the same? %t\n", publicKey.Equal(derivedPublicKey))
 	fmt.Printf("Is the signature legit w.r.t the original public key? %t\n\n", schnorr.Verify(group, message, r, s, publicKey, ed25519.Hash))
 
-	fakePublicKey := curve.Point().Mul(curve.Scalar().Neg(curve.Scalar().One()), publicKey)
+	fakePrivateKey := group.RandomScalar()
+	fakePublicKey := group.Mul(fakePrivateKey, group.G())
 	fmt.Printf("Fake public key: %s\n", fakePublicKey)
 	fmt.Printf("Is the signature legit w.r.t a fake public key? %t\n", schnorr.Verify(group, message, r, s, fakePublicKey, ed25519.Hash))
 }
