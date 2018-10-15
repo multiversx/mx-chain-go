5b54ee28a27d4f695e9b5e5f3e8a47ac71965b98
Sebastian Marian <sebastian.marian@oxsy.ro>
2018-10-15 19:04:24 +0300

    Implemented Chronology as a dynamic state machine mechanism. It uses implementation of Subrounder interface as an item in its list through which it iterates chronology and calls DoWork method for every Subrounder. Please see unit tests for more clearly understanding.

--- chronology/chronology.go
+++ chronology/chronology.go
@@ -1,19 +1,239 @@
 package chronology
 
 import (
-	epoch "github.com/ElrondNetwork/elrond-go-sandbox/chronology/epoch"
-	round "github.com/ElrondNetwork/elrond-go-sandbox/chronology/round"
+	"fmt"
+	"math"
 	"time"
+
+	"github.com/ElrondNetwork/elrond-go-sandbox/chronology/ntp"
 )
 
-type Epocher interface {
-	Print(epoch *epoch.Epoch)
+// SLEEP_TIME defines the time in milliseconds between each iteration made in StartRounds method
+const SLEEP_TIME = 5
+
+// Subround defines the type used to reffer the current subround
+type Subround int
+
+const (
+	// SR_UNKNOWN defines an unknown state of the round
+	SR_UNKNOWN Subround = math.MinInt64 // xzxzx
+	// SR_ABORDED defines an aborded state of the round
+	SR_ABORDED Subround = math.MinInt64 + 1 // xzxzxz
+	// SR_BEFORE_ROUND defines the state of the round before it's start
+	SR_BEFORE_ROUND Subround = math.MinInt64 + 2 // xzxzxz
+	// SR_AFTER_ROUND defines the state of the round after it's finish
+	SR_AFTER_ROUND Subround = math.MaxInt64
+)
+
+// Subrounder defines an interface which chronology deals with
+type Subrounder interface {
+	DoWork(*Chronology) bool
+	Next() Subround
+	Current() Subround
+	EndTime() int64
+	Name() string
 }
 
-type Rounder interface {
-	CreateRoundFromDateTime(genesisRoundTimeStamp time.Time, timeStamp time.Time, roundTimeDuration time.Duration, roundTimeDivision []time.Duration) *round.Round
-	UpdateRoundFromDateTime(genesisRoundTimeStamp time.Time, timeStamp time.Time, round *round.Round)
-	CreateRoundTimeDivision(time.Duration) []time.Duration
-	GetRoundStateFromDateTime(round *round.Round, timeStamp time.Time) round.RoundState
-	GetRoundStateName(roundState round.RoundState) string
+// Chronology defines the data needed by the chronology
+type Chronology struct {
+	doLog      bool
+	DoRun      bool
+	doSyncMode bool
+
+	round       *Round
+	genesisTime time.Time
+
+	selfSubround Subround
+	timeSubround Subround
+	clockOffset  time.Duration
+
+	subrounders []Subrounder
+	subrounds   map[Subround]int
+
+	syncTime ntp.SyncTimer
+
+	rounds int // only for statistic
+}
+
+// NewChronology defines a new Chronology object
+func NewChronology(doLog bool, doSyncMode bool, round *Round, genesisTime time.Time, syncTime ntp.SyncTimer) Chronology {
+	chr := Chronology{}
+
+	chr.doLog = doLog
+	chr.DoRun = true
+	chr.doSyncMode = doSyncMode
+
+	chr.round = round
+	chr.genesisTime = genesisTime
+
+	chr.selfSubround = SR_BEFORE_ROUND
+	chr.timeSubround = SR_BEFORE_ROUND
+	chr.clockOffset = syncTime.GetClockOffset()
+
+	chr.subrounders = make([]Subrounder, 0)
+	chr.subrounds = make(map[Subround]int)
+
+	chr.syncTime = syncTime
+
+	chr.rounds = 0
+
+	return chr
+}
+
+// initRound is call when a new round begins and do some init stuff
+func (chr *Chronology) initRound() {
+	chr.selfSubround = SR_BEFORE_ROUND
+
+	if len(chr.subrounders) > 0 {
+		chr.selfSubround = chr.subrounders[0].Current()
+	}
+
+	chr.clockOffset = chr.syncTime.GetClockOffset()
+}
+
+// AddSubrounder adds new Subrounder implementation to the chronology
+func (chr *Chronology) AddSubrounder(subRounder Subrounder) {
+	if chr.subrounds == nil {
+		chr.subrounds = make(map[Subround]int)
+	}
+
+	chr.subrounds[subRounder.Current()] = len(chr.subrounders)
+	chr.subrounders = append(chr.subrounders, subRounder)
+}
+
+// StartRounds actually starts the chronology and runs the subrounders loaded
+func (chr *Chronology) StartRounds() {
+	for chr.DoRun {
+		time.Sleep(SLEEP_TIME * time.Millisecond)
+		chr.startRound()
+	}
+}
+
+// startRound calls the current subrounder, given by the current time or by the finished tasks in this round
+func (chr *Chronology) startRound() {
+	subRound := chr.updateRound()
+
+	if chr.selfSubround == subRound {
+		sr := chr.loadSubrounder(subRound)
+		if sr != nil {
+			if sr.DoWork(chr) {
+				chr.selfSubround = sr.Next()
+			}
+		}
+	}
+}
+
+// updateRound updates rounds and subrounds inside round depending of the current time and sync mode
+func (chr *Chronology) updateRound() Subround {
+	oldRoundIndex := chr.round.index
+	oldTimeSubRound := chr.timeSubround
+
+	currentTime := chr.syncTime.GetCurrentTime(chr.clockOffset)
+	chr.round.UpdateRound(chr.genesisTime, currentTime)
+	chr.timeSubround = chr.GetSubroundFromDateTime(currentTime)
+
+	if oldRoundIndex != chr.round.index {
+		chr.rounds++ // only for statistic
+		chr.log(fmt.Sprintf("\n"+chr.GetFormatedCurrentTime()+"############################## ROUND %d BEGINS ##############################\n", chr.round.index))
+		chr.initRound()
+	}
+
+	if oldTimeSubRound != chr.timeSubround {
+		sr := chr.loadSubrounder(chr.timeSubround)
+		if sr != nil {
+			chr.log(fmt.Sprintf("\n" + chr.GetFormatedCurrentTime() + ".................... SUBROUND " + sr.Name() + " BEGINS ....................\n"))
+		}
+	}
+
+	subRound := chr.selfSubround
+
+	if chr.doSyncMode {
+		subRound = chr.timeSubround
+	}
+
+	return subRound
+}
+
+// loadSubrounder returns the implementation of Subrounder attached to the subround given
+func (chr *Chronology) loadSubrounder(subround Subround) Subrounder {
+	index := chr.subrounds[subround]
+	if index < 0 || index >= len(chr.subrounders) {
+		return nil
+	}
+
+	return chr.subrounders[index]
+}
+
+// GetSubroundFromDateTime returns subround in the current round related to the time given
+func (chr *Chronology) GetSubroundFromDateTime(timeStamp time.Time) Subround {
+
+	delta := timeStamp.Sub(chr.round.timeStamp).Nanoseconds()
+
+	if delta < 0 {
+		return SR_BEFORE_ROUND
+	}
+
+	if delta > chr.round.timeDuration.Nanoseconds() {
+		return SR_AFTER_ROUND
+	}
+
+	for i := 0; i < len(chr.subrounders); i++ {
+		if delta <= chr.subrounders[i].EndTime() {
+			return chr.subrounders[i].Current()
+		}
+	}
+
+	return SR_UNKNOWN
+}
+
+// log do logs of the chronology if doLog variable is set on true
+func (chr *Chronology) log(message string) {
+	if chr.doLog {
+		fmt.Printf(message + "\n")
+	}
+}
+
+// GetRoundIndex returns the current round index
+func (chr *Chronology) GetRoundIndex() int {
+	return chr.round.index
+}
+
+// GetSelfSubround returns the subround, related to the finished tasks in the current round
+func (chr *Chronology) GetSelfSubround() Subround {
+	return chr.selfSubround
+}
+
+// SetSelfSubround set self subround depending of the finished tasks in the current round
+func (chr *Chronology) SetSelfSubround(subRound Subround) {
+	chr.selfSubround = subRound
+}
+
+// GetTimeSubround returns the subround, related to the current time
+func (chr *Chronology) GetTimeSubround() Subround {
+	return chr.timeSubround
+}
+
+// GetClockOffset returns the current offset between local time and NTP
+func (chr *Chronology) GetClockOffset() time.Duration {
+	return chr.clockOffset
+}
+
+// SetClockOffset set current offset
+func (chr *Chronology) SetClockOffset(clockOffset time.Duration) {
+	chr.clockOffset = clockOffset
+}
+
+// GetSyncTimer returns the current implementation of SynchTimer interface
+func (chr *Chronology) GetSyncTimer() ntp.SyncTimer {
+	return chr.syncTime
+}
+
+// GetFormatedCurrentTime returns formated current time for printing synched with NTP
+func (chr *Chronology) GetFormatedCurrentTime() string {
+	return chr.syncTime.GetFormatedCurrentTime(chr.clockOffset)
+}
+
+// GetCurrentTime returns current time synched with NTP
+func (chr *Chronology) GetCurrentTime() time.Time {
+	return chr.syncTime.GetCurrentTime(chr.clockOffset)
 }
--- chronology/chronology_test.go
+++ chronology/chronology_test.go
@@ -1,46 +1,312 @@
 package chronology
 
 import (
-	epoch "github.com/ElrondNetwork/elrond-go-sandbox/chronology/epoch"
-	round "github.com/ElrondNetwork/elrond-go-sandbox/chronology/round"
-	"github.com/davecgh/go-spew/spew"
+	"fmt"
 	"testing"
 	"time"
+
+	"github.com/ElrondNetwork/elrond-go-sandbox/chronology/ntp"
+	"github.com/stretchr/testify/assert"
 )
 
+const (
+	SR_START_ROUND Subround = iota
+	SR_BLOCK
+	SR_COMITMENT_HASH
+	SR_BITMAP
+	SR_COMITMENT
+	SR_SIGNATURE
+	SR_END_ROUND
+)
+
+const ROUND_TIME_DURATION = time.Duration(10 * time.Millisecond)
+
+var GENESIS_TIME = time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), 0, 0, 0, 0, time.Local)
+
+// #################### <START_ROUND> ####################
+
+type SRStartRound struct {
+	Hits int
+}
+
+func (sr *SRStartRound) DoWork(chr *Chronology) bool {
+	sr.Hits++
+	fmt.Printf("DoStartRound with %d hits\n", sr.Hits)
+	return true
+}
+
+func (sr *SRStartRound) Current() Subround {
+	return SR_START_ROUND
+}
+
+func (sr *SRStartRound) Next() Subround {
+	return SR_BLOCK
+}
+
+func (sr *SRStartRound) EndTime() int64 {
+	return int64(5 * ROUND_TIME_DURATION / 100)
+}
+
+func (sr *SRStartRound) Name() string {
+	return "<START_ROUND>"
+}
+
+// #################### <BLOCK> ####################
+
+type SRBlock struct {
+	Hits int
+}
+
+func (sr *SRBlock) DoWork(chr *Chronology) bool {
+	sr.Hits++
+	fmt.Printf("DoBlock with %d hits\n", sr.Hits)
+	return true
+}
+
+func (sr *SRBlock) Current() Subround {
+	return SR_BLOCK
+}
+
+func (sr *SRBlock) Next() Subround {
+	return SR_COMITMENT_HASH
+}
+
+func (sr *SRBlock) EndTime() int64 {
+	return int64(25 * ROUND_TIME_DURATION / 100)
+}
+
+func (sr *SRBlock) Name() string {
+	return "<BLOCK>"
+}
+
+// #################### <COMITMENT_HASH> ####################
+
+type SRComitmentHash struct {
+	Hits int
+}
+
+func (sr *SRComitmentHash) DoWork(chr *Chronology) bool {
+	sr.Hits++
+	fmt.Printf("DoComitmentHash with %d hits\n", sr.Hits)
+	return true
+}
+
+func (sr *SRComitmentHash) Current() Subround {
+	return SR_COMITMENT_HASH
+}
+
+func (sr *SRComitmentHash) Next() Subround {
+	return SR_BITMAP
+}
+
+func (sr *SRComitmentHash) EndTime() int64 {
+	return int64(40 * ROUND_TIME_DURATION / 100)
+}
+
+func (sr *SRComitmentHash) Name() string {
+	return "<COMITMENT_HASH>"
+}
+
+// #################### <BITMAP> ####################
+
+type SRBitmap struct {
+	Hits int
+}
+
+func (sr *SRBitmap) DoWork(chr *Chronology) bool {
+	sr.Hits++
+	fmt.Printf("DoBitmap with %d hits\n", sr.Hits)
+	return true
+}
+
+func (sr *SRBitmap) Current() Subround {
+	return SR_BITMAP
+}
+
+func (sr *SRBitmap) Next() Subround {
+	return SR_COMITMENT
+}
+
+func (sr *SRBitmap) EndTime() int64 {
+	return int64(55 * ROUND_TIME_DURATION / 100)
+}
+
+func (sr *SRBitmap) Name() string {
+	return "<BITMAP>"
+}
+
+// #################### <COMITMENT> ####################
+
+type SRComitment struct {
+	Hits int
+}
+
+func (sr *SRComitment) DoWork(chr *Chronology) bool {
+	sr.Hits++
+	fmt.Printf("DoComitment with %d hits\n", sr.Hits)
+	return true
+}
+
+func (sr *SRComitment) Current() Subround {
+	return SR_COMITMENT
+}
+
+func (sr *SRComitment) Next() Subround {
+	return SR_SIGNATURE
+}
+
+func (sr *SRComitment) EndTime() int64 {
+	return int64(70 * ROUND_TIME_DURATION / 100)
+}
+
+func (sr *SRComitment) Name() string {
+	return "<COMITMENT>"
+}
+
+// #################### <SIGNATURE> ####################
+
+type SRSignature struct {
+	Hits int
+}
+
+func (sr *SRSignature) DoWork(chr *Chronology) bool {
+	sr.Hits++
+	fmt.Printf("DoSignature with %d hits\n", sr.Hits)
+	return true
+}
+
+func (sr *SRSignature) Current() Subround {
+	return SR_SIGNATURE
+}
+
+func (sr *SRSignature) Next() Subround {
+	return SR_END_ROUND
+}
+
+func (sr *SRSignature) EndTime() int64 {
+	return int64(85 * ROUND_TIME_DURATION / 100)
+}
+
+func (sr *SRSignature) Name() string {
+	return "<SIGNATURE>"
+}
+
+// #################### <END_ROUND> ####################
+
+type SREndRound struct {
+	Hits int
+}
+
+func (sr *SREndRound) DoWork(chr *Chronology) bool {
+	sr.Hits++
+	fmt.Printf("DoEndRound with %d hits\n", sr.Hits)
+	return true
+}
+
+func (sr *SREndRound) Current() Subround {
+	return SR_END_ROUND
+}
+
+func (sr *SREndRound) Next() Subround {
+	return SR_START_ROUND
+}
+
+func (sr *SREndRound) EndTime() int64 {
+	return int64(100 * ROUND_TIME_DURATION / 100)
+}
+
+func (sr *SREndRound) Name() string {
+	return "<END_ROUND>"
+}
+
+func TestStartRound(t *testing.T) {
+
+	genesisTime := time.Now()
+	currentTime := genesisTime
+
+	rnd := NewRound(genesisTime, currentTime, ROUND_TIME_DURATION)
+	syncTime := &ntp.LocalTime{ClockOffset: ROUND_TIME_DURATION + 1}
+
+	chr := NewChronology(true, true, &rnd, genesisTime, syncTime)
+
+	chr.AddSubrounder(&SRStartRound{})
+	chr.AddSubrounder(&SRBlock{})
+	chr.AddSubrounder(&SRComitmentHash{})
+	chr.AddSubrounder(&SRBitmap{})
+	chr.AddSubrounder(&SRComitment{})
+	chr.AddSubrounder(&SRSignature{})
+	chr.AddSubrounder(&SREndRound{})
+
+	for {
+		chr.startRound()
+		if len(chr.subrounders) > 0 {
+			if chr.selfSubround == chr.subrounders[len(chr.subrounders)-1].Next() {
+				break
+			}
+		}
+	}
+}
+
 func TestRoundState(t *testing.T) {
+	currentTime := time.Now()
 
-	if round.RS_START_ROUND != 1 || round.RS_PROPOSE_BLOCK != 2 || round.RS_END_ROUND != 7 {
-		t.Fatal("Wrong values in round state enum")
-	}
+	rnd := NewRound(currentTime, currentTime, ROUND_TIME_DURATION)
+	chr := Chronology{round: &rnd}
+
+	state := chr.GetSubroundFromDateTime(currentTime.Add(-1 * time.Hour))
+	assert.Equal(t, SR_BEFORE_ROUND, state)
+
+	state = chr.GetSubroundFromDateTime(currentTime.Add(1 * time.Hour))
+	assert.Equal(t, SR_AFTER_ROUND, state)
+
+	state = chr.GetSubroundFromDateTime(currentTime)
+	assert.Equal(t, SR_UNKNOWN, state)
+
+	chr.AddSubrounder(&SRStartRound{})
+
+	state = chr.GetSubroundFromDateTime(currentTime)
+	assert.NotEqual(t, SR_UNKNOWN, state)
 }
 
-func TestEpochBehaviour(t *testing.T) {
+func TestLoadSubrounder(t *testing.T) {
+	chr := Chronology{}
 
-	es := GetEpocherService()
+	sr := chr.loadSubrounder(SR_BEFORE_ROUND)
+	assert.Nil(t, sr)
 
-	e := epoch.New(1, time.Now())
-	es.Print(&e)
+	sr = chr.loadSubrounder(SR_AFTER_ROUND)
+	assert.Nil(t, sr)
+
+	chr.AddSubrounder(&SRStartRound{})
+
+	sr = chr.loadSubrounder(SR_START_ROUND)
+	assert.NotNil(t, sr)
+
+	assert.Equal(t, sr.Name(), chr.subrounders[0].Name())
 }
 
-func TestRoundBehaviour(t *testing.T) {
+func TestGetters(t *testing.T) {
+	genesisTime := time.Now()
+	currentTime := genesisTime
 
-	rs := GetRounderService()
+	rnd := NewRound(genesisTime, currentTime, ROUND_TIME_DURATION)
+	syncTime := &ntp.LocalTime{ClockOffset: 0}
 
-	genesisRoundTimeStamp := time.Date(2018, time.September, 18, 14, 0, 0, 0, time.UTC)
+	chr := NewChronology(true, true, &rnd, genesisTime, syncTime)
 
-	duration := time.Duration(4 * time.Second)
-	division := rs.CreateRoundTimeDivision(duration)
+	assert.Equal(t, 0, chr.GetRoundIndex())
+	assert.Equal(t, SR_BEFORE_ROUND, chr.GetSelfSubround())
 
-	r := rs.CreateRoundFromDateTime(genesisRoundTimeStamp, time.Now(), duration, division)
+	chr.SetSelfSubround(SR_START_ROUND)
+	assert.Equal(t, SR_START_ROUND, chr.GetSelfSubround())
 
-	roundState := rs.GetRoundStateFromDateTime(r, time.Now())
+	assert.Equal(t, SR_BEFORE_ROUND, chr.GetTimeSubround())
+	assert.Equal(t, time.Duration(0), chr.GetClockOffset())
+	assert.NotNil(t, chr.GetSyncTimer())
+	assert.Equal(t, time.Duration(0), chr.GetSyncTimer().GetClockOffset())
 
-	if roundState < round.RS_START_ROUND || roundState > round.RS_END_ROUND {
-		t.Fatal("Wrong round state")
-	}
+	chr.SetClockOffset(time.Duration(5))
+	assert.Equal(t, time.Duration(5), chr.GetClockOffset())
 
-	spew.Dump(r)
-	spew.Dump(roundState)
-	spew.Dump(rs.GetRoundStateName(roundState))
+	fmt.Printf("%v\n%v", chr.GetCurrentTime(), chr.GetFormatedCurrentTime())
 }
