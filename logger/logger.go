2b6e77b33819a7309b86fa4bf9765b913e1ad5c8
ccorcov <cristi.corcoveanu@gmail.com>
2018-10-26 11:02:47 +0300

    Finished implementing logger. Also implemented all unit tests.

--- logger/logger.go
+++ logger/logger.go
@@ -2,40 +2,135 @@ package logger
 
 import (
 	"elrond/elrond-go-sandbox/config"
-	"fmt"
 	"io"
 	"os"
 	"path/filepath"
+	"runtime"
 	"time"
 
 	log "github.com/sirupsen/logrus"
 )
 
-func init() {
-	os.MkdirAll(config.ElrondLoggerConfig.LogPath, os.ModePerm)
-	file, err := currentLogFile()
-	if err == nil {
-		mw := io.MultiWriter(os.Stdout, file)
-		log.SetOutput(mw)
-	} else {
-		fmt.Println(err)
-		log.SetOutput(os.Stdout)
-	}
-	log.SetFormatter(&log.JSONFormatter{})
-	log.SetLevel(log.WarnLevel)
+const skip = 2
+const (
+	LvlDebugString 		= "DEBUG"
+	LvlInfoString 		= "INFO"
+	LvlWarningString 	= "WARNING"
+	LvlErrorString 		= "ERROR"
+	LvlPanicString	 	= "PANIC"
+)
+
+type ElrondLogger struct {
+	logger *log.Logger
 }
 
-func InvalidArgument(name string, value interface{}) {
-	log.WithFields(log.Fields{
-		"type": "invalid_argument",
-		"argument": name,
-		"value": value,
-	}).Warn("Invalid argument provided")
+var EL ElrondLogger
+func init() {
+	file, _ := currentLogFile()
+	EL = *NewElrondLogger(file)
+}
+
+// NewElrondLogger will setup the defaults of the application logger.
+// If the requested log file is writeble it will setup a MultiWriter on both the file
+//  and the standard output. Also sets up the level and the format for the logger
+func NewElrondLogger(file *os.File) *ElrondLogger {
+	el := &ElrondLogger{
+		log.New(),
+	}
+
+	if file != nil {
+		mw := io.MultiWriter(os.Stdout, file)
+		el.logger.SetOutput(mw)
+	} else {
+		el.logger.SetOutput(os.Stdout)
+	}
+
+	el.logger.SetFormatter(&log.JSONFormatter{})
+	el.logger.SetLevel(log.WarnLevel)
+	return el
+}
+
+// SetLevel sets the log level according to this package's defined levels
+func (el *ElrondLogger) SetLevel(level string) {
+	switch level {
+	case LvlDebugString:
+		el.logger.SetLevel(log.DebugLevel)
+	case LvlInfoString:
+		el.logger.SetLevel(log.InfoLevel)
+	case LvlWarningString:
+		el.logger.SetLevel(log.WarnLevel)
+	case LvlErrorString:
+		el.logger.SetLevel(log.ErrorLevel)
+	case LvlPanicString:
+		el.logger.SetLevel(log.PanicLevel)
+	default:
+		el.logger.SetLevel(log.ErrorLevel)
+	}
+}
+
+// SetOutput enables the posibility to change the output of the logger on demand
+func (el *ElrondLogger) SetOutput(out io.Writer) {
+	el.logger.SetOutput(out)
+}
+
+// Debug is an alias for Logrus.Debug, adding some default useful fields
+func (el *ElrondLogger) Debug(message string, extra ...interface{}) {
+	cl := el.defaultFields()
+	cl.WithFields(log.Fields{
+		"level": LvlDebugString,
+		"extra": extra,
+	}).Debug(message)
+}
+
+// Info is an alias for Logrus.Info, adding some default useful fields
+func (el *ElrondLogger) Info(message string, extra ...interface{}) {
+	cl := el.defaultFields()
+	cl.WithFields(log.Fields{
+		"level": LvlInfoString,
+		"extra": extra,
+	}).Info(message)
+}
+
+// Warn is an alias for Logrus.Warn, adding some default useful fields
+func (el *ElrondLogger) Warn(message string, extra ...interface{}) {
+	cl := el.defaultFields()
+	cl.WithFields(log.Fields{
+		"level": LvlWarningString,
+		"extra": extra,
+	}).Warn(message)
+}
+
+// Error is an alias for Logrus.Error, adding some default useful fields
+func (el *ElrondLogger) Error(message string, extra ...interface{}) {
+	cl := el.defaultFields()
+	cl.WithFields(log.Fields{
+		"level": LvlErrorString,
+		"extra": extra,
+	}).Error(message)
+}
+
+// Panic is an alias for Logrus.Panic, adding some default useful fields
+func (el *ElrondLogger) Panic(message string, extra ...interface{}) {
+	cl := el.defaultFields()
+	cl.WithFields(log.Fields{
+		"level": LvlPanicString,
+		"extra": extra,
+	}).Panic(message)
+}
+
+func (el *ElrondLogger) defaultFields() *log.Entry {
+	_, file, line, ok := runtime.Caller(skip)
+	return el.logger.WithFields(log.Fields{
+		"file": file,
+		"line_number": line,
+		"caller_ok": ok,
+	})
 }
 
 func currentLogFile() (*os.File, error) {
+	os.MkdirAll(config.ElrondLoggerConfig.LogPath, os.ModePerm)
 	return os.OpenFile(
 		filepath.Join(config.ElrondLoggerConfig.LogPath, time.Now().Format("01-02-2006") + ".log"),
-		os.O_APPEND|os.O_WRONLY,
+		os.O_CREATE|os.O_APPEND|os.O_WRONLY,
 		0666)
 }
--- logger/logger_test.go
+++ logger/logger_test.go
@@ -1,10 +1,129 @@
 package logger_test
 
 import (
+	"bytes"
 	"elrond/elrond-go-sandbox/logger"
+	"strings"
 	"testing"
+
+	"gotest.tools/assert"
 )
 
-func TestInvalidArgument(t *testing.T) {
-	logger.InvalidArgument("test", "4234242342342")
+func init() {
+	logger.EL.SetLevel(logger.LvlDebugString)
+}
+
+func TestDebug(t *testing.T) {
+	var str bytes.Buffer
+	logger.EL.SetOutput(&str)
+	logger.EL.Debug("abc")
+	logString := str.String()
+	assert.Assert(t, strings.Contains(logString, `"fields.level":"DEBUG"`))
+	assert.Assert(t, strings.Contains(logString, `"msg":"abc"`))
+}
+
+func TestInfo(t *testing.T) {
+	var str bytes.Buffer
+	logger.EL.SetOutput(&str)
+	logger.EL.Info("abc")
+	logString := str.String()
+	assert.Assert(t, strings.Contains(logString, `"fields.level":"INFO"`))
+	assert.Assert(t, strings.Contains(logString, `"msg":"abc"`))
+}
+
+func TestWarn(t *testing.T) {
+	var str bytes.Buffer
+	logger.EL.SetOutput(&str)
+	logger.EL.Warn("abc")
+	logString := str.String()
+	assert.Assert(t, strings.Contains(logString, `"fields.level":"WARNING"`))
+	assert.Assert(t, strings.Contains(logString, `"msg":"abc"`))
+}
+
+func TestError(t *testing.T) {
+	var str bytes.Buffer
+	logger.EL.SetOutput(&str)
+	logger.EL.Error("abc")
+	logString := str.String()
+	assert.Assert(t, strings.Contains(logString, `"fields.level":"ERROR"`))
+	assert.Assert(t, strings.Contains(logString, `"msg":"abc"`))
+}
+
+func TestPanic(t *testing.T) {
+	var str bytes.Buffer
+	logger.EL.SetOutput(&str)
+	swallowPanicLog(t, "abc", "TestPanic should have panic")
+
+	logString := str.String()
+	assert.Assert(t, strings.Contains(logString, `"fields.level":"PANIC"`))
+	assert.Assert(t, strings.Contains(logString, `"msg":"abc"`))
+}
+
+func TestSetLevel(t *testing.T) {
+	var str bytes.Buffer
+	logger.EL.SetOutput(&str)
+
+	logger.EL.SetLevel(logger.LvlDebugString)
+	logger.EL.Debug("abc")
+	assert.Assert(t, strings.Contains(str.String(), `"msg":"abc"`))
+	str.Reset()
+
+	logger.EL.SetLevel(logger.LvlInfoString)
+	logger.EL.Debug("abc")
+	assert.Assert(t, len(str.String()) == 0)
+	str.Reset()
+	logger.EL.Info("abc")
+	assert.Assert(t, strings.Contains(str.String(), `"msg":"abc"`))
+	str.Reset()
+
+	logger.EL.SetLevel(logger.LvlWarningString)
+	logger.EL.Info("abc")
+	assert.Assert(t, len(str.String()) == 0)
+	str.Reset()
+	logger.EL.Warn("abc")
+	assert.Assert(t, strings.Contains(str.String(), `"msg":"abc"`))
+	str.Reset()
+
+	logger.EL.SetLevel(logger.LvlErrorString)
+	logger.EL.Warn("abc")
+	assert.Assert(t, len(str.String()) == 0)
+	str.Reset()
+	logger.EL.Error("abc")
+	assert.Assert(t, strings.Contains(str.String(), `"msg":"abc"`))
+	str.Reset()
+
+	logger.EL.SetLevel(logger.LvlPanicString)
+	logger.EL.Error("abc")
+	assert.Assert(t, len(str.String()) == 0)
+	str.Reset()
+
+	swallowPanicLog(t, "abc", "TestSetLevel should have panic")
+	assert.Assert(t, strings.Contains(str.String(), `"msg":"abc"`))
+	str.Reset()
+
+	logger.EL.SetLevel("this should go on the default case")
+	logger.EL.Warn("abc")
+	assert.Assert(t, len(str.String()) == 0)
+	str.Reset()
+	logger.EL.Error("abc")
+	assert.Assert(t, strings.Contains(str.String(), `"msg":"abc"`))
+	str.Reset()
+}
+
+func TestNoOutputFile(t *testing.T) {
+	var str bytes.Buffer
+	el := logger.NewElrondLogger(nil)
+	el.SetLevel(logger.LvlWarningString)
+	el.SetOutput(&str)
+	el.Warn("abc")
+	assert.Assert(t, strings.Contains(str.String(), `"msg":"abc"`))
+}
+
+func swallowPanicLog(t *testing.T, logMsg string, panicMsg string) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf(panicMsg)
+		}
+	}()
+	logger.EL.Panic(logMsg)
 }
