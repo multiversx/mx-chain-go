e1a6b8ceb506fa7305c6bf7fbff09af1e13005c3
Radu Chis <radu@elrond.network>
2019-01-16 16:57:17 +0200

    Refactored routes for multipleTransaction generation

--- api/address/mock/facade.go
+++ api/address/mock/facade.go
@@ -6,14 +6,14 @@ import (
 
 type Facade struct {
 	BalanceHandler             func(string) (*big.Int, error)
-	GetCurrentPublicKeyHandler func() (string, error)
+	GetCurrentPublicKeyHandler func() string
 }
 
 func (f *Facade) GetBalance(address string) (*big.Int, error) {
 	return f.BalanceHandler(address)
 }
 
-func (f *Facade) GetCurrentPublicKey() (string, error) {
+func (f *Facade) GetCurrentPublicKey() string {
 	return f.GetCurrentPublicKeyHandler()
 }
 
