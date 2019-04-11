7450e8d59fcc97c30cfb90af43e73fe1388282a8
esimov <esimovaq@yahoo.com>
2019-04-11 09:16:23 +0300

    Include comments, small refactoring

--- storage/ccache/ccache.go
+++ storage/ccache/ccache.go
@@ -12,6 +12,7 @@ var log = logger.NewDefaultLogger()
 
 var mapKeys = []string{}
 
+// CCache implements a Concurrency type cache
 type CCache struct {
 	maxSize              int
 	cache                cmap.ConcurrentMap
@@ -19,6 +20,7 @@ type CCache struct {
 	addedDataHandlers    []func([]byte)
 }
 
+// NewCCache instantiates a new concurrency map cache
 func NewCCache(maxSize int) (*CCache, error) {
 	if maxSize <= 0 {
 		return nil, errors.New("must provide a positive value for cache size")
@@ -35,6 +37,7 @@ func NewCCache(maxSize int) (*CCache, error) {
 	}, nil
 }
 
+// Clear remove the cache items and clears the keys slice
 func (c *CCache) Clear() {
 	for key := range c.cache.Items() {
 		c.cache.Remove(key)
@@ -42,6 +45,8 @@ func (c *CCache) Clear() {
 	mapKeys = nil
 }
 
+// Put inserts a new element into the cache.
+// It also removes the oldest entry in case the cache size exeeds the maximum size.
 func (c *CCache) Put(key []byte, value interface{}) (evicted bool) {
 	// Remove the oldest values in case the cache size exceeds the maximum allowed size
 	if c.cache.Count() > c.maxSize {
@@ -57,6 +62,7 @@ func (c *CCache) Put(key []byte, value interface{}) (evicted bool) {
 	return false
 }
 
+// Get retrieves a cache element based on the provided key
 func (c *CCache) Get(key []byte) (interface{}, bool) {
 	item, ok := c.cache.Get(string(key))
 	if !ok {
@@ -65,6 +71,7 @@ func (c *CCache) Get(key []byte) (interface{}, bool) {
 	return item, ok
 }
 
+// Has checks if the cache contains the key
 func (c *CCache) Has(key []byte) bool {
 	if c.cache.Has(string(key)) {
 		return true
@@ -72,12 +79,9 @@ func (c *CCache) Has(key []byte) bool {
 	return false
 }
 
-func (c *CCache) Peek(key []byte) (value interface{}, ok bool) {
-	// Remove the oldest values in case the cache size exceeds the maximum allowed size
-	if c.cache.Count() > c.maxSize {
-		c.RemoveOldest()
-	}
-
+// Peek is identical with the Get method.
+// It has been implemented only to satisfy the interface method signatures.
+func (c *CCache) Peek(key []byte) (interface{}, bool) {
 	item, ok := c.cache.Items()[string(key)]
 	if !ok {
 		return nil, false
@@ -85,6 +89,8 @@ func (c *CCache) Peek(key []byte) (value interface{}, ok bool) {
 	return item, ok
 }
 
+// HasOrAdd checks if a key exists in the cache map and if not inserts the value.
+// In case the cache size exeeds the maximum allowed value, it removes the oldest entries.
 func (c *CCache) HasOrAdd(key []byte, value interface{}) (ok, evicted bool) {
 	if c.cache.Has(string(key)) {
 		return true, false
@@ -101,11 +107,13 @@ func (c *CCache) HasOrAdd(key []byte, value interface{}) (ok, evicted bool) {
 	return ok, false
 }
 
+// Remove removes the cache item based on the key
 func (c *CCache) Remove(key []byte) {
 	c.removeMapKey(key)
 	c.cache.Remove(string(key))
 }
 
+// RemoveOldest removes the oldest item from the cache
 func (c *CCache) RemoveOldest() {
 	if c.cache.IsEmpty() {
 		return
@@ -117,6 +125,7 @@ func (c *CCache) RemoveOldest() {
 	c.removeMapKey(oldest)
 }
 
+// FindOldest finds the oldest entry
 func (c *CCache) FindOldest() []byte {
 	for item := range c.cache.IterBuffered() {
 		if item.Key == mapKeys[0] {
@@ -126,6 +135,7 @@ func (c *CCache) FindOldest() []byte {
 	return []byte{}
 }
 
+// Keys returns a slice of the keys in the cache, from oldest to newest
 func (c *CCache) Keys() [][]byte {
 	res := c.cache.Keys()
 	r := make([][]byte, len(res))
@@ -137,10 +147,12 @@ func (c *CCache) Keys() [][]byte {
 	return r
 }
 
+// Len returns the number of items in the cache
 func (c *CCache) Len() int {
 	return c.cache.Count()
 }
 
+// RegisterHandler registers a new handler to be called when a new data is added
 func (c *CCache) RegisterHandler(handler func(key []byte)) {
 	if handler == nil {
 		log.Error("attempt to register a nil handler to a cacher object")
@@ -160,6 +172,7 @@ func (c *CCache) callAddedDataHandlers(key []byte) {
 	c.mutAddedDataHandlers.RUnlock()
 }
 
+// removeMapKey checks if a key from the cache exists in the keys slice and removes it
 func (c *CCache) removeMapKey(key []byte) {
 	if len(mapKeys) > 0 {
 		for i := 0; i < len(mapKeys); i++ {
