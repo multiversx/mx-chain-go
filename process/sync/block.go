7d3a6df0d7fa5d8b1c9a802be23599694844fea7
Sebastian Marian <sebastian.marian@oxsy.ro>
2019-04-12 20:05:10 +0300

    Removed the assignment boot.forkNonce = boot.forkDetector.GetHighestFinalBlockNonce() + 1 as this could opened an attack vector when a node would intentionally send an invalid block when a node is bootstrapping to make it rollback forever

--- process/sync/block.go
+++ process/sync/block.go
@@ -451,7 +451,6 @@ func (boot *Bootstrap) SyncBlock() error {
 		if isForkDetected {
 			log.Info(err.Error())
 			boot.removeHeaderFromPools(hdr)
-			boot.forkNonce = boot.forkDetector.GetHighestFinalBlockNonce() + 1
 			err = boot.forkChoice()
 		}
 
