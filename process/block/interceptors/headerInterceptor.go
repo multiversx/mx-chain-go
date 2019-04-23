c6ac9b99856ff341db8fa8e848ec23b7a132a53e
Sebastian Marian <sebastian.marian@oxsy.ro>
2019-04-23 17:56:36 +0300

    Implemented meta blcok processor

---
+++ process/block/interceptors/headerInterceptor.go
@@ -0,0 +1,84 @@
+package interceptors
+
+import (
+	"github.com/ElrondNetwork/elrond-go-sandbox/crypto"
+	"github.com/ElrondNetwork/elrond-go-sandbox/dataRetriever"
+	"github.com/ElrondNetwork/elrond-go-sandbox/hashing"
+	"github.com/ElrondNetwork/elrond-go-sandbox/marshal"
+	"github.com/ElrondNetwork/elrond-go-sandbox/p2p"
+	"github.com/ElrondNetwork/elrond-go-sandbox/process"
+	"github.com/ElrondNetwork/elrond-go-sandbox/process/block"
+	"github.com/ElrondNetwork/elrond-go-sandbox/sharding"
+	"github.com/ElrondNetwork/elrond-go-sandbox/storage"
+)
+
+// HeaderInterceptor represents an interceptor used for block headers
+type HeaderInterceptor struct {
+	hdrInterceptorBase *HeaderInterceptorBase
+	headers            storage.Cacher
+	headersNonces      dataRetriever.Uint64Cacher
+	shardCoordinator   sharding.Coordinator
+}
+
+// NewHeaderInterceptor hooks a new interceptor for block headers
+// Fetched block headers will be placed in a data pool
+func NewHeaderInterceptor(
+	marshalizer marshal.Marshalizer,
+	headers storage.Cacher,
+	headersNonces dataRetriever.Uint64Cacher,
+	storer storage.Storer,
+	multiSigVerifier crypto.MultiSigVerifier,
+	hasher hashing.Hasher,
+	shardCoordinator sharding.Coordinator,
+) (*HeaderInterceptor, error) {
+
+	if headersNonces == nil {
+		return nil, process.ErrNilHeadersNoncesDataPool
+	}
+	if headers == nil {
+		return nil, process.ErrNilHeadersDataPool
+	}
+	hdrBaseInterceptor, err := NewHeaderInterceptorBase(
+		marshalizer,
+		storer,
+		multiSigVerifier,
+		hasher,
+		shardCoordinator,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	hdrIntercept := &HeaderInterceptor{
+		hdrInterceptorBase: hdrBaseInterceptor,
+		headers:            headers,
+		headersNonces:      headersNonces,
+		shardCoordinator:   shardCoordinator,
+	}
+
+	return hdrIntercept, nil
+}
+
+// ProcessReceivedMessage will be the callback func from the p2p.Messenger and will be called each time a new message was received
+// (for the topic this validator was registered to)
+func (hi *HeaderInterceptor) ProcessReceivedMessage(message p2p.MessageP2P) error {
+	hdrIntercepted, err := hi.hdrInterceptorBase.ParseReceivedMessage(message)
+	if err == process.ErrHeaderIsInStorage {
+		log.Debug("intercepted block header already processed")
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+	if !hi.checkHeaderForCurrentShard(hdrIntercepted) {
+		return nil
+	}
+
+	_, _ = hi.headers.HasOrAdd(hdrIntercepted.Hash(), hdrIntercepted.GetHeader())
+	_, _ = hi.headersNonces.HasOrAdd(hdrIntercepted.GetHeader().Nonce, hdrIntercepted.Hash())
+	return nil
+}
+
+func (hi *HeaderInterceptor) checkHeaderForCurrentShard(header *block.InterceptedHeader) bool {
+	return hi.shardCoordinator.SelfId() == header.GetHeader().ShardId
+}
---
+++ process/block/interceptors/txBlockBodyInterceptors_test.go
@@ -0,0 +1,157 @@
+package interceptors_test
+
+import (
+	"bytes"
+	"errors"
+	"testing"
+
+	dataBlock "github.com/ElrondNetwork/elrond-go-sandbox/data/block"
+	"github.com/ElrondNetwork/elrond-go-sandbox/process"
+	"github.com/ElrondNetwork/elrond-go-sandbox/process/block/interceptors"
+	"github.com/ElrondNetwork/elrond-go-sandbox/process/mock"
+	"github.com/stretchr/testify/assert"
+)
+
+//------- NewMiniBlocksInterceptor
+
+func TestNewTxBlockBodyInterceptor_WithNilParameterShouldErr(t *testing.T) {
+	t.Parallel()
+
+	cache := &mock.CacherStub{}
+	storer := &mock.StorerStub{}
+
+	tbbi, err := interceptors.NewMiniBlocksInterceptor(
+		nil,
+		cache,
+		storer,
+		mock.HasherMock{},
+		mock.NewOneShardCoordinatorMock())
+
+	assert.Equal(t, process.ErrNilMarshalizer, err)
+	assert.Nil(t, tbbi)
+}
+
+func TestNewTxBlockBodyInterceptor_OkValsShouldWork(t *testing.T) {
+	t.Parallel()
+
+	cache := &mock.CacherStub{}
+	storer := &mock.StorerStub{}
+
+	tbbi, err := interceptors.NewMiniBlocksInterceptor(
+		&mock.MarshalizerMock{},
+		cache,
+		storer,
+		mock.HasherMock{},
+		mock.NewOneShardCoordinatorMock())
+
+	assert.Nil(t, err)
+	assert.NotNil(t, tbbi)
+}
+
+//------- ProcessReceivedMessage
+
+func TestTxBlockBodyInterceptor_ProcessReceivedMessageNilMessageShouldErr(t *testing.T) {
+	t.Parallel()
+
+	cache := &mock.CacherStub{}
+	storer := &mock.StorerStub{}
+
+	tbbi, _ := interceptors.NewMiniBlocksInterceptor(
+		&mock.MarshalizerMock{},
+		cache,
+		storer,
+		mock.HasherMock{},
+		mock.NewOneShardCoordinatorMock())
+
+	assert.Equal(t, process.ErrNilMessage, tbbi.ProcessReceivedMessage(nil))
+}
+
+func TestTxBlockBodyInterceptor_ProcessReceivedMessageNilMessageDataShouldErr(t *testing.T) {
+	t.Parallel()
+
+	cache := &mock.CacherStub{}
+	storer := &mock.StorerStub{}
+
+	tbbi, _ := interceptors.NewMiniBlocksInterceptor(
+		&mock.MarshalizerMock{},
+		cache,
+		storer,
+		mock.HasherMock{},
+		mock.NewOneShardCoordinatorMock())
+
+	msg := &mock.P2PMessageMock{}
+
+	assert.Equal(t, process.ErrNilDataToProcess, tbbi.ProcessReceivedMessage(msg))
+}
+
+func TestTxBlockBodyInterceptor_ProcessReceivedMessageMarshalizerErrorsAtUnmarshalingShouldErr(t *testing.T) {
+	t.Parallel()
+
+	errMarshalizer := errors.New("marshalizer error")
+
+	cache := &mock.CacherStub{}
+	storer := &mock.StorerStub{}
+
+	tbbi, _ := interceptors.NewMiniBlocksInterceptor(
+		&mock.MarshalizerStub{
+			UnmarshalCalled: func(obj interface{}, buff []byte) error {
+				return errMarshalizer
+			},
+		},
+		cache,
+		storer,
+		mock.HasherMock{},
+		mock.NewOneShardCoordinatorMock())
+
+	msg := &mock.P2PMessageMock{
+		DataField: make([]byte, 0),
+	}
+
+	assert.Equal(t, errMarshalizer, tbbi.ProcessReceivedMessage(msg))
+}
+
+func TestTxBlockBodyInterceptor_ProcessReceivedMessageBlockShouldWork(t *testing.T) {
+	t.Parallel()
+
+	marshalizer := &mock.MarshalizerMock{}
+
+	cache := &mock.CacherStub{}
+	storer := &mock.StorerStub{
+		HasCalled: func(key []byte) (b bool, e error) {
+			return false, nil
+		},
+	}
+
+	tbbi, _ := interceptors.NewMiniBlocksInterceptor(
+		marshalizer,
+		cache,
+		storer,
+		mock.HasherMock{},
+		mock.NewOneShardCoordinatorMock())
+
+	mb := dataBlock.MiniBlock{
+		ReceiverShardID: 0,
+		SenderShardID:   0,
+		TxHashes:        [][]byte{[]byte("tx hash 1")},
+	}
+	miniBlocks := dataBlock.Body{&mb}
+
+	buff, _ := marshalizer.Marshal(miniBlocks)
+	miniBlockBuff, _ := marshalizer.Marshal(mb)
+
+	msg := &mock.P2PMessageMock{
+		DataField: buff,
+	}
+
+	putInCacheWasCalled := false
+	cache.HasOrAddCalled = func(key []byte, value interface{}) (ok, evicted bool) {
+		if bytes.Equal(key, mock.HasherMock{}.Compute(string(miniBlockBuff))) {
+			putInCacheWasCalled = true
+		}
+
+		return false, false
+	}
+
+	assert.Nil(t, tbbi.ProcessReceivedMessage(msg))
+	assert.True(t, putInCacheWasCalled)
+}
