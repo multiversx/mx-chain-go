ffa655692d21255a1694d0c108ec8b2a0f33e034
Robert Sasu <sasu.robert@gmail.com>
2019-04-24 14:29:06 +0300

    Forgotten tests.

--- data/state/export_test.go
+++ data/state/export_test.go
@@ -1,9 +1,19 @@
 package state
 
+import "github.com/ElrondNetwork/elrond-go-sandbox/data/trie"
+
 func (adb *AccountsDB) LoadCode(accountWrapper AccountWrapper) error {
 	return adb.loadCode(accountWrapper)
 }
 
 func (adb *AccountsDB) GetAccount(addressContainer AddressContainer) (AccountWrapper, error) {
 	return adb.getAccount(addressContainer)
+}
+
+func (tdaw *TrackableDataTrie) OriginalData() map[string][]byte {
+	return tdaw.originalData
+}
+
+func (tdaw *TrackableDataTrie) DataTrie() trie.PatriciaMerkelTree {
+	return tdaw.tr
 }
