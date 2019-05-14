c1721d64d61dd1f895f9cac45e5339da0f292bce
iulianpascalau <iulian.pascalau@gmail.com>
2019-05-14 18:21:33 +0300

    node:  - added externalResolver struct

---
+++ node/externalResolver.go
@@ -0,0 +1,12 @@
+package node
+
+type ExternalResolver struct {
+}
+
+func NewExternalResolver() (*ExternalResolver, error) {
+
+}
+
+func (er *ExternalResolver) RecentShardBlocks() []interface{} {
+
+}
