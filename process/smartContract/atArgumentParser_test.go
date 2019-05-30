6b39f3f5cd7a82d1dfc8378d91a7d4153ce73b24
Robert Sasu <sasu.robert@gmail.com>
2019-05-30 13:07:51 +0300

    unit tests.

--- process/smartContract/atArgumentParser_test.go
+++ process/smartContract/atArgumentParser_test.go
@@ -1,1 +1,60 @@
 package smartContract
+
+import (
+	"github.com/ElrondNetwork/elrond-go-sandbox/process"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewAtArgumentParser(t *testing.T) {
+	t.Parallel()
+
+	parser, err := NewAtArgumentParser()
+
+	assert.Nil(t, err)
+	assert.NotNil(t, parser)
+
+	args, err := parser.GetArguments()
+	assert.Nil(t, args)
+	assert.Equal(t, process.ErrNilArguments, err)
+
+	code, err := parser.GetCode()
+	assert.Nil(t, code)
+	assert.Equal(t, process.ErrNilCode, err)
+
+	function, err := parser.GetFunction()
+	assert.Nil(t, function)
+	assert.Equal(t, process.ErrNilFunction, err)
+}
+
+func TestAtArgumentParser_GetArguments(t *testing.T) {
+	t.Parallel()
+}
+
+func TestAtArgumentParser_GetArgumentsNil(t *testing.T) {
+	t.Parallel()
+}
+
+func TestAtArgumentParser_GetCode(t *testing.T) {
+	t.Parallel()
+}
+
+func TestAtArgumentParser_GetCodeNil(t *testing.T) {
+	t.Parallel()
+}
+
+func TestAtArgumentParser_GetFunction(t *testing.T) {
+	t.Parallel()
+}
+
+func TestAtArgumentParser_GetFunctionNil(t *testing.T) {
+	t.Parallel()
+}
+
+func TestAtArgumentParser_ParseData(t *testing.T) {
+	t.Parallel()
+}
+
+func TestAtArgumentParser_ParseDataNil(t *testing.T) {
+	t.Parallel()
+}
--- process/smartContract/process_test.go
+++ process/smartContract/process_test.go
@@ -1,1 +1,31 @@
 package smartContract
+
+import "testing"
+
+func TestNewSmartContractProcessor(t *testing.T) {
+	t.Parallel()
+}
+
+func TestScProcessor_ComputeTransactionType(t *testing.T) {
+	t.Parallel()
+}
+
+func TestScProcessor_DeploySmartContract(t *testing.T) {
+	t.Parallel()
+}
+
+func TestScProcessor_ExecuteSmartContractTransaction(t *testing.T) {
+	t.Parallel()
+}
+
+func TestScProcessor_CreateVMCallInput(t *testing.T) {
+	t.Parallel()
+}
+
+func TestScProcessor_CreateVMCreateInput(t *testing.T) {
+	t.Parallel()
+}
+
+func TestScProcessor_CreateVMInput(t *testing.T) {
+	t.Parallel()
+}
