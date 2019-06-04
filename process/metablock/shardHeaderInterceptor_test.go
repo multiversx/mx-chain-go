12aa59cdf185e03a100ed2a8ab7de0693d7040c1
Robert Sasu <sasu.robert@gmail.com>
2019-06-04 18:25:59 +0300

    merge with master

--- process/metablock/shardHeaderInterceptor_test.go
+++ process/metablock/shardHeaderInterceptor_test.go
@@ -116,6 +116,13 @@ func TestShardHeaderInterceptor_ProcessReceivedMessageValsOkShouldWork(t *testin
 		return errors.New("Key not found")
 	}
 	hdrsNonces := &mock.Uint64CacherStub{}
+	hdrsNonces.PeekCalled = func(u uint64) (i interface{}, b bool) {
+		return nil, false
+	}
+	hdrsNonces.PutCalled = func(u uint64, i interface{}) bool {
+		return true
+	}
+
 	hi, _ := metablock.NewShardHeaderInterceptor(
 		marshalizer,
 		headers,
@@ -124,6 +131,76 @@ func TestShardHeaderInterceptor_ProcessReceivedMessageValsOkShouldWork(t *testin
 		multisigner,
 		mock.HasherMock{},
 		mock.NewOneShardCoordinatorMock(),
+		chronologyValidator,
+	)
+
+	hdr := block.NewInterceptedHeader(multisigner, chronologyValidator)
+	hdr.Nonce = testedNonce
+	hdr.ShardId = 0
+	hdr.PrevHash = make([]byte, 0)
+	hdr.PubKeysBitmap = make([]byte, 0)
+	hdr.BlockBodyType = dataBlock.TxBlock
+	hdr.Signature = make([]byte, 0)
+	hdr.SetHash([]byte("aaa"))
+	hdr.RootHash = make([]byte, 0)
+	hdr.PrevRandSeed = make([]byte, 0)
+	hdr.RandSeed = make([]byte, 0)
+	hdr.MiniBlockHeaders = make([]dataBlock.MiniBlockHeader, 0)
+
+	buff, _ := marshalizer.Marshal(hdr)
+	msg := &mock.P2PMessageMock{
+		DataField: buff,
+	}
+
+	headers.HasOrAddCalled = func(key []byte, value interface{}) (ok, evicted bool) {
+		aaaHash := mock.HasherMock{}.Compute(string(buff))
+		if bytes.Equal(aaaHash, key) {
+			chanDone <- struct{}{}
+		}
+		return false, false
+	}
+
+	assert.Nil(t, hi.ProcessReceivedMessage(msg))
+	select {
+	case <-chanDone:
+	case <-time.After(durTimeout):
+		assert.Fail(t, "timeout while waiting for block to be inserted in the pool")
+	}
+}
+
+func TestShardHeaderInterceptor_ProcessReceivedMessageVTestHdrNonces(t *testing.T) {
+	t.Parallel()
+
+	marshalizer := &mock.MarshalizerMock{}
+	chanDone := make(chan struct{}, 1)
+	testedNonce := uint64(67)
+	headers := &mock.CacherStub{}
+	multisigner := mock.NewMultiSigner()
+	chronologyValidator := &mock.ChronologyValidatorStub{
+		ValidateReceivedBlockCalled: func(shardID uint32, epoch uint32, nonce uint64, round uint32) error {
+			return nil
+		},
+	}
+	storer := &mock.StorerStub{}
+	storer.HasCalled = func(key []byte) error {
+		return errors.New("Key not found")
+	}
+	hdrsNonces := &mock.Uint64CacherStub{}
+	hdrsNonces.PeekCalled = func(u uint64) (i interface{}, b bool) {
+		return nil, false
+	}
+	hdrsNonces.PutCalled = func(u uint64, i interface{}) bool {
+		return true
+	}
+
+	hi, _ := metablock.NewShardHeaderInterceptor(
+		marshalizer,
+		headers,
+		hdrsNonces,
+		storer,
+		multisigner,
+		mock.HasherMock{},
+		mock.NewMultiShardsCoordinatorMock(2),
 		chronologyValidator,
 	)
 
