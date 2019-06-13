d1bcef02acb69678c164f71cf7105d2603afcb1b
Robert Sasu <sasu.robert@gmail.com>
2019-06-13 06:54:26 -0400

    Feat/en 2281 vm integration tests backend (#193)
    * vm integration tests:
     - created testInitializer.go
    
    * AddressConverter interface change. Added AddressLen method. Changed all implementations.
    Added first wip vm integration test
    
    * repairing some small stuff.
    
    * repairing some small stuff.
    
    * updated smartContract/process.go
    deploy SC test WIP
    
    * unit tests and fixed on smart processor.
    
    * deploy SC test finished
    bugfixes in smart contract processor and fake vm
    
    * deploy SC test finished
    bugfixes in smart contract processor and fake vm
    smartcontract processor tests fixed
    
    * added integration tests.
    
    * added more tests on vm integration - deploy (tests with transfer and gas)
    
    * updated test
    
    * integration tests for vm execute smart contract.
    
    * added a new test. wip.
    
    * finished tests. Added infinite tests for storage that are skipped now.
    
    * fix after review:
     - fixes on mock vm: added output for sender address
     - redesigned integrationTests/vm
     - removed storae integration tests
     - code reorg on smartContract/process
     - code reorg on transaction/process
    
    * integrationTests/vm:
     - removed commented code
    
    * process:
     - changed fake naming to temporary
    updated go.mod with latest vm-common release

---
+++ process/mock/temporaryAccountsHandlerMock.go
@@ -0,0 +1,37 @@
+package mock
+
+import (
+	"math/big"
+
+	"github.com/ElrondNetwork/elrond-go-sandbox/data/state"
+)
+
+type TemporaryAccountsHandlerMock struct {
+	AddTempAccountCalled    func(address []byte, balance *big.Int, nonce uint64)
+	CleanTempAccountsCalled func()
+	TempAccountCalled       func(address []byte) state.AccountHandler
+}
+
+func (tahm *TemporaryAccountsHandlerMock) AddTempAccount(address []byte, balance *big.Int, nonce uint64) {
+	if tahm.AddTempAccountCalled == nil {
+		return
+	}
+
+	tahm.AddTempAccountCalled(address, balance, nonce)
+}
+
+func (tahm *TemporaryAccountsHandlerMock) CleanTempAccounts() {
+	if tahm.CleanTempAccountsCalled == nil {
+		return
+	}
+
+	tahm.CleanTempAccountsCalled()
+}
+
+func (tahm *TemporaryAccountsHandlerMock) TempAccount(address []byte) state.AccountHandler {
+	if tahm.TempAccountCalled == nil {
+		return nil
+	}
+
+	return tahm.TempAccountCalled(address)
+}
