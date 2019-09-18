a97faf45c9493b4e7f99fc43d716388bf41ff8c5
iulianpascalau <iulian.pascalau@gmail.com>
2019-09-18 18:35:38 +0300

    added integration test for empty block double signing

--- integrationTests/sync/basicSync_test.go
+++ integrationTests/sync/basicSync_test.go
@@ -6,6 +6,8 @@ import (
 	"testing"
 	"time"
 
+	"github.com/ElrondNetwork/elrond-go/core"
+	"github.com/ElrondNetwork/elrond-go/data"
 	"github.com/ElrondNetwork/elrond-go/integrationTests"
 	"github.com/ElrondNetwork/elrond-go/sharding"
 	"github.com/stretchr/testify/assert"
@@ -86,6 +88,88 @@ func TestSyncWorksInShard_EmptyBlocksNoForks(t *testing.T) {
 	testAllNodesHaveTheSameBlockHeightInBlockchain(t, nodes)
 }
 
+func TestSyncWorksInShard_EmptyBlocksDoubleSign(t *testing.T) {
+	if testing.Short() {
+		t.Skip("this is not a short test")
+	}
+
+	maxShards := uint32(1)
+	shardId := uint32(0)
+	numNodesPerShard := 6
+
+	advertiser := integrationTests.CreateMessengerWithKadDht(context.Background(), "")
+	_ = advertiser.Bootstrap()
+	advertiserAddr := integrationTests.GetConnectableAddress(advertiser)
+
+	nodes := make([]*integrationTests.TestProcessorNode, numNodesPerShard)
+	for i := 0; i < numNodesPerShard; i++ {
+		nodes[i] = integrationTests.NewTestSyncNode(
+			maxShards,
+			shardId,
+			shardId,
+			advertiserAddr,
+		)
+	}
+
+	idxProposerShard0 := 0
+	idxProposers := []int{idxProposerShard0}
+
+	defer func() {
+		_ = advertiser.Close()
+		for _, n := range nodes {
+			_ = n.Messenger.Close()
+		}
+	}()
+
+	for _, n := range nodes {
+		_ = n.Messenger.Bootstrap()
+		_ = n.StartSync()
+	}
+
+	fmt.Println("Delaying for nodes p2p bootstrap...")
+	time.Sleep(delayP2pBootstrap)
+
+	round := uint64(0)
+	nonce := uint64(0)
+	round = integrationTests.IncrementAndPrintRound(round)
+	updateRound(nodes, round)
+	nonce++
+
+	numRoundsToTest := 2
+	for i := 0; i < numRoundsToTest; i++ {
+		integrationTests.ProposeBlock(nodes, idxProposers, round, nonce)
+
+		time.Sleep(stepDelay)
+
+		round = integrationTests.IncrementAndPrintRound(round)
+		updateRound(nodes, round)
+		nonce++
+	}
+
+	pubKeysVariant1 := []byte("1")
+	pubKeysVariant2 := []byte("2")
+
+	proposeBlockWithPubKeyBitmap(nodes[idxProposerShard0], round, nonce, pubKeysVariant1)
+	proposeBlockWithPubKeyBitmap(nodes[1], round, nonce, pubKeysVariant2)
+
+	time.Sleep(stepDelay)
+
+	round = integrationTests.IncrementAndPrintRound(round)
+	updateRound(nodes, round)
+
+	time.Sleep(3 * stepDelay)
+
+	testAllNodesHaveTheSameBlockHeightInBlockchain(t, nodes)
+	testAllNodesHaveSameLastBlock(t, nodes)
+}
+
+func proposeBlockWithPubKeyBitmap(n *integrationTests.TestProcessorNode, round uint64, nonce uint64, pubKeys []byte) {
+	body, header, _ := n.ProposeBlock(round, nonce)
+	header.SetPubKeysBitmap(pubKeys)
+	n.BroadcastBlock(body, header)
+	n.CommitBlock(body, header)
+}
+
 func testAllNodesHaveTheSameBlockHeightInBlockchain(t *testing.T, nodes []*integrationTests.TestProcessorNode) {
 	expectedNonce := nodes[0].BlockChain.GetCurrentBlockHeader().GetNonce()
 	for i := 1; i < len(nodes); i++ {
@@ -95,6 +179,19 @@ func testAllNodesHaveTheSameBlockHeightInBlockchain(t *testing.T, nodes []*integ
 			assert.Equal(t, expectedNonce, nodes[i].BlockChain.GetCurrentBlockHeader().GetNonce())
 		}
 	}
+}
+
+func testAllNodesHaveSameLastBlock(t *testing.T, nodes []*integrationTests.TestProcessorNode) {
+	mapBlocksByHash := make(map[string]data.HeaderHandler)
+
+	for _, n := range nodes {
+		hdr := n.BlockChain.GetCurrentBlockHeader()
+		buff, _ := core.CalculateHash(integrationTests.TestMarshalizer, integrationTests.TestHasher, hdr)
+
+		mapBlocksByHash[string(buff)] = hdr
+	}
+
+	assert.Equal(t, 1, len(mapBlocksByHash))
 }
 
 func updateRound(nodes []*integrationTests.TestProcessorNode, round uint64) {
