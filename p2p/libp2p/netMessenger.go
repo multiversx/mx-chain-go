066a4b17e8d7f427166d3a883d962cbb7805a8ce
iulianpascalau <iulian.pascalau@gmail.com>
2019-09-20 09:31:13 +0300

    added todo for DisableRelay

--- p2p/libp2p/netMessenger.go
+++ p2p/libp2p/netMessenger.go
@@ -86,6 +86,7 @@ func NewNetworkMessenger(
 		libp2p.DefaultSecurity,
 		libp2p.ConnectionManager(conMgr),
 		libp2p.DefaultTransports,
+		//TODO investigate if the DisableRelay is really needed and why
 		libp2p.DisableRelay(),
 		libp2p.NATPortMap(),
 	}
