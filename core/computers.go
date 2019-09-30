34fd69fb36cfded3d41760c0edc574f9c2666504
Sebastian Marian <sebastian.marian@oxsy.ro>
2019-09-30 11:40:03 +0300

    * Fixed unit tests

--- core/computers.go
+++ core/computers.go
@@ -12,6 +12,14 @@ func Max(a uint32, b uint32) uint32 {
 	return b
 }
 
+// Min returns the minimum number between two given
+func Min(a uint32, b uint32) uint32 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
 // IsHashInList signals if the given hash exists in the given list of hashes
 func IsHashInList(hash []byte, hashes [][]byte) bool {
 	for i := 0; i < len(hashes); i++ {
