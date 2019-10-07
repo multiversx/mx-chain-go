5ef23391e00b87f6b1d010d45f344d182cd057e9
Radu Chis <radu.chis@gmail.com>
2019-10-07 12:39:07 +0300

    En 4164 heartbeat refactor (#508)
    * EN-4164: heartbeat refactor
    
    * EN-1754: fix failing tests
    
    * heartbeatMessageInfo refactor
    
    * EN-4164: added a component
    
    * EN-4164: some changes
    
    * heartbeatStorer proposal
    
    * EN-4164: storer
    
    * EN-4164: added tests for storer
    
    * Update monitor
    
    * EN-4164: small fixes
    
    * updated heartbeat tests
    
    * EN-4164: merge with dev
    
    * Updated tests for heartbeat and monitor
    
    * Updated tests for heartbeat and monitor edge cases
    
    * Fix after review
    
    * Fix after review 2
    
    
    * Updated batchSize for HeartBeatDb to 1 fro 100
    
    * Moved heartbeatMessagInfo_test package from heartbeat to heartbeat_test

--- node/heartbeat/monitor.go
+++ node/heartbeat/monitor.go
@@ -1,7 +1,9 @@
 package heartbeat
 
 import (
+	"bytes"
 	"encoding/hex"
+	"fmt"
 	"sort"
 	"strings"
 	"sync"
@@ -9,7 +11,6 @@ import (
 
 	"github.com/ElrondNetwork/elrond-go/core"
 	"github.com/ElrondNetwork/elrond-go/core/logger"
-	"github.com/ElrondNetwork/elrond-go/crypto"
 	"github.com/ElrondNetwork/elrond-go/marshal"
 	"github.com/ElrondNetwork/elrond-go/p2p"
 	"github.com/ElrondNetwork/elrond-go/statusHandler"
@@ -19,64 +20,133 @@ var log = logger.DefaultLogger()
 
 // Monitor represents the heartbeat component that processes received heartbeat messages
 type Monitor struct {
-	singleSigner                crypto.SingleSigner
 	maxDurationPeerUnresponsive time.Duration
-	keygen                      crypto.KeyGenerator
 	marshalizer                 marshal.Marshalizer
 	heartbeatMessages           map[string]*heartbeatMessageInfo
 	mutHeartbeatMessages        sync.RWMutex
 	pubKeysMap                  map[uint32][]string
+	fullPeersSlice              [][]byte
 	mutPubKeysMap               sync.RWMutex
 	appStatusHandler            core.AppStatusHandler
+	genesisTime                 time.Time
+	messageHandler              MessageHandler
+	storer                      HeartbeatStorageHandler
+	timer                       Timer
 }
 
 // NewMonitor returns a new monitor instance
 func NewMonitor(
-	singleSigner crypto.SingleSigner,
-	keygen crypto.KeyGenerator,
 	marshalizer marshal.Marshalizer,
 	maxDurationPeerUnresponsive time.Duration,
 	pubKeysMap map[uint32][]string,
+	genesisTime time.Time,
+	messageHandler MessageHandler,
+	storer HeartbeatStorageHandler,
+	timer Timer,
 ) (*Monitor, error) {
 
-	if singleSigner == nil || singleSigner.IsInterfaceNil() {
-		return nil, ErrNilSingleSigner
-	}
-	if keygen == nil || keygen.IsInterfaceNil() {
-		return nil, ErrNilKeyGenerator
-	}
 	if marshalizer == nil || marshalizer.IsInterfaceNil() {
 		return nil, ErrNilMarshalizer
 	}
 	if len(pubKeysMap) == 0 {
 		return nil, ErrEmptyPublicKeysMap
 	}
-
-	pubKeysMapCopy := make(map[uint32][]string, 0)
+	if messageHandler == nil || messageHandler.IsInterfaceNil() {
+		return nil, ErrNilMessageHandler
+	}
+	if storer == nil || storer.IsInterfaceNil() {
+		return nil, ErrNilHeartbeatStorer
+	}
+	if timer == nil || timer.IsInterfaceNil() {
+		return nil, ErrNilTimer
+	}
 
 	mon := &Monitor{
-		singleSigner:                singleSigner,
-		keygen:                      keygen,
 		marshalizer:                 marshalizer,
 		heartbeatMessages:           make(map[string]*heartbeatMessageInfo),
 		maxDurationPeerUnresponsive: maxDurationPeerUnresponsive,
 		appStatusHandler:            &statusHandler.NilStatusHandler{},
+		genesisTime:                 genesisTime,
+		messageHandler:              messageHandler,
+		storer:                      storer,
+		timer:                       timer,
 	}
 
+	err := mon.storer.UpdateGenesisTime(genesisTime)
+	if err != nil {
+		return nil, err
+	}
+
+	err = mon.initializeHeartbeatMessagesInfo(pubKeysMap)
+	if err != nil {
+		return nil, err
+	}
+
+	err = mon.loadRestOfPubKeysFromStorage()
+	if err != nil {
+		log.Warn(fmt.Sprintf("heartbeat can't load public keys from storage: %s", err.Error()))
+	}
+
+	return mon, nil
+}
+
+func (m *Monitor) initializeHeartbeatMessagesInfo(pubKeysMap map[uint32][]string) error {
+	pubKeysMapCopy := make(map[uint32][]string, 0)
 	for shardId, pubKeys := range pubKeysMap {
 		for _, pubkey := range pubKeys {
-			pubKeysMapCopy[shardId] = append(pubKeysMapCopy[shardId], pubkey)
-			mhbi, err := newHeartbeatMessageInfo(maxDurationPeerUnresponsive, true)
-			if err != nil {
-				return nil, err
-			}
+			err := m.loadHbmiFromStorer(pubkey)
+			if err != nil { // if pubKey not found in DB, create a new instance
+				mhbi, errNewHbmi := newHeartbeatMessageInfo(m.maxDurationPeerUnresponsive, true, m.genesisTime, m.timer)
+				if errNewHbmi != nil {
+					return errNewHbmi
+				}
 
-			mhbi.computedShardID = shardId
-			mon.heartbeatMessages[pubkey] = mhbi
+				mhbi.genesisTime = m.genesisTime
+				mhbi.computedShardID = shardId
+				m.heartbeatMessages[pubkey] = mhbi
+			}
+			pubKeysMapCopy[shardId] = append(pubKeysMapCopy[shardId], pubkey)
 		}
 	}
-	mon.pubKeysMap = pubKeysMapCopy
-	return mon, nil
+
+	m.pubKeysMap = pubKeysMapCopy
+	return nil
+}
+
+func (m *Monitor) loadRestOfPubKeysFromStorage() error {
+	peersSlice, err := m.storer.LoadKeys()
+	if err != nil {
+		return err
+	}
+
+	for _, peer := range peersSlice {
+		_, ok := m.heartbeatMessages[string(peer)]
+		if !ok { // peer not in nodes map
+			err = m.loadHbmiFromStorer(string(peer))
+			if err != nil {
+				continue
+			}
+		}
+	}
+
+	return nil
+}
+
+func (m *Monitor) loadHbmiFromStorer(pubKey string) error {
+	hbmiDTO, err := m.storer.LoadHbmiDTO(pubKey)
+	if err != nil {
+		return err
+	}
+
+	receivedHbmi := m.convertFromExportedStruct(*hbmiDTO, m.maxDurationPeerUnresponsive)
+	receivedHbmi.getTimeHandler = m.timer.Now
+	receivedHbmi.isActive = m.timer.Now().Sub(receivedHbmi.lastUptimeDowntime) <= m.maxDurationPeerUnresponsive
+	receivedHbmi.lastUptimeDowntime = m.timer.Now()
+	receivedHbmi.genesisTime = m.genesisTime
+
+	m.heartbeatMessages[pubKey] = &receivedHbmi
+
+	return nil
 }
 
 // SetAppStatusHandler will set the AppStatusHandler which will be used for monitoring
@@ -92,46 +162,61 @@ func (m *Monitor) SetAppStatusHandler(ash core.AppStatusHandler) error {
 // ProcessReceivedMessage satisfies the p2p.MessageProcessor interface so it can be called
 // by the p2p subsystem each time a new heartbeat message arrives
 func (m *Monitor) ProcessReceivedMessage(message p2p.MessageP2P) error {
-	if message == nil || message.IsInterfaceNil() {
-		return ErrNilMessage
-	}
-	if message.Data() == nil {
-		return ErrNilDataToProcess
-	}
-
-	hbRecv := &Heartbeat{}
-
-	err := m.marshalizer.Unmarshal(hbRecv, message.Data())
-	if err != nil {
-		return err
-	}
-
-	err = m.verifySignature(hbRecv)
+	hbRecv, err := m.messageHandler.CreateHeartbeatFromP2pMessage(message)
 	if err != nil {
 		return err
 	}
 
 	//message is validated, process should be done async, method can return nil
-	go func(msg p2p.MessageP2P, hb *Heartbeat) {
-		m.mutHeartbeatMessages.Lock()
-		defer m.mutHeartbeatMessages.Unlock()
-
-		pe := m.heartbeatMessages[string(hb.Pubkey)]
-		if pe == nil {
-			pe, err = newHeartbeatMessageInfo(m.maxDurationPeerUnresponsive, false)
-			if err != nil {
-				log.Error(err.Error())
-				return
-			}
-			m.heartbeatMessages[string(hb.Pubkey)] = pe
-		}
-
-		computedShardID := m.computeShardID(string(hb.Pubkey))
-		pe.HeartbeatReceived(computedShardID, hb.ShardID, hb.VersionNumber, hb.NodeDisplayName)
-		m.updateAllHeartbeatMessages()
-	}(message, hbRecv)
+	go m.addHeartbeatMessageToMap(hbRecv)
 
 	return nil
+}
+
+func (m *Monitor) addHeartbeatMessageToMap(hb *Heartbeat) {
+	m.mutHeartbeatMessages.Lock()
+	defer m.mutHeartbeatMessages.Unlock()
+
+	pubKeyStr := string(hb.Pubkey)
+	hbmi, ok := m.heartbeatMessages[pubKeyStr]
+	if hbmi == nil || !ok {
+		var err error
+		hbmi, err = newHeartbeatMessageInfo(m.maxDurationPeerUnresponsive, false, m.genesisTime, m.timer)
+		if err != nil {
+			log.Error(err.Error())
+			return
+		}
+		m.heartbeatMessages[pubKeyStr] = hbmi
+	}
+
+	computedShardID := m.computeShardID(pubKeyStr)
+	hbmi.HeartbeatReceived(computedShardID, hb.ShardID, hb.VersionNumber, hb.NodeDisplayName)
+	hbDTO := m.convertToExportedStruct(hbmi)
+	err := m.storer.SavePubkeyData(hb.Pubkey, &hbDTO)
+	if err != nil {
+		log.Warn(fmt.Sprintf("cannot save heartbeat to db: %s", err.Error()))
+	}
+	m.addPeerToFullPeersSlice(hb.Pubkey)
+}
+
+func (m *Monitor) addPeerToFullPeersSlice(pubKey []byte) {
+	if !m.isPeerInFullPeersSlice(pubKey) {
+		m.fullPeersSlice = append(m.fullPeersSlice, pubKey)
+		err := m.storer.SaveKeys(m.fullPeersSlice)
+		if err != nil {
+			log.Warn(fmt.Sprintf("can't store the keys slice: %s", err.Error()))
+		}
+	}
+}
+
+func (m *Monitor) isPeerInFullPeersSlice(pubKey []byte) bool {
+	for _, peer := range m.fullPeersSlice {
+		if bytes.Equal(peer, pubKey) {
+			return true
+		}
+	}
+
+	return false
 }
 
 func (m *Monitor) computeShardID(pubkey string) uint32 {
@@ -151,27 +236,18 @@ func (m *Monitor) computeShardID(pubkey string) uint32 {
 	return m.heartbeatMessages[pubkey].computedShardID
 }
 
-func (m *Monitor) verifySignature(hbRecv *Heartbeat) error {
-	senderPubKey, err := m.keygen.PublicKeyFromByteArray(hbRecv.Pubkey)
-	if err != nil {
-		return err
-	}
-
-	copiedHeartbeat := *hbRecv
-	copiedHeartbeat.Signature = nil
-	buffCopiedHeartbeat, err := m.marshalizer.Marshal(copiedHeartbeat)
-	if err != nil {
-		return err
-	}
-
-	return m.singleSigner.Verify(senderPubKey, buffCopiedHeartbeat, hbRecv.Signature)
-}
-
-func (m *Monitor) updateAllHeartbeatMessages() {
+func (m *Monitor) computeAllHeartbeatMessages() {
 	counterActiveValidators := 0
 	counterConnectedNodes := 0
-	for _, v := range m.heartbeatMessages {
-		v.updateFields()
+	for pk, v := range m.heartbeatMessages {
+		//TODO change here
+		v.computeActive(m.timer.Now())
+
+		hbDTO := m.convertToExportedStruct(v)
+		err := m.storer.SavePubkeyData([]byte(pk), &hbDTO)
+		if err != nil {
+			log.Warn(fmt.Sprintf("cannot save heartbeat to db: %s", err.Error()))
+		}
 
 		if v.isActive {
 			counterConnectedNodes++
@@ -188,8 +264,10 @@ func (m *Monitor) updateAllHeartbeatMessages() {
 
 // GetHeartbeats returns the heartbeat status
 func (m *Monitor) GetHeartbeats() []PubKeyHeartbeat {
-	m.mutHeartbeatMessages.RLock()
+	m.mutHeartbeatMessages.Lock()
 	status := make([]PubKeyHeartbeat, len(m.heartbeatMessages))
+
+	m.computeAllHeartbeatMessages()
 
 	idx := 0
 	for k, v := range m.heartbeatMessages {
@@ -200,16 +278,15 @@ func (m *Monitor) GetHeartbeats() []PubKeyHeartbeat {
 			IsActive:        v.isActive,
 			ReceivedShardID: v.receivedShardID,
 			ComputedShardID: v.computedShardID,
-			TotalUpTime:     v.totalUpTime,
-			TotalDownTime:   v.totalDownTime,
+			TotalUpTime:     int(v.totalUpTime.Seconds()),
+			TotalDownTime:   int(v.totalDownTime.Seconds()),
 			VersionNumber:   v.versionNumber,
 			IsValidator:     v.isValidator,
 			NodeDisplayName: v.nodeDisplayName,
 		}
 		idx++
-
 	}
-	m.mutHeartbeatMessages.RUnlock()
+	m.mutHeartbeatMessages.Unlock()
 
 	sort.Slice(status, func(i, j int) bool {
 		return strings.Compare(status[i].HexPublicKey, status[j].HexPublicKey) < 0
@@ -224,4 +301,41 @@ func (m *Monitor) IsInterfaceNil() bool {
 		return true
 	}
 	return false
+}
+
+func (m *Monitor) convertToExportedStruct(v *heartbeatMessageInfo) HeartbeatDTO {
+	return HeartbeatDTO{
+		TimeStamp:          v.timeStamp,
+		MaxInactiveTime:    v.maxInactiveTime,
+		IsActive:           v.isActive,
+		ReceivedShardID:    v.receivedShardID,
+		ComputedShardID:    v.computedShardID,
+		TotalUpTime:        v.totalUpTime,
+		TotalDownTime:      v.totalDownTime,
+		VersionNumber:      v.versionNumber,
+		IsValidator:        v.isValidator,
+		NodeDisplayName:    v.nodeDisplayName,
+		LastUptimeDowntime: v.lastUptimeDowntime,
+		GenesisTime:        v.genesisTime,
+	}
+}
+
+func (m *Monitor) convertFromExportedStruct(hbDTO HeartbeatDTO, maxDuration time.Duration) heartbeatMessageInfo {
+	hbmi := heartbeatMessageInfo{
+		maxDurationPeerUnresponsive: maxDuration,
+		maxInactiveTime:             hbDTO.MaxInactiveTime,
+		timeStamp:                   hbDTO.TimeStamp,
+		isActive:                    hbDTO.IsActive,
+		totalUpTime:                 hbDTO.TotalUpTime,
+		totalDownTime:               hbDTO.TotalDownTime,
+		receivedShardID:             hbDTO.ReceivedShardID,
+		computedShardID:             hbDTO.ComputedShardID,
+		versionNumber:               hbDTO.VersionNumber,
+		nodeDisplayName:             hbDTO.NodeDisplayName,
+		isValidator:                 hbDTO.IsValidator,
+		lastUptimeDowntime:          hbDTO.LastUptimeDowntime,
+		genesisTime:                 hbDTO.GenesisTime,
+	}
+
+	return hbmi
 }
