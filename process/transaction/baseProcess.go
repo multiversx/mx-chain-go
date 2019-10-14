54aa2a34aadbc7ae21b2accf2e5e83c7c53457cd
Robert Sasu <sasu.robert@gmail.com>
2019-10-14 09:29:34 +0300

    started integrating system VM to metachain.

---
+++ process/transaction/baseProcess.go
@@ -0,0 +1,110 @@
+package transaction
+
+import (
+	"bytes"
+
+	"github.com/ElrondNetwork/elrond-go/data/state"
+	"github.com/ElrondNetwork/elrond-go/data/transaction"
+	"github.com/ElrondNetwork/elrond-go/process"
+	"github.com/ElrondNetwork/elrond-go/sharding"
+)
+
+type baseTxProcessor struct {
+	accounts         state.AccountsAdapter
+	shardCoordinator sharding.Coordinator
+	adrConv          state.AddressConverter
+}
+
+func (txProc *baseTxProcessor) getAccounts(
+	adrSrc, adrDst state.AddressContainer,
+) (*state.Account, *state.Account, error) {
+
+	var acntSrc, acntDst *state.Account
+
+	shardForCurrentNode := txProc.shardCoordinator.SelfId()
+	shardForSrc := txProc.shardCoordinator.ComputeId(adrSrc)
+	shardForDst := txProc.shardCoordinator.ComputeId(adrDst)
+
+	srcInShard := shardForSrc == shardForCurrentNode
+	dstInShard := shardForDst == shardForCurrentNode
+
+	if srcInShard && adrSrc == nil ||
+		dstInShard && adrDst == nil {
+		return nil, nil, process.ErrNilAddressContainer
+	}
+
+	if bytes.Equal(adrSrc.Bytes(), adrDst.Bytes()) {
+		acntWrp, err := txProc.accounts.GetAccountWithJournal(adrSrc)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		account, ok := acntWrp.(*state.Account)
+		if !ok {
+			return nil, nil, process.ErrWrongTypeAssertion
+		}
+
+		return account, account, nil
+	}
+
+	if srcInShard {
+		acntSrcWrp, err := txProc.accounts.GetAccountWithJournal(adrSrc)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		account, ok := acntSrcWrp.(*state.Account)
+		if !ok {
+			return nil, nil, process.ErrWrongTypeAssertion
+		}
+
+		acntSrc = account
+	}
+
+	if dstInShard {
+		acntDstWrp, err := txProc.accounts.GetAccountWithJournal(adrDst)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		account, ok := acntDstWrp.(*state.Account)
+		if !ok {
+			return nil, nil, process.ErrWrongTypeAssertion
+		}
+
+		acntDst = account
+	}
+
+	return acntSrc, acntDst, nil
+}
+
+func (txProc *baseTxProcessor) getAccountFromAddress(adrSrc state.AddressContainer) (state.AccountHandler, error) {
+	shardForCurrentNode := txProc.shardCoordinator.SelfId()
+	shardForSrc := txProc.shardCoordinator.ComputeId(adrSrc)
+	if shardForCurrentNode != shardForSrc {
+		return nil, nil
+	}
+
+	acnt, err := txProc.accounts.GetAccountWithJournal(adrSrc)
+	if err != nil {
+		return nil, err
+	}
+
+	return acnt, nil
+}
+
+func (txProc *baseTxProcessor) getAddresses(
+	tx *transaction.Transaction,
+) (state.AddressContainer, state.AddressContainer, error) {
+	adrSrc, err := txProc.adrConv.CreateAddressFromPublicKeyBytes(tx.SndAddr)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	adrDst, err := txProc.adrConv.CreateAddressFromPublicKeyBytes(tx.RcvAddr)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return adrSrc, adrDst, nil
+}
