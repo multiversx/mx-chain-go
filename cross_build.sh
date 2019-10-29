d02810cd457a28bc001041b9dfa7c263e0ccd58a
Traian Schiau <traian.schiau@gmail.com>
2019-10-29 16:59:59 +0200

    Add keygen build and archive

--- cross_build.sh
+++ cross_build.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-package=node
-package_split=(${package//\// })
-package_name=${package_split[-1]}
+packages=("node" "keygenerator")
 
 platforms=("windows/amd64" "darwin/amd64" "linux/amd64" "linux/arm64")
 BASEDIR=$(pwd)/cross_build
@@ -10,23 +8,30 @@ APP_VER=$(git describe --tags --long --dirty)
 echo "##teamcity[testSuiteStarted name='Cross_Build']"
 for platform in "${platforms[@]}"
 do
-    echo "##teamcity[testStarted name='$platform' captureStandardOutput='true']"
     platform_split=(${platform//\// })
     GOOS=${platform_split[0]}
     GOARCH=${platform_split[1]}
-    output_name=$BASEDIR/$GOOS/$GOARCH/$package_name
     echo "Building $output_name"
-    if [ $GOOS = "windows" ]; then
-        output_name+='.exe'
-    fi
+    for package_name in "${packages[@]}"
+    do
+        output_name=$BASEDIR/$GOOS/$GOARCH/$package_name
+        if [ $GOOS = "windows" ]; then
+            output_name+='.exe'
+        fi
 
-    pushd cmd/node
-    env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name -a -i -v -ldflags="-X main.appVersion=$APP_VER"
-    if [ $? -ne 0 ]; then
-        echo 'An error has occurred!'
-	echo "##teamcity[testFailed type='comparisonFailure' name='$platform']"
-    fi
-    popd
-    echo "##teamcity[testFinished name='$platform']"
+        echo "##teamcity[testStarted name='$package_name-$platform' captureStandardOutput='true']"
+        pushd "cmd/$package_name"
+        env GOOS=$GOOS GOARCH=$GOARCH go build -o "$output_name" -a -i -v -ldflags="-X main.appVersion=$APP_VER"
+        if [ $? -ne 0 ]; then
+            echo 'An error has occurred!'
+            echo "##teamcity[testFailed type='comparisonFailure' name='$package_name-$platform']"
+        fi
+            echo "##teamcity[testFinished name='$package_name-$platform']"
+        popd
+
+	pushd "$BASEDIR/$GOOS/$GOARCH/"
+	  tar czvf "$BASEDIR/elrond_""$APP_VER""_""$GOOS""_""$GOARCH"".tgz" *
+	popd
+    done
 done
 echo "##teamcity[testSuiteFinished name='Cross_Build']"
