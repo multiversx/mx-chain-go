49882708e4fa6b5319cf95888f35988555f9b913
Radu Chis <radu@elrond.network>
2019-10-30 14:18:41 +0200

    Verify Heartbeat properties length -> stop flood

--- node/heartbeat/messageProcessor_test.go
+++ node/heartbeat/messageProcessor_test.go
@@ -280,7 +280,7 @@ func TestNewMessageProcessor_CreateHeartbeatFromP2pMessageWithUnmarshaliableData
 	assert.Equal(t, expectedErr, err)
 }
 
-func TestNewMessageProcessor_CreateHeartbeatFromP2pMessageWithTooLongLengthsShouldTrim(t *testing.T) {
+func TestNewMessageProcessor_CreateHeartbeatFromP2pMessageWithTooLongLengthsShouldErr(t *testing.T) {
 	t.Parallel()
 
 	length := 129
@@ -335,9 +335,8 @@ func TestNewMessageProcessor_CreateHeartbeatFromP2pMessageWithTooLongLengthsShou
 
 	ret, err := mon.CreateHeartbeatFromP2pMessage(message)
 
-	assert.NotNil(t, ret)
-	assert.Equal(t, 128, len(ret.NodeDisplayName))
-	assert.Nil(t, err)
+	assert.Nil(t, ret)
+	assert.Equal(t, heartbeat.ErrPropertyTooLong, err)
 }
 
 func TestNewMessageProcessor_CreateHeartbeatFromP2pNilMessageShouldErr(t *testing.T) {
