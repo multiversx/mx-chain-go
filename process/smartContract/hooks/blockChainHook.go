86b7eb2f609429c765a029e9d7a91b5cc7a1cd17
Robert Sasu <sasu.robert@gmail.com>
2019-11-06 16:02:36 +0200

    create empty header at blockchainhookimpl creation.

--- process/smartContract/hooks/blockChainHook.go
+++ process/smartContract/hooks/blockChainHook.go
@@ -2,12 +2,12 @@ package hooks
 
 import (
 	"encoding/binary"
-	"github.com/ElrondNetwork/elrond-go/data/block"
 	"math/big"
 	"sync"
 
 	"github.com/ElrondNetwork/elrond-go/core"
 	"github.com/ElrondNetwork/elrond-go/data"
+	"github.com/ElrondNetwork/elrond-go/data/block"
 	"github.com/ElrondNetwork/elrond-go/data/state"
 	"github.com/ElrondNetwork/elrond-go/data/typeConverters"
 	"github.com/ElrondNetwork/elrond-go/dataRetriever"
