e534010f9669c7b8568be3aec5e9326de218d5e3
iulianpascalau <iulian.pascalau@gmail.com>
2019-11-06 08:48:34 +0200

    Added Logger interface

--- logger/interface.go
+++ logger/interface.go
@@ -2,6 +2,17 @@ package logger
 
 import "io"
 
+// Logger defines the behavior of a data logger component
+type Logger interface {
+	Trace(message string, args ...interface{})
+	Debug(message string, args ...interface{})
+	Info(message string, args ...interface{})
+	Warn(message string, args ...interface{})
+	Error(message string, args ...interface{})
+	LogIfError(err error, args ...interface{})
+	SetLevel(logLevel LogLevel)
+}
+
 // Formatter describes what an log formatter should be able to do
 type Formatter interface {
 	Output(line *LogLine) []byte
