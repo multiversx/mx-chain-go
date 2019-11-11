eb728805893fab89811148d1d9566202337bc2d4
Robert Sasu <sasu.robert@gmail.com>
2019-11-11 14:22:32 +0200

    started implementation of trigger in shards.

--- endOfEpoch/shardchain/trigger.go
+++ endOfEpoch/shardchain/trigger.go
@@ -1,8 +1,14 @@
 package shardchain
 
 import (
+	"github.com/ElrondNetwork/elrond-go/core"
 	"github.com/ElrondNetwork/elrond-go/data"
+	"github.com/ElrondNetwork/elrond-go/data/block"
 	"github.com/ElrondNetwork/elrond-go/endOfEpoch"
+	"github.com/ElrondNetwork/elrond-go/hashing"
+	"github.com/ElrondNetwork/elrond-go/marshal"
+	"github.com/ElrondNetwork/elrond-go/storage"
+	"sync"
 )
 
 // ArgsNewShardEndOfEpochTrigger defines the arguments needed for new end of epoch trigger
@@ -10,6 +16,23 @@ type ArgsNewShardEndOfEpochTrigger struct {
 }
 
 type trigger struct {
+	epoch             uint32
+	currentRoundIndex int64
+	epochStartRound   int64
+	isEndOfEpoch      bool
+	finality          uint64
+
+	newEpochHdrReceived bool
+
+	mutReceived    sync.Mutex
+	mapHashHdr     map[string]*block.MetaBlock
+	mapNonceHashes map[uint64][]string
+
+	metaHdrPool    storage.Cacher
+	metaHdrStorage storage.Storer
+
+	marshalizer marshal.Marshalizer
+	hasher      hashing.Hasher
 }
 
 // NewEndOfEpochTrigger creates a trigger to signal end of epoch
@@ -23,12 +46,12 @@ func NewEndOfEpochTrigger(args *ArgsNewShardEndOfEpochTrigger) (*trigger, error)
 
 // IsEndOfEpoch returns true if conditions are fullfilled for end of epoch
 func (t *trigger) IsEndOfEpoch() bool {
-	return false
+	return t.isEndOfEpoch
 }
 
 // Epoch returns the current epoch number
 func (t *trigger) Epoch() uint32 {
-	return 0
+	return t.epoch
 }
 
 // ForceEndOfEpoch sets the conditions for end of epoch to true in case of edge cases
@@ -38,6 +61,26 @@ func (t *trigger) ForceEndOfEpoch(round int64) error {
 
 // ReceivedHeader saves the header into pool to verify if end-of-epoch conditions are fullfilled
 func (t *trigger) ReceivedHeader(header data.HeaderHandler) {
+	metaHdr, ok := header.(*block.MetaBlock)
+	if !ok {
+		return
+	}
+
+	t.mutReceived.Lock()
+	defer t.mutReceived.Unlock()
+
+	hdrHash, err := core.CalculateHash(t.marshalizer, t.hasher, metaHdr)
+	if err != nil {
+		return
+	}
+
+	if _, ok := t.mapHashHdr[string(hdrHash)]; ok {
+		return
+	}
+
+	if len(metaHdr.EndOfEpoch.LastFinalizedHeaders) > 0 {
+
+	}
 }
 
 // Update updates the end-of-epoch trigger
@@ -46,6 +89,10 @@ func (t *trigger) Update(round int64) {
 
 // Processed signals that end-of-epoch has been processed
 func (t *trigger) Processed() {
+	t.isEndOfEpoch = false
+	t.newEpochHdrReceived = false
+
+	//TODO: do a cleanup
 }
 
 // IsInterfaceNil returns true if underlying object is nil
