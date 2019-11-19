16b0030022628b32e5496a3b09bfeac748cdc34c
BeniaminDrasovean <beniamin.drasovean@elrond.network>
2019-11-19 17:51:03 +0200

    fix tests

--- process/block/shardblock.go
+++ process/block/shardblock.go
@@ -788,7 +788,7 @@ func (sp *shardProcessor) cleanTxsPools() {
 }
 
 // CreateNewHeader creates a new header
-func (sp *shardProcessor) CreateNewHeader() data.HeaderHandler {
+func (mp *shardProcessor) CreateNewHeader() data.HeaderHandler {
 	return &block.Header{}
 }
 
@@ -813,6 +813,10 @@ func (sp *shardProcessor) getHighestHdrForOwnShardFromMetachain(
 		}
 
 		ownShIdHdrs = append(ownShIdHdrs, hdrs...)
+	}
+
+	if len(ownShIdHdrs) == 0 {
+		ownShIdHdrs = append(ownShIdHdrs, &block.Header{})
 	}
 
 	process.SortHeadersByNonce(ownShIdHdrs)
