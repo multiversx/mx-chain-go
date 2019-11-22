79895d91e45ef80a1825f878d22fed9a98dfb18d
Robert Sasu <sasu.robert@gmail.com>
2019-11-22 13:24:36 +0200

    new arwen integrated

--- integrationTests/vm/arwen/arwenVM_test.go
+++ integrationTests/vm/arwen/arwenVM_test.go
@@ -11,12 +11,13 @@ import (
 	"github.com/ElrondNetwork/elrond-go/core"
 	"github.com/ElrondNetwork/elrond-go/data/transaction"
 	"github.com/ElrondNetwork/elrond-go/integrationTests/vm"
+	"github.com/ElrondNetwork/elrond-go/logger"
 	"github.com/ElrondNetwork/elrond-go/process/factory"
 	"github.com/stretchr/testify/assert"
 )
 
 func init() {
-	//_ = logger.SetLogLevel("*:INFO,process/smartcontract:DEBUG")
+	_ = logger.SetLogLevel("*:INFO,process/smartcontract:DEBUG")
 }
 
 func TestVmDeployWithTransferAndGasShouldDeploySCCode(t *testing.T) {
@@ -181,7 +182,7 @@ func TestVmDeployWithTransferAndExecuteERC20(t *testing.T) {
 	aliceNonce++
 
 	start := time.Now()
-	nrTxs := 10000
+	nrTxs := 10
 
 	for i := 0; i < nrTxs; i++ {
 		tx = vm.CreateTransferTx(aliceNonce, transferOnCalls, scAddress, alice, bob)
