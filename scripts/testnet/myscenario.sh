01dbe175c614adcced0fd952c74fc92dfb8b7ef0
Camil Băncioiu <ioan.camil.bancioiu@gmail.com>
2019-11-26 17:36:32 +0200

    Improve existing scripts and add validate.sh

--- scripts/testnet/myscenario.sh
+++ scripts/testnet/myscenario.sh
@@ -5,15 +5,28 @@ source "$ELRONDTESTNETSCRIPTSDIR/variables.sh"
 
 DEPLOYED_SC_ADDRESS=$(cat $TESTNETDIR/txgen/deployedSCAddress.txt)
 
-curl -d '{
-  "value": 10,
-  "numOfTxs": 5,
-  "numOfShards": '$SHARDCOUNT',
-  "gasPrice": 1,
-  "gasLimit": 5000000,
-  "crossShard": true,
-  "recallNonce": true,
-  "scAddress": "'$DEPLOYED_SC_ADDRESS'",
-  "data": "transfer"
-}' \
-  -H "Content-Type: application/json" -X POST http://localhost:$PORT_TXGEN/transaction/send-multiple
+
+sendTransactions() {
+  curl -d '{
+    "value": 10,
+    "numOfTxs": 1000,
+    "numOfShards": '$SHARDCOUNT',
+    "gasPrice": 1,
+    "gasLimit": 3000000,
+    "crossShard": true,
+    "recallNonce": true,
+    "scAddress": "'$DEPLOYED_SC_ADDRESS'",
+    "data": "transfer"
+  }' \
+    -H "Content-Type: application/json" -X POST http://localhost:$PORT_TXGEN/transaction/send-multiple
+}
+
+echo "Waiting 3600s..."
+sleep 3600
+
+while true
+do
+  sendTransactions
+  echo "Transaction batch emitted"
+  sleep 600
+done
