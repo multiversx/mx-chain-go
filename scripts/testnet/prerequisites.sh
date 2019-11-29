c6c67898124da576830bd8629f7d9d3b31dcb91e
Camil Băncioiu <ioan.camil.bancioiu@gmail.com>
2019-11-29 12:16:05 +0200

    Handle golang installation on Ubuntu

--- scripts/testnet/prerequisites.sh
+++ scripts/testnet/prerequisites.sh
@@ -5,19 +5,35 @@ source "$ELRONDTESTNETSCRIPTSDIR/variables.sh"
 
 export DISTRIBUTION=$(cat /etc/os-release | grep "^ID=" | sed 's/ID=//')
 
-export REQUIRED_PACKAGES="git golang gcc lsof jq"
 
 if [[ "$DISTRIBUTION" =~ ^(fedora|centos|rhel)$ ]]; then
   export PACKAGE_MANAGER="dnf"
+  export REQUIRED_PACKAGES="git golang gcc lsof jq curl"
   echo "Using DNF to install required packages: $REQUIRED_PACKAGES"
 fi
 
 if [[ "$DISTRIBUTION" =~ ^(ubuntu|debian)$ ]]; then
   export PACKAGE_MANAGER="apt-get"
+  export REQUIRED_PACKAGES="git gcc lsof jq curl"
+
   echo "Using APT to install required packages: $REQUIRED_PACKAGES"
 fi
 
 sudo $PACKAGE_MANAGER install -y $REQUIRED_PACKAGES
+
+if [[ "$DISTRIBUTION" =~ ^(ubuntu|debian)$ ]]; then
+  echo "Installing Go..."
+  GO_LATEST=$(curl -sS https://golang.org/VERSION?m=text) 
+  wget https://dl.google.com/go/$GO_LATEST.linux-amd64.tar.gz
+  sudo tar -C /usr/local -xzf $GO_LATEST.linux-amd64.tar.gz
+  rm $GO_LATEST.linux-amd64.tar.gz
+
+  echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.profile
+  mkdir -p "$HOME/go"
+  echo "export GOPATH=$HOME/go" >> ~/.profile
+  source ~/.profile 
+fi
+
 
 cd $(dirname $ELRONDDIR)
 git clone https://github.com/ElrondNetwork/elrond-deploy-go.git
