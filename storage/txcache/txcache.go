bbc2e68d3af7c4b2709e1d733f77d35220f4153d
Andrei Bancioiu <andrei.bancioiu@elrond.com>
2019-12-04 17:25:46 +0200

    Sketch removal, insertion in lists.

--- storage/txcache/txcache.go
+++ storage/txcache/txcache.go
@@ -7,8 +7,8 @@ import (
 
 // TxCache is
 type TxCache struct {
-	TxListBySender *cmap.ConcurrentMap
-	TxByHash       *cmap.ConcurrentMap
+	txListBySender *cmap.ConcurrentMap
+	txByHash       *cmap.ConcurrentMap
 }
 
 // NewTxCache creates
@@ -18,8 +18,8 @@ func NewTxCache(size int, shards int) *TxCache {
 	txByHash := cmap.New(size, shards)
 
 	txCache := &TxCache{
-		TxListBySender: txBySender,
-		TxByHash:       txByHash,
+		txListBySender: txBySender,
+		txByHash:       txByHash,
 	}
 
 	return txCache
@@ -29,19 +29,19 @@ func NewTxCache(size int, shards int) *TxCache {
 func (cache *TxCache) AddTx(txHash []byte, tx data.TransactionHandler) {
 	sender := string(tx.GetSndAddress())
 
-	cache.TxByHash.Set(string(txHash), tx)
+	cache.txByHash.Set(string(txHash), tx)
 
 	listForSender, ok := cache.getListForSender(sender)
 	if !ok {
 		listForSender = &TxListForSender{}
-		cache.TxListBySender.Set(sender, listForSender)
+		cache.txListBySender.Set(sender, listForSender)
 	}
 
 	listForSender.addTransaction(tx)
 }
 
 func (cache *TxCache) getListForSender(sender string) (*TxListForSender, bool) {
-	listForSenderUntyped, ok := cache.TxListBySender.Get(sender)
+	listForSenderUntyped, ok := cache.txListBySender.Get(sender)
 	if !ok {
 		return nil, false
 	}
@@ -56,7 +56,7 @@ func (cache *TxCache) GetByTxHash(txHash []byte) (data.TransactionHandler, bool)
 }
 
 func (cache *TxCache) getTx(txHash string) (data.TransactionHandler, bool) {
-	txUntyped, ok := cache.TxByHash.Get(string(txHash))
+	txUntyped, ok := cache.txByHash.Get(string(txHash))
 	if !ok {
 		return nil, false
 	}
@@ -76,7 +76,7 @@ func (cache *TxCache) RemoveByTxHash(txHash []byte) {
 		return
 	}
 
-	cache.TxByHash.Remove(string(txHash))
+	cache.txByHash.Remove(string(txHash))
 	sender := string(tx.GetSndAddress())
 	listForSender, ok := cache.getListForSender(sender)
 	if !ok {
@@ -88,10 +88,24 @@ func (cache *TxCache) RemoveByTxHash(txHash []byte) {
 
 // TxListForSender is
 type TxListForSender struct {
+	Items []data.TransactionHandler
 }
 
 func (list *TxListForSender) addTransaction(tx data.TransactionHandler) {
+	list.Items = append(list.Items, tx)
 }
 
 func (list *TxListForSender) removeTransaction(tx data.TransactionHandler) {
+	index := list.findTx(tx)
+	list.Items = append(list.Items[:index], list.Items[index+1:]...)
+}
+
+func (list *TxListForSender) findTx(tx data.TransactionHandler) int {
+	for i, item := range list.Items {
+		if item == tx {
+			return i
+		}
+	}
+
+	return -1
 }
--- storage/txcache/txcache_test.go
+++ storage/txcache/txcache_test.go
@@ -4,11 +4,30 @@ import (
 	"testing"
 
 	"github.com/ElrondNetwork/elrond-go/data/transaction"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestPut(t *testing.T) {
+func Test_AddTx(t *testing.T) {
 	cache := NewTxCache(1000, 16)
-
+	txHash := []byte("hash-1")
 	tx := &transaction.Transaction{}
-	cache.AddTx([]byte("test"), tx)
+
+	cache.AddTx(txHash, tx)
+	foundTx, ok := cache.GetByTxHash(txHash)
+
+	assert.True(t, ok)
+	assert.Equal(t, tx, foundTx)
+}
+
+func Test_RemoveByTxHash(t *testing.T) {
+	cache := NewTxCache(1000, 16)
+	txHash := []byte("hash-1")
+	tx := &transaction.Transaction{}
+
+	cache.AddTx(txHash, tx)
+	cache.RemoveByTxHash(txHash)
+	foundTx, ok := cache.GetByTxHash(txHash)
+
+	assert.False(t, ok)
+	assert.Nil(t, foundTx)
 }
