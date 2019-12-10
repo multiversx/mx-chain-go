00f91fabe85848fd1925c94a6641b6349b7fc8b4
Iuga Mihai <mihai9625@gmail.com>
2019-12-10 11:56:11 +0200

    EN-5296 : small fix

--- cmd/node/factory/interface.go
+++ cmd/node/factory/interface.go
@@ -2,11 +2,12 @@ package factory
 
 import (
 	"github.com/ElrondNetwork/elrond-go/data"
-	"github.com/ElrondNetwork/elrond-go/process"
 )
 
 //HeaderSigVerifierHandler is the interface needed to check a header if is correct
 type HeaderSigVerifierHandler interface {
 	VerifyRandSeed(header data.HeaderHandler) error
-	process.InterceptedHeaderSigVerifier
+	VerifyRandSeedAndLeaderSignature(header data.HeaderHandler) error
+	VerifySignature(header data.HeaderHandler) error
+	IsInterfaceNil() bool
 }
