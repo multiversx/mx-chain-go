57c35c752c84ac2e2ca14d22b45f2cfe79c50710
Sebastian Marian <sebastian.marian@oxsy.ro>
2019-12-10 22:10:50 +0200

    * Added mechanism for computing longest chain * Added first basic cleaning mechanism behind final nonce

--- consensus/spos/worker_test.go
+++ consensus/spos/worker_test.go
@@ -592,7 +592,7 @@ func TestWorker_ProcessReceivedMessageWrongHeaderShouldErr(t *testing.T) {
 	broadcastMessengerMock := &mock.BroadcastMessengerMock{}
 	consensusState := initConsensusState()
 	forkDetectorMock := &mock.ForkDetectorMock{}
-	forkDetectorMock.AddHeaderCalled = func(header data.HeaderHandler, hash []byte, state process.BlockHeaderState, finalsHeaders []data.HeaderHandler, finalHeadersHashes [][]byte, isNotarizedShardStuck bool) error {
+	forkDetectorMock.AddHeaderCalled = func(header data.HeaderHandler, hash []byte, state process.BlockHeaderState, finalsHeaders []data.HeaderHandler, finalHeadersHashes [][]byte) error {
 		return nil
 	}
 	keyGeneratorMock, _, _ := mock.InitKeys()
