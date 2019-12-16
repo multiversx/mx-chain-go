8e8f8211d428cba9ec422965b9f09664c29df01d
iulianpascalau <iulian.pascalau@gmail.com>
2019-12-16 13:56:00 +0200

    fixed tab problems

--- process/interceptors/factory/argInterceptedDataFactory.go
+++ process/interceptors/factory/argInterceptedDataFactory.go
@@ -24,5 +24,5 @@ type ArgInterceptedDataFactory struct {
 	AddrConv          state.AddressConverter
 	FeeHandler        process.FeeHandler
 	HeaderSigVerifier process.InterceptedHeaderSigVerifier
-	ChainID			  []byte
+	ChainID           []byte
 }
