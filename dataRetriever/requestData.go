93f897cb01cfb77c4343c67a872b859d71735676
Traian Schiau <traian.schiau@gmail.com>
2020-01-07 12:39:45 +0200

    [dataRetriever] Protobuf for requestData

--- dataRetriever/requestData.go
+++ dataRetriever/requestData.go
@@ -1,47 +1,14 @@
+//go:generate protoc -I=proto -I=$GOPATH/src -I=$GOPATH/src/github.com/gogo/protobuf/protobuf  --gogoslick_out=. requestData.proto
 package dataRetriever
 
 import (
-	"fmt"
-
 	"github.com/ElrondNetwork/elrond-go/marshal"
 	"github.com/ElrondNetwork/elrond-go/p2p"
 )
 
-// RequestDataType represents the data type for the requested data
-type RequestDataType byte
-
-func (rdt RequestDataType) String() string {
-	switch rdt {
-	case HashType:
-		return "hash type"
-	case HashArrayType:
-		return "hash array type"
-	case NonceType:
-		return "nonce type"
-	default:
-		return fmt.Sprintf("unknown type %d", rdt)
-	}
-}
-
-const (
-	// HashType indicates that the request data object is of type hash
-	HashType RequestDataType = iota + 1
-	// HashArrayType that the request data object contains a serialised array of hashes
-	HashArrayType
-	// NonceType indicates that the request data object is of type nonce (uint64)
-	NonceType
-)
-
-// RequestData holds the requested data
-// This struct will be serialized and sent to the other peers
-type RequestData struct {
-	Type  RequestDataType
-	Value []byte
-}
-
 // Unmarshal sets the fields according to p2p.MessageP2P.Data() contents
 // Errors if something went wrong
-func (rd *RequestData) Unmarshal(marshalizer marshal.Marshalizer, message p2p.MessageP2P) error {
+func (rd *RequestData) UnmarshalWith(marshalizer marshal.Marshalizer, message p2p.MessageP2P) error {
 	if marshalizer == nil || marshalizer.IsInterfaceNil() {
 		return ErrNilMarshalizer
 	}
