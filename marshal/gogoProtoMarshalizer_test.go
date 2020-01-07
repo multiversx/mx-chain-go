7deb60a6ee7cb04f4b1db275b2eb5754271fb055
Traian Schiau <traian.schiau@gmail.com>
2020-01-07 10:30:28 +0200

    [marshal] Move gogoProto interface

--- marshal/gogoProtoMarshalizer_test.go
+++ marshal/gogoProtoMarshalizer_test.go
@@ -1,6 +1,7 @@
 package marshal_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/ElrondNetwork/elrond-go/data/block"
@@ -11,30 +12,60 @@ import (
 var miniblock = &block.MiniBlock{}
 var gogoMarsh = marshal.GogoProtoMarshalizer{}
 
+func recovedMarshal(obj interface{}) (buf []byte, err error) {
+	defer func() {
+		if p := recover(); p != nil {
+			if panicError, ok := p.(error); ok {
+				err = panicError
+			} else {
+				err = fmt.Errorf("%#v", p)
+			}
+			buf = nil
+		}
+	}()
+	buf, err = gogoMarsh.Marshal(obj)
+	return
+}
+
+func recovedUnmarshal(obj interface{}, buf []byte) (err error) {
+	defer func() {
+		if p := recover(); p != nil {
+			if panicError, ok := p.(error); ok {
+				err = panicError
+			} else {
+				err = fmt.Errorf("%#v", p)
+			}
+		}
+	}()
+	err = gogoMarsh.Unmarshal(obj, buf)
+	return
+}
+
 func TestGogoProtoMarshalizer_Marshal(t *testing.T) {
-	encNode, err := gogoMarsh.Marshal(miniblock)
+	encNode, err := recovedMarshal(miniblock)
 	assert.Nil(t, err)
 	assert.NotNil(t, encNode)
 }
 
 func TestGogoProtoMarshalizer_MarshalWrongObj(t *testing.T) {
+
 	obj := "elrond"
-	encNode, err := gogoMarsh.Marshal(obj)
+	encNode, err := recovedMarshal(obj)
 	assert.Nil(t, encNode)
-	assert.Equal(t, marshal.ErrMarshallingProto, err)
+	assert.NotNil(t, err)
 }
 
 func TestGogoProtoMarshalizer_Unmarshal(t *testing.T) {
 	encNode, _ := gogoMarsh.Marshal(miniblock)
 	newNode := &block.MiniBlock{}
 
-	err := gogoMarsh.Unmarshal(newNode, encNode)
+	err := recovedUnmarshal(newNode, encNode)
 	assert.Nil(t, err)
 	assert.Equal(t, miniblock, newNode)
 }
 
 func TestGogoProtoMarshalizer_UnmarshalWrongObj(t *testing.T) {
 	encNode, _ := gogoMarsh.Marshal(miniblock)
-	err := gogoMarsh.Unmarshal([]byte{}, encNode)
-	assert.Equal(t, marshal.ErrUnmarshallingProto, err)
+	err := recovedUnmarshal([]byte{}, encNode)
+	assert.NotNil(t, err)
 }
