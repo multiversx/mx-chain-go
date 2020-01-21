10b3aeeb0506a739fe22013eb28e24def765ee1f
Andrei Bancioiu <andrei.bancioiu@elrond.com>
2020-01-21 13:45:02 +0200

    Run goimports.

--- storage/txcache/txListForSenderAsSortedMapItem_test.go
+++ storage/txcache/txListForSenderAsSortedMapItem_test.go
@@ -1,8 +1,9 @@
 package txcache
 
 import (
-	"github.com/stretchr/testify/require"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestSenderAsBucketSortedMapItem_ComputeScore(t *testing.T) {
