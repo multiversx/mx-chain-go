e4509d5c8f10e164354e789ca2ad3d20f830b13c
Andrei Bancioiu <andrei.bancioiu@elrond.com>
2020-01-24 15:48:26 +0200

    Use require instead of assert.

--- storage/txcache/txListForSender_test.go
+++ storage/txcache/txListForSender_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 
 	"github.com/ElrondNetwork/elrond-go/data"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func Test_AddTx_Sorts(t *testing.T) {
@@ -17,13 +17,13 @@ func Test_AddTx_Sorts(t *testing.T) {
 
 	txHashes := list.getTxHashes()
 
-	assert.Equal(t, 4, list.items.Len())
-	assert.Equal(t, 4, len(txHashes))
+	require.Equal(t, 4, list.items.Len())
+	require.Equal(t, 4, len(txHashes))
 
-	assert.Equal(t, []byte("a"), txHashes[0])
-	assert.Equal(t, []byte("b"), txHashes[1])
-	assert.Equal(t, []byte("c"), txHashes[2])
-	assert.Equal(t, []byte("d"), txHashes[3])
+	require.Equal(t, []byte("a"), txHashes[0])
+	require.Equal(t, []byte("b"), txHashes[1])
+	require.Equal(t, []byte("c"), txHashes[2])
+	require.Equal(t, []byte("d"), txHashes[3])
 }
 
 func Test_findTx(t *testing.T) {
@@ -42,10 +42,10 @@ func Test_findTx(t *testing.T) {
 	elementWithB := list.findListElementWithTx(txB)
 	noElementWithD := list.findListElementWithTx(txD)
 
-	assert.Equal(t, txA, elementWithA.Value.(txListForSenderNode).tx)
-	assert.Equal(t, txANewer, elementWithANewer.Value.(txListForSenderNode).tx)
-	assert.Equal(t, txB, elementWithB.Value.(txListForSenderNode).tx)
-	assert.Nil(t, noElementWithD)
+	require.Equal(t, txA, elementWithA.Value.(txListForSenderNode).tx)
+	require.Equal(t, txANewer, elementWithANewer.Value.(txListForSenderNode).tx)
+	require.Equal(t, txB, elementWithB.Value.(txListForSenderNode).tx)
+	require.Nil(t, noElementWithD)
 }
 
 func Test_findTx_CoverNonceComparisonOptimization(t *testing.T) {
@@ -54,7 +54,7 @@ func Test_findTx_CoverNonceComparisonOptimization(t *testing.T) {
 
 	// Find one with a lower nonce, not added to cache
 	noElement := list.findListElementWithTx(createTx(".", 41))
-	assert.Nil(t, noElement)
+	require.Nil(t, noElement)
 }
 
 func Test_RemoveTransaction(t *testing.T) {
@@ -62,10 +62,10 @@ func Test_RemoveTransaction(t *testing.T) {
 	tx := createTx(".", 1)
 
 	list.AddTx([]byte("a"), tx)
-	assert.Equal(t, 1, list.items.Len())
+	require.Equal(t, 1, list.items.Len())
 
 	list.RemoveTx(tx)
-	assert.Equal(t, 0, list.items.Len())
+	require.Equal(t, 0, list.items.Len())
 }
 
 func Test_RemoveTransaction_NoPanicWhenTxMissing(t *testing.T) {
@@ -73,7 +73,7 @@ func Test_RemoveTransaction_NoPanicWhenTxMissing(t *testing.T) {
 	tx := createTx(".", 1)
 
 	list.RemoveTx(tx)
-	assert.Equal(t, 0, list.items.Len())
+	require.Equal(t, 0, list.items.Len())
 }
 
 func Test_RemoveHighNonceTransactions(t *testing.T) {
@@ -84,16 +84,16 @@ func Test_RemoveHighNonceTransactions(t *testing.T) {
 	}
 
 	list.RemoveHighNonceTxs(50)
-	assert.Equal(t, 50, list.items.Len())
-	assert.Equal(t, uint64(49), list.getHighestNonceTx().GetNonce())
+	require.Equal(t, 50, list.items.Len())
+	require.Equal(t, uint64(49), list.getHighestNonceTx().GetNonce())
 
 	list.RemoveHighNonceTxs(20)
-	assert.Equal(t, 30, list.items.Len())
-	assert.Equal(t, uint64(29), list.getHighestNonceTx().GetNonce())
+	require.Equal(t, 30, list.items.Len())
+	require.Equal(t, uint64(29), list.getHighestNonceTx().GetNonce())
 
 	list.RemoveHighNonceTxs(30)
-	assert.Equal(t, 0, list.items.Len())
-	assert.Nil(t, list.getHighestNonceTx())
+	require.Equal(t, 0, list.items.Len())
+	require.Nil(t, list.getHighestNonceTx())
 }
 
 func Test_RemoveHighNonceTransactions_NoPanicWhenCornerCases(t *testing.T) {
@@ -104,10 +104,10 @@ func Test_RemoveHighNonceTransactions_NoPanicWhenCornerCases(t *testing.T) {
 	}
 
 	list.RemoveHighNonceTxs(0)
-	assert.Equal(t, 100, list.items.Len())
+	require.Equal(t, 100, list.items.Len())
 
 	list.RemoveHighNonceTxs(500)
-	assert.Equal(t, 0, list.items.Len())
+	require.Equal(t, 0, list.items.Len())
 }
 
 func Test_CopyBatchTo(t *testing.T) {
@@ -122,22 +122,22 @@ func Test_CopyBatchTo(t *testing.T) {
 
 	// First batch
 	copied := list.copyBatchTo(true, destination, destinationHashes, 50)
-	assert.Equal(t, 50, copied)
-	assert.NotNil(t, destination[49])
-	assert.Nil(t, destination[50])
+	require.Equal(t, 50, copied)
+	require.NotNil(t, destination[49])
+	require.Nil(t, destination[50])
 
 	// Second batch
 	copied = list.copyBatchTo(false, destination[50:], destinationHashes[50:], 50)
-	assert.Equal(t, 50, copied)
-	assert.NotNil(t, destination[99])
+	require.Equal(t, 50, copied)
+	require.NotNil(t, destination[99])
 
 	// No third batch
 	copied = list.copyBatchTo(false, destination, destinationHashes, 50)
-	assert.Equal(t, 0, copied)
+	require.Equal(t, 0, copied)
 
 	// Restart copy
 	copied = list.copyBatchTo(true, destination, destinationHashes, 12345)
-	assert.Equal(t, 100, copied)
+	require.Equal(t, 100, copied)
 }
 
 func Test_CopyBatchTo_NoPanicWhenCornerCases(t *testing.T) {
@@ -151,11 +151,11 @@ func Test_CopyBatchTo_NoPanicWhenCornerCases(t *testing.T) {
 	destination := make([]data.TransactionHandler, 0)
 	destinationHashes := make([][]byte, 0)
 	copied := list.copyBatchTo(true, destination, destinationHashes, 10)
-	assert.Equal(t, 0, copied)
+	require.Equal(t, 0, copied)
 
 	// When small destination
 	destination = make([]data.TransactionHandler, 5)
 	destinationHashes = make([][]byte, 5)
 	copied = list.copyBatchTo(false, destination, destinationHashes, 10)
-	assert.Equal(t, 5, copied)
+	require.Equal(t, 5, copied)
 }
