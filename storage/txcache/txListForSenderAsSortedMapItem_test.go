c1981e02c449c5ca56a51dbe2e77df6a714de100
Andrei Bancioiu <andrei.bancioiu@elrond.com>
2020-01-28 16:26:04 +0200

    Fix after review, split lines.

--- storage/txcache/txListForSenderAsSortedMapItem_test.go
+++ storage/txcache/txListForSenderAsSortedMapItem_test.go
@@ -22,10 +22,17 @@ func TestSenderAsBucketSortedMapItem_ComputeScore(t *testing.T) {
 }
 
 func Test_computeSenderScore(t *testing.T) {
-	require.InDelta(t, float64(0.1789683371), computeSenderScore(senderScoreParams{count: 14000, size: kBToBytes(100000), fee: toMicroERD(300000), gas: 2500000000, minGasPrice: 100}), delta)
-	require.InDelta(t, float64(0.2517997181), computeSenderScore(senderScoreParams{count: 19000, size: kBToBytes(3000), fee: toMicroERD(2300000), gas: 19000000000, minGasPrice: 100}), delta)
-	require.InDelta(t, float64(5.795382396), computeSenderScore(senderScoreParams{count: 3, size: kBToBytes(2), fee: toMicroERD(40), gas: 400000, minGasPrice: 100}), delta)
-	require.InDelta(t, float64(100), computeSenderScore(senderScoreParams{count: 1, size: kBToBytes(0.3), fee: toMicroERD(50), gas: 100000, minGasPrice: 100}), delta)
+	score := computeSenderScore(senderScoreParams{count: 14000, size: kBToBytes(100000), fee: toMicroERD(300000), gas: 2500000000, minGasPrice: 100})
+	require.InDelta(t, float64(0.1789683371), score, delta)
+
+	score = computeSenderScore(senderScoreParams{count: 19000, size: kBToBytes(3000), fee: toMicroERD(2300000), gas: 19000000000, minGasPrice: 100})
+	require.InDelta(t, float64(0.2517997181), score, delta)
+
+	score = computeSenderScore(senderScoreParams{count: 3, size: kBToBytes(2), fee: toMicroERD(40), gas: 400000, minGasPrice: 100})
+	require.InDelta(t, float64(5.795382396), score, delta)
+
+	score = computeSenderScore(senderScoreParams{count: 1, size: kBToBytes(0.3), fee: toMicroERD(50), gas: 100000, minGasPrice: 100})
+	require.InDelta(t, float64(100), score, delta)
 }
 
 func Benchmark_computeSenderScore(b *testing.B) {
