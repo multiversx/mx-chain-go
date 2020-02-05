f1e69e4683f0185e91d951b4a827315a948aee0a
Sebastian Marian <sebastian.marian@oxsy.ro>
2020-02-05 21:00:18 +0200

    * Removed TODO

--- process/coordinator/process.go
+++ process/coordinator/process.go
@@ -94,9 +94,7 @@ func NewTransactionCoordinator(
 	}
 
 	tc.miniBlockPool = miniBlockPool
-	//TODO: This registration should be activated when we can determine if the received miniblock is for a header
-	//which has been already committed or for one which is out of range related to the node's interest:
-	//tc.miniBlockPool.RegisterHandler(tc.receivedMiniBlock)
+	tc.miniBlockPool.RegisterHandler(tc.receivedMiniBlock)
 
 	tc.onRequestMiniBlock = requestHandler.RequestMiniBlock
 	tc.requestedTxs = make(map[block.Type]int)
