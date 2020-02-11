c6ab5ba772e36ef2c22a01ace4c4a70f7425e7c0
Iulian Pascalau <iulian.pascalau@gmail.com>
2020-02-11 18:18:42 +0200

    removed faulty log.Warn in tx shard processor

--- process/transaction/shardProcess.go
+++ process/transaction/shardProcess.go
@@ -11,7 +11,6 @@ import (
 	"github.com/ElrondNetwork/elrond-go/data/state"
 	"github.com/ElrondNetwork/elrond-go/data/transaction"
 	"github.com/ElrondNetwork/elrond-go/hashing"
-	"github.com/ElrondNetwork/elrond-go/logger"
 	"github.com/ElrondNetwork/elrond-go/marshal"
 	"github.com/ElrondNetwork/elrond-go/process"
 	"github.com/ElrondNetwork/elrond-go/sharding"
@@ -112,11 +111,6 @@ func (txProc *txProcessor) ProcessTransaction(tx *transaction.Transaction) error
 	if err != nil {
 		return err
 	}
-
-	log := logger.GetOrCreate("aaa")
-	txHash, _ := core.CalculateHash(txProc.marshalizer, txProc.hasher, tx)
-	log.Warn("sender", "balance", acntSnd.(*state.Account).Balance, "nonce", acntSnd.GetNonce(),
-		"txHash", txHash, "txNonce", tx.Nonce, "txValue", tx.Value)
 
 	err = txProc.checkTxValues(tx, acntSnd)
 	if err != nil {
