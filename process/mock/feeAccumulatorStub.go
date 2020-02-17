b86ecefbb821dd099daa4a9eee25846e465fbd1d
Robert Sasu <sasu.robert@gmail.com>
2020-02-17 16:26:33 +0200

    fix after review

--- process/mock/feeAccumulatorStub.go
+++ process/mock/feeAccumulatorStub.go
@@ -2,7 +2,7 @@ package mock
 
 import "math/big"
 
-// FeeAccumulatorSub is a stub which implement TransactionFeeHandler interface
+// FeeAccumulatorSub is a stub which implements TransactionFeeHandler interface
 type FeeAccumulatorStub struct {
 	CreateBlockStartedCalled    func()
 	GetAccumulatedFeesCalled    func() *big.Int
