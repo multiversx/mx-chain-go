f616feed64b6a5071139e75c6480942cd2416222
Andrei Bancioiu <andrei.bancioiu@elrond.com>
2020-02-26 11:45:04 +0200

    Refactor - smaller interface (Interceptor to Pool).

--- process/interceptors/processor/txInterceptorProcessor.go
+++ process/interceptors/processor/txInterceptorProcessor.go
@@ -2,15 +2,14 @@ package processor
 
 import (
 	"github.com/ElrondNetwork/elrond-go/core/check"
-	"github.com/ElrondNetwork/elrond-go/dataRetriever"
 	"github.com/ElrondNetwork/elrond-go/process"
 )
 
 // TxInterceptorProcessor is the processor used when intercepting transactions
 // (smart contract results, receipts, transaction) structs which satisfy TransactionHandler interface.
 type TxInterceptorProcessor struct {
-	shardedDataCache dataRetriever.ShardedDataCacherNotifier
-	txValidator      process.TxValidator
+	shardedPool ShardedPool
+	txValidator process.TxValidator
 }
 
 // NewTxInterceptorProcessor creates a new TxInterceptorProcessor instance
@@ -26,8 +25,8 @@ func NewTxInterceptorProcessor(argument *ArgTxInterceptorProcessor) (*TxIntercep
 	}
 
 	return &TxInterceptorProcessor{
-		shardedDataCache: argument.ShardedDataCache,
-		txValidator:      argument.TxValidator,
+		shardedPool: argument.ShardedDataCache,
+		txValidator: argument.TxValidator,
 	}, nil
 }
 
@@ -54,7 +53,7 @@ func (txip *TxInterceptorProcessor) Save(data process.InterceptedData) error {
 	}
 
 	cacherIdentifier := process.ShardCacherIdentifier(interceptedTx.SenderShardId(), interceptedTx.ReceiverShardId())
-	txip.shardedDataCache.AddData(
+	txip.shardedPool.AddData(
 		data.Hash(),
 		interceptedTx.Transaction(),
 		cacherIdentifier,
