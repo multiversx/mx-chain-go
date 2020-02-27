05e8a1e2a041bbd2fccf164efecfeb8acfe904cc
Iulian Pascalau <iulian.pascalau@gmail.com>
2020-02-27 18:34:00 +0200

    minor code changes: fixed golang ci bot messages, refactored comments

--- statusHandler/export_test.go
+++ statusHandler/export_test.go
@@ -1,9 +1,9 @@
 package statusHandler
 
 // StatusMetricsMap will return all metrics in a map
-func (nd *statusMetrics) StatusMetricsMap() map[string]interface{} {
+func (sm *statusMetrics) StatusMetricsMap() map[string]interface{} {
 	statusMetricsMap := make(map[string]interface{})
-	nd.nodeMetrics.Range(func(key, value interface{}) bool {
+	sm.nodeMetrics.Range(func(key, value interface{}) bool {
 		statusMetricsMap[key.(string)] = value
 		return true
 	})
