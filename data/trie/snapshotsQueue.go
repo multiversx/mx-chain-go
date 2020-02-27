8c242d8e923bcc9d61135b7faee5950ae531f71b
BeniaminDrasovean <beniamin.drasovean@elrond.com>
2020-02-27 17:52:05 +0200

    add prints to snapshotsQueue

--- data/trie/snapshotsQueue.go
+++ data/trie/snapshotsQueue.go
@@ -27,13 +27,19 @@ func (sq *snapshotsQueue) add(rootHash []byte, newDb bool) {
 		newDb:    newDb,
 	}
 	sq.queue = append(sq.queue, newSnapshot)
+	for i := range sq.queue {
+		log.Trace("snapshots queue add", "rootHash", sq.queue[i].rootHash)
+	}
+
 	sq.mut.Unlock()
 }
 
 func (sq *snapshotsQueue) len() int {
 	sq.mut.Lock()
 	defer sq.mut.Unlock()
-
+	for i := range sq.queue {
+		log.Trace("snapshots queue len", "rootHash", sq.queue[i].rootHash)
+	}
 	return len(sq.queue)
 }
 
@@ -56,7 +62,9 @@ func (sq *snapshotsQueue) clone() snapshotsBuffer {
 func (sq *snapshotsQueue) getFirst() *snapshotsQueueEntry {
 	sq.mut.Lock()
 	defer sq.mut.Unlock()
-
+	for i := range sq.queue {
+		log.Trace("snapshots queue getFirst", "rootHash", sq.queue[i].rootHash)
+	}
 	return sq.queue[0]
 }
 
@@ -64,7 +72,12 @@ func (sq *snapshotsQueue) removeFirst() {
 	sq.mut.Lock()
 
 	if len(sq.queue) != 0 {
+		log.Trace("snapshots queue removeFirst", "firstRootHash", sq.queue[0].rootHash)
 		sq.queue = sq.queue[1:]
+	}
+
+	for i := range sq.queue {
+		log.Trace("snapshots queue removeFirst", "rootHash", sq.queue[i].rootHash)
 	}
 
 	sq.mut.Unlock()
