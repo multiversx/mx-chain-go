1809ece09c82c9c7d570ab4a956d81f6e3072125
AdoAdoAdo <anaridado@gmail.com>
2020-03-06 15:37:48 +0200

    node: move heartbeat reply data into different file

--- node/heartbeat/monitor.go
+++ node/heartbeat/monitor.go
@@ -4,8 +4,6 @@ package heartbeat
 import (
 	"bytes"
 	"encoding/hex"
-	"encoding/json"
-	"errors"
 	"sort"
 	"strings"
 	"sync"
@@ -17,32 +15,10 @@ import (
 	"github.com/ElrondNetwork/elrond-go/marshal"
 	"github.com/ElrondNetwork/elrond-go/p2p"
 	"github.com/ElrondNetwork/elrond-go/statusHandler"
-	types "github.com/gogo/protobuf/types"
+	"github.com/gogo/protobuf/types"
 )
 
 var log = logger.GetOrCreate("node/heartbeat")
-
-// Duration is a wrapper of the original Duration struct
-// that has JSON marshal and unmarshal capabilities
-// golang issue: https://github.com/golang/go/issues/10275
-type Duration struct {
-	time.Duration
-}
-
-// PubKeyHeartbeat returns the heartbeat status for a public key
-type PubKeyHeartbeat struct {
-	HexPublicKey    string    `json:"hexPublicKey"`
-	TimeStamp       time.Time `json:"timeStamp"`
-	MaxInactiveTime Duration  `json:"maxInactiveTime"`
-	IsActive        bool      `json:"isActive"`
-	ReceivedShardID uint32    `json:"receivedShardID"`
-	ComputedShardID uint32    `json:"computedShardID"`
-	TotalUpTime     int64     `json:"totalUpTimeSec"`
-	TotalDownTime   int64     `json:"totalDownTimeSec"`
-	VersionNumber   string    `json:"versionNumber"`
-	IsValidator     bool      `json:"isValidator"`
-	NodeDisplayName string    `json:"nodeDisplayName"`
-}
 
 // Monitor represents the heartbeat component that processes received heartbeat messages
 type Monitor struct {
@@ -415,31 +391,4 @@ func (m *Monitor) convertFromExportedStruct(hbDTO HeartbeatDTO, maxDuration time
 	hbmi.lastUptimeDowntime, _ = types.TimestampFromProto(hbDTO.LastUptimeDowntime)
 	hbmi.genesisTime, _ = types.TimestampFromProto(hbDTO.GenesisTime)
 	return hbmi
-}
-
-// MarshalJSON is called when a json marshal is triggered on this field
-func (d Duration) MarshalJSON() ([]byte, error) {
-	return json.Marshal(d.String())
-}
-
-// UnmarshalJSON is called when a json unmarshal is triggered on this field
-func (d *Duration) UnmarshalJSON(b []byte) error {
-	var v interface{}
-	if err := json.Unmarshal(b, &v); err != nil {
-		return err
-	}
-	switch value := v.(type) {
-	case float64:
-		d.Duration = time.Duration(value)
-		return nil
-	case string:
-		var err error
-		d.Duration, err = time.ParseDuration(value)
-		if err != nil {
-			return err
-		}
-		return nil
-	default:
-		return errors.New("invalid duration")
-	}
 }
