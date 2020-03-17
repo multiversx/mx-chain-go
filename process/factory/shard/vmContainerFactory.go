8d62374004036c2b4a008decda7fdc290e642925
Andrei Bancioiu <andrei.bancioiu@elrond.com>
2020-03-17 17:27:46 +0200

    Update refs, adjust constructor.

--- process/factory/shard/vmContainerFactory.go
+++ process/factory/shard/vmContainerFactory.go
@@ -3,6 +3,9 @@ package shard
 import (
 	//arwen "github.com/ElrondNetwork/arwen-wasm-vm/arwen/host"
 
+	ipcCommon "github.com/ElrondNetwork/arwen-wasm-vm/ipc/common"
+	ipcLogger "github.com/ElrondNetwork/arwen-wasm-vm/ipc/logger"
+	ipcMarshaling "github.com/ElrondNetwork/arwen-wasm-vm/ipc/marshaling"
 	arwenNodePart "github.com/ElrondNetwork/arwen-wasm-vm/ipc/nodepart"
 	"github.com/ElrondNetwork/elrond-go/logger"
 	"github.com/ElrondNetwork/elrond-go/process"
@@ -65,7 +68,21 @@ func (vmf *vmContainerFactory) Create() (process.VirtualMachinesContainer, error
 func (vmf *vmContainerFactory) createArwenVM() (vmcommon.VMExecutionHandler, error) {
 	//arwenVM, err := arwen.NewArwenVM(vmf.blockChainHookImpl, vmf.cryptoHook, factory.ArwenVirtualMachine, vmf.blockGasLimit, vmf.gasSchedule)
 	logArwenDriver.Info("arwenNodePart.NewArwenDriver")
-	arwenVM, err := arwenNodePart.NewArwenDriver(logArwenDriver, vmf.blockChainHookImpl, factory.ArwenVirtualMachine, vmf.blockGasLimit, vmf.gasSchedule)
+	arwenVM, err := arwenNodePart.NewArwenDriver(
+		logArwenDriver,
+		vmf.blockChainHookImpl,
+		ipcCommon.ArwenArguments{
+			VMHostArguments: ipcCommon.VMHostArguments{
+				VMType:        factory.ArwenVirtualMachine,
+				BlockGasLimit: vmf.blockGasLimit,
+				GasSchedule:   vmf.gasSchedule,
+			},
+			LogLevel:            ipcLogger.LogDebug,
+			LogsMarshalizer:     ipcMarshaling.JSON,
+			MessagesMarshalizer: ipcMarshaling.JSON,
+		},
+		arwenNodePart.Config{MaxLoopTime: 1000},
+	)
 	return arwenVM, err
 }
 
