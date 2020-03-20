0adaeba1c65eb8001f8ef5ea554ae84854b2e05a
Glonk <mihaibarca79@yahoo.com>
2020-03-20 22:44:30 +0200

    added some missing files

---
+++ integrationTests/multiShard/endOfEpoch/ExecuteBlocksWithTransactionsAndCheckRewards/ExecuteBlocksWithTransactionsAndCheckRewards_test.go
@@ -0,0 +1,76 @@
+package ExecuteBlocksWithTransactionsAndCheckRewards
+
+import (
+	"context"
+	"github.com/ElrondNetwork/elrond-go/integrationTests"
+	"github.com/ElrondNetwork/elrond-go/integrationTests/multiShard/endOfEpoch"
+	"testing"
+	"time"
+)
+
+func TestExecuteBlocksWithTransactionsAndCheckRewards(t *testing.T) {
+	if testing.Short() {
+		t.Skip("this is not a short test")
+	}
+
+	nodesPerShard := 4
+	nbMetaNodes := 2
+	nbShards := 2
+	consensusGroupSize := 2
+
+	advertiser := integrationTests.CreateMessengerWithKadDht(context.Background(), "")
+	_ = advertiser.Bootstrap()
+
+	seedAddress := integrationTests.GetConnectableAddress(advertiser)
+
+	// create map of shard - testNodeProcessors for metachain and shard chain
+	nodesMap := integrationTests.CreateNodesWithNodesCoordinator(
+		nodesPerShard,
+		nbMetaNodes,
+		nbShards,
+		consensusGroupSize,
+		consensusGroupSize,
+		seedAddress,
+	)
+	roundsPerEpoch := uint64(5)
+	maxGasLimitPerBlock := uint64(100000)
+	gasPrice := uint64(10)
+	gasLimit := uint64(100)
+	for _, nodes := range nodesMap {
+		integrationTests.SetEconomicsParameters(nodes, maxGasLimitPerBlock, gasPrice, gasLimit)
+		integrationTests.DisplayAndStartNodes(nodes)
+
+		for _, node := range nodes {
+			node.EpochStartTrigger.SetRoundsPerEpoch(roundsPerEpoch)
+		}
+	}
+
+	defer func() {
+		_ = advertiser.Close()
+		for _, nodes := range nodesMap {
+			for _, n := range nodes {
+				_ = n.Node.Stop()
+			}
+		}
+	}()
+
+	round := uint64(1)
+	nonce := uint64(1)
+	nbBlocksProduced := 2 * roundsPerEpoch
+
+	var consensusNodes map[uint32][]*integrationTests.TestProcessorNode
+
+	for i := uint64(0); i < nbBlocksProduced; i++ {
+		for _, nodes := range nodesMap {
+			integrationTests.UpdateRound(nodes, round)
+		}
+		_, _, consensusNodes = integrationTests.AllShardsProposeBlock(round, nonce, nodesMap)
+
+		indexesProposers := endOfEpoch.GetBlockProposersIndexes(consensusNodes, nodesMap)
+		integrationTests.SyncAllShardsWithRoundBlock(t, nodesMap, indexesProposers, round)
+		round++
+		nonce++
+	}
+
+	time.Sleep(5 * time.Second)
+}
