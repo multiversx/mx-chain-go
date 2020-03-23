403bdadcbba201ffdb4020349e66194979b8ca06
Andrei Bancioiu <andrei.bancioiu@elrond.com>
2020-03-23 22:10:17 +0200

    Fix tests after merge.

--- integrationTests/vm/arwen/badcontracts/badcontracts_test.go
+++ integrationTests/vm/arwen/badcontracts/badcontracts_test.go
@@ -8,55 +8,55 @@ import (
 
 func Test_Bad_C_NoPanic(t *testing.T) {
 	context := arwen.SetupTestContext(t)
-	defer context.close()
+	defer context.Close()
 
-	context.deploySC("./testdata/bad/bad.wasm", "")
+	context.DeploySC("../testdata/bad-misc/bad.wasm", "")
 
-	context.executeSC(&context.Owner, "memoryFault")
-	context.executeSC(&context.Owner, "divideByZero")
+	context.ExecuteSC(&context.Owner, "memoryFault")
+	context.ExecuteSC(&context.Owner, "divideByZero")
 
-	context.executeSC(&context.Owner, "badGetOwner1")
-	context.executeSC(&context.Owner, "badBigIntStorageStore1")
+	context.ExecuteSC(&context.Owner, "badGetOwner1")
+	context.ExecuteSC(&context.Owner, "badBigIntStorageStore1")
 
-	context.executeSC(&context.Owner, "badWriteLog1")
-	context.executeSC(&context.Owner, "badWriteLog2")
-	context.executeSC(&context.Owner, "badWriteLog3")
-	context.executeSC(&context.Owner, "badWriteLog4")
+	context.ExecuteSC(&context.Owner, "badWriteLog1")
+	context.ExecuteSC(&context.Owner, "badWriteLog2")
+	context.ExecuteSC(&context.Owner, "badWriteLog3")
+	context.ExecuteSC(&context.Owner, "badWriteLog4")
 
-	context.executeSC(&context.Owner, "badGetBlockHash1")
-	context.executeSC(&context.Owner, "badGetBlockHash2")
-	context.executeSC(&context.Owner, "badGetBlockHash3")
+	context.ExecuteSC(&context.Owner, "badGetBlockHash1")
+	context.ExecuteSC(&context.Owner, "badGetBlockHash2")
+	context.ExecuteSC(&context.Owner, "badGetBlockHash3")
 
-	context.executeSC(&context.Owner, "badRecursive")
+	context.ExecuteSC(&context.Owner, "badRecursive")
 }
 
 func Test_Empty_C_NoPanic(t *testing.T) {
 	context := arwen.SetupTestContext(t)
-	defer context.close()
+	defer context.Close()
 
-	context.deploySC("./testdata/bad/empty.wasm", "")
-	context.executeSC(&context.Owner, "thisDoesNotExist")
+	context.DeploySC("../testdata/bad-empty/empty.wasm", "")
+	context.ExecuteSC(&context.Owner, "thisDoesNotExist")
 }
 
 func Test_Corrupt_NoPanic(t *testing.T) {
 	context := arwen.SetupTestContext(t)
-	defer context.close()
+	defer context.Close()
 
-	context.deploySC("./testdata/bad/corrupt.wasm", "")
-	context.executeSC(&context.Owner, "thisDoesNotExist")
+	context.DeploySC("../testdata/bad_corrupt.wasm", "")
+	context.ExecuteSC(&context.Owner, "thisDoesNotExist")
 }
 
 func Test_NoMemoryDeclaration_NoPanic(t *testing.T) {
 	context := arwen.SetupTestContext(t)
-	defer context.close()
+	defer context.Close()
 
-	context.deploySC("./testdata/bad/nomemory/nomemory.wasm", "")
-	context.executeSC(&context.Owner, "memoryFault")
+	context.DeploySC("../testdata/bad-nomemory/nomemory.wasm", "")
+	context.ExecuteSC(&context.Owner, "memoryFault")
 }
 
 func Test_BadFunctionNames_NoPanic(t *testing.T) {
 	context := arwen.SetupTestContext(t)
-	defer context.close()
+	defer context.Close()
 
-	context.deploySC("./testdata/bad/badFunctionNames/badFunctionNames.wasm", "")
+	context.DeploySC("../testdata/bad-functionNames/badFunctionNames.wasm", "")
 }
