b016d675ba98c527dca2c93f74e0e7c20d8cb4b7
Robert Sasu <sasu.robert@gmail.com>
2020-04-01 17:09:13 +0300

    update transaction proto

--- data/transaction/transaction.go
+++ data/transaction/transaction.go
@@ -2,7 +2,6 @@
 package transaction
 
 import (
-	"encoding/json"
 	"math/big"
 
 	"github.com/ElrondNetwork/elrond-go/data"
@@ -33,66 +32,6 @@ func (tx *Transaction) SetRcvAddr(addr []byte) {
 // SetSndAddr sets the sender address of the transaction
 func (tx *Transaction) SetSndAddr(addr []byte) {
 	tx.SndAddr = addr
-}
-
-type transactionJson struct {
-	Nonce       uint64 `json:"nonce"`
-	Value       string `json:"value"`
-	RcvAddr     []byte `json:"receiver"`
-	RcvUserName []byte `json:"rcvUserName"`
-	SndAddr     []byte `json:"sender"`
-	GasPrice    uint64 `json:"gasPrice,omitempty"`
-	GasLimit    uint64 `json:"gasLimit,omitempty"`
-	Data        []byte `json:"data,omitempty"`
-	Signature   []byte `json:"signature,omitempty"`
-}
-
-// MarshalJSON converts the Transaction data type into its corresponding equivalent in byte slice.
-// Note that Value data type is converted in a string
-func (tx *Transaction) MarshalJSON() ([]byte, error) {
-	valAsString := "nil"
-	if tx.Value != nil {
-		valAsString = tx.Value.String()
-	}
-
-	txAsJsonStruct := &transactionJson{
-		Nonce:       tx.Nonce,
-		Value:       valAsString,
-		RcvAddr:     tx.RcvAddr,
-		RcvUserName: tx.RcvUserName,
-		SndAddr:     tx.SndAddr,
-		GasPrice:    tx.GasPrice,
-		GasLimit:    tx.GasLimit,
-		Data:        tx.Data,
-		Signature:   tx.Signature,
-	}
-	return json.Marshal(txAsJsonStruct)
-}
-
-// UnmarshalJSON converts the provided bytes into a Transaction data type.
-func (tx *Transaction) UnmarshalJSON(dataBuff []byte) error {
-
-	txAsJsonStruct := &transactionJson{}
-	err := json.Unmarshal(dataBuff, txAsJsonStruct)
-	if err != nil {
-		return err
-	}
-
-	tx.Nonce = txAsJsonStruct.Nonce
-	tx.RcvAddr = txAsJsonStruct.RcvAddr
-	tx.SndAddr = txAsJsonStruct.SndAddr
-	tx.GasPrice = txAsJsonStruct.GasPrice
-	tx.GasLimit = txAsJsonStruct.GasLimit
-	tx.Data = txAsJsonStruct.Data
-	tx.Signature = txAsJsonStruct.Signature
-
-	var ok bool
-	tx.Value, ok = big.NewInt(0).SetString(txAsJsonStruct.Value, 10)
-	if !ok {
-		tx.Value = nil
-	}
-
-	return nil
 }
 
 // TrimSlicePtr creates a copy of the provided slice without the excess capacity
