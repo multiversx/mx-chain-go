7d54fe2baa752d8c53c139e1dd56384fe24e481b
raduchis <radu.chis@gmail.com>
2020-04-07 17:23:39 +0300

    ratings tests april

--- data/state/peerAccount_test.go
+++ data/state/peerAccount_test.go
@@ -245,6 +245,7 @@ func TestPeerAccount_ResetAtNewEpoch(t *testing.T) {
 	acc.IncreaseValidatorSuccessRate(2)
 	acc.DecreaseValidatorSuccessRate(2)
 	acc.IncreaseNumSelectedInSuccessBlocks()
+	acc.ConsecutiveProposerMisses = 7
 
 	acc.ResetAtNewEpoch()
 	assert.Equal(t, big.NewInt(0), acc.GetAccumulatedFees())
@@ -254,6 +255,7 @@ func TestPeerAccount_ResetAtNewEpoch(t *testing.T) {
 	assert.Equal(t, uint32(0), acc.GetValidatorSuccessRate().NumSuccess)
 	assert.Equal(t, uint32(0), acc.GetValidatorSuccessRate().NumFailure)
 	assert.Equal(t, uint32(0), acc.GetNumSelectedInSuccessBlocks())
+	assert.Equal(t, uint32(7), acc.GetConsecutiveProposerMisses())
 }
 
 func TestPeerAccount_IncreaseAndGetNonce(t *testing.T) {
