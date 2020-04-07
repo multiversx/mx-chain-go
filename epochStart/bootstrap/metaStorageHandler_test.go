aefd0ddd9801fcee01c03d611e1146089da146f0
Iuga Mihai <mihai9625@gmail.com>
2020-04-07 21:36:51 +0300

    En-5944 : add unit tests for metaStorageHandler and shardStorageHandler

---
+++ epochStart/bootstrap/metaStorageHandler_test.go
@@ -0,0 +1,147 @@
+package bootstrap
+
+import (
+	"github.com/ElrondNetwork/elrond-go/data"
+	"os"
+	"testing"
+
+	"github.com/ElrondNetwork/elrond-go/config"
+	"github.com/ElrondNetwork/elrond-go/core"
+	"github.com/ElrondNetwork/elrond-go/core/check"
+	"github.com/ElrondNetwork/elrond-go/data/block"
+	"github.com/ElrondNetwork/elrond-go/epochStart/mock"
+	"github.com/ElrondNetwork/elrond-go/process/block/bootstrapStorage"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewMetaStorageHandler_InvalidConfigErr(t *testing.T) {
+	gCfg := config.Config{}
+	coordinator := &mock.CoordinatorStub{}
+	pathManager := &mock.PathManagerStub{}
+	marshalizer := &mock.MarshalizerMock{}
+	hasher := &mock.HasherMock{}
+	uit64Cvt := &mock.Uint64ByteSliceConverterMock{}
+
+	mtStrHandler, err := NewMetaStorageHandler(gCfg, coordinator, pathManager, marshalizer, hasher, 1, uit64Cvt)
+	assert.True(t, check.IfNil(mtStrHandler))
+	assert.NotNil(t, err)
+}
+
+func TestNewMetaStorageHandler_CreateForMetaErr(t *testing.T) {
+	gCfg := getGeneralConfig()
+	coordinator := &mock.CoordinatorStub{}
+	pathManager := &mock.PathManagerStub{}
+	marshalizer := &mock.MarshalizerMock{}
+	hasher := &mock.HasherMock{}
+	uit64Cvt := &mock.Uint64ByteSliceConverterMock{}
+
+	mtStrHandler, err := NewMetaStorageHandler(gCfg, coordinator, pathManager, marshalizer, hasher, 1, uit64Cvt)
+	assert.False(t, check.IfNil(mtStrHandler))
+	assert.Nil(t, err)
+
+	_ = os.RemoveAll("./Epoch_0")
+}
+
+func TestMetaStorageHandler_saveLastHeader(t *testing.T) {
+	defer func() {
+		_ = os.RemoveAll("./Epoch_0")
+	}()
+
+	gCfg := getGeneralConfig()
+	coordinator := &mock.CoordinatorStub{}
+	pathManager := &mock.PathManagerStub{}
+	marshalizer := &mock.MarshalizerMock{}
+	hasher := &mock.HasherMock{}
+	uit64Cvt := &mock.Uint64ByteSliceConverterMock{}
+
+	mtStrHandler, _ := NewMetaStorageHandler(gCfg, coordinator, pathManager, marshalizer, hasher, 1, uit64Cvt)
+
+	header := &block.MetaBlock{Nonce: 0}
+
+	headerHash, _ := core.CalculateHash(marshalizer, hasher, header)
+	expectedBootInfo := bootstrapStorage.BootstrapHeaderInfo{
+		ShardId: core.MetachainShardId, Hash: headerHash,
+	}
+
+	bootHeaderInfo, err := mtStrHandler.saveLastHeader(header)
+	assert.Nil(t, err)
+	assert.Equal(t, expectedBootInfo, bootHeaderInfo)
+}
+
+func TestMetaStorageHandler_saveLastCrossNotarizedHeaders(t *testing.T) {
+	defer func() {
+		_ = os.RemoveAll("./Epoch_0")
+	}()
+
+	gCfg := getGeneralConfig()
+	coordinator := &mock.CoordinatorStub{}
+	pathManager := &mock.PathManagerStub{}
+	marshalizer := &mock.MarshalizerMock{}
+	hasher := &mock.HasherMock{}
+	uit64Cvt := &mock.Uint64ByteSliceConverterMock{}
+
+	mtStrHandler, _ := NewMetaStorageHandler(gCfg, coordinator, pathManager, marshalizer, hasher, 1, uit64Cvt)
+
+	hdr1 := &block.Header{Nonce: 1}
+	hdr2 := &block.Header{Nonce: 2}
+	hdrHash1, _ := core.CalculateHash(marshalizer, hasher, hdr1)
+	hdrHash2, _ := core.CalculateHash(marshalizer, hasher, hdr2)
+
+	hdr3 := &block.MetaBlock{
+		Nonce: 3,
+		EpochStart: block.EpochStart{LastFinalizedHeaders: []block.EpochStartShardData{
+			{HeaderHash: hdrHash1}, {HeaderHash: hdrHash2},
+		}},
+	}
+
+	hdrs := map[string]data.HeaderHandler{string(hdrHash1): hdr1, string(hdrHash2): hdr2}
+	crossNotarizedHdrs, err := mtStrHandler.saveLastCrossNotarizedHeaders(hdr3, hdrs)
+	assert.Nil(t, err)
+	assert.Equal(t, 2, len(crossNotarizedHdrs))
+}
+
+func TestMetaStorageHandler_saveTriggerRegistry(t *testing.T) {
+	defer func() {
+		_ = os.RemoveAll("./Epoch_0")
+	}()
+
+	gCfg := getGeneralConfig()
+	coordinator := &mock.CoordinatorStub{}
+	pathManager := &mock.PathManagerStub{}
+	marshalizer := &mock.MarshalizerMock{}
+	hasher := &mock.HasherMock{}
+	uit64Cvt := &mock.Uint64ByteSliceConverterMock{}
+
+	mtStrHandler, _ := NewMetaStorageHandler(gCfg, coordinator, pathManager, marshalizer, hasher, 1, uit64Cvt)
+
+	components := &ComponentsNeededForBootstrap{
+		EpochStartMetaBlock: &block.MetaBlock{Nonce: 3},
+		PreviousEpochStart:  &block.MetaBlock{Nonce: 2},
+	}
+
+	_, err := mtStrHandler.saveTriggerRegistry(components)
+	assert.Nil(t, err)
+}
+
+func TestMetaStorageHandler_saveDataToStorage(t *testing.T) {
+	defer func() {
+		_ = os.RemoveAll("./Epoch_0")
+	}()
+
+	gCfg := getGeneralConfig()
+	coordinator := &mock.CoordinatorStub{}
+	pathManager := &mock.PathManagerStub{}
+	marshalizer := &mock.MarshalizerMock{}
+	hasher := &mock.HasherMock{}
+	uit64Cvt := &mock.Uint64ByteSliceConverterMock{}
+
+	mtStrHandler, _ := NewMetaStorageHandler(gCfg, coordinator, pathManager, marshalizer, hasher, 1, uit64Cvt)
+
+	components := &ComponentsNeededForBootstrap{
+		EpochStartMetaBlock: &block.MetaBlock{Nonce: 3},
+		PreviousEpochStart:  &block.MetaBlock{Nonce: 2},
+	}
+
+	err := mtStrHandler.SaveDataToStorage(components)
+	assert.Nil(t, err)
+}
---
+++ epochStart/bootstrap/shardStorageHandler_test.go
@@ -0,0 +1,154 @@
+package bootstrap
+
+import (
+	"os"
+	"testing"
+
+	"github.com/ElrondNetwork/elrond-go/core/check"
+	"github.com/ElrondNetwork/elrond-go/data"
+	"github.com/ElrondNetwork/elrond-go/data/block"
+	"github.com/ElrondNetwork/elrond-go/epochStart"
+	"github.com/ElrondNetwork/elrond-go/epochStart/mock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewShardStorageHandler_ShouldWork(t *testing.T) {
+	defer func() {
+		_ = os.RemoveAll("./Epoch_0")
+	}()
+
+	gCfg := getGeneralConfig()
+	coordinator := &mock.CoordinatorStub{}
+	pathManager := &mock.PathManagerStub{}
+	marshalizer := &mock.MarshalizerMock{}
+	hasher := &mock.HasherMock{}
+	uit64Cvt := &mock.Uint64ByteSliceConverterMock{}
+
+	shardStrHandler, err := NewShardStorageHandler(gCfg, coordinator, pathManager, marshalizer, hasher, 1, uit64Cvt)
+	assert.False(t, check.IfNil(shardStrHandler))
+	assert.Nil(t, err)
+}
+
+func TestShardStorageHandler_SaveDataToStorageShardDataNotFound(t *testing.T) {
+	defer func() {
+		_ = os.RemoveAll("./Epoch_0")
+	}()
+
+	gCfg := getGeneralConfig()
+	coordinator := &mock.CoordinatorStub{}
+	pathManager := &mock.PathManagerStub{}
+	marshalizer := &mock.MarshalizerMock{}
+	hasher := &mock.HasherMock{}
+	uit64Cvt := &mock.Uint64ByteSliceConverterMock{}
+
+	shardStrHandler, _ := NewShardStorageHandler(gCfg, coordinator, pathManager, marshalizer, hasher, 1, uit64Cvt)
+
+	components := &ComponentsNeededForBootstrap{
+		EpochStartMetaBlock: &block.MetaBlock{Epoch: 1},
+		PreviousEpochStart:  &block.MetaBlock{Epoch: 1},
+		ShardHeader:         &block.Header{Nonce: 1},
+	}
+
+	err := shardStrHandler.SaveDataToStorage(components)
+	assert.Equal(t, epochStart.ErrEpochStartDataForShardNotFound, err)
+}
+
+func TestShardStorageHandler_SaveDataToStorageMissingHeader(t *testing.T) {
+	defer func() {
+		_ = os.RemoveAll("./Epoch_0")
+	}()
+
+	gCfg := getGeneralConfig()
+	coordinator := &mock.CoordinatorStub{}
+	pathManager := &mock.PathManagerStub{}
+	marshalizer := &mock.MarshalizerMock{}
+	hasher := &mock.HasherMock{}
+	uit64Cvt := &mock.Uint64ByteSliceConverterMock{}
+
+	shardStrHandler, _ := NewShardStorageHandler(gCfg, coordinator, pathManager, marshalizer, hasher, 1, uit64Cvt)
+
+	components := &ComponentsNeededForBootstrap{
+		EpochStartMetaBlock: &block.MetaBlock{
+			Epoch: 1,
+			EpochStart: block.EpochStart{
+				LastFinalizedHeaders: []block.EpochStartShardData{
+					{ShardID: 0, Nonce: 1},
+				},
+			},
+		},
+		PreviousEpochStart: &block.MetaBlock{Epoch: 1},
+		ShardHeader:        &block.Header{Nonce: 1},
+	}
+
+	err := shardStrHandler.SaveDataToStorage(components)
+	assert.Equal(t, epochStart.ErrMissingHeader, err)
+}
+
+func TestShardStorageHandler_SaveDataToStorage(t *testing.T) {
+	defer func() {
+		_ = os.RemoveAll("./Epoch_0")
+	}()
+
+	gCfg := getGeneralConfig()
+	coordinator := &mock.CoordinatorStub{}
+	pathManager := &mock.PathManagerStub{}
+	marshalizer := &mock.MarshalizerMock{}
+	hasher := &mock.HasherMock{}
+	uit64Cvt := &mock.Uint64ByteSliceConverterMock{}
+
+	shardStrHandler, _ := NewShardStorageHandler(gCfg, coordinator, pathManager, marshalizer, hasher, 1, uit64Cvt)
+
+	hash1 := []byte("hash1")
+	hdr1 := block.MetaBlock{
+		Nonce: 1,
+	}
+	headers := map[string]data.HeaderHandler{
+		string(hash1): &hdr1,
+	}
+
+	components := &ComponentsNeededForBootstrap{
+		EpochStartMetaBlock: &block.MetaBlock{
+			Epoch: 1,
+			EpochStart: block.EpochStart{
+				LastFinalizedHeaders: []block.EpochStartShardData{
+					{ShardID: 0, Nonce: 1, FirstPendingMetaBlock: hash1, LastFinishedMetaBlock: hash1},
+				},
+			},
+		},
+		PreviousEpochStart: &block.MetaBlock{Epoch: 1},
+		ShardHeader:        &block.Header{Nonce: 1},
+		Headers:            headers,
+	}
+
+	err := shardStrHandler.SaveDataToStorage(components)
+	assert.Nil(t, err)
+}
+
+func TestGetAllMiniBlocksWithDst(t *testing.T) {
+	t.Parallel()
+
+	hash1 := []byte("hash1")
+	hash2 := []byte("hash2")
+	shardMiniBlockHeader := block.ShardMiniBlockHeader{SenderShardID: 1, Hash: hash1}
+	miniBlockHeader := block.MiniBlockHeader{SenderShardID: 1, Hash: hash2}
+	metablock := &block.MetaBlock{
+		ShardInfo: []block.ShardData{
+			{
+				ShardID: 1,
+				ShardMiniBlockHeaders: []block.ShardMiniBlockHeader{
+					shardMiniBlockHeader,
+					{SenderShardID: 0},
+				},
+			},
+			{ShardID: 0},
+		},
+		MiniBlockHeaders: []block.MiniBlockHeader{
+			{SenderShardID: 0},
+			miniBlockHeader,
+		},
+	}
+
+	shardMbHeaders := getAllMiniBlocksWithDst(metablock, 0)
+	assert.Equal(t, shardMbHeaders[string(hash1)], shardMiniBlockHeader)
+	assert.NotNil(t, shardMbHeaders[string(hash2)])
+}
