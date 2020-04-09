47e18f5e42ff818963f801e3e86d90fac9c10d5c
AdoAdoAdo <anaridado@gmail.com>
2020-04-09 14:30:26 +0300

    integrationTests: remove logging

--- integrationTests/vm/systemVM/systemVm_test.go
+++ integrationTests/vm/systemVM/systemVm_test.go
@@ -10,7 +10,6 @@ import (
 	"testing"
 	"time"
 
-	logger "github.com/ElrondNetwork/elrond-go-logger"
 	"github.com/ElrondNetwork/elrond-go/data/state"
 	"github.com/ElrondNetwork/elrond-go/integrationTests"
 	"github.com/ElrondNetwork/elrond-go/vm/factory"
@@ -112,7 +111,6 @@ func TestStakingUnstakingAndUnboundingOnMultiShardEnvironmentWithValidatorStatis
 		t.Skip("this is not a short test")
 	}
 
-	_ = logger.SetLogLevel("process:TRACE")
 	numOfShards := 2
 	nodesPerShard := 2
 	numMetachainNodes := 2
