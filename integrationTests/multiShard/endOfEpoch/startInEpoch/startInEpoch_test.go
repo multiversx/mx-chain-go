a91613c3defba117f98917af27dd655f645d338c
Robert Sasu <sasu.robert@gmail.com>
2020-04-10 14:42:43 +0300

    use rounder in epoch start.

--- integrationTests/multiShard/endOfEpoch/startInEpoch/startInEpoch_test.go
+++ integrationTests/multiShard/endOfEpoch/startInEpoch/startInEpoch_test.go
@@ -178,6 +178,8 @@ func testNodeStartsInEpoch(t *testing.T, shardID uint32, expectedHighestRound ui
 	time.Sleep(integrationTests.P2pBootstrapDelay)
 	nodeToJoinLate.Messenger = messenger
 
+	rounder := &mock.RounderMock{IndexField: int64(round)}
+
 	trieStorageManager, triesHolder, _ := createTries(getGeneralConfig(), integrationTests.TestMarshalizer, integrationTests.TestHasher, 0, &mock.PathManagerStub{})
 	argsBootstrapHandler := bootstrap.ArgsEpochStartBootstrap{
 		PublicKey:                  nodeToJoinLate.NodeKeys.Pk,
@@ -204,6 +206,7 @@ func testNodeStartsInEpoch(t *testing.T, shardID uint32, expectedHighestRound ui
 		TrieStorageManagers:        trieStorageManager,
 		Uint64Converter:            uint64Converter,
 		NodeShuffler:               &mock.NodeShufflerMock{},
+		Rounder:                    rounder,
 	}
 	epochStartBootstrap, err := bootstrap.NewEpochStartBootstrap(argsBootstrapHandler)
 	assert.Nil(t, err)
