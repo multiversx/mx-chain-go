7f3f366a2d30939cf534cf49a1e90bd2b769664a
Iuga Mihai <mihai9625@gmail.com>
2020-04-13 09:34:01 +0300

    En-6082 : add unit tests miniBlocksPoolsCleaner.go

---
+++ process/block/poolsCleaner/miniBlocksPoolsCleaner_test.go
@@ -0,0 +1,132 @@
+package poolsCleaner
+
+import (
+	"testing"
+
+	"github.com/ElrondNetwork/elrond-go/data/block"
+	"github.com/ElrondNetwork/elrond-go/process"
+	"github.com/ElrondNetwork/elrond-go/process/mock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewMiniBlocksPoolsCleaner_NilMiniblockPoolShouldErr(t *testing.T) {
+	t.Parallel()
+
+	miniblockCleaner, err := NewMiniBlocksPoolsCleaner(nil, &mock.RounderMock{}, &mock.CoordinatorStub{})
+
+	assert.Equal(t, process.ErrNilMiniBlockPool, err)
+	assert.Nil(t, miniblockCleaner)
+}
+
+func TestNewMiniBlocksPoolsCleaner_NilRounderShouldErr(t *testing.T) {
+	t.Parallel()
+
+	miniblockCleaner, err := NewMiniBlocksPoolsCleaner(&mock.CacherStub{}, nil, &mock.CoordinatorStub{})
+
+	assert.Equal(t, process.ErrNilRounder, err)
+	assert.Nil(t, miniblockCleaner)
+}
+
+func TestNewMiniBlocksPoolsCleaner_NilShardCoordinatorShouldErr(t *testing.T) {
+	t.Parallel()
+
+	miniblockCleaner, err := NewMiniBlocksPoolsCleaner(&mock.CacherStub{}, &mock.RoundStub{}, nil)
+
+	assert.Equal(t, process.ErrNilShardCoordinator, err)
+	assert.Nil(t, miniblockCleaner)
+}
+
+func TestNewMiniBlocksPoolsCleaner_ShouldWork(t *testing.T) {
+	t.Parallel()
+
+	miniblockCleaner, err := NewMiniBlocksPoolsCleaner(&mock.CacherStub{}, &mock.RoundStub{}, &mock.CoordinatorStub{})
+
+	assert.Nil(t, err)
+	assert.NotNil(t, miniblockCleaner)
+}
+
+func TestReceivedMiniBlock_WrongTypeShouldBeIgnored(t *testing.T) {
+	t.Parallel()
+
+	miniblockCleaner, _ := NewMiniBlocksPoolsCleaner(&mock.CacherStub{}, &mock.RounderMock{}, &mock.CoordinatorStub{})
+
+	key := []byte("mbKey")
+	miniblock := &block.MiniBlockHeader{}
+	miniblockCleaner.receivedMiniBlock(key, miniblock)
+	assert.Nil(t, miniblockCleaner.mapMiniBlocksRounds[string(key)])
+}
+
+func TestReceivedMiniBlock_ShouldBeAddedInMap(t *testing.T) {
+	t.Parallel()
+
+	miniblockCleaner, _ := NewMiniBlocksPoolsCleaner(&mock.CacherStub{}, &mock.RounderMock{}, &mock.CoordinatorStub{})
+
+	key := []byte("mbKey")
+	miniblock := &block.MiniBlock{}
+	miniblockCleaner.receivedMiniBlock(key, miniblock)
+	assert.NotNil(t, miniblockCleaner.mapMiniBlocksRounds[string(key)])
+}
+
+func TestCleanMiniblocksPoolsIfNeeded_MiniblockNotInPoolShouldBeRemovedFromMap(t *testing.T) {
+	t.Parallel()
+
+	miniblockCleaner, _ := NewMiniBlocksPoolsCleaner(&mock.CacherStub{
+		GetCalled: func(key []byte) (value interface{}, ok bool) {
+			return nil, false
+		},
+	}, &mock.RounderMock{}, &mock.CoordinatorStub{})
+
+	key := []byte("mbKey")
+	miniblock := &block.MiniBlock{}
+	miniblockCleaner.receivedMiniBlock(key, miniblock)
+
+	result := miniblockCleaner.cleanMiniblocksPoolsIfNeeded()
+	assert.Equal(t, 0, result)
+}
+
+func TestCleanMiniblocksPoolsIfNeeded_RoundDiffToShouldMiniblockShouldRemainInMap(t *testing.T) {
+	t.Parallel()
+
+	miniblockCleaner, _ := NewMiniBlocksPoolsCleaner(&mock.CacherStub{
+		GetCalled: func(key []byte) (value interface{}, ok bool) {
+			return nil, true
+		},
+	}, &mock.RounderMock{}, &mock.CoordinatorStub{})
+
+	key := []byte("mbKey")
+	miniblock := &block.MiniBlock{}
+	miniblockCleaner.receivedMiniBlock(key, miniblock)
+
+	result := miniblockCleaner.cleanMiniblocksPoolsIfNeeded()
+	assert.Equal(t, 1, result)
+}
+
+func TestCleanMiniblocksPoolsIfNeeded_MbShouldBeRemovedFromPoolAndMap(t *testing.T) {
+	t.Parallel()
+
+	called := false
+	rounder := &mock.RoundStub{
+		IndexCalled: func() int64 {
+			return 0
+		},
+	}
+	miniblockCleaner, _ := NewMiniBlocksPoolsCleaner(&mock.CacherStub{
+		GetCalled: func(key []byte) (value interface{}, ok bool) {
+			return nil, true
+		},
+		RemoveCalled: func(key []byte) {
+			called = true
+		},
+	}, rounder, &mock.CoordinatorStub{})
+
+	key := []byte("mbKey")
+	miniblock := &block.MiniBlock{}
+	miniblockCleaner.receivedMiniBlock(key, miniblock)
+
+	rounder.IndexCalled = func() int64 {
+		return process.MaxRoundsToKeepUnprocessedMiniBlocks + 1
+	}
+	result := miniblockCleaner.cleanMiniblocksPoolsIfNeeded()
+	assert.Equal(t, 0, result)
+	assert.True(t, called)
+}
