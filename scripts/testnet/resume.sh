4ebb2f7ecaa1bbcdf2f982d591a0f20f8de2c57c
Camil Băncioiu <ioan.camil.bancioiu@gmail.com>
2020-04-13 16:19:49 +0300

    Forgot to add pause.sh and resume.sh to the repository

---
+++ scripts/testnet/resume.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+export ELRONDTESTNETSCRIPTSDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+if [ "$1" == "keep" ]; then
+  KEEPOPEN=1
+else
+  KEEPOPEN=0
+fi
+
+source "$ELRONDTESTNETSCRIPTSDIR/include/nodes.sh"
+source "$ELRONDTESTNETSCRIPTSDIR/include/tools.sh"
+
+if [ $USE_PROXY -eq 1 ]; then
+  resumeProxy
+fi
+
+if [ $USE_TXGEN -eq 1 ]; then
+  resumeTxGen
+fi
+
+resumeValidators
+resumeObservers
+resumeSeednode
