f1d2b06393701a0c17651d1f0fde761a578b17d6
AdoAdoAdo <anaridado@gmail.com>
2020-04-23 20:47:57 +0300

    rename mutex

--- storage/leveldb/batch.go
+++ storage/leveldb/batch.go
@@ -9,50 +9,50 @@ import (
 const removed = "removed"
 
 type batch struct {
-	batch         *leveldb.Batch
-	cachedData    map[string][]byte
-	mutCachedData sync.RWMutex
+	batch      *leveldb.Batch
+	cachedData map[string][]byte
+	mutBatch   sync.RWMutex
 }
 
 // NewBatch creates a batch
 func NewBatch() *batch {
 	return &batch{
-		batch:         &leveldb.Batch{},
-		cachedData:    make(map[string][]byte),
-		mutCachedData: sync.RWMutex{},
+		batch:      &leveldb.Batch{},
+		cachedData: make(map[string][]byte),
+		mutBatch:   sync.RWMutex{},
 	}
 }
 
 // Put inserts one entry - key, value pair - into the batch
 func (b *batch) Put(key []byte, val []byte) error {
-	b.mutCachedData.Lock()
+	b.mutBatch.Lock()
 	b.batch.Put(key, val)
 	b.cachedData[string(key)] = val
-	b.mutCachedData.Unlock()
+	b.mutBatch.Unlock()
 	return nil
 }
 
 // Delete deletes the entry for the provided key from the batch
 func (b *batch) Delete(key []byte) error {
-	b.mutCachedData.Lock()
+	b.mutBatch.Lock()
 	b.batch.Delete(key)
 	b.cachedData[string(key)] = []byte(removed)
-	b.mutCachedData.Unlock()
+	b.mutBatch.Unlock()
 	return nil
 }
 
 // Reset clears the contents of the batch
 func (b *batch) Reset() {
-	b.mutCachedData.Lock()
+	b.mutBatch.Lock()
 	b.batch.Reset()
 	b.cachedData = make(map[string][]byte)
-	b.mutCachedData.Unlock()
+	b.mutBatch.Unlock()
 }
 
 // Get returns the value
 func (b *batch) Get(key []byte) []byte {
-	b.mutCachedData.RLock()
-	defer b.mutCachedData.RUnlock()
+	b.mutBatch.RLock()
+	defer b.mutBatch.RUnlock()
 
 	return b.cachedData[string(key)]
 }
