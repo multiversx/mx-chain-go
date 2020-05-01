43ef1ed2fb9d7312f7b887696466717b26144d05
Andrei Bancioiu <andrei.bancioiu@elrond.com>
2020-05-01 08:17:21 +0300

    Add race test.

--- storage/txcache/eviction.go
+++ storage/txcache/eviction.go
@@ -112,6 +112,7 @@ func (cache *TxCache) evictHighNonceTransactions() (uint32, uint32) {
 	return cache.doEvictItems(txsToEvict, sendersToEvict)
 }
 
+// This is called concurrently by two goroutines: the eviction one and the sweeping one
 func (cache *TxCache) doEvictItems(txsToEvict [][]byte, sendersToEvict []string) (countTxs uint32, countSenders uint32) {
 	countTxs = cache.txByHash.RemoveTxsBulk(txsToEvict)
 	countSenders = cache.txListBySender.RemoveSendersBulk(sendersToEvict)
