8653604de27b26d00f311629275c286999f9b9f4
Iulian Pascalau <iulian.pascalau@gmail.com>
2020-05-06 11:08:25 +0300

    fixed script to copy the systemSmartContractsConfig.toml file

--- scripts/testnet/include/config.sh
+++ scripts/testnet/include/config.sh
@@ -56,6 +56,7 @@ copyNodeConfig() {
   cp $NODEDIR/config/external.toml ./node/config
   cp $NODEDIR/config/p2p.toml ./node/config
   cp $NODEDIR/config/gasSchedule.toml ./node/config
+  cp $NODEDIR/config/systemSmartContractsConfig.toml ./node/config
   mkdir ./node/config/genesisContracts -p
   cp $NODEDIR/config/genesisContracts/*.* ./node/config/genesisContracts
 
