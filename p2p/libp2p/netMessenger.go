47290a5e7208b573738e2d4115720de1de4034fe
Iulian Pascalau <iulian.pascalau@gmail.com>
2020-05-09 00:45:07 +0300

    ForceReachabilityPublic

--- p2p/libp2p/netMessenger.go
+++ p2p/libp2p/netMessenger.go
@@ -101,6 +101,8 @@ func NewNetworkMessenger(args ArgsNetworkMessenger) (*networkMessenger, error) {
 	opts := []libp2p.Option{
 		libp2p.ListenAddrStrings(address),
 		libp2p.Identity(p2pPrivKey),
+		libp2p.DisableRelay(),
+		libp2p.ForceReachabilityPublic(),
 		libp2p.EnableNATService(),
 		libp2p.NATPortMap(),
 	}
