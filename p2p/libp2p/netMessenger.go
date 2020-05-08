d9221cd017d0b60363f7d9ede2b62a5e17172996
Iulian Pascalau <iulian.pascalau@gmail.com>
2020-05-09 00:36:50 +0300

    removed Enable NAT service

--- p2p/libp2p/netMessenger.go
+++ p2p/libp2p/netMessenger.go
@@ -101,7 +101,6 @@ func NewNetworkMessenger(args ArgsNetworkMessenger) (*networkMessenger, error) {
 	opts := []libp2p.Option{
 		libp2p.ListenAddrStrings(address),
 		libp2p.Identity(p2pPrivKey),
-		libp2p.EnableNATService(),
 		libp2p.DisableRelay(),
 	}
 
