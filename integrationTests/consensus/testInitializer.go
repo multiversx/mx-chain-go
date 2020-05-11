879add50e7e6914e62af48b6eff400c187bbcd3e
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2020-05-11 14:54:53 +0300

    Fix initialization of mocks etc.

--- integrationTests/consensus/testInitializer.go
+++ integrationTests/consensus/testInitializer.go
@@ -164,9 +164,11 @@ func createTestShardDataPool() dataRetriever.PoolsHolder {
 	txPool, _ := txpool.NewShardedTxPool(
 		txpool.ArgShardedTxPool{
 			Config: storageUnit.CacheConfig{
-				Size:        100000,
-				SizeInBytes: 1000000000,
-				Shards:      1,
+				Size:                 100000,
+				SizePerSender:        1000,
+				SizeInBytes:          1000000000,
+				SizeInBytesPerSender: 10000000,
+				Shards:               16,
 			},
 			MinGasPrice:    100000000000000,
 			NumberOfShards: 1,
