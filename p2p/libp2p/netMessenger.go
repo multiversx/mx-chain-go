95f41c8ec183711e79e325598ea22f9cf4e48b04
Iulian Pascalau <iulian.pascalau@gmail.com>
2020-05-11 14:47:40 +0300

    try

--- p2p/libp2p/netMessenger.go
+++ p2p/libp2p/netMessenger.go
@@ -20,6 +20,7 @@ import (
 	randFactory "github.com/ElrondNetwork/elrond-go/p2p/libp2p/rand/factory"
 	"github.com/ElrondNetwork/elrond-go/p2p/loadBalancer"
 	"github.com/btcsuite/btcd/btcec"
+	logging "github.com/ipfs/go-log"
 	"github.com/libp2p/go-libp2p"
 	libp2pCrypto "github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/libp2p/go-libp2p-core/host"
@@ -97,9 +98,9 @@ func NewNetworkMessenger(args ArgsNetworkMessenger) (*networkMessenger, error) {
 		return nil, err
 	}
 
-	//logging.SetLogLevel("dht", "DEBUG")
-	//logging.SetLogLevel("nat", "DEBUG")
-	//logging.SetLogLevel("basichost", "DEBUG")
+	logging.SetLogLevel("dht", "DEBUG")
+	logging.SetLogLevel("nat", "DEBUG")
+	logging.SetLogLevel("basichost", "DEBUG")
 
 	address := fmt.Sprintf(args.ListenAddress+"%d", args.P2pConfig.Node.Port)
 	opts := []libp2p.Option{
