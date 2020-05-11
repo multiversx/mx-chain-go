c4c4f1fe586217e6d3155e939aa6ae44540bf263
Iuga Mihai <mihai9625@gmail.com>
2020-05-11 14:34:48 +0300

    EN-6404 : fix comment

--- marshal/interface.go
+++ marshal/interface.go
@@ -1,6 +1,6 @@
 package marshal
 
-// Sizer contains method size that is needed get size of a proto obj
+// Sizer contains method Size that is needed to get the size of a proto obj
 type Sizer interface {
 	Size() (n int)
 }
