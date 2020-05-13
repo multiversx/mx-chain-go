4fd8fcb017b2dd0d5807f4881881c8560e0aa7ec
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2020-05-13 22:19:02 +0300

    Fix test.

--- integrationTests/multiShard/block/executingMiniblocksSc/executingMiniblocksSc_test.go
+++ integrationTests/multiShard/block/executingMiniblocksSc/executingMiniblocksSc_test.go
@@ -352,7 +352,7 @@ func TestShouldProcessWithScTxsJoinNoCommitShouldProcessedByValidators(t *testin
 	round = integrationTests.IncrementAndPrintRound(round)
 	nonce++
 
-	initialVal := big.NewInt(10000000)
+	initialVal := big.NewInt(100000000)
 	topUpValue := big.NewInt(500)
 	integrationTests.MintAllNodes(nodes, initialVal)
 
