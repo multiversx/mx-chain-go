4a5d5abc1f36847abbb6162560279b64844bfc3f
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2020-05-13 14:11:19 +0300

    Fix some tests.

--- integrationTests/vm/arwen/testdata/erc20-c-03/elrond/bigInt.h
+++ integrationTests/vm/arwen/testdata/erc20-c-03/elrond/bigInt.h
@@ -1,3 +1,6 @@
+#ifndef _BIGINT_H_
+#define _BIGINT_H_
+
 #include "types.h"
 
 typedef unsigned int bigInt;
@@ -7,8 +10,8 @@ bigInt    bigIntNew(long long value);
 void      bigIntGetUnsignedArgument(int argumentIndex, bigInt argument);
 void      bigIntGetSignedArgument(int argumentIndex, bigInt argument);
 
-int       bigIntStorageLoadUnsigned(byte *key, bigInt value);
-int       bigIntStorageStoreUnsigned(byte *key, bigInt value);
+int       bigIntStorageLoadUnsigned(byte *key, int keyLength, bigInt value);
+int       bigIntStorageStoreUnsigned(byte *key, int keyLength, bigInt value);
 
 void      bigIntAdd(bigInt destination, bigInt op1, bigInt op2);
 void      bigIntSub(bigInt destination, bigInt op1, bigInt op2);
@@ -20,9 +23,14 @@ long long bigIntGetInt64(bigInt reference);
 void      bigIntSetInt64(bigInt destination, long long value);
 
 void      bigIntFinishUnsigned(bigInt reference);
+void      bigIntFinishSigned(bigInt reference);
 void      bigIntGetCallValue(bigInt destination);
-void      bigIntGetExternalBalance(byte *address, bigInt result);
+void      bigIntgetExternalBalance(byte *address, bigInt result);
 
 int       bigIntByteLength(bigInt reference);
 int       bigIntGetUnsignedBytes(bigInt reference, byte *byte);
+int       bigIntGetSignedBytes(bigInt reference, byte *byte);
 void      bigIntSetUnsignedBytes(bigInt destination, byte *byte, int byteLength);
+void      bigIntSetSignedBytes(bigInt destination, byte *byte, int byteLength);
+
+#endif
