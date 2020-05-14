824eb7642da359dfab268e1747842592590453ab
Andrei Bancioiu <andrei.bancioiu@elrond.com>
2020-05-14 22:26:19 +0300

    Move tests.

--- storage/txcache/txListForSenderAsSortedMapItem_test.go
+++ storage/txcache/txListForSenderAsSortedMapItem_test.go
@@ -53,25 +53,3 @@ func TestSenderAsBucketSortedMapItem_ScoreFluctuatesDeterministicallyWhenTransac
 	require.Equal(t, 8, scoreAB)
 	require.Equal(t, 5, scoreABC)
 }
-
-func Test_computeSenderScore(t *testing.T) {
-	score := computeSenderScore(senderScoreParams{count: 14000, size: kBToBytes(100000), fee: toNanoERD(300), gas: 2500000000, minGasPrice: 100})
-	require.InDelta(t, float64(0.1789683371), score, delta)
-
-	score = computeSenderScore(senderScoreParams{count: 19000, size: kBToBytes(3000), fee: toNanoERD(2300), gas: 19000000000, minGasPrice: 100})
-	require.InDelta(t, float64(0.2517997181), score, delta)
-
-	score = computeSenderScore(senderScoreParams{count: 3, size: kBToBytes(2), fee: toNanoERD(0.04), gas: 400000, minGasPrice: 100})
-	require.InDelta(t, float64(5.795382396), score, delta)
-
-	score = computeSenderScore(senderScoreParams{count: 1, size: kBToBytes(0.3), fee: toNanoERD(0.05), gas: 100000, minGasPrice: 100})
-	require.InDelta(t, float64(100), score, delta)
-}
-
-func Benchmark_computeSenderScore(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		for j := uint64(0); j < 10000000; j++ {
-			computeSenderScore(senderScoreParams{count: j, size: (j + 1) * 500, fee: toNanoERD(float64(0.011) * float64(j)), gas: 100000 * j})
-		}
-	}
-}
