b18bd72b7a7e8eebd5e5c05885c798d78bf6a034
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2020-05-17 13:20:56 +0300

    Remove old tx cache and its tests.

--- process/block/preprocess/adapterFactory.go
+++ process/block/preprocess/adapterFactory.go
@@ -2,6 +2,7 @@ package preprocess
 
 import (
 	"github.com/ElrondNetwork/elrond-go/storage"
+	"github.com/ElrondNetwork/elrond-go/storage/txcache"
 )
 
 // createSortedTransactionsProvider creates a sorted transactions provider for a given cache
@@ -11,5 +12,23 @@ func createSortedTransactionsProvider(transactionsPreprocessor *transactions, ca
 		return newAdapterTxCacheToSortedTransactionsProvider(txCache)
 	}
 
-	return newAdapterGenericCacheToSortedTransactionsProvider(transactionsPreprocessor, cache, cacheKey)
+	log.Error("Could not create a real [SortedTransactionsProvider], will create a disabled one")
+	return &disabledSortedTransactionsProvider{}
+}
+
+type disabledSortedTransactionsProvider struct {
+}
+
+// GetSortedTransactions returns an empty slice
+func (adapter *disabledSortedTransactionsProvider) GetSortedTransactions() []*txcache.WrappedTransaction {
+	return make([]*txcache.WrappedTransaction, 0)
+}
+
+// NotifyAccountNonce does nothing
+func (adapter *disabledSortedTransactionsProvider) NotifyAccountNonce(_ []byte, _ uint64) {
+}
+
+// IsInterfaceNil returns true if there is no value under the interface
+func (adapter *disabledSortedTransactionsProvider) IsInterfaceNil() bool {
+	return adapter == nil
 }
