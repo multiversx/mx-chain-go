3b7c7678e1cd45ba8d41cb27de9989a431db569c
Robert Sasu <sasu.robert@gmail.com>
2020-05-21 12:27:44 +0300

    small fixes.

--- vm/systemSmartContracts/governance.go
+++ vm/systemSmartContracts/governance.go
@@ -1,2 +1,9 @@
 //go:generate protoc -I=proto -I=$GOPATH/src -I=$GOPATH/src/github.com/gogo/protobuf/protobuf  --gogoslick_out=. governance.proto
 package systemSmartContracts
+
+// ArgsNewGovernanceContract defines the arguments needed for the on-chain governance contract
+type ArgsNewGovernanceContract struct {
+}
+
+type governanceContract struct {
+}
