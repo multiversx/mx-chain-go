7df7e2888693d9c50e49dc2db8cc6f23e94b8dc5
Iulian Pascalau <iulian.pascalau@gmail.com>
2020-06-08 12:58:25 +0300

    - fixed TestNewLibp2pConnectionMonitorSimple_ShouldWork test

--- p2p/mock/reconnecterStub.go
+++ p2p/mock/reconnecterStub.go
@@ -9,7 +9,11 @@ type ReconnecterStub struct {
 
 // ReconnectToNetwork -
 func (rs *ReconnecterStub) ReconnectToNetwork() <-chan struct{} {
-	return rs.ReconnectToNetworkCalled()
+	if rs.ReconnectToNetworkCalled != nil {
+		return rs.ReconnectToNetworkCalled()
+	}
+
+	return make(chan struct{}, 1000)
 }
 
 // IsInterfaceNil returns true if there is no value under the interface
