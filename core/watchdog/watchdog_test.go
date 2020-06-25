900e662efd0741ec9210c5d5f69767d83fdaeb7e
BeniaminDrasovean <beniamin.drasovean@elrond.network>
2020-06-25 14:29:09 +0300

    update unit test

--- core/watchdog/watchdog_test.go
+++ core/watchdog/watchdog_test.go
@@ -14,7 +14,7 @@ import (
 func TestNewWatchdog_NilAlarmSchedulerShouldErr(t *testing.T) {
 	t.Parallel()
 
-	w, err := watchdog.NewWatchdog(nil, make(chan endProcess.ArgEndProcess))
+	w, err := watchdog.NewWatchdog(nil, make(chan endProcess.ArgEndProcess, 1))
 	assert.True(t, check.IfNil(w))
 	assert.Equal(t, watchdog.ErrNilAlarmScheduler, err)
 }
@@ -36,7 +36,7 @@ func TestWatchdog_Set(t *testing.T) {
 			addCalled = true
 		},
 	}
-	w, _ := watchdog.NewWatchdog(alarmScheduler, make(chan endProcess.ArgEndProcess))
+	w, _ := watchdog.NewWatchdog(alarmScheduler, make(chan endProcess.ArgEndProcess, 1))
 
 	w.Set(func(alarmID string) {}, time.Second, "alarm")
 
@@ -46,16 +46,25 @@ func TestWatchdog_Set(t *testing.T) {
 func TestWatchdog_SetDefault(t *testing.T) {
 	t.Parallel()
 
+	alarm := "testComponent"
+	channel := make(chan endProcess.ArgEndProcess, 1)
 	addCalled := false
 	alarmScheduler := &mock.AlarmSchedulerStub{
 		AddCalled: func(f func(alarmID string), duration time.Duration, s string) {
+			f(alarm)
 			addCalled = true
 		},
 	}
-	w, _ := watchdog.NewWatchdog(alarmScheduler, make(chan endProcess.ArgEndProcess))
+	w, _ := watchdog.NewWatchdog(alarmScheduler, channel)
 
-	w.SetDefault(time.Second, "alarm")
+	w.SetDefault(time.Second, alarm)
 
+	arg := <-channel
+	expectedReason := "alarm " + alarm + " has expired"
+	expectedDescription := "the " + alarm + " is stuck"
+
+	assert.Equal(t, expectedReason, arg.Reason)
+	assert.Equal(t, expectedDescription, arg.Description)
 	assert.True(t, addCalled)
 }
 
@@ -68,7 +77,7 @@ func TestWatchdog_Stop(t *testing.T) {
 			stopCalled = true
 		},
 	}
-	w, _ := watchdog.NewWatchdog(alarmScheduler, make(chan endProcess.ArgEndProcess))
+	w, _ := watchdog.NewWatchdog(alarmScheduler, make(chan endProcess.ArgEndProcess, 1))
 
 	w.Stop("alarm")
 
@@ -84,7 +93,7 @@ func TestWatchdog_Reset(t *testing.T) {
 			resetCalled = true
 		},
 	}
-	w, _ := watchdog.NewWatchdog(alarmScheduler, make(chan endProcess.ArgEndProcess))
+	w, _ := watchdog.NewWatchdog(alarmScheduler, make(chan endProcess.ArgEndProcess, 1))
 
 	w.Reset("alarm")
 
