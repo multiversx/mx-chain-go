450221bc4a9c99713504d07845f134d1cc0ac716
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2020-06-30 10:44:12 +0300

    Larger duration for sc execution WARN

--- process/smartContract/process.go
+++ process/smartContract/process.go
@@ -7,6 +7,7 @@ import (
 	"math/big"
 	"sort"
 	"strings"
+	"time"
 
 	logger "github.com/ElrondNetwork/elrond-go-logger"
 	"github.com/ElrondNetwork/elrond-go/core"
@@ -27,7 +28,7 @@ var _ process.SmartContractProcessor = (*scProcessor)(nil)
 
 var log = logger.GetOrCreate("process/smartcontract")
 
-const executeDurationWarnThreshold = float64(0.100)
+const executeDurationWarnThreshold = time.Duration(500) * time.Millisecond
 
 var zero = big.NewInt(0)
 
@@ -172,12 +173,12 @@ func (sc *scProcessor) ExecuteSmartContractTransaction(
 	sw.Stop("execute")
 	duration := sw.GetMeasurement("execute")
 
-	logFunc := log.Trace
 	if duration > executeDurationWarnThreshold {
-		logFunc = log.Warn
+		log.Warn(fmt.Sprintf("scProcessor.ExecuteSmartContractTransaction(): execution took > %s", duration), "sc", tx.GetRcvAddr(), "data", string(tx.GetData()), "duration", duration, "returnCode", returnCode, "err", err)
+	} else {
+		log.Trace("scProcessor.ExecuteSmartContractTransaction()", "sc", tx.GetRcvAddr(), "data", string(tx.GetData()), "duration", duration, "returnCode", returnCode, "err", err)
 	}
 
-	logFunc("scProcessor.ExecuteSmartContractTransaction()", "sc", tx.GetRcvAddr(), "data", string(tx.GetData()), "duration", duration, "returnCode", returnCode, "err", err)
 	return returnCode, err
 }
 
