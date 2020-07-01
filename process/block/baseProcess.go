49c00c2294212422b4636fb260fe979498586927
AdoAdoAdo <anaridado@gmail.com>
2020-07-01 10:52:50 +0300

    process: move some logs from debug to trace

--- process/block/baseProcess.go
+++ process/block/baseProcess.go
@@ -944,7 +944,7 @@ func (bp *baseProcessor) saveBody(body *block.Body) {
 	if errNotCritical != nil {
 		log.Warn("saveBody.SaveBlockDataToStorage", "error", errNotCritical.Error())
 	}
-	log.Debug("saveBody.SaveBlockDataToStorage", "time", time.Since(startTime))
+	log.Trace("saveBody.SaveBlockDataToStorage", "time", time.Since(startTime))
 
 	var marshalizedMiniBlock []byte
 	for i := 0; i < len(body.MiniBlocks); i++ {
@@ -959,7 +959,7 @@ func (bp *baseProcessor) saveBody(body *block.Body) {
 		if errNotCritical != nil {
 			log.Warn("saveBody.Put -> MiniBlockUnit", "error", errNotCritical.Error())
 		}
-		log.Debug("saveBody.Put -> MiniBlockUnit", "time", time.Since(startTime))
+		log.Trace("saveBody.Put -> MiniBlockUnit", "time", time.Since(startTime))
 	}
 
 	elapsedTime := time.Since(startTime)
