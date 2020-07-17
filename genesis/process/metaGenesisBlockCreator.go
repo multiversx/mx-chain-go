80de175416ed503ac9b89d37a55fe36ede8c8867
Sebastian Marian <sebastian.marian@oxsy.ro>
2020-07-17 13:09:31 +0300

    * Called commit for validator stats before set the root hash

--- genesis/process/metaGenesisBlockCreator.go
+++ genesis/process/metaGenesisBlockCreator.go
@@ -86,7 +86,7 @@ func CreateMetaGenesisBlock(arg ArgsGenesisBlockCreator, nodesListSplitter genes
 		NodePrice:         big.NewInt(0).Set(arg.Economics.GenesisNodePrice()),
 	}
 
-	validatorRootHash, err := arg.ValidatorAccounts.RootHash()
+	validatorRootHash, err := arg.ValidatorAccounts.Commit()
 	if err != nil {
 		return nil, err
 	}
