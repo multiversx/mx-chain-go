9561dc25081992ff0e80f18f41060d9ed685d7db
Robert Sasu <sasu.robert@gmail.com>
2020-07-24 21:37:46 +0300

    out of process arwen is fixed now.

--- cmd/node/factory/structs.go
+++ cmd/node/factory/structs.go
@@ -955,7 +955,7 @@ func generateGenesisHeadersAndApplyInitialBalances(args *processComponentsFactor
 	economicsData := args.economicsData
 
 	genesisVmConfig := args.mainConfig.VirtualMachineConfig
-	genesisVmConfig.OutOfProcessEnabled = false
+	genesisVmConfig.OutOfProcessConfig.MaxLoopTime = 5000 // 5 seconds
 
 	arg := genesisProcess.ArgsGenesisBlockCreator{
 		GenesisTime:              uint64(nodesSetup.StartTime),
