3c4271b209b274a007910caa430598786e710f4a
Iulian Pascalau <iulian.pascalau@gmail.com>
2020-08-20 13:16:14 +0300

    - fixed indent, fixed integration test

--- integrationTests/node/heartbeat/heartbeat_test.go
+++ integrationTests/node/heartbeat/heartbeat_test.go
@@ -224,7 +224,7 @@ func createSenderWithName(messenger p2p.Messenger, topic string, nodeName string
 		VersionNumber:        version,
 		NodeDisplayName:      nodeName,
 		HardforkTrigger:      &mock.HardforkTriggerStub{},
-		ChainHandler:         &mock.BlockChainMock{},
+		CurrentBlockProvider: &mock.BlockChainMock{},
 	}
 
 	sender, _ := process.NewSender(argSender)
