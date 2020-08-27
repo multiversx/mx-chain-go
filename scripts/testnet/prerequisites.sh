3a9ac706a06f488885f010e63c29ec12f3d289db
Daniel Luca <lucadanielcostin@gmail.com>
2020-08-27 10:24:12 +0300

    Adapt prerequisites.sh for Arch based distributions.

--- scripts/testnet/prerequisites.sh
+++ scripts/testnet/prerequisites.sh
@@ -9,17 +9,24 @@ export DISTRIBUTION=$(cat /etc/os-release | grep "^ID=" | sed 's/ID=//')
 if [[ "$DISTRIBUTION" =~ ^(fedora|centos|rhel)$ ]]; then
   export PACKAGE_MANAGER="dnf"
   export REQUIRED_PACKAGES="git golang gcc lsof jq curl"
-  echo "Using DNF to install required packages: $REQUIRED_PACKAGES"
-fi
+  export INSTALL_PACKAGES_COMMAND="sudo $PACKAGE_MANAGER install -y $REQUIRED_PACKAGES"
 
-if [[ "$DISTRIBUTION" =~ ^(ubuntu|debian)$ ]]; then
+  echo "Using DNF to install required packages: $REQUIRED_PACKAGES"
+elif [[ "$DISTRIBUTION" =~ ^(ubuntu|debian)$ ]]; then
   export PACKAGE_MANAGER="apt-get"
   export REQUIRED_PACKAGES="git gcc lsof jq curl"
+  export INSTALL_PACKAGES_COMMAND="sudo $PACKAGE_MANAGER install -y $REQUIRED_PACKAGES"
 
   echo "Using APT to install required packages: $REQUIRED_PACKAGES"
+elif [[ "$DISTRIBUTION" =~ ^(arch)$ ]]; then
+  export PACKAGE_MANAGER="pacman"
+  export REQUIRED_PACKAGES="git gcc lsof jq curl"
+  export INSTALL_PACKAGES_COMMAND="sudo $PACKAGE_MANAGER -S $REQUIRED_PACKAGES"
+
+  echo "Using PACMAN to install required packages: $REQUIRED_PACKAGES"
 fi
 
-sudo $PACKAGE_MANAGER install -y $REQUIRED_PACKAGES
+$INSTALL_PACKAGES_COMMAND
 
 if [[ "$DISTRIBUTION" =~ ^(ubuntu|debian)$ ]]; then
 
