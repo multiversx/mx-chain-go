aa558d50c62d0f1f116dc2575e728e9fba1e8c25
Robert Sasu <sasu.robert@gmail.com>
2020-08-31 13:37:21 +0300

    resetting at end of epoch

--- epochStart/metachain/systemSCs.go
+++ epochStart/metachain/systemSCs.go
@@ -203,7 +203,7 @@ func (s *systemSCProcessor) stakingToValidatorStatistics(
 	}
 
 	jailedAccount.SetListAndIndex(jailedValidator.ShardId, string(core.JailedList), jailedValidator.Index)
-	jailedAccount.SetRating(jailedAccount.GetTempRating())
+	jailedAccount.ResetAtNewEpoch()
 	err = s.peerAccountsDB.SaveAccount(jailedAccount)
 	if err != nil {
 		return err
