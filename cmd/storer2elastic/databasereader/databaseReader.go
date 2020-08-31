2428b1d3bedc960e76135cfc7515d2e82e27ae9d
bogdan-rosianu <bogdan.rosianu@yahoo.com>
2020-08-31 17:39:22 +0300

    replaced magic string

--- cmd/storer2elastic/databasereader/databaseReader.go
+++ cmd/storer2elastic/databasereader/databaseReader.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 
 	logger "github.com/ElrondNetwork/elrond-go-logger"
+	"github.com/ElrondNetwork/elrond-go/cmd/node/factory"
 	"github.com/ElrondNetwork/elrond-go/config"
 	"github.com/ElrondNetwork/elrond-go/core"
 	"github.com/ElrondNetwork/elrond-go/core/check"
@@ -81,7 +82,7 @@ func (dr *databaseReader) GetDatabaseInfo() ([]*DatabaseInfo, error) {
 		epochStr := re.FindString(dirname)
 		epoch, errParseInt := strconv.ParseInt(epochStr, 10, 64)
 		if errParseInt != nil {
-			if dirname != "Static" {
+			if dirname != factory.DefaultStaticDbString {
 				log.Warn("cannot parse epoch number from directory name", "directory name", dirname)
 			}
 			continue
@@ -164,7 +165,7 @@ func (dr *databaseReader) getShardDirectoriesForEpoch(dirEpoch string) ([]uint32
 // GetStaticDatabaseInfo returns all the databases' data found in the path specified on the constructor
 func (dr *databaseReader) GetStaticDatabaseInfo() ([]*DatabaseInfo, error) {
 	dbs := make([]*DatabaseInfo, 0)
-	dirname := "Static"
+	dirname := factory.DefaultStaticDbString
 	shardDirectories, err := dr.getShardDirectoriesForEpoch(filepath.Join(dr.dbPathWithChainID, dirname))
 	if err != nil {
 		log.Warn("cannot parse shard directories for epoch", "epoch directory name", dirname)
