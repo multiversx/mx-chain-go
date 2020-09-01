b0f410aba24594fa8c8cdf589d2c340efb914a2e
Sebastian Marian <sebastian.marian@oxsy.ro>
2020-09-01 23:12:26 +0300

    * Added functionality for notification of final metachain blocks in block tracker. This notification works either from shard nodes or meta nodes.

--- process/track/errors.go
+++ process/track/errors.go
@@ -10,11 +10,17 @@ var ErrNilBlockTrackerHandler = errors.New("nil block tracker handler")
 // ErrNilCrossNotarizer signals that a nil block notarizer handler has been provided
 var ErrNilCrossNotarizer = errors.New("nil cross notarizer")
 
+// ErrNilSelfNotarizer signals that a nil block notarizer handler has been provided
+var ErrNilSelfNotarizer = errors.New("nil self notarizer")
+
 // ErrNilCrossNotarizedHeadersNotifier signals that a nil block notifier handler has been provided
 var ErrNilCrossNotarizedHeadersNotifier = errors.New("nil cross notarized header notifier")
 
 // ErrNilSelfNotarizedHeadersNotifier signals that a nil block notifier handler has been provided
 var ErrNilSelfNotarizedHeadersNotifier = errors.New("nil self notarized header notifier")
+
+// ErrNilFinalMetachainHeadersNotifier signals that a nil block notifier handler has been provided
+var ErrNilFinalMetachainHeadersNotifier = errors.New("nil final metachain header notifier")
 
 // ErrNotarizedHeaderOffsetIsOutOfBound signals that a requested offset of the notarized header is out of bound
 var ErrNotarizedHeaderOffsetIsOutOfBound = errors.New("requested offset of the notarized header is out of bound")
