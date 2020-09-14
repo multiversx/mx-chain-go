c7b4983c533e2a1a1c4c2f2e8fbf884a66b17dde
Robert Sasu <sasu.robert@gmail.com>
2020-09-14 13:19:37 +0300

    switch jail with new only the waiting list num of validators

--- integrationTests/mock/epochStartSystemSCStub.go
+++ integrationTests/mock/epochStartSystemSCStub.go
@@ -4,13 +4,13 @@ import "github.com/ElrondNetwork/elrond-go/data/state"
 
 // EpochStartSystemSCStub -
 type EpochStartSystemSCStub struct {
-	ProcessSystemSmartContractCalled func(validatorInfos map[uint32][]*state.ValidatorInfo, epoch uint32) error
+	ProcessSystemSmartContractCalled func(validatorInfos map[uint32][]*state.ValidatorInfo) error
 }
 
 // ProcessSystemSmartContract -
-func (e *EpochStartSystemSCStub) ProcessSystemSmartContract(validatorInfos map[uint32][]*state.ValidatorInfo, epoch uint32) error {
+func (e *EpochStartSystemSCStub) ProcessSystemSmartContract(validatorInfos map[uint32][]*state.ValidatorInfo) error {
 	if e.ProcessSystemSmartContractCalled != nil {
-		return e.ProcessSystemSmartContractCalled(validatorInfos, epoch)
+		return e.ProcessSystemSmartContractCalled(validatorInfos)
 	}
 	return nil
 }
