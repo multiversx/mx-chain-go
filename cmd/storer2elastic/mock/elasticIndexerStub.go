350f50ea2f78d3cd22206cc2cece983ba8284bbe
Iuga Mihai <mihai9625@gmail.com>
2020-09-14 09:17:02 +0300

    fix stub

--- cmd/storer2elastic/mock/elasticIndexerStub.go
+++ cmd/storer2elastic/mock/elasticIndexerStub.go
@@ -1,7 +1,7 @@
 package mock
 
 import (
-	"github.com/ElrondNetwork/elrond-go/core/indexer"
+	"github.com/ElrondNetwork/elrond-go/core/indexer/workItems"
 	"github.com/ElrondNetwork/elrond-go/core/statistics"
 	"github.com/ElrondNetwork/elrond-go/data"
 	"github.com/ElrondNetwork/elrond-go/process"
@@ -11,10 +11,10 @@ import (
 type ElasticIndexerStub struct {
 	SetTxLogsProcessorCalled    func(txLogsProc process.TransactionLogProcessorDatabase)
 	SaveBlockCalled             func(body data.BodyHandler, header data.HeaderHandler, txPool map[string]data.TransactionHandler, signersIndexes []uint64, notarizedHeadersHashes []string)
-	SaveRoundsInfosCalled       func(roundsInfos []indexer.RoundInfo)
+	SaveRoundsInfosCalled       func(roundsInfos []workItems.RoundInfo)
 	UpdateTPSCalled             func(tpsBenchmark statistics.TPSBenchmark)
 	SaveValidatorsPubKeysCalled func(validatorsPubKeys map[uint32][][]byte, epoch uint32)
-	SaveValidatorsRatingCalled  func(indexID string, infoRating []indexer.ValidatorRatingInfo)
+	SaveValidatorsRatingCalled  func(indexID string, infoRating []workItems.ValidatorRatingInfo)
 }
 
 // SetTxLogsProcessor -
@@ -32,7 +32,7 @@ func (e *ElasticIndexerStub) SaveBlock(body data.BodyHandler, header data.Header
 }
 
 // SaveRoundsInfos -
-func (e *ElasticIndexerStub) SaveRoundsInfos(roundsInfos []indexer.RoundInfo) {
+func (e *ElasticIndexerStub) SaveRoundsInfos(roundsInfos []workItems.RoundInfo) {
 	if e.SaveRoundsInfosCalled != nil {
 		e.SaveRoundsInfosCalled(roundsInfos)
 	}
@@ -53,7 +53,7 @@ func (e *ElasticIndexerStub) SaveValidatorsPubKeys(validatorsPubKeys map[uint32]
 }
 
 // SaveValidatorsRating -
-func (e *ElasticIndexerStub) SaveValidatorsRating(indexID string, infoRating []indexer.ValidatorRatingInfo) {
+func (e *ElasticIndexerStub) SaveValidatorsRating(indexID string, infoRating []workItems.ValidatorRatingInfo) {
 	if e.SaveValidatorsRatingCalled != nil {
 		e.SaveValidatorsRatingCalled(indexID, infoRating)
 	}
