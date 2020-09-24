462b5c6c9c2352ffe3dae5e791b5817499b00645
raduchis <radu.chis@elrond.com>
2020-09-24 11:15:03 +0300

    moved create node, shardCoordinator, nodesCoordinator to factory helper

--- factory/shardHelper.go
+++ factory/shardHelper.go
@@ -3,12 +3,21 @@ package factory
 import (
 	"errors"
 	"fmt"
+	"time"
 
 	"github.com/ElrondNetwork/elrond-go-logger"
 	"github.com/ElrondNetwork/elrond-go/config"
 	"github.com/ElrondNetwork/elrond-go/core"
+	"github.com/ElrondNetwork/elrond-go/core/closing"
 	"github.com/ElrondNetwork/elrond-go/crypto"
+	"github.com/ElrondNetwork/elrond-go/data/endProcess"
+	"github.com/ElrondNetwork/elrond-go/epochStart"
+	"github.com/ElrondNetwork/elrond-go/hashing"
+	"github.com/ElrondNetwork/elrond-go/marshal"
 	"github.com/ElrondNetwork/elrond-go/sharding"
+	"github.com/ElrondNetwork/elrond-go/storage"
+	"github.com/ElrondNetwork/elrond-go/storage/lrucache"
+	"github.com/ElrondNetwork/elrond-go/update"
 )
 
 func CreateShardCoordinator(
@@ -68,4 +77,137 @@ func getShardIdFromNodePubKey(pubKey crypto.PublicKey, nodesConfig NodesSetupHan
 	}
 
 	return selfShardId, err
+}
+
+func CreateNodesCoordinator(
+	log logger.Logger,
+	nodesConfig NodesSetupHandler,
+	prefsConfig config.PreferencesConfig,
+	epochStartNotifier epochStart.RegistrationHandler,
+	pubKey crypto.PublicKey,
+	marshalizer marshal.Marshalizer,
+	hasher hashing.Hasher,
+	ratingAndListIndexHandler sharding.PeerAccountListAndRatingHandler,
+	bootStorer storage.Storer,
+	nodeShuffler sharding.NodesShuffler,
+	epochConfig config.EpochStartConfig,
+	currentShardID uint32,
+	chanStopNodeProcess chan endProcess.ArgEndProcess,
+	bootstrapParameters BootstrapParamsHandler,
+	startEpoch uint32,
+) (sharding.NodesCoordinator, update.Closer, error) {
+	shardIDAsObserver, err := core.ProcessDestinationShardAsObserver(prefsConfig.DestinationShardAsObserver)
+	if err != nil {
+		return nil, nil, err
+	}
+	if shardIDAsObserver == core.DisabledShardIDAsObserver {
+		shardIDAsObserver = uint32(0)
+	}
+
+	nbShards := nodesConfig.NumberOfShards()
+	shardConsensusGroupSize := int(nodesConfig.GetShardConsensusGroupSize())
+	metaConsensusGroupSize := int(nodesConfig.GetMetaConsensusGroupSize())
+	eligibleNodesInfo, waitingNodesInfo := nodesConfig.InitialNodesInfo()
+
+	eligibleValidators, errEligibleValidators := sharding.NodesInfoToValidators(eligibleNodesInfo)
+	if errEligibleValidators != nil {
+		return nil, nil, errEligibleValidators
+	}
+
+	waitingValidators, errWaitingValidators := sharding.NodesInfoToValidators(waitingNodesInfo)
+	if errWaitingValidators != nil {
+		return nil, nil, errWaitingValidators
+	}
+
+	currentEpoch := startEpoch
+	if bootstrapParameters.NodesConfig() != nil {
+		nodeRegistry := bootstrapParameters.NodesConfig()
+		currentEpoch = bootstrapParameters.Epoch()
+		eligibles := nodeRegistry.EpochsConfig[fmt.Sprintf("%d", currentEpoch)].EligibleValidators
+		eligibleValidators, err = sharding.SerializableValidatorsToValidators(eligibles)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		waitings := nodeRegistry.EpochsConfig[fmt.Sprintf("%d", currentEpoch)].WaitingValidators
+		waitingValidators, err = sharding.SerializableValidatorsToValidators(waitings)
+		if err != nil {
+			return nil, nil, err
+		}
+	}
+
+	pubKeyBytes, err := pubKey.ToByteArray()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	consensusGroupCache, err := lrucache.NewCache(25000)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	maxThresholdEpochDuration := epochConfig.MaxShuffledOutRestartThreshold
+	if !(maxThresholdEpochDuration >= 0.0 && maxThresholdEpochDuration <= 1.0) {
+		return nil, nil, fmt.Errorf("invalid max threshold for shuffled out handler")
+	}
+	minThresholdEpochDuration := epochConfig.MinShuffledOutRestartThreshold
+	if !(minThresholdEpochDuration >= 0.0 && minThresholdEpochDuration <= 1.0) {
+		return nil, nil, fmt.Errorf("invalid min threshold for shuffled out handler")
+	}
+
+	epochDuration := int64(nodesConfig.GetRoundDuration()) * epochConfig.RoundsPerEpoch
+	minDurationBeforeStopProcess := int64(minThresholdEpochDuration * float64(epochDuration))
+	maxDurationBeforeStopProcess := int64(maxThresholdEpochDuration * float64(epochDuration))
+
+	minDurationInterval := time.Millisecond * time.Duration(minDurationBeforeStopProcess)
+	maxDurationInterval := time.Millisecond * time.Duration(maxDurationBeforeStopProcess)
+
+	log.Debug("closing.NewShuffleOutCloser",
+		"minDurationInterval", minDurationInterval,
+		"maxDurationInterval", maxDurationInterval,
+	)
+
+	nodeShufflerOut, err := closing.NewShuffleOutCloser(
+		minDurationInterval,
+		maxDurationInterval,
+		chanStopNodeProcess,
+	)
+	if err != nil {
+		return nil, nil, err
+	}
+	shuffledOutHandler, err := sharding.NewShuffledOutTrigger(pubKeyBytes, currentShardID, nodeShufflerOut.EndOfProcessingHandler)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	argumentsNodesCoordinator := sharding.ArgNodesCoordinator{
+		ShardConsensusGroupSize: shardConsensusGroupSize,
+		MetaConsensusGroupSize:  metaConsensusGroupSize,
+		Marshalizer:             marshalizer,
+		Hasher:                  hasher,
+		Shuffler:                nodeShuffler,
+		EpochStartNotifier:      epochStartNotifier,
+		BootStorer:              bootStorer,
+		ShardIDAsObserver:       shardIDAsObserver,
+		NbShards:                nbShards,
+		EligibleNodes:           eligibleValidators,
+		WaitingNodes:            waitingValidators,
+		SelfPublicKey:           pubKeyBytes,
+		ConsensusGroupCache:     consensusGroupCache,
+		ShuffledOutHandler:      shuffledOutHandler,
+		Epoch:                   currentEpoch,
+		StartEpoch:              startEpoch,
+	}
+
+	baseNodesCoordinator, err := sharding.NewIndexHashedNodesCoordinator(argumentsNodesCoordinator)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	nodesCoordinator, err := sharding.NewIndexHashedNodesCoordinatorWithRater(baseNodesCoordinator, ratingAndListIndexHandler)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return nodesCoordinator, nodeShufflerOut, nil
 }
--- integrationTests/factory/constants.go
+++ integrationTests/factory/constants.go
@@ -5,6 +5,7 @@ const (
 	ratingsPath        = "../../cmd/node/config/ratings.toml"
 	economicsPath      = "../../cmd/node/config/economics.toml"
 	prefsPath          = "../../cmd/node/config/prefs.toml"
+	externalPath       = "../../cmd/node/config/external.toml"
 	p2pPath            = "../../cmd/node/config/p2p.toml"
 	nodesSetupPath     = "../../cmd/node/config/nodesSetup.json"
 	systemSCConfigPath = "../../cmd/node/config/systemSmartContractsConfig.toml"
