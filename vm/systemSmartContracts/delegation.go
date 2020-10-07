a94e84da0bfb2f76ced7d6cfee98cd7e8ad591c7
Robert Sasu <sasu.robert@gmail.com>
2020-10-07 13:22:55 +0300

    delegation init and owner config functions.

--- vm/systemSmartContracts/delegation.go
+++ vm/systemSmartContracts/delegation.go
@@ -1,7 +1,9 @@
 package systemSmartContracts
 
 import (
+	"bytes"
 	"fmt"
+	"math/big"
 
 	"github.com/ElrondNetwork/elrond-go/config"
 	"github.com/ElrondNetwork/elrond-go/core"
@@ -11,6 +13,8 @@ import (
 	"github.com/ElrondNetwork/elrond-go/vm"
 	vmcommon "github.com/ElrondNetwork/elrond-vm-common"
 )
+
+const delegationConfigKey = "delegationConfigKey"
 
 type delegation struct {
 	eei                    vm.SystemEI
@@ -86,13 +90,144 @@ func (d *delegation) Execute(args *vmcommon.ContractCallInput) vmcommon.ReturnCo
 	switch args.Function {
 	case core.SCDeployInitFunctionName:
 		return d.init(args)
+	case "addNodes":
+		return d.addNodes(args)
+	case "removeNodes":
+		return d.removeNodes(args)
+	case "stakeNodes":
+		return d.stakeNodes(args)
+	case "unStakeNodes":
+		return d.unStakeNodes(args)
+	case "unBondNodes":
+		return d.unBondNodes(args)
+	case "delegate":
+		return d.delegate(args)
+	case "unDelegate":
+		return d.unDelegate(args)
+	case "withDraw":
+		return d.withDraw(args)
+	case "changeServiceFee":
+		return d.changeServiceFee(args)
+	case "modifyTotalDelegationCap":
+		return d.modifyTotalDelegationCap(args)
 	}
 
 	return vmcommon.UserError
 }
 
-func (d *delegation) init(_ *vmcommon.ContractCallInput) vmcommon.ReturnCode {
+func (d *delegation) init(args *vmcommon.ContractCallInput) vmcommon.ReturnCode {
+	ownerAddress := d.eei.GetStorage([]byte(ownerKey))
+	if len(ownerAddress) == 0 {
+		d.eei.AddReturnMessage("smart contract was already initialized")
+		return vmcommon.UserError
+	}
+
+	if len(args.Arguments) != 3 {
+		d.eei.AddReturnMessage("not enough arguments to init delegation contract")
+		return vmcommon.UserError
+	}
+
+	d.eei.SetStorage([]byte(ownerKey), args.Arguments[0])
+	dConfig := &DelegationConfig{
+		OwnerAddress:         args.Arguments[0],
+		ServiceFee:           big.NewInt(0).SetBytes(args.Arguments[2]).Uint64(),
+		MaxDelegationCap:     big.NewInt(0).SetBytes(args.Arguments[1]),
+		InitialOwnerFunds:    big.NewInt(0).Set(args.CallValue),
+		AutomaticActivation:  false,
+		NoDelegationCap:      false,
+		ChangeableServiceFee: true,
+		CreatedNonce:         d.eei.BlockChainHook().CurrentNonce(),
+	}
+
+	if dConfig.MaxDelegationCap.Cmp(zero) == 0 {
+		dConfig.NoDelegationCap = true
+	}
+
+	err := d.saveDelegationContractConfig(dConfig)
+	if err != nil {
+		d.eei.AddReturnMessage(err.Error())
+		return vmcommon.UserError
+	}
+
 	return vmcommon.Ok
+}
+
+func (d *delegation) setAutomaticActivation(args *vmcommon.ContractCallInput) vmcommon.ReturnCode {
+	ownerAddress := d.eei.GetStorage([]byte(ownerKey))
+	if !bytes.Equal(args.CallerAddr, ownerAddress) {
+		d.eei.AddReturnMessage("only owner can change delegation config")
+		return vmcommon.UserError
+	}
+	return vmcommon.Ok
+}
+
+func (d *delegation) changeServiceFee(_ *vmcommon.ContractCallInput) vmcommon.ReturnCode {
+	return vmcommon.Ok
+}
+
+func (d *delegation) modifyTotalDelegationCap(_ *vmcommon.ContractCallInput) vmcommon.ReturnCode {
+	return vmcommon.Ok
+}
+
+func (d *delegation) addNodes(_ *vmcommon.ContractCallInput) vmcommon.ReturnCode {
+	return vmcommon.Ok
+}
+
+func (d *delegation) removeNodes(_ *vmcommon.ContractCallInput) vmcommon.ReturnCode {
+	return vmcommon.Ok
+}
+
+func (d *delegation) stakeNodes(_ *vmcommon.ContractCallInput) vmcommon.ReturnCode {
+	return vmcommon.Ok
+}
+
+func (d *delegation) unStakeNodes(_ *vmcommon.ContractCallInput) vmcommon.ReturnCode {
+	return vmcommon.Ok
+}
+
+func (d *delegation) unBondNodes(_ *vmcommon.ContractCallInput) vmcommon.ReturnCode {
+	return vmcommon.Ok
+}
+
+func (d *delegation) delegate(_ *vmcommon.ContractCallInput) vmcommon.ReturnCode {
+	return vmcommon.Ok
+}
+
+func (d *delegation) unDelegate(_ *vmcommon.ContractCallInput) vmcommon.ReturnCode {
+	return vmcommon.Ok
+}
+
+func (d *delegation) withDraw(_ *vmcommon.ContractCallInput) vmcommon.ReturnCode {
+	return vmcommon.Ok
+}
+
+func (d *delegation) executeOnAuctionSC(address []byte, data []byte) (*vmcommon.VMOutput, error) {
+	return d.eei.ExecuteOnDestContext(d.auctionSCAddr, address, big.NewInt(0), data)
+}
+
+func (d *delegation) getDelegationContractConfig() (*DelegationConfig, error) {
+	dConfig := &DelegationConfig{}
+	marshalledData := d.eei.GetStorage([]byte(delegationConfigKey))
+	if len(marshalledData) == 0 {
+		return dConfig, nil
+	}
+
+	err := d.marshalizer.Unmarshal(dConfig, marshalledData)
+	if err != nil {
+		return nil, err
+	}
+
+	return dConfig, nil
+}
+
+func (d *delegation) saveDelegationContractConfig(dConfig *DelegationConfig) error {
+	marshalledData, err := d.marshalizer.Marshal(dConfig)
+	if err != nil {
+		return err
+	}
+
+	d.eei.SetStorage([]byte(delegationConfigKey), marshalledData)
+	return nil
 }
 
 // EpochConfirmed  is called whenever a new epoch is confirmed
