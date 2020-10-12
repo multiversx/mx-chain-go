85f4b4817c38f8d5b55e33bb149c071091fe38fe
Robert Sasu <sasu.robert@gmail.com>
2020-10-12 23:08:40 +0300

    small integration test fix

--- integrationTests/multiShard/smartContract/scCallingSC_test.go
+++ integrationTests/multiShard/smartContract/scCallingSC_test.go
@@ -345,16 +345,15 @@ func TestScDeployAndClaimSmartContractDeveloperRewards(t *testing.T) {
 	}
 
 	txData := "ClaimDeveloperRewards"
-	integrationTests.CreateAndSendTransaction(nodes[0], nodes, big.NewInt(0), firstSCAddress, txData, integrationTests.AdditionalGasLimit)
+	integrationTests.CreateAndSendTransaction(nodes[0], nodes, big.NewInt(0), firstSCAddress, txData, 1)
 
-	for i := 0; i < numRoundsToPropagateMultiShard; i++ {
-		integrationTests.UpdateRound(nodes, round)
-		integrationTests.AddSelfNotarizedHeaderByMetachain(nodes)
-		integrationTests.ProposeBlock(nodes, idxProposers, round, nonce)
-		integrationTests.SyncBlock(t, nodes, idxProposers, round)
-		round = integrationTests.IncrementAndPrintRound(round)
-		nonce++
-	}
+	time.Sleep(time.Second)
+	integrationTests.UpdateRound(nodes, round)
+	integrationTests.AddSelfNotarizedHeaderByMetachain(nodes)
+	integrationTests.ProposeBlock(nodes, idxProposers, round, nonce)
+	integrationTests.SyncBlock(t, nodes, idxProposers, round)
+	round = integrationTests.IncrementAndPrintRound(round)
+	nonce++
 
 	account = getAccountFromAddrBytes(nodes[0].AccntState, nodes[0].OwnAccount.Address)
 	fmt.Println("smart contract owner after claim", account.GetBalance())
