1864d48b989f3660c0a7285d1a7711d0bcf759d1
Camil Băncioiu <ioan.camil.bancioiu@gmail.com>
2020-11-09 16:17:57 +0200

    Fix storage100

--- integrationTests/vm/arwen/testdata/storage100/storage100.c
+++ integrationTests/vm/arwen/testdata/storage100/storage100.c
@@ -1,17 +1,25 @@
 #include "context.h"
 
+byte key[10] = {};
+byte data[100] = {};
+
 void store100() {
-  byte i;
-  byte key[10] = {'m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 0};
-  byte data[100];
+	byte i;
 
-  for (i = 0; i < 100; i++) {
-    data[i] = 'a';
-  }
+	// Fill the key with letters
+	for (i = 0; i < 10; i++) {
+		key[i] = 'f' + i;
+	}
 
-  for (i = 0; i < 10; i++) {
-    key[9] = i;
-    data[99] = i;
-    storageStore(key, 10, data, 100);
-  }
+	// Fill the data with letters / characters
+	for (i = 0; i < 100; i++) {
+		data[i] = 'a' + i;
+	}
+
+	// Store
+	for (i = 0; i < 10; i++) {
+		key[9] = i;
+		data[99] = i;
+		storageStore(key, 10, data, 100);
+	}
 }
