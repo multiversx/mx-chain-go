4c53b1ed05b4192ec4a45651a4443a68194c352f
Cristi Corcoveanu <cristi.corcoveanu@gmail.com>
2020-11-27 10:54:33 +0200

    fixed integration test

--- integrationTests/multiShard/hardFork/hardFork_test.go
+++ integrationTests/multiShard/hardFork/hardFork_test.go
@@ -442,10 +442,12 @@ func hardForkImport(
 					OwnerAddress:    "aaaaaa",
 				},
 				GovernanceSystemSCConfig: config.GovernanceSystemSCConfig{
-					ProposalCost:     "500",
-					MinQuorumV2:        "50",
-					MinPassThresholdV2: "50",
-					MinVetoThresholdV2: "50",
+					Active: config.GovernanceSystemSCConfigActive{
+						ProposalCost:     "500",
+						MinQuorum:        "50",
+						MinPassThreshold: "50",
+						MinVetoThreshold: "50",
+					},
 				},
 				StakingSystemSCConfig: config.StakingSystemSCConfig{
 					GenesisNodePrice:                     "1000",
