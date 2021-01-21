34a93d6da15fee69af3087592bcab1cacfc698c5
Iulian Pascalau <iulian.pascalau@gmail.com>
2021-01-21 19:36:32 +0200

    - added web server test processor node - api small refactor to apply Inversed Dependency Principle - add integration test that prove backwards compatibility on gas cost route

--- integrationTests/interface.go
+++ integrationTests/interface.go
@@ -1,8 +1,19 @@
 package integrationTests
 
 import (
+	"math/big"
+
+	"github.com/ElrondNetwork/elrond-go/api"
 	"github.com/ElrondNetwork/elrond-go/core"
+	"github.com/ElrondNetwork/elrond-go/core/statistics"
+	dataApi "github.com/ElrondNetwork/elrond-go/data/api"
+	"github.com/ElrondNetwork/elrond-go/data/state"
+	"github.com/ElrondNetwork/elrond-go/data/transaction"
+	"github.com/ElrondNetwork/elrond-go/data/vm"
+	"github.com/ElrondNetwork/elrond-go/debug"
 	"github.com/ElrondNetwork/elrond-go/epochStart"
+	"github.com/ElrondNetwork/elrond-go/heartbeat/data"
+	"github.com/ElrondNetwork/elrond-go/node/external"
 	"github.com/ElrondNetwork/elrond-go/process"
 	"github.com/ElrondNetwork/elrond-go/sharding"
 )
@@ -39,3 +50,42 @@ type NetworkShardingUpdater interface {
 	UpdatePeerIdShardId(pid core.PeerID, shardId uint32)
 	IsInterfaceNil() bool
 }
+type (
+	// Facade is the node facade used to decouple the node implementation with the web server. Used in integration tests
+	Facade interface {
+		GetBalance(address string) (*big.Int, error)
+		GetUsername(address string) (string, error)
+		GetValueForKey(address string, key string) (string, error)
+		GetAccount(address string) (state.UserAccountHandler, error)
+		GetCode(account state.UserAccountHandler) []byte
+		GetESDTBalance(address string, key string) (string, string, error)
+		GetAllESDTTokens(address string) ([]string, error)
+		GetBlockByHash(hash string, withTxs bool) (*dataApi.Block, error)
+		GetBlockByNonce(nonce uint64, withTxs bool) (*dataApi.Block, error)
+		Trigger(epoch uint32, withEarlyEndOfEpoch bool) error
+		IsSelfTrigger() bool
+		GetTotalStakedValue() (*big.Int, error)
+		GetHeartbeats() ([]data.PubKeyHeartbeat, error)
+		TpsBenchmark() *statistics.TpsBenchmark
+		StatusMetrics() external.StatusMetricsHandler
+		GetQueryHandler(name string) (debug.QueryHandler, error)
+		GetPeerInfo(pid string) ([]core.QueryP2PPeerInfo, error)
+		GetNumCheckpointsFromAccountState() uint32
+		GetNumCheckpointsFromPeerState() uint32
+		CreateTransaction(nonce uint64, value string, receiver string, receiverUsername []byte, sender string, senderUsername []byte, gasPrice uint64,
+			gasLimit uint64, data []byte, signatureHex string, chainID string, version uint32, options uint32) (*transaction.Transaction, []byte, error)
+		ValidateTransaction(tx *transaction.Transaction) error
+		ValidateTransactionForSimulation(tx *transaction.Transaction) error
+		SendBulkTransactions([]*transaction.Transaction) (uint64, error)
+		SimulateTransactionExecution(tx *transaction.Transaction) (*transaction.SimulationResults, error)
+		GetTransaction(hash string, withResults bool) (*transaction.ApiTransactionResult, error)
+		ComputeTransactionGasLimit(tx *transaction.Transaction) (uint64, error)
+		EncodeAddressPubkey(pk []byte) (string, error)
+		GetThrottlerForEndpoint(endpoint string) (core.Throttler, bool)
+		ValidatorStatisticsApi() (map[string]*state.ValidatorApiResponse, error)
+		ExecuteSCQuery(*process.SCQuery) (*vm.VMOutputApi, error)
+		DecodeAddressPubkey(pk string) ([]byte, error)
+		CreateMiddlewareLimiters() ([]api.MiddlewareProcessor, error)
+		IsInterfaceNil() bool
+	}
+)
