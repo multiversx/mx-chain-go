68c6c0b932174c89ca48831020ae59e226dfd7d5
Robert Sasu <sasu.robert@gmail.com>
2021-01-26 20:12:54 +0200

    added fix and unit test

--- process/smartContract/builtInFunctions/esdtTransfer.go
+++ process/smartContract/builtInFunctions/esdtTransfer.go
@@ -133,6 +133,13 @@ func (e *esdtTransfer) ProcessBuiltinFunction(
 				vmInput.RecipientAddr,
 				vmInput.GasLocked,
 				vmOutput)
+
+			return vmOutput, nil
+		}
+
+		if vmInput.CallType == vmcommon.AsynchronousCallBack && check.IfNil(acntSnd) {
+			// gas was already consumed on sender shard
+			vmOutput.GasRemaining = vmInput.GasProvided
 		}
 
 		return vmOutput, nil
