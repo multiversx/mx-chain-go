bb57768b4c716542f0644d386c89aa896a46bdf7
Iulian Pascalau <iulian.pascalau@gmail.com>
2021-01-27 12:23:40 +0200

    - fixed typo

--- cmd/assessment/benchmarks/errors.go
+++ cmd/assessment/benchmarks/errors.go
@@ -8,5 +8,5 @@ var ErrEmptyBenchmarksSlice = errors.New("empty benchmarks slice provided")
 // ErrNilBenchmark signals that a nil benchmark was provided
 var ErrNilBenchmark = errors.New("nil benchmark")
 
-// ErrFileDoesNotExists signals that the required file does not exists
-var ErrFileDoesNotExists = errors.New("file does not exists")
+// ErrFileDoesNotExists signals that the required file does not exist
+var ErrFileDoesNotExists = errors.New("file does not exist")
