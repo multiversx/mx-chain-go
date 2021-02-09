e3a7846fb3105b8a7e7c12e332150a95e078ff89
Iulian Pascalau <iulian.pascalau@gmail.com>
2021-02-09 21:43:24 +0200

    - fixed 2 more tests

--- core/dblookupext/historyRepository_test.go
+++ core/dblookupext/historyRepository_test.go
@@ -630,7 +630,12 @@ func TestHistoryRepository_ConcurrentlyRecordAndNotarizeSameBlockMultipleTimes(t
 	}
 
 	var wg sync.WaitGroup
+	//wgStartOnNotarizedBlocks is used to synchronize the 2 go routines: the second go routine should
+	// start after the first iteration is done by the first go routine. Otherwise the code coverage can have
+	// unpredictible values in the getMiniblockMetadataByMiniblockHash method, L247-L249
+	var wgStartOnNotarizedBlocks sync.WaitGroup
 	wg.Add(2)
+	wgStartOnNotarizedBlocks.Add(1)
 
 	go func() {
 		// Simulate commit & rollback
@@ -643,12 +648,18 @@ func TestHistoryRepository_ConcurrentlyRecordAndNotarizeSameBlockMultipleTimes(t
 					},
 				}, nil, nil,
 			)
+
+			if i == 0 {
+				wgStartOnNotarizedBlocks.Done()
+			}
 		}
 
 		wg.Done()
 	}()
 
 	go func() {
+		wgStartOnNotarizedBlocks.Wait()
+
 		// Receive less notifications (to test more aggressively)
 		for i := 0; i < 50; i++ {
 			repo.OnNotarizedBlocks(core.MetachainShardId, []data.HeaderHandler{metablock}, [][]byte{[]byte("metablockFoo")})
