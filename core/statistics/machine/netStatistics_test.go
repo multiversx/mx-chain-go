b79ec29261bb69b59780fde874d92fadf8a8ff89
Iulian Pascalau <iulian.pascalau@gmail.com>
2021-02-10 11:02:56 +0200

    - refactored netStatistics - added more unit tests

--- core/statistics/machine/netStatistics_test.go
+++ core/statistics/machine/netStatistics_test.go
@@ -1,6 +1,7 @@
 package machine
 
 import (
+	"errors"
 	"fmt"
 	"sync/atomic"
 	"testing"
@@ -10,10 +11,150 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestNewNetStatistics_ShouldWorkAndNotPanic(t *testing.T) {
+	t.Parallel()
+
+	defer func() {
+		r := recover()
+		if r != nil {
+			assert.Fail(t, fmt.Sprintf("should have not paniced %v", r))
+		}
+	}()
+
+	ns := NewNetStatistics()
+	assert.NotNil(t, ns)
+
+	ns.ComputeStatistics()
+}
+
+func TestNetStatistics_ComputeStatisticsGetStatisticsErrorsFirstTime(t *testing.T) {
+	t.Parallel()
+
+	numCalls := 0
+	expectedErr := errors.New("expected error")
+	ns := &netStatistics{
+		getStatisticsHandler: func() ([]net.IOCountersStat, error) {
+			numCalls++
+			if numCalls == 1 {
+				return nil, expectedErr
+			}
+
+			return []net.IOCountersStat{
+				{
+					BytesSent: 1,
+					BytesRecv: 2,
+				},
+			}, nil
+		},
+	}
+	populateFields(ns)
+
+	ns.ComputeStatistics()
+	checkResetFieldsAreZero(t, ns)
+}
+
+func TestNetStatistics_ComputeStatisticsGetStatisticsReturnsEmptyFirstTime(t *testing.T) {
+	t.Parallel()
+
+	numCalls := 0
+	ns := &netStatistics{
+		getStatisticsHandler: func() ([]net.IOCountersStat, error) {
+			numCalls++
+			if numCalls == 1 {
+				return make([]net.IOCountersStat, 0), nil
+			}
+
+			return []net.IOCountersStat{
+				{
+					BytesSent: 1,
+					BytesRecv: 2,
+				},
+			}, nil
+		},
+	}
+	populateFields(ns)
+
+	ns.ComputeStatistics()
+	checkResetFieldsAreZero(t, ns)
+}
+
+func TestNetStatistics_ComputeStatisticsGetStatisticsErrorsSecondTime(t *testing.T) {
+	t.Parallel()
+
+	numCalls := 0
+	expectedErr := errors.New("expected error")
+	ns := &netStatistics{
+		getStatisticsHandler: func() ([]net.IOCountersStat, error) {
+			numCalls++
+			if numCalls == 2 {
+				return nil, expectedErr
+			}
+
+			return []net.IOCountersStat{
+				{
+					BytesSent: 1,
+					BytesRecv: 2,
+				},
+			}, nil
+		},
+	}
+	populateFields(ns)
+
+	ns.ComputeStatistics()
+	checkResetFieldsAreZero(t, ns)
+}
+
+func TestNetStatistics_ComputeStatisticsGetStatisticsReturnsEmptySecondTime(t *testing.T) {
+	t.Parallel()
+
+	numCalls := 0
+	ns := &netStatistics{
+		getStatisticsHandler: func() ([]net.IOCountersStat, error) {
+			numCalls++
+			if numCalls == 2 {
+				return make([]net.IOCountersStat, 0), nil
+			}
+
+			return []net.IOCountersStat{
+				{
+					BytesSent: 1,
+					BytesRecv: 2,
+				},
+			}, nil
+		},
+	}
+	populateFields(ns)
+
+	ns.ComputeStatistics()
+	checkResetFieldsAreZero(t, ns)
+}
+
+func populateFields(ns *netStatistics) {
+	ns.bpsSent = 1
+	ns.bpsRecv = 2
+	ns.bpsSentPeak = 3
+	ns.bpsRecvPeak = 4
+	ns.percentSent = 5
+	ns.percentRecv = 6
+	ns.totalBytesSentInEpoch = 7
+	ns.totalBytesReceivedInEpoch = 8
+}
+
+func checkResetFieldsAreZero(tb testing.TB, ns *netStatistics) {
+	assert.Equal(tb, uint64(0), ns.BpsSent())
+	assert.Equal(tb, uint64(0), ns.BpsRecv())
+	assert.NotEqual(tb, uint64(0), ns.BpsSentPeak())
+	assert.NotEqual(tb, uint64(0), ns.BpsRecvPeak())
+	assert.Equal(tb, uint64(0), ns.PercentSent())
+	assert.Equal(tb, uint64(0), ns.PercentRecv())
+	assert.NotEqual(tb, uint64(0), ns.TotalBytesSentInCurrentEpoch())
+	assert.NotEqual(tb, uint64(0), ns.TotalBytesReceivedInCurrentEpoch())
+}
+
 func TestNetStatistics_ResetShouldWork(t *testing.T) {
 	t.Parallel()
 
-	ns := &NetStatistics{}
+	ns := NewNetStatistics()
 
 	ns.setZeroStatsAndWait()
 
@@ -37,7 +178,7 @@ func TestNetStatistics_ResetShouldWork(t *testing.T) {
 }
 
 func TestNetStatistics_processStatistics(t *testing.T) {
-	ns := &NetStatistics{}
+	ns := NewNetStatistics()
 
 	start := net.IOCountersStat{
 		BytesRecv: 1000,
@@ -59,7 +200,7 @@ func TestNetStatistics_processStatistics(t *testing.T) {
 }
 
 func TestNetStatistics_processStatisticsInvalidEndVsStart(t *testing.T) {
-	ns := &NetStatistics{}
+	ns := NewNetStatistics()
 
 	start := net.IOCountersStat{
 		BytesRecv: 1000,
@@ -78,4 +219,15 @@ func TestNetStatistics_processStatisticsInvalidEndVsStart(t *testing.T) {
 	assert.Equal(t, uint64(0), atomic.LoadUint64(&ns.bpsSentPeak))
 	assert.Equal(t, uint64(0), atomic.LoadUint64(&ns.percentRecv))
 	assert.Equal(t, uint64(0), atomic.LoadUint64(&ns.percentSent))
+}
+
+func TestNetStatistics_EpochStartEventHandler(t *testing.T) {
+	ns := NewNetStatistics()
+	populateFields(ns)
+
+	handler := ns.EpochStartEventHandler()
+	handler.EpochStartAction(nil)
+
+	assert.Equal(t, uint64(0), ns.TotalBytesSentInCurrentEpoch())
+	assert.Equal(t, uint64(0), ns.TotalBytesReceivedInCurrentEpoch())
 }
