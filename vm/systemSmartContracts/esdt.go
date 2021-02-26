2ac7d753a2790495c497fbd2c5dcc0033b3576db
miiu96 <mihai9625@gmail.com>
2021-02-26 13:45:09 +0200

    more unit tests

--- vm/systemSmartContracts/esdt.go
+++ vm/systemSmartContracts/esdt.go
@@ -871,6 +871,7 @@ func (e *esdt) unSetSpecialRole(args *vmcommon.ContractCallInput) vmcommon.Retur
 	if returnCode != vmcommon.Ok {
 		return returnCode
 	}
+
 	token, returnCode := e.basicOwnershipChecks(args)
 	if returnCode != vmcommon.Ok {
 		return returnCode
@@ -925,6 +926,7 @@ func (e *esdt) getAllAddressesAndRoles(args *vmcommon.ContractCallInput) vmcommo
 		e.eei.AddReturnMessage("needs 1 argument")
 		return vmcommon.UserError
 	}
+
 	if args.CallValue.Cmp(zero) != 0 {
 		e.eei.AddReturnMessage("callValue must be 0")
 		return vmcommon.UserError
