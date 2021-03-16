c82e1203e266fcfd81b3bac8928f01c7dca75c51
Robert Sasu <sasu.robert@gmail.com>
2021-03-16 14:05:36 +0200

    unit tests for constructor

--- process/smartContract/builtInFunctions/esdtNFTCreateRoleTransfer_test.go
+++ process/smartContract/builtInFunctions/esdtNFTCreateRoleTransfer_test.go
@@ -1,1 +1,25 @@
 package builtInFunctions
+
+import (
+	"testing"
+
+	"github.com/ElrondNetwork/elrond-go/process"
+	"github.com/ElrondNetwork/elrond-go/process/mock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEsdtNFTCreateRoleTransfer_Constructor(t *testing.T) {
+	t.Parallel()
+
+	e, err := NewESDTNFTCreateRoleTransfer(nil, &mock.AccountsStub{}, mock.NewMultiShardsCoordinatorMock(2))
+	assert.Nil(t, e)
+	assert.Equal(t, err, process.ErrNilMarshalizer)
+
+	e, err = NewESDTNFTCreateRoleTransfer(&mock.MarshalizerMock{}, &mock.AccountsStub{}, mock.NewMultiShardsCoordinatorMock(2))
+	assert.Nil(t, e)
+	assert.Equal(t, err, process.ErrNilAccountsAdapter)
+
+	e, err := NewESDTNFTCreateRoleTransfer(nil, &mock.AccountsStub{}, mock.NewMultiShardsCoordinatorMock(2))
+	assert.Nil(t, e)
+	assert.Equal(t, err, process.ErrNilMarshalizer)
+}
