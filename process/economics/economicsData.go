b636f5c6cd82bec0e5c05eff3b04d60b46aa2eed
mateigraura <grauramatei@gmail.com>
2021-03-18 20:32:40 +0200

    fix wrong flags in logs

--- process/economics/economicsData.go
+++ process/economics/economicsData.go
@@ -100,7 +100,7 @@ func NewEconomicsData(args ArgsNewEconomicsData) (*economicsData, error) {
 		statusHandler:                    statusHandler.NewNilStatusHandler(),
 	}
 	log.Debug("economicsData: enable epoch for penalized too much gas", "epoch", ed.penalizedTooMuchGasEnableEpoch)
-	log.Debug("economicsData: enable epoch for gas price modifier", "epoch", ed.gasPriceModifier)
+	log.Debug("economicsData: enable epoch for gas price modifier", "epoch", ed.gasPriceModifierEnableEpoch)
 
 	ed.yearSettings = make(map[uint32]*config.YearSetting)
 	for _, yearSetting := range args.Economics.GlobalSettings.YearSettings {
--- process/smartContract/process.go
+++ process/smartContract/process.go
@@ -190,7 +190,7 @@ func NewSmartContractProcessor(args ArgsNewSmartContractProcessor) (*scProcessor
 	}
 
 	log.Debug("smartContract/process: enable epoch for sc deploy", "epoch", sc.deployEnableEpoch)
-	log.Debug("smartContract/process: enable epoch for built in function", "epoch", sc.builtinEnableEpoch)
+	log.Debug("smartContract/process: enable epoch for built in functions", "epoch", sc.builtinEnableEpoch)
 	log.Debug("smartContract/process: enable epoch for repair callback", "epoch", sc.repairCallBackEnableEpoch)
 	log.Debug("smartContract/process: enable epoch for penalized too much gas", "epoch", sc.penalizedTooMuchGasEnableEpoch)
 	log.Debug("smartContract/process: enable epoch for staking v2", "epoch", sc.stakingV2EnableEpoch)
