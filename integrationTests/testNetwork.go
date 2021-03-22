a444251d21de1d927007ef2afa8519b6436bb44e
Camil Băncioiu <ioan.camil.bancioiu@gmail.com>
2021-03-22 19:11:43 +0200

    Make TestNetwork exported

--- integrationTests/testNetwork.go
+++ integrationTests/testNetwork.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-type testNetwork struct {
+type TestNetwork struct {
 	NumShards         int
 	NodesPerShard     int
 	NodesInMetashard  int
@@ -21,14 +21,14 @@ type testNetwork struct {
 	T                 *testing.T
 }
 
-func NewTestNetwork(t *testing.T) *testNetwork {
+func NewTestNetwork(t *testing.T) *TestNetwork {
 	// TODO replace testing.T with testing.TB everywhere in the integrationTest utilities
-	return &testNetwork{
+	return &TestNetwork{
 		T: t,
 	}
 }
 
-func (net *testNetwork) Start() {
+func (net *TestNetwork) Start() {
 	net.createAdvertiser()
 	net.createNodes()
 	net.indexProposers()
@@ -38,12 +38,12 @@ func (net *testNetwork) Start() {
 	net.Nonce = 0
 }
 
-func (net *testNetwork) Step() {
+func (net *TestNetwork) Step() {
 	net.Round = IncrementAndPrintRound(net.Round)
 	net.Nonce++
 }
 
-func (net *testNetwork) Steps(steps int) {
+func (net *TestNetwork) Steps(steps int) {
 	net.Nonce, net.Round = WaitOperationToBeDone(
 		net.T,
 		net.Nodes,
@@ -54,26 +54,26 @@ func (net *testNetwork) Steps(steps int) {
 	)
 }
 
-func (net *testNetwork) Close() {
+func (net *TestNetwork) Close() {
 	net.closeAdvertiser()
 	net.closeNodes()
 }
 
-func (net *testNetwork) MintNodeAccounts(value *big.Int) {
+func (net *TestNetwork) MintNodeAccounts(value *big.Int) {
 	MintAllNodes(net.Nodes, value)
 }
 
-func (net *testNetwork) MintNodeAccountsInt64(value int64) {
+func (net *TestNetwork) MintNodeAccountsInt64(value int64) {
 	MintAllNodes(net.Nodes, big.NewInt(value))
 }
 
-func (net *testNetwork) createAdvertiser() {
+func (net *TestNetwork) createAdvertiser() {
 	net.Advertiser = CreateMessengerWithKadDht(net.AdvertiserAddress)
 	err := net.Advertiser.Bootstrap()
 	require.Nil(net.T, err)
 }
 
-func (net *testNetwork) createNodes() {
+func (net *TestNetwork) createNodes() {
 	net.Nodes = CreateNodes(
 		net.NumShards,
 		net.NodesPerShard,
@@ -82,7 +82,7 @@ func (net *testNetwork) createNodes() {
 	)
 }
 
-func (net *testNetwork) indexProposers() {
+func (net *TestNetwork) indexProposers() {
 	net.Proposers = make([]int, net.NumShards+1)
 	for i := 0; i < net.NumShards; i++ {
 		net.Proposers[i] = i * net.NumShards
@@ -90,16 +90,16 @@ func (net *testNetwork) indexProposers() {
 	net.Proposers[net.NumShards] = net.NumShards * net.NodesPerShard
 }
 
-func (net *testNetwork) startNodes() {
+func (net *TestNetwork) startNodes() {
 	DisplayAndStartNodes(net.Nodes)
 }
 
-func (net *testNetwork) closeAdvertiser() {
+func (net *TestNetwork) closeAdvertiser() {
 	err := net.Advertiser.Close()
 	require.Nil(net.T, err)
 }
 
-func (net *testNetwork) closeNodes() {
+func (net *TestNetwork) closeNodes() {
 	for _, node := range net.Nodes {
 		err := node.Messenger.Close()
 		require.Nil(net.T, err)
