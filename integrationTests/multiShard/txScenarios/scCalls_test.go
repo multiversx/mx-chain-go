6433e2ddd45ff0e64adc4e570a4ee65210df10dd
Camil Băncioiu <ioan.camil.bancioiu@gmail.com>
2021-03-23 16:43:54 +0200

    Improve TestNetwork; work in progress

--- integrationTests/multiShard/txScenarios/scCalls_test.go
+++ integrationTests/multiShard/txScenarios/scCalls_test.go
@@ -12,7 +12,257 @@ import (
 	"github.com/ElrondNetwork/elrond-go/integrationTests/vm"
 	"github.com/ElrondNetwork/elrond-go/process/factory"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func TestTransactionSigning(t *testing.T) {
+	initialBalance := uint64(1000000000000)
+	net := createGeneralTestnetForTxTest(t, initialBalance)
+	defer net.Close()
+
+	net.Increment()
+
+	sender := net.Wallets[0]
+	receiver := sender
+	tx := net.CreateTxUint64(sender, receiver.Address, 0, []byte("some data"))
+	tx.GasLimit = net.MaxGasLimit
+
+	fmt.Println(tx)
+
+	err := net.DefaultNode.Node.ValidateTransaction(tx)
+	require.Nil(t, err)
+}
+
+func TestTransactionSigning2(t *testing.T) {
+	initialBalance := big.NewInt(1000000000000)
+	nodes, _, players, advertiser := createGeneralSetupForTxTest(initialBalance)
+	defer func() {
+		_ = advertiser.Close()
+		for _, n := range nodes {
+			_ = n.Messenger.Close()
+		}
+	}()
+
+	round := uint64(0)
+	nonce := uint64(0)
+	round = integrationTests.IncrementAndPrintRound(round)
+	nonce++
+
+	txData := []byte("some data")
+	tx := createUserTx(
+		players[0],
+		players[0].Address,
+		big.NewInt(0),
+		txData,
+		integrationTests.MinTxGasPrice,
+		nodes[0].EconomicsData.MaxGasLimitPerBlock(0)-1)
+
+	fmt.Println(tx)
+	err := nodes[0].Node.ValidateTransaction(tx)
+	require.Nil(t, err)
+}
+
+func TestTransactionSigning3(t *testing.T) {
+	txData := []byte("some data")
+	initialBalance := big.NewInt(1000000000000)
+
+	// init old network ===================
+	nodes, _, players, advertiser := createGeneralSetupForTxTest(initialBalance)
+	defer func() {
+		_ = advertiser.Close()
+		for _, n := range nodes {
+			_ = n.Messenger.Close()
+		}
+	}()
+
+	round := uint64(0)
+	nonce := uint64(0)
+	round = integrationTests.IncrementAndPrintRound(round)
+	nonce++
+
+	// init TestNetwork ===================
+	net := createGeneralTestnetForTxTest(t, initialBalance.Uint64())
+	defer net.Close()
+
+	net.Increment()
+
+	// create old-style tx ===================
+	oldTxByOldWallet := createUserTx(players[0], players[0].Address, big.NewInt(0), txData, integrationTests.MinTxGasPrice, nodes[0].EconomicsData.MaxGasLimitPerBlock(0)-1)
+	players[0].Nonce--
+	oldTxByNewWallet := createUserTx(net.Wallets[0], net.Wallets[0].Address, big.NewInt(0), txData, integrationTests.MinTxGasPrice, nodes[0].EconomicsData.MaxGasLimitPerBlock(0)-1)
+	net.Wallets[0].Nonce--
+
+	// create new-style tx ==============
+	newTxByOldWallet := net.CreateTxUint64(players[0], players[0].Address, 0, txData)
+	players[0].Nonce--
+	newTxByOldWallet.GasLimit = net.MaxGasLimit
+
+	newTxByNewWallet := net.CreateTxUint64(net.Wallets[0], net.Wallets[0].Address, 0, txData)
+	newTxByNewWallet.GasLimit = net.MaxGasLimit
+	net.Wallets[0].Nonce--
+
+	// transaction comparations: tx using old vs new wallets, created by old network
+	expectedForSigning, err := oldTxByOldWallet.GetDataForSigning(integrationTests.TestAddressPubkeyConverter, integrationTests.TestTxSignMarshalizer)
+	require.Nil(t, err)
+	actualForSigning, err := newTxByOldWallet.GetDataForSigning(integrationTests.TestAddressPubkeyConverter, integrationTests.TestTxSignMarshalizer)
+	require.Nil(t, err)
+	require.Equal(t, expectedForSigning, actualForSigning)
+
+	signatureByOldWallet, err := players[0].SingleSigner.Sign(players[0].SkTxSign, actualForSigning)
+
+	// transaction comparations: tx using old vs new wallets, created by new network
+	expectedForSigning, err = oldTxByNewWallet.GetDataForSigning(integrationTests.TestAddressPubkeyConverter, integrationTests.TestTxSignMarshalizer)
+	require.Nil(t, err)
+	actualForSigning, err = newTxByNewWallet.GetDataForSigning(integrationTests.TestAddressPubkeyConverter, integrationTests.TestTxSignMarshalizer)
+	require.Nil(t, err)
+	require.Equal(t, expectedForSigning, actualForSigning)
+
+	signatureByNewWallet, err := net.Wallets[0].SingleSigner.Sign(net.Wallets[0].SkTxSign, actualForSigning)
+
+	// transaction comparations: signatures
+	require.Equal(t, signatureByOldWallet, oldTxByOldWallet.Signature)
+	require.Equal(t, signatureByOldWallet, newTxByOldWallet.Signature)
+	require.Equal(t, signatureByNewWallet, oldTxByNewWallet.Signature)
+	require.Equal(t, signatureByNewWallet, newTxByNewWallet.Signature)
+
+	// old net validates old tx
+	err = nodes[0].Node.ValidateTransaction(oldTxByOldWallet)
+	assert.Nil(t, err)
+
+	// new net validates old tx
+	err = net.DefaultNode.Node.ValidateTransaction(oldTxByNewWallet)
+	assert.Nil(t, err)
+
+	// old net validates new tx
+	err = nodes[0].Node.ValidateTransaction(newTxByOldWallet)
+	assert.Nil(t, err)
+
+	// new net validates new tx
+	err = net.DefaultNode.Node.ValidateTransaction(newTxByNewWallet)
+	assert.Nil(t, err)
+}
+
+func TestTransaction_TransactionSCScenarios_Rewritten(t *testing.T) {
+	if testing.Short() {
+		t.Skip("this is not a short test")
+	}
+
+	initialBalance := uint64(1000000000000)
+	net := createGeneralTestnetForTxTest(t, initialBalance)
+	defer net.Close()
+
+	net.Increment()
+
+	scPath := "./../../vm/arwen/testdata/counter/counter.wasm"
+	scCode, err := ioutil.ReadFile(scPath)
+
+	if err != nil {
+		panic(fmt.Sprintf("cannotReadContractCode: %s", err))
+	}
+
+	players := net.Wallets
+
+	// TODO rewrite the following lines with the TxDataBuilder when
+	//	merged into development
+	scCodeString := hex.EncodeToString(scCode)
+	scCodeMetadataString := "0000"
+	txData := []byte(scCodeString + "@" + hex.EncodeToString(factory.ArwenVirtualMachine) + "@" + scCodeMetadataString)
+
+	// // deploy contract insufficient gas limit
+	player0 := players[0]
+	// tx := net.CreateTxUint64(player0, net.DeploymentAddress, 0, txData)
+
+	// net.BypassErrorsOnce = true
+	// net.SendTx(tx)
+	// time.Sleep(time.Second)
+
+	// // Nonce decremented because the previous tx was deliberately wrong
+	// player0.Nonce--
+	// net.RequireWalletNoncesInSyncWithState()
+
+	// contract data is not ok
+	invalidContractData := make([]byte, len(txData))
+	_ = copy(invalidContractData, txData)
+	invalidContractData[123] = 50
+	invalidContractData[124] = 51
+
+	player2 := players[2]
+	tx2 := net.CreateTxUint64(player2, net.DeploymentAddress, 0, txData)
+	tx2.GasLimit = net.MaxGasLimit
+
+	err = net.DefaultNode.Node.ValidateTransaction(tx2)
+	require.Nil(t, err)
+
+	net.SendTx(tx2)
+	time.Sleep(time.Second)
+
+	// contract deploy should work
+	player4 := players[4]
+	scAddress := net.NewAddress(player4)
+	tx3 := net.CreateTxUint64(player4, net.DeploymentAddress, 0, txData)
+	tx3.GasLimit = net.MaxGasLimit
+
+	net.SendTx(tx3)
+	time.Sleep(time.Second)
+
+	// delay for processing
+	nrRoundsToTest := int64(5)
+	for i := int64(0); i < nrRoundsToTest; i++ {
+		net.Step()
+		integrationTests.AddSelfNotarizedHeaderByMetachain(net.Nodes)
+		time.Sleep(time.Second)
+	}
+
+	//check balance address that try to deploy contract should not be modified
+	senderAccount := net.GetAccountHandler(player0.Address)
+	require.Equal(t, uint64(0), senderAccount.GetNonce())
+	require.Equal(t, initialBalance, senderAccount.GetBalance())
+
+	//check balance address that try to deploy contract should consume all the
+	//gas provided
+	txFee := net.ComputeTxFeeUint64(tx2)
+	expectedBalance := initialBalance - txFee
+	senderAccount = net.GetAccountHandler(player2.Address)
+	require.Equal(t, player2.Nonce, senderAccount.GetNonce())
+	require.Equal(t, expectedBalance, senderAccount.GetBalance().Int64())
+
+	//deploy should work gas used should be greater than estimation and small that all gas provided
+	gasUsed := net.ComputeGasLimit(tx3)
+	txFee = gasUsed * integrationTests.MinTxGasPrice
+	expectedBalance = initialBalance - txFee
+
+	senderAccount = net.GetAccountHandler(player4.Address)
+	require.Equal(t, player4.Nonce, senderAccount.GetNonce())
+	require.Equal(t, expectedBalance, senderAccount.GetBalance().Uint64())
+
+	// do some smart contract call from shard 1 to shard 0
+	// address is in shard 1
+	sender := players[1]
+	txData = []byte("increment")
+	numIncrement := 10
+	for i := 0; i < numIncrement; i++ {
+		tx := net.CreateTxUint64(sender, scAddress, 0, txData)
+		tx.GasLimit = net.MaxGasLimit
+
+		net.SendTxFromNode(tx, net.Nodes[1])
+		time.Sleep(time.Millisecond)
+	}
+
+	// wait transaction to propagate cross shard
+	for i := int64(0); i < 15; i++ {
+		net.Step()
+		integrationTests.AddSelfNotarizedHeaderByMetachain(net.Nodes)
+		time.Sleep(time.Second)
+	}
+
+	// check account nonce that called increment method
+	senderAccount = net.GetAccountHandler(players[1].Address)
+	require.Equal(t, players[1].Nonce, senderAccount.GetNonce())
+
+	// check value of counter from contract
+	counterValue := vm.GetIntValueFromSC(nil, net.DefaultNode.AccntState, scAddress, "get", nil)
+	require.Equal(t, int64(numIncrement+1), counterValue.Int64())
+}
 
 func TestTransaction_TransactionSCScenarios(t *testing.T) {
 	if testing.Short() {
--- integrationTests/testNetwork.go
+++ integrationTests/testNetwork.go
@@ -4,45 +4,93 @@ import (
 	"math/big"
 	"testing"
 
+	"github.com/ElrondNetwork/elrond-go/data/state"
+	"github.com/ElrondNetwork/elrond-go/data/transaction"
 	"github.com/ElrondNetwork/elrond-go/p2p"
+	"github.com/ElrondNetwork/elrond-go/process/factory"
 	"github.com/stretchr/testify/require"
 )
 
+type ShardIdentifier = uint32
+type Address = []byte
+type NodeSlice = []*TestProcessorNode
+type NodesByShardMap = map[ShardIdentifier]NodeSlice
+type GasScheduleMap = map[string]map[string]uint64
+
+// TODO combine TestNetwork with the preexisting TestContext and OneNodeNetwork
+// into a single struct containing the functionality of all three
 type TestNetwork struct {
-	NumShards         int
-	NodesPerShard     int
-	NodesInMetashard  int
-	Nodes             []*TestProcessorNode
-	Proposers         []int
-	Advertiser        p2p.Messenger
-	AdvertiserAddress string
-	Round             uint64
-	Nonce             uint64
-	T                 *testing.T
+	NumShards          int
+	NodesPerShard      int
+	NodesInMetashard   int
+	Nodes              NodeSlice
+	NodesSharded       NodesByShardMap
+	Wallets            []*TestWalletAccount
+	DeploymentAddress  Address
+	Proposers          []int
+	Advertiser         p2p.Messenger
+	AdvertiserAddress  string
+	Round              uint64
+	Nonce              uint64
+	T                  *testing.T
+	DefaultNode        *TestProcessorNode
+	DefaultGasPrice    uint64
+	MinGasLimit        uint64
+	MaxGasLimit        uint64
+	DefaultVM          []byte
+	DefaultGasSchedule GasScheduleMap
+	BypassErrorsOnce   bool
 }
 
 func NewTestNetwork(t *testing.T) *TestNetwork {
-	// TODO replace testing.T with testing.TB everywhere in the integrationTest utilities
+	// TODO replace testing.T with testing.TB everywhere in integrationTest
 	return &TestNetwork{
-		T: t,
+		T:                t,
+		BypassErrorsOnce: false,
 	}
 }
 
+func NewTestNetworkSized(t *testing.T, numShards int, nodesPerShard int, nodesInMetashard int) *TestNetwork {
+	net := NewTestNetwork(t)
+	net.NumShards = numShards
+	net.NodesPerShard = nodesPerShard
+	net.NodesInMetashard = nodesInMetashard
+
+	return net
+}
+
+// Start initializes the test network and starts its nodes
 func (net *TestNetwork) Start() {
+	net.Round = 0
+	net.Nonce = 0
+
 	net.createAdvertiser()
 	net.createNodes()
 	net.indexProposers()
 	net.startNodes()
-
-	net.Round = 0
-	net.Nonce = 0
+	net.mapNodesByShard()
+	net.initDefaults()
 }
 
-func (net *TestNetwork) Step() {
+// Increment only increments the Round and the Nonce, without triggering the
+// processing of a block; use Step to process a block as well.
+func (net *TestNetwork) Increment() {
 	net.Round = IncrementAndPrintRound(net.Round)
 	net.Nonce++
 }
 
+// Step increments the Round and Nonce and triggers the production and
+// synchronization of a single block.
+func (net *TestNetwork) Step() {
+	net.Round, net.Nonce = ProposeAndSyncOneBlock(
+		net.T,
+		net.Nodes,
+		net.Proposers,
+		net.Round,
+		net.Nonce)
+}
+
+// Steps repeatedly increments the Round and Nonce and processes blocks.
 func (net *TestNetwork) Steps(steps int) {
 	net.Nonce, net.Round = WaitOperationToBeDone(
 		net.T,
@@ -50,27 +98,165 @@ func (net *TestNetwork) Steps(steps int) {
 		steps,
 		net.Nonce,
 		net.Round,
-		net.Proposers,
-	)
+		net.Proposers)
 }
 
+// Close shuts down the test network.
 func (net *TestNetwork) Close() {
 	net.closeAdvertiser()
 	net.closeNodes()
 }
 
+// MintNodeAccounts adds the specified value to the accounts owned by the nodes
+// of the TestNetwork.
 func (net *TestNetwork) MintNodeAccounts(value *big.Int) {
 	MintAllNodes(net.Nodes, value)
 }
 
-func (net *TestNetwork) MintNodeAccountsInt64(value int64) {
-	MintAllNodes(net.Nodes, big.NewInt(value))
+// MintNodeAccountsInt64 adds the specified value to the accounts owned by the
+// nodes of the TestNetwork.
+func (net *TestNetwork) MintNodeAccountsUint64(value uint64) {
+	MintAllNodes(net.Nodes, big.NewInt(0).SetUint64(value))
 }
+
+// CreateWallets initializes the internal test wallets
+func (net *TestNetwork) CreateWallets(count int) {
+	net.Wallets = make([]*TestWalletAccount, count)
+
+	for i := 0; i < count; i++ {
+		shardID := ShardIdentifier(i % net.NumShards)
+		node := net.firstNodeInShard(shardID)
+		net.Wallets[i] = CreateTestWalletAccount(node.ShardCoordinator, shardID)
+	}
+}
+
+// MintWallets adds the specified value to the test wallets.
+func (net *TestNetwork) MintWallets(value *big.Int) {
+	MintAllPlayers(net.Nodes, net.Wallets, value)
+}
+
+// MintWalletsInt64 adds the specified value to the test wallets.
+func (net *TestNetwork) MintWalletsUint64(value uint64) {
+	MintAllPlayers(net.Nodes, net.Wallets, big.NewInt(0).SetUint64(value))
+}
+
+func (net *TestNetwork) SendTx(tx *transaction.Transaction) string {
+	node := net.firstNodeInShardOfAddress(tx.SndAddr)
+	return net.SendTxFromNode(tx, node)
+}
+
+func (net *TestNetwork) SendTxFromNode(tx *transaction.Transaction, node *TestProcessorNode,
+) string {
+	hash, err := node.SendTransaction(tx)
+	net.handleOrBypassError(err)
+
+	return hash
+}
+
+func (net *TestNetwork) CreateTx(
+	sender *TestWalletAccount,
+	recvAddress Address,
+	value *big.Int,
+	txData []byte,
+) *transaction.Transaction {
+	tx := &transaction.Transaction{
+		Nonce:    sender.Nonce,
+		Value:    big.NewInt(0).Set(value),
+		RcvAddr:  recvAddress,
+		SndAddr:  sender.Address,
+		GasPrice: net.DefaultGasPrice,
+		GasLimit: net.MinGasLimit,
+		Data:     txData,
+		ChainID:  ChainID,
+		Version:  MinTransactionVersion,
+	}
+
+	txBuff, err := tx.GetDataForSigning(TestAddressPubkeyConverter, TestTxSignMarshalizer)
+	net.handleOrBypassError(err)
+
+	signature, err := sender.SingleSigner.Sign(sender.SkTxSign, txBuff)
+	net.handleOrBypassError(err)
+
+	tx.Signature = signature
+	sender.Nonce++
+	return tx
+}
+
+func (net *TestNetwork) CreateTxUint64(
+	sender *TestWalletAccount,
+	recvAddress Address,
+	value uint64,
+	txData []byte,
+) *transaction.Transaction {
+	return net.CreateTx(
+		sender,
+		recvAddress,
+		big.NewInt(0).SetUint64(value),
+		txData)
+}
+
+func (net *TestNetwork) NewAddress(creator *TestWalletAccount) Address {
+	address, err := net.DefaultNode.BlockchainHook.NewAddress(
+		creator.Address,
+		creator.Nonce,
+		net.DefaultVM)
+	net.handleOrBypassError(err)
+
+	return address
+}
+
+func (net *TestNetwork) GetAccountHandler(address Address) state.UserAccountHandler {
+	node := net.firstNodeInShardOfAddress(address)
+	account, err := node.AccntState.GetExistingAccount(address)
+	net.handleOrBypassError(err)
+
+	accountHandler := account.(state.UserAccountHandler)
+	require.NotNil(net.T, accountHandler)
+
+	return accountHandler
+}
+
+func (net *TestNetwork) ShardOfAddress(address Address) ShardIdentifier {
+	return net.DefaultNode.ShardCoordinator.ComputeId(address)
+}
+
+func (net *TestNetwork) ComputeTxFee(tx *transaction.Transaction) *big.Int {
+	return net.DefaultNode.EconomicsData.ComputeTxFee(tx)
+}
+
+func (net *TestNetwork) ComputeTxFeeUint64(tx *transaction.Transaction) uint64 {
+	return net.DefaultNode.EconomicsData.ComputeTxFee(tx).Uint64()
+}
+
+func (net *TestNetwork) ComputeGasLimit(tx *transaction.Transaction) uint64 {
+	return net.DefaultNode.EconomicsData.ComputeGasLimit(tx)
+}
+
+func (net *TestNetwork) RequireWalletNoncesInSyncWithState() {
+	for _, wallet := range net.Wallets {
+		account := net.GetAccountHandler(wallet.Address)
+		require.Equal(net.T, wallet.Nonce, account.GetNonce(), "wallet nonce out of sync")
+	}
+}
+
+// TODO cannot define SCQueryInt() because it requires vm.GetIntValueFromSC()
+// which causes an import cycle.
+// func (net *TestNetwork) SCQueryInt(contract Address, function string, args ...[]byte) *big.Int {
+// 	shardID := net.ShardOfAddress(contract)
+// 	firstNodeInShard := net.NodesSharded[shardID][0]
+
+// 	return vm.GetIntValueFromSC(
+// 		net.DefaultGasSchedule,
+// 		firstNodeInShard.AccntState,
+// 		contract,
+// 		function,
+// 		args...)
+// }
 
 func (net *TestNetwork) createAdvertiser() {
 	net.Advertiser = CreateMessengerWithKadDht(net.AdvertiserAddress)
 	err := net.Advertiser.Bootstrap(0)
-	require.Nil(net.T, err)
+	net.handleOrBypassError(err)
 }
 
 func (net *TestNetwork) createNodes() {
@@ -78,8 +264,7 @@ func (net *TestNetwork) createNodes() {
 		net.NumShards,
 		net.NodesPerShard,
 		net.NodesInMetashard,
-		GetConnectableAddress(net.Advertiser),
-	)
+		GetConnectableAddress(net.Advertiser))
 }
 
 func (net *TestNetwork) indexProposers() {
@@ -90,18 +275,64 @@ func (net *TestNetwork) indexProposers() {
 	net.Proposers[net.NumShards] = net.NumShards * net.NodesPerShard
 }
 
+func (net *TestNetwork) mapNodesByShard() {
+	net.NodesSharded = make(NodesByShardMap)
+	for _, node := range net.Nodes {
+		shardID := node.ShardCoordinator.SelfId()
+		_, exists := net.NodesSharded[shardID]
+		if !exists {
+			net.NodesSharded[shardID] = make(NodeSlice, 0)
+		}
+		net.NodesSharded[shardID] = append(net.NodesSharded[shardID], node)
+	}
+}
+
 func (net *TestNetwork) startNodes() {
 	DisplayAndStartNodes(net.Nodes)
 }
 
+func (net *TestNetwork) initDefaults() {
+	net.DeploymentAddress = make(Address, 32)
+	net.DefaultNode = net.Nodes[0]
+	net.DefaultGasPrice = MinTxGasPrice
+	net.DefaultGasSchedule = nil
+	net.DefaultVM = factory.ArwenVirtualMachine
+
+	defaultNodeShardID := net.DefaultNode.ShardCoordinator.SelfId()
+	net.MinGasLimit = MinTxGasLimit
+	net.MaxGasLimit = net.DefaultNode.EconomicsData.MaxGasLimitPerBlock(defaultNodeShardID) - 1
+}
+
 func (net *TestNetwork) closeAdvertiser() {
 	err := net.Advertiser.Close()
-	require.Nil(net.T, err)
+	net.handleOrBypassError(err)
 }
 
 func (net *TestNetwork) closeNodes() {
 	for _, node := range net.Nodes {
 		err := node.Messenger.Close()
-		require.Nil(net.T, err)
+		net.handleOrBypassError(err)
 	}
+}
+
+func (net *TestNetwork) firstNodeInShard(shardID ShardIdentifier) *TestProcessorNode {
+	firstNodeInShard := net.NodesSharded[shardID][0]
+	require.NotNil(net.T, firstNodeInShard)
+	return firstNodeInShard
+}
+
+func (net *TestNetwork) firstNodeInShardOfAddress(address Address) *TestProcessorNode {
+	shardID := net.ShardOfAddress(address)
+	firstNodeInShard := net.NodesSharded[shardID][0]
+	require.NotNil(net.T, firstNodeInShard)
+	return firstNodeInShard
+}
+
+func (net *TestNetwork) handleOrBypassError(err error) {
+	if net.BypassErrorsOnce == true {
+		net.BypassErrorsOnce = false
+		return
+	}
+
+	require.Nil(net.T, err)
 }
