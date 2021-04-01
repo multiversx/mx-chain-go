55bec36731379aa55cc7a8ba0d316c7f1f3ef11b
Sebastian Marian <sebastian.marian@elrond.com>
2021-04-01 14:39:41 +0300

    * Added unit tests

--- process/block/preprocess/scheduledTxsExecution_test.go
+++ process/block/preprocess/scheduledTxsExecution_test.go
@@ -1,3 +1,85 @@
 package preprocess
 
-// TODO: Unit tests should be added
+import (
+	"testing"
+
+	"github.com/ElrondNetwork/elrond-go/data/transaction"
+	"github.com/ElrondNetwork/elrond-go/process"
+	"github.com/ElrondNetwork/elrond-go/process/mock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestScheduledTxsExecution_NewScheduledTxsExecutionNilTxProcessor(t *testing.T) {
+	t.Parallel()
+
+	scheduledTxsExec, err := NewScheduledTxsExecution(
+		nil,
+	)
+
+	assert.Nil(t, scheduledTxsExec)
+	assert.Equal(t, process.ErrNilTxProcessor, err)
+}
+
+func TestScheduledTxsExecution_NewScheduledTxsExecutionOk(t *testing.T) {
+	t.Parallel()
+
+	scheduledTxsExec, err := NewScheduledTxsExecution(
+		&mock.TxProcessorMock{},
+	)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, scheduledTxsExec)
+}
+
+func TestScheduledTxsExecution_InitShouldWork(t *testing.T) {
+	t.Parallel()
+
+	scheduledTxsExec, _ := NewScheduledTxsExecution(
+		&mock.TxProcessorMock{},
+	)
+
+	scheduledTxsExec.Add([]byte("txHash1"), &transaction.Transaction{Nonce: 0})
+	scheduledTxsExec.Add([]byte("txHash2"), &transaction.Transaction{Nonce: 1})
+	scheduledTxsExec.Add([]byte("txHash3"), &transaction.Transaction{Nonce: 2})
+
+	assert.Equal(t, 3, len(scheduledTxsExec.mapScheduledTxs))
+	assert.Equal(t, 3, len(scheduledTxsExec.scheduledTxs))
+
+	scheduledTxsExec.Init()
+
+	assert.Equal(t, 0, len(scheduledTxsExec.mapScheduledTxs))
+	assert.Equal(t, 0, len(scheduledTxsExec.scheduledTxs))
+}
+
+func TestScheduledTxsExecution_AddShouldWork(t *testing.T) {
+	t.Parallel()
+
+	scheduledTxsExec, _ := NewScheduledTxsExecution(
+		&mock.TxProcessorMock{},
+	)
+
+	res := scheduledTxsExec.Add([]byte("txHash1"), &transaction.Transaction{Nonce: 0})
+	assert.True(t, res)
+	assert.Equal(t, 1, len(scheduledTxsExec.mapScheduledTxs))
+	assert.Equal(t, 1, len(scheduledTxsExec.scheduledTxs))
+
+	res = scheduledTxsExec.Add([]byte("txHash1"), &transaction.Transaction{Nonce: 0})
+	assert.False(t, res)
+	assert.Equal(t, 1, len(scheduledTxsExec.mapScheduledTxs))
+	assert.Equal(t, 1, len(scheduledTxsExec.scheduledTxs))
+
+	res = scheduledTxsExec.Add([]byte("txHash2"), &transaction.Transaction{Nonce: 1})
+	assert.True(t, res)
+	assert.Equal(t, 2, len(scheduledTxsExec.mapScheduledTxs))
+	assert.Equal(t, 2, len(scheduledTxsExec.scheduledTxs))
+
+	res = scheduledTxsExec.Add([]byte("txHash3"), &transaction.Transaction{Nonce: 1})
+	assert.True(t, res)
+	assert.Equal(t, 3, len(scheduledTxsExec.mapScheduledTxs))
+	assert.Equal(t, 3, len(scheduledTxsExec.scheduledTxs))
+
+	res = scheduledTxsExec.Add([]byte("txHash2"), &transaction.Transaction{Nonce: 2})
+	assert.False(t, res)
+	assert.Equal(t, 3, len(scheduledTxsExec.mapScheduledTxs))
+	assert.Equal(t, 3, len(scheduledTxsExec.scheduledTxs))
+}
