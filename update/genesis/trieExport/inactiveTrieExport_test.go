98bd7f91f3fa186d2f29d96863a5ffad1ce0d261
BeniaminDrasovean <beniamin.drasovean@elrond.network>
2021-04-29 12:32:12 +0300

    add unit tests

---
+++ update/genesis/trieExport/inactiveTrieExport_test.go
@@ -0,0 +1,121 @@
+package trieExport
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/ElrondNetwork/elrond-go/core/keyValStorage"
+	"github.com/ElrondNetwork/elrond-go/data/state"
+
+	"github.com/ElrondNetwork/elrond-go/core"
+
+	"github.com/ElrondNetwork/elrond-go/data"
+	"github.com/ElrondNetwork/elrond-go/process/mock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewInactiveTrieExporter_NilMarshalizerShouldErr(t *testing.T) {
+	t.Parallel()
+
+	ite, err := NewInactiveTrieExporter(nil)
+	assert.Nil(t, ite)
+	assert.Equal(t, data.ErrNilMarshalizer, err)
+}
+
+func TestNewInactiveTrieExporter(t *testing.T) {
+	t.Parallel()
+
+	ite, err := NewInactiveTrieExporter(&mock.MarshalizerMock{})
+	assert.Nil(t, err)
+	assert.NotNil(t, ite)
+}
+
+func TestInactiveTrieExport_ExportValidatorTrieDoesNothing(t *testing.T) {
+	t.Parallel()
+
+	ite, _ := NewInactiveTrieExporter(&mock.MarshalizerMock{})
+	err := ite.ExportValidatorTrie(nil, nil)
+	assert.Nil(t, err)
+}
+
+func TestInactiveTrieExport_ExportDataTrieDoesNothing(t *testing.T) {
+	t.Parallel()
+
+	ite, _ := NewInactiveTrieExporter(&mock.MarshalizerMock{})
+	err := ite.ExportDataTrie("", nil, nil)
+	assert.Nil(t, err)
+}
+
+func TestInactiveTrieExport_ExportMainTrieInvalidTrieRootHashShouldErr(t *testing.T) {
+	t.Parallel()
+
+	ite, _ := NewInactiveTrieExporter(&mock.MarshalizerMock{})
+
+	expectedErr := fmt.Errorf("rootHash err")
+	tr := &mock.TrieStub{
+		RootCalled: func() ([]byte, error) {
+			return nil, expectedErr
+		},
+	}
+
+	rootHashes, err := ite.ExportMainTrie("", tr, context.Background())
+	assert.Equal(t, expectedErr, err)
+	assert.Nil(t, rootHashes)
+}
+
+func TestInactiveTrieExport_ExportMainTrieGetAllLeavesOnChannelErrShouldErr(t *testing.T) {
+	t.Parallel()
+
+	ite, _ := NewInactiveTrieExporter(&mock.MarshalizerMock{})
+
+	expectedErr := fmt.Errorf("getAllLeavesOnChannel err")
+	tr := &mock.TrieStub{
+		RootCalled: func() ([]byte, error) {
+			return nil, nil
+		},
+		GetAllLeavesOnChannelCalled: func(_ []byte) (chan core.KeyValueHolder, error) {
+			return nil, expectedErr
+		},
+	}
+
+	rootHashes, err := ite.ExportMainTrie("", tr, context.Background())
+	assert.Equal(t, expectedErr, err)
+	assert.Nil(t, rootHashes)
+}
+
+func TestInactiveTrieExport_ExportMainTrieShouldReturnDataTrieRootHashes(t *testing.T) {
+	t.Parallel()
+
+	marshalizer := &mock.MarshalizerMock{}
+	trieExporter, _ := NewInactiveTrieExporter(marshalizer)
+
+	expectedRootHash := []byte("rootHash")
+	account1 := state.NewEmptyUserAccount()
+	account2 := state.NewEmptyUserAccount()
+	account2.RootHash = expectedRootHash
+
+	serializedAcc1, err := marshalizer.Marshal(account1)
+	assert.Nil(t, err)
+	serializedAcc2, err := marshalizer.Marshal(account2)
+	assert.Nil(t, err)
+
+	tr := &mock.TrieStub{
+		RootCalled: func() ([]byte, error) {
+			return nil, nil
+		},
+		GetAllLeavesOnChannelCalled: func(_ []byte) (chan core.KeyValueHolder, error) {
+			leavesChannel := make(chan core.KeyValueHolder, 3)
+			leavesChannel <- keyValStorage.NewKeyValStorage([]byte("key1"), []byte("val1"))
+			leavesChannel <- keyValStorage.NewKeyValStorage([]byte("key2"), serializedAcc1)
+			leavesChannel <- keyValStorage.NewKeyValStorage([]byte("key3"), serializedAcc2)
+			close(leavesChannel)
+			return leavesChannel, nil
+		},
+	}
+
+	rootHashes, err := trieExporter.ExportMainTrie("", tr, context.Background())
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(rootHashes))
+	assert.Equal(t, expectedRootHash, rootHashes[0])
+}
--- update/genesis/trieExport/trieExport_test.go
+++ update/genesis/trieExport/trieExport_test.go
@@ -3,21 +3,564 @@ package trieExport
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/ElrondNetwork/elrond-go/core"
 	"github.com/ElrondNetwork/elrond-go/core/check"
 	"github.com/ElrondNetwork/elrond-go/core/keyValStorage"
+	"github.com/ElrondNetwork/elrond-go/data"
 	"github.com/ElrondNetwork/elrond-go/data/state"
 	"github.com/ElrondNetwork/elrond-go/sharding"
+	"github.com/ElrondNetwork/elrond-go/update"
 	"github.com/ElrondNetwork/elrond-go/update/mock"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-//TODO add unit tests
+func TestNewTrieExport_InvalidExportFolderShouldErr(t *testing.T) {
+	t.Parallel()
+
+	trieExporter, err := NewTrieExport(
+		"",
+		mock.NewOneShardCoordinatorMock(),
+		&mock.MarshalizerMock{},
+		&mock.HardforkStorerStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.GenesisNodesSetupHandlerStub{},
+	)
+	assert.Nil(t, trieExporter)
+	assert.Equal(t, update.ErrEmptyExportFolderPath, err)
+}
+
+func TestNewTrieExport_NilShardCoordinatorShouldErr(t *testing.T) {
+	t.Parallel()
+
+	trieExporter, err := NewTrieExport(
+		"testFilesExportNodes",
+		nil,
+		&mock.MarshalizerMock{},
+		&mock.HardforkStorerStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.GenesisNodesSetupHandlerStub{},
+	)
+	assert.Nil(t, trieExporter)
+	assert.Equal(t, data.ErrNilShardCoordinator, err)
+}
+
+func TestNewTrieExport_NilMarshalizerShouldErr(t *testing.T) {
+	t.Parallel()
+
+	trieExporter, err := NewTrieExport(
+		"testFilesExportNodes",
+		mock.NewOneShardCoordinatorMock(),
+		nil,
+		&mock.HardforkStorerStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.GenesisNodesSetupHandlerStub{},
+	)
+	assert.Nil(t, trieExporter)
+	assert.Equal(t, data.ErrNilMarshalizer, err)
+}
+
+func TestNewTrieExport_NilHardforkStorerShouldErr(t *testing.T) {
+	t.Parallel()
+
+	trieExporter, err := NewTrieExport(
+		"testFilesExportNodes",
+		mock.NewOneShardCoordinatorMock(),
+		&mock.MarshalizerMock{},
+		nil,
+		&mock.PubkeyConverterStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.GenesisNodesSetupHandlerStub{},
+	)
+	assert.Nil(t, trieExporter)
+	assert.Equal(t, update.ErrNilHardforkStorer, err)
+}
+
+func TestNewTrieExport_NilValidatorPubKeyConverterShouldErr(t *testing.T) {
+	t.Parallel()
+
+	trieExporter, err := NewTrieExport(
+		"testFilesExportNodes",
+		mock.NewOneShardCoordinatorMock(),
+		&mock.MarshalizerMock{},
+		&mock.HardforkStorerStub{},
+		nil,
+		&mock.PubkeyConverterStub{},
+		&mock.GenesisNodesSetupHandlerStub{},
+	)
+	assert.Nil(t, trieExporter)
+	assert.NotNil(t, err)
+	assert.True(t, strings.Contains(err.Error(), update.ErrNilPubKeyConverter.Error()))
+}
+
+func TestNewTrieExport_NilAddressPubKeyConverterShouldErr(t *testing.T) {
+	t.Parallel()
+
+	trieExporter, err := NewTrieExport(
+		"testFilesExportNodes",
+		mock.NewOneShardCoordinatorMock(),
+		&mock.MarshalizerMock{},
+		&mock.HardforkStorerStub{},
+		&mock.PubkeyConverterStub{},
+		nil,
+		&mock.GenesisNodesSetupHandlerStub{},
+	)
+	assert.Nil(t, trieExporter)
+	assert.NotNil(t, err)
+	assert.True(t, strings.Contains(err.Error(), update.ErrNilPubKeyConverter.Error()))
+}
+
+func TestNewTrieExport_NilGenesisNodesSetupHandlerShouldErr(t *testing.T) {
+	t.Parallel()
+
+	trieExporter, err := NewTrieExport(
+		"testFilesExportNodes",
+		mock.NewOneShardCoordinatorMock(),
+		&mock.MarshalizerMock{},
+		&mock.HardforkStorerStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.PubkeyConverterStub{},
+		nil,
+	)
+	assert.Nil(t, trieExporter)
+	assert.Equal(t, update.ErrNilGenesisNodesSetupHandler, err)
+}
+
+func TestNewTrieExport(t *testing.T) {
+	t.Parallel()
+
+	trieExporter, err := NewTrieExport(
+		"testFilesExportNodes",
+		mock.NewOneShardCoordinatorMock(),
+		&mock.MarshalizerMock{},
+		&mock.HardforkStorerStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.GenesisNodesSetupHandlerStub{},
+	)
+	assert.Nil(t, err)
+	assert.NotNil(t, trieExporter)
+}
+
+func TestTrieExport_ExportValidatorTrieInvalidTrieRootHashShouldErr(t *testing.T) {
+	t.Parallel()
+
+	trieExporter, _ := NewTrieExport(
+		"testFilesExportNodes",
+		mock.NewOneShardCoordinatorMock(),
+		&mock.MarshalizerMock{},
+		&mock.HardforkStorerStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.GenesisNodesSetupHandlerStub{},
+	)
+
+	expectedErr := fmt.Errorf("rootHash err")
+	tr := &mock.TrieStub{
+		RootCalled: func() ([]byte, error) {
+			return nil, expectedErr
+		},
+	}
+
+	err := trieExporter.ExportValidatorTrie(tr, context.Background())
+	assert.Equal(t, expectedErr, err)
+}
+
+func TestTrieExport_ExportValidatorTrieGetAllLeavesOnChannelErrShouldErr(t *testing.T) {
+	t.Parallel()
+
+	trieExporter, _ := NewTrieExport(
+		"testFilesExportNodes",
+		mock.NewOneShardCoordinatorMock(),
+		&mock.MarshalizerMock{},
+		&mock.HardforkStorerStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.GenesisNodesSetupHandlerStub{},
+	)
+
+	expectedErr := fmt.Errorf("getAllLeavesOnChannel err")
+	tr := &mock.TrieStub{
+		RootCalled: func() ([]byte, error) {
+			return nil, nil
+		},
+		GetAllLeavesOnChannelCalled: func(_ []byte) (chan core.KeyValueHolder, error) {
+			return nil, expectedErr
+		},
+	}
+
+	err := trieExporter.ExportValidatorTrie(tr, context.Background())
+	assert.Equal(t, expectedErr, err)
+}
+
+func TestTrieExport_ExportMainTrieInvalidIdentifierShouldErr(t *testing.T) {
+	t.Parallel()
+
+	trieExporter, _ := NewTrieExport(
+		"testFilesExportNodes",
+		mock.NewOneShardCoordinatorMock(),
+		&mock.MarshalizerMock{},
+		&mock.HardforkStorerStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.GenesisNodesSetupHandlerStub{},
+	)
+
+	rootHashes, err := trieExporter.ExportMainTrie("invalid identifier", &mock.TrieStub{}, context.Background())
+	assert.Nil(t, rootHashes)
+	assert.NotNil(t, err)
+}
+
+func TestTrieExport_ExportMainTrieInvalidTrieRootHashShouldErr(t *testing.T) {
+	t.Parallel()
+
+	trieExporter, _ := NewTrieExport(
+		"testFilesExportNodes",
+		mock.NewOneShardCoordinatorMock(),
+		&mock.MarshalizerMock{},
+		&mock.HardforkStorerStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.GenesisNodesSetupHandlerStub{},
+	)
+
+	expectedErr := fmt.Errorf("rootHash err")
+	tr := &mock.TrieStub{
+		RootCalled: func() ([]byte, error) {
+			return nil, expectedErr
+		},
+	}
+
+	rootHashes, err := trieExporter.ExportMainTrie("a@1@8", tr, context.Background())
+	assert.Equal(t, expectedErr, err)
+	assert.Nil(t, rootHashes)
+}
+
+func TestTrieExport_ExportMainTrieGetAllLeavesOnChannelErrShouldErr(t *testing.T) {
+	t.Parallel()
+
+	trieExporter, _ := NewTrieExport(
+		"testFilesExportNodes",
+		mock.NewOneShardCoordinatorMock(),
+		&mock.MarshalizerMock{},
+		&mock.HardforkStorerStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.GenesisNodesSetupHandlerStub{},
+	)
+
+	expectedErr := fmt.Errorf("getAllLeavesOnChannel err")
+	tr := &mock.TrieStub{
+		RootCalled: func() ([]byte, error) {
+			return nil, nil
+		},
+		GetAllLeavesOnChannelCalled: func(_ []byte) (chan core.KeyValueHolder, error) {
+			return nil, expectedErr
+		},
+	}
+
+	rootHashes, err := trieExporter.ExportMainTrie("a@1@8", tr, context.Background())
+	assert.Equal(t, expectedErr, err)
+	assert.Nil(t, rootHashes)
+}
+
+func TestTrieExport_ExportMainTrieInvalidShardIdShouldErr(t *testing.T) {
+	t.Parallel()
+
+	trieExporter, _ := NewTrieExport(
+		"testFilesExportNodes",
+		mock.NewOneShardCoordinatorMock(),
+		&mock.MarshalizerMock{},
+		&mock.HardforkStorerStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.GenesisNodesSetupHandlerStub{},
+	)
+
+	tr := &mock.TrieStub{
+		RootCalled: func() ([]byte, error) {
+			return nil, nil
+		},
+		GetAllLeavesOnChannelCalled: func(_ []byte) (chan core.KeyValueHolder, error) {
+			return nil, nil
+		},
+	}
+
+	rootHashes, err := trieExporter.ExportMainTrie("a@5@8", tr, context.Background())
+	assert.Equal(t, sharding.ErrInvalidShardId, err)
+	assert.Nil(t, rootHashes)
+}
+
+func TestTrieExport_ExportMainTrieHardforkStorerWriteErrShouldErr(t *testing.T) {
+	t.Parallel()
+
+	expectedErr := fmt.Errorf("hardforkStorer write err")
+	trieExporter, _ := NewTrieExport(
+		"testFilesExportNodes",
+		mock.NewOneShardCoordinatorMock(),
+		&mock.MarshalizerMock{},
+		&mock.HardforkStorerStub{
+			WriteCalled: func(_ string, _ []byte, _ []byte) error {
+				return expectedErr
+			},
+		},
+		&mock.PubkeyConverterStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.GenesisNodesSetupHandlerStub{},
+	)
+
+	tr := &mock.TrieStub{
+		RootCalled: func() ([]byte, error) {
+			return nil, nil
+		},
+		GetAllLeavesOnChannelCalled: func(_ []byte) (chan core.KeyValueHolder, error) {
+			return nil, nil
+		},
+	}
+
+	rootHashes, err := trieExporter.ExportMainTrie("a@0@8", tr, context.Background())
+	assert.Equal(t, expectedErr, err)
+	assert.Nil(t, rootHashes)
+}
+
+func TestTrieExport_ExportMainTrieShouldWork(t *testing.T) {
+	t.Parallel()
+
+	marshalizer := &mock.MarshalizerMock{}
+	writeCalled := 0
+	finishIdentifierCalled := 0
+
+	trieExporter, _ := NewTrieExport(
+		"testFilesExportNodes",
+		mock.NewOneShardCoordinatorMock(),
+		marshalizer,
+		&mock.HardforkStorerStub{
+			WriteCalled: func(_ string, _ []byte, _ []byte) error {
+				writeCalled++
+				return nil
+			},
+			FinishedIdentifierCalled: func(_ string) error {
+				finishIdentifierCalled++
+				return nil
+			},
+		},
+		&mock.PubkeyConverterStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.GenesisNodesSetupHandlerStub{},
+	)
+
+	expectedRootHash := []byte("rootHash")
+	account1 := state.NewEmptyUserAccount()
+	account2 := state.NewEmptyUserAccount()
+	account2.RootHash = expectedRootHash
+
+	serializedAcc1, err := marshalizer.Marshal(account1)
+	assert.Nil(t, err)
+	serializedAcc2, err := marshalizer.Marshal(account2)
+	assert.Nil(t, err)
+
+	tr := &mock.TrieStub{
+		RootCalled: func() ([]byte, error) {
+			return nil, nil
+		},
+		GetAllLeavesOnChannelCalled: func(_ []byte) (chan core.KeyValueHolder, error) {
+			leavesChannel := make(chan core.KeyValueHolder, 3)
+			leavesChannel <- keyValStorage.NewKeyValStorage([]byte("key1"), []byte("val1"))
+			leavesChannel <- keyValStorage.NewKeyValStorage([]byte("key2"), serializedAcc1)
+			leavesChannel <- keyValStorage.NewKeyValStorage([]byte("key3"), serializedAcc2)
+			close(leavesChannel)
+			return leavesChannel, nil
+		},
+	}
+
+	rootHashes, err := trieExporter.ExportMainTrie("a@0@8", tr, context.Background())
+	assert.Nil(t, err)
+	assert.Equal(t, 4, writeCalled)
+	assert.Equal(t, 1, finishIdentifierCalled)
+	assert.Equal(t, 1, len(rootHashes))
+	assert.Equal(t, expectedRootHash, rootHashes[0])
+}
+
+func TestTrieExport_ExportDataTrieInvalidIdentifierShouldErr(t *testing.T) {
+	t.Parallel()
+
+	trieExporter, _ := NewTrieExport(
+		"testFilesExportNodes",
+		mock.NewOneShardCoordinatorMock(),
+		&mock.MarshalizerMock{},
+		&mock.HardforkStorerStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.GenesisNodesSetupHandlerStub{},
+	)
+
+	err := trieExporter.ExportDataTrie("invalid identifier", &mock.TrieStub{}, context.Background())
+	assert.NotNil(t, err)
+}
+
+func TestTrieExport_ExportDataTrieInvalidTrieRootHashShouldErr(t *testing.T) {
+	t.Parallel()
+
+	trieExporter, _ := NewTrieExport(
+		"testFilesExportNodes",
+		mock.NewOneShardCoordinatorMock(),
+		&mock.MarshalizerMock{},
+		&mock.HardforkStorerStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.GenesisNodesSetupHandlerStub{},
+	)
+
+	expectedErr := fmt.Errorf("rootHash err")
+	tr := &mock.TrieStub{
+		RootCalled: func() ([]byte, error) {
+			return nil, expectedErr
+		},
+	}
+
+	err := trieExporter.ExportDataTrie("a@1@8", tr, context.Background())
+	assert.Equal(t, expectedErr, err)
+}
+
+func TestTrieExport_ExportDataTrieGetAllLeavesOnChannelErrShouldErr(t *testing.T) {
+	t.Parallel()
+
+	trieExporter, _ := NewTrieExport(
+		"testFilesExportNodes",
+		mock.NewOneShardCoordinatorMock(),
+		&mock.MarshalizerMock{},
+		&mock.HardforkStorerStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.GenesisNodesSetupHandlerStub{},
+	)
+
+	expectedErr := fmt.Errorf("getAllLeavesOnChannel err")
+	tr := &mock.TrieStub{
+		RootCalled: func() ([]byte, error) {
+			return nil, nil
+		},
+		GetAllLeavesOnChannelCalled: func(_ []byte) (chan core.KeyValueHolder, error) {
+			return nil, expectedErr
+		},
+	}
+
+	err := trieExporter.ExportDataTrie("a@1@8", tr, context.Background())
+	assert.Equal(t, expectedErr, err)
+}
+
+func TestTrieExport_ExportDataTrieInvalidShardIdShouldErr(t *testing.T) {
+	t.Parallel()
+
+	trieExporter, _ := NewTrieExport(
+		"testFilesExportNodes",
+		mock.NewOneShardCoordinatorMock(),
+		&mock.MarshalizerMock{},
+		&mock.HardforkStorerStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.GenesisNodesSetupHandlerStub{},
+	)
+
+	tr := &mock.TrieStub{
+		RootCalled: func() ([]byte, error) {
+			return nil, nil
+		},
+		GetAllLeavesOnChannelCalled: func(_ []byte) (chan core.KeyValueHolder, error) {
+			return nil, nil
+		},
+	}
+
+	err := trieExporter.ExportDataTrie("a@5@8", tr, context.Background())
+	assert.Equal(t, sharding.ErrInvalidShardId, err)
+}
+
+func TestTrieExport_ExportDataTrieHardforkStorerWriteErrShouldErr(t *testing.T) {
+	t.Parallel()
+
+	expectedErr := fmt.Errorf("hardforkStorer write err")
+	trieExporter, _ := NewTrieExport(
+		"testFilesExportNodes",
+		mock.NewOneShardCoordinatorMock(),
+		&mock.MarshalizerMock{},
+		&mock.HardforkStorerStub{
+			WriteCalled: func(_ string, _ []byte, _ []byte) error {
+				return expectedErr
+			},
+		},
+		&mock.PubkeyConverterStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.GenesisNodesSetupHandlerStub{},
+	)
+
+	tr := &mock.TrieStub{
+		RootCalled: func() ([]byte, error) {
+			return nil, nil
+		},
+		GetAllLeavesOnChannelCalled: func(_ []byte) (chan core.KeyValueHolder, error) {
+			return nil, nil
+		},
+	}
+
+	err := trieExporter.ExportDataTrie("a@0@8", tr, context.Background())
+	assert.Equal(t, expectedErr, err)
+}
+
+func TestTrieExport_ExportDataTrieShouldWork(t *testing.T) {
+	t.Parallel()
+
+	marshalizer := &mock.MarshalizerMock{}
+	writeCalled := 0
+	finishIdentifierCalled := 0
+
+	trieExporter, _ := NewTrieExport(
+		"testFilesExportNodes",
+		mock.NewOneShardCoordinatorMock(),
+		marshalizer,
+		&mock.HardforkStorerStub{
+			WriteCalled: func(_ string, _ []byte, _ []byte) error {
+				writeCalled++
+				return nil
+			},
+			FinishedIdentifierCalled: func(_ string) error {
+				finishIdentifierCalled++
+				return nil
+			},
+		},
+		&mock.PubkeyConverterStub{},
+		&mock.PubkeyConverterStub{},
+		&mock.GenesisNodesSetupHandlerStub{},
+	)
+
+	tr := &mock.TrieStub{
+		RootCalled: func() ([]byte, error) {
+			return nil, nil
+		},
+		GetAllLeavesOnChannelCalled: func(_ []byte) (chan core.KeyValueHolder, error) {
+			leavesChannel := make(chan core.KeyValueHolder, 3)
+			leavesChannel <- keyValStorage.NewKeyValStorage([]byte("key1"), []byte("val1"))
+			leavesChannel <- keyValStorage.NewKeyValStorage([]byte("key2"), []byte("val2"))
+			leavesChannel <- keyValStorage.NewKeyValStorage([]byte("key3"), []byte("val3"))
+			close(leavesChannel)
+			return leavesChannel, nil
+		},
+	}
+
+	err := trieExporter.ExportDataTrie("a@0@8", tr, context.Background())
+	assert.Nil(t, err)
+	assert.Equal(t, 4, writeCalled)
+	assert.Equal(t, 1, finishIdentifierCalled)
+}
 
 func TestTrieExport_ExportTrieShouldExportNodesSetupJson(t *testing.T) {
 	t.Parallel()
