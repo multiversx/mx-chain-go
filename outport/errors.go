c44dc3575ba7daa24bb23a60fa45db1a9102e416
miiu96 <mihai9625@gmail.com>
2021-04-29 15:18:18 +0300

    remove unused error

--- outport/errors.go
+++ outport/errors.go
@@ -5,8 +5,5 @@ import "errors"
 // ErrNilDriver signals that a nil driver has been provided
 var ErrNilDriver = errors.New("nil driver")
 
-// ErrNilArgsOutportFactory signals that arguments that are needed for outport factory are nil
-var ErrNilArgsOutportFactory = errors.New("nil args outport factory")
-
 // ErrNilArgsElasticDriverFactory signals that arguments that are needed for elastic driver factory are nil
 var ErrNilArgsElasticDriverFactory = errors.New("nil args elastic driver factory")
