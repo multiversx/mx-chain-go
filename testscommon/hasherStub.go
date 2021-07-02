f2f9e5b707abd5dced148ebaf91b5ea2b84c52b7
miiu96 <mihai9625@gmail.com>
2021-07-02 17:23:51 +0300

    fixes after merge

--- testscommon/hasherStub.go
+++ testscommon/hasherStub.go
@@ -1,6 +1,5 @@
 package testscommon
 
-<<<<<<<< HEAD:testscommon/hasherStub.go
 // HasherStub -
 type HasherStub struct {
 	ComputeCalled   func(s string) []byte
@@ -20,32 +19,10 @@ func (hash *HasherStub) Compute(s string) []byte {
 func (hash *HasherStub) EmptyHash() []byte {
 	if hash.EmptyHashCalled != nil {
 		hash.EmptyHashCalled()
-========
-import "crypto/sha256"
-
-var sha256EmptyHash []byte
-
-// HasherMock that will be used for testing
-type HasherMock struct {
-}
-
-// Compute will output the SHA's equivalent of the input string
-func (sha HasherMock) Compute(s string) []byte {
-	h := sha256.New()
-	_, _ = h.Write([]byte(s))
-	return h.Sum(nil)
-}
-
-// EmptyHash will return the equivalent of empty string SHA's
-func (sha HasherMock) EmptyHash() []byte {
-	if len(sha256EmptyHash) == 0 {
-		sha256EmptyHash = sha.Compute("")
->>>>>>>> development:testscommon/hasherMock.go
 	}
-	return sha256EmptyHash
+	return nil
 }
 
-<<<<<<<< HEAD:testscommon/hasherStub.go
 // Size -
 func (hash *HasherStub) Size() int {
 	if hash.SizeCalled != nil {
@@ -57,14 +34,5 @@ func (hash *HasherStub) Size() int {
 
 // IsInterfaceNil returns true if there is no value under the interface
 func (hash *HasherStub) IsInterfaceNil() bool {
-========
-// Size returns the required size in bytes
-func (HasherMock) Size() int {
-	return sha256.Size
-}
-
-// IsInterfaceNil returns true if there is no value under the interface
-func (sha HasherMock) IsInterfaceNil() bool {
->>>>>>>> development:testscommon/hasherMock.go
 	return false
 }
