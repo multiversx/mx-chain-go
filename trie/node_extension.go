ef9b7417a33b4ad256a3a9c6e359f1aa1f7408ed
Iulian Pascalau <iulian.pascalau@gmail.com>
2021-08-19 11:31:52 +0300

    - fixed trie node test point

--- trie/node_extension.go
+++ trie/node_extension.go
@@ -13,7 +13,7 @@ func shouldTestNode(n node, key []byte) bool {
 	randomness := string(key) + core.GetAnonymizedMachineID("") + fmt.Sprintf("%d", time.Now().UnixNano())
 	buff := hasher.Compute(randomness)
 	checkVal := binary.BigEndian.Uint32(buff)
-	if checkVal%1000 == 0 {
+	if checkVal%1000000 == 0 {
 		log.Debug("deliberately not saving hash", "hash", key)
 		return true
 	}
