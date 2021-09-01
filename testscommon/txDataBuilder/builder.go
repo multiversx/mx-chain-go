061e553d809ee91ec802561680fa45aa7ab89f27
Iulian Pascalau <iulian.pascalau@gmail.com>
2021-09-01 17:31:26 +0300

    - made the txBuilder struct exported to be better used in tests

--- testscommon/txDataBuilder/builder.go
+++ testscommon/txDataBuilder/builder.go
@@ -7,16 +7,16 @@ import (
 	"github.com/ElrondNetwork/elrond-go-core/core"
 )
 
-// txDataBuilder constructs a string to be used for transaction arguments
-type txDataBuilder struct {
+// TxDataBuilder constructs a string to be used for transaction arguments
+type TxDataBuilder struct {
 	function  string
 	elements  []string
 	separator string
 }
 
 // NewBuilder creates a new txDataBuilder instance.
-func NewBuilder() *txDataBuilder {
-	return &txDataBuilder{
+func NewBuilder() *TxDataBuilder {
+	return &TxDataBuilder{
 		function:  "",
 		elements:  make([]string, 0),
 		separator: "@",
@@ -25,7 +25,7 @@ func NewBuilder() *txDataBuilder {
 
 // Clear resets the internal state of the txDataBuilder, allowing a new data
 // string to be built.
-func (builder *txDataBuilder) Clear() *txDataBuilder {
+func (builder *TxDataBuilder) Clear() *TxDataBuilder {
 	builder.function = ""
 	builder.elements = make([]string, 0)
 
@@ -33,12 +33,12 @@ func (builder *txDataBuilder) Clear() *txDataBuilder {
 }
 
 // Elements returns the individual elements added to the builder
-func (builder *txDataBuilder) Elements() []string {
+func (builder *TxDataBuilder) Elements() []string {
 	return builder.elements
 }
 
 // ToString returns the data as a string.
-func (builder *txDataBuilder) ToString() string {
+func (builder *TxDataBuilder) ToString() string {
 	if len(builder.function) > 0 {
 		return builder.toStringWithFunction()
 	}
@@ -47,12 +47,12 @@ func (builder *txDataBuilder) ToString() string {
 }
 
 // ToBytes returns the data as a slice of bytes.
-func (builder *txDataBuilder) ToBytes() []byte {
+func (builder *TxDataBuilder) ToBytes() []byte {
 	return []byte(builder.ToString())
 }
 
 // GetLast returns the currently last element.
-func (builder *txDataBuilder) GetLast() string {
+func (builder *TxDataBuilder) GetLast() string {
 	if len(builder.elements) == 0 {
 		return ""
 	}
@@ -61,7 +61,7 @@ func (builder *txDataBuilder) GetLast() string {
 }
 
 // SetLast replaces the last element with the provided one.
-func (builder *txDataBuilder) SetLast(element string) {
+func (builder *TxDataBuilder) SetLast(element string) {
 	if len(builder.elements) == 0 {
 		builder.elements = []string{element}
 	}
@@ -70,14 +70,14 @@ func (builder *txDataBuilder) SetLast(element string) {
 }
 
 // Func sets the function to be invoked by the data string.
-func (builder *txDataBuilder) Func(function string) *txDataBuilder {
+func (builder *TxDataBuilder) Func(function string) *TxDataBuilder {
 	builder.function = function
 
 	return builder
 }
 
 // Byte appends a single byte to the data string.
-func (builder *txDataBuilder) Byte(value byte) *txDataBuilder {
+func (builder *TxDataBuilder) Byte(value byte) *TxDataBuilder {
 	element := hex.EncodeToString([]byte{value})
 	builder.elements = append(builder.elements, element)
 
@@ -85,7 +85,7 @@ func (builder *txDataBuilder) Byte(value byte) *txDataBuilder {
 }
 
 // Bytes appends a slice of bytes to the data string.
-func (builder *txDataBuilder) Bytes(bytes []byte) *txDataBuilder {
+func (builder *TxDataBuilder) Bytes(bytes []byte) *TxDataBuilder {
 	element := hex.EncodeToString(bytes)
 	builder.elements = append(builder.elements, element)
 
@@ -93,7 +93,7 @@ func (builder *txDataBuilder) Bytes(bytes []byte) *txDataBuilder {
 }
 
 // Str appends a string to the data string.
-func (builder *txDataBuilder) Str(str string) *txDataBuilder {
+func (builder *TxDataBuilder) Str(str string) *TxDataBuilder {
 	element := hex.EncodeToString([]byte(str))
 	builder.elements = append(builder.elements, element)
 
@@ -101,7 +101,7 @@ func (builder *txDataBuilder) Str(str string) *txDataBuilder {
 }
 
 // Int appends an integer to the data string.
-func (builder *txDataBuilder) Int(value int) *txDataBuilder {
+func (builder *TxDataBuilder) Int(value int) *TxDataBuilder {
 	element := hex.EncodeToString(big.NewInt(int64(value)).Bytes())
 	builder.elements = append(builder.elements, element)
 
@@ -109,7 +109,7 @@ func (builder *txDataBuilder) Int(value int) *txDataBuilder {
 }
 
 // Int64 appends an int64 to the data string.
-func (builder *txDataBuilder) Int64(value int64) *txDataBuilder {
+func (builder *TxDataBuilder) Int64(value int64) *TxDataBuilder {
 	element := hex.EncodeToString(big.NewInt(value).Bytes())
 	builder.elements = append(builder.elements, element)
 
@@ -117,18 +117,18 @@ func (builder *txDataBuilder) Int64(value int64) *txDataBuilder {
 }
 
 // True appends the string "true" to the data string.
-func (builder *txDataBuilder) True() *txDataBuilder {
+func (builder *TxDataBuilder) True() *TxDataBuilder {
 	return builder.Str("true")
 }
 
 // False appends the string "false" to the data string.
-func (builder *txDataBuilder) False() *txDataBuilder {
+func (builder *TxDataBuilder) False() *TxDataBuilder {
 	return builder.Str("false")
 }
 
 // Bool appends either "true" or "false" to the data string, depending on the
 // `value` argument.
-func (builder *txDataBuilder) Bool(value bool) *txDataBuilder {
+func (builder *TxDataBuilder) Bool(value bool) *TxDataBuilder {
 	if value {
 		return builder.True()
 	}
@@ -137,71 +137,71 @@ func (builder *txDataBuilder) Bool(value bool) *txDataBuilder {
 }
 
 // BigInt appends the bytes of a big.Int to the data string.
-func (builder *txDataBuilder) BigInt(value *big.Int) *txDataBuilder {
+func (builder *TxDataBuilder) BigInt(value *big.Int) *TxDataBuilder {
 	return builder.Bytes(value.Bytes())
 }
 
 // IssueESDT appends to the data string all the elements required to request an ESDT issuing.
-func (builder *txDataBuilder) IssueESDT(token string, ticker string, supply int64, numDecimals byte) *txDataBuilder {
+func (builder *TxDataBuilder) IssueESDT(token string, ticker string, supply int64, numDecimals byte) *TxDataBuilder {
 	return builder.Func("issue").Str(token).Str(ticker).Int64(supply).Byte(numDecimals)
 }
 
 // TransferESDT appends to the data string all the elements required to request an ESDT transfer.
-func (builder *txDataBuilder) TransferESDT(token string, value int64) *txDataBuilder {
+func (builder *TxDataBuilder) TransferESDT(token string, value int64) *TxDataBuilder {
 	return builder.Func(core.BuiltInFunctionESDTTransfer).Str(token).Int64(value)
 }
 
 //TransferESDTNFT appends to the data string all the elements required to request an ESDT NFT transfer.
-func (builder *txDataBuilder) TransferESDTNFT(token string, nonce int, value int64) *txDataBuilder {
+func (builder *TxDataBuilder) TransferESDTNFT(token string, nonce int, value int64) *TxDataBuilder {
 	return builder.Func(core.BuiltInFunctionESDTNFTTransfer).Str(token).Int(nonce).Int64(value)
 }
 
 // BurnESDT appends to the data string all the elements required to burn ESDT tokens.
-func (builder *txDataBuilder) BurnESDT(token string, value int64) *txDataBuilder {
+func (builder *TxDataBuilder) BurnESDT(token string, value int64) *TxDataBuilder {
 	return builder.Func(core.BuiltInFunctionESDTBurn).Str(token).Int64(value)
 }
 
 // CanFreeze appends "canFreeze" followed by the provided boolean value.
-func (builder *txDataBuilder) CanFreeze(prop bool) *txDataBuilder {
+func (builder *TxDataBuilder) CanFreeze(prop bool) *TxDataBuilder {
 	return builder.Str("canFreeze").Bool(prop)
 }
 
 // CanWipe appends "canWipe" followed by the provided boolean value.
-func (builder *txDataBuilder) CanWipe(prop bool) *txDataBuilder {
+func (builder *TxDataBuilder) CanWipe(prop bool) *TxDataBuilder {
 	return builder.Str("canWipe").Bool(prop)
 }
 
 // CanPause appends "canPause" followed by the provided boolean value.
-func (builder *txDataBuilder) CanPause(prop bool) *txDataBuilder {
+func (builder *TxDataBuilder) CanPause(prop bool) *TxDataBuilder {
 	return builder.Str("canPause").Bool(prop)
 }
 
 // CanMint appends "canMint" followed by the provided boolean value.
-func (builder *txDataBuilder) CanMint(prop bool) *txDataBuilder {
+func (builder *TxDataBuilder) CanMint(prop bool) *TxDataBuilder {
 	return builder.Str("canMint").Bool(prop)
 }
 
 // CanBurn appends "canBurn" followed by the provided boolean value.
-func (builder *txDataBuilder) CanBurn(prop bool) *txDataBuilder {
+func (builder *TxDataBuilder) CanBurn(prop bool) *TxDataBuilder {
 	return builder.Str("canBurn").Bool(prop)
 }
 
 // CanTransferNFTCreateRole appends "canTransferNFTCreateRole" followed by the provided boolean value.
-func (builder *txDataBuilder) CanTransferNFTCreateRole(prop bool) *txDataBuilder {
+func (builder *TxDataBuilder) CanTransferNFTCreateRole(prop bool) *TxDataBuilder {
 	return builder.Str("canTransferNFTCreateRole").Bool(prop)
 }
 
 // CanAddSpecialRoles appends "canAddSpecialRoles" followed by the provided boolean value.
-func (builder *txDataBuilder) CanAddSpecialRoles(prop bool) *txDataBuilder {
+func (builder *TxDataBuilder) CanAddSpecialRoles(prop bool) *TxDataBuilder {
 	return builder.Str("canAddSpecialRoles").Bool(prop)
 }
 
 // IsInterfaceNil returns true if there is no value under the interface
-func (builder *txDataBuilder) IsInterfaceNil() bool {
+func (builder *TxDataBuilder) IsInterfaceNil() bool {
 	return builder == nil
 }
 
-func (builder *txDataBuilder) toStringWithFunction() string {
+func (builder *TxDataBuilder) toStringWithFunction() string {
 	data := builder.function
 	for _, element := range builder.elements {
 		data = data + builder.separator + element
@@ -210,7 +210,7 @@ func (builder *txDataBuilder) toStringWithFunction() string {
 	return data
 }
 
-func (builder *txDataBuilder) toStringWithoutFunction() string {
+func (builder *TxDataBuilder) toStringWithoutFunction() string {
 	data := ""
 	for i, element := range builder.elements {
 		if i == 0 {
