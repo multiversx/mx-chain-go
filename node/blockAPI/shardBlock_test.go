227dd98285e753704d925c6fa7e0d76cbfe3cba2
Marius <marius.ciucam@gmail.com>
2021-09-10 16:01:32 +0300

    FEAT: Add tests for shardBlock

--- node/blockAPI/shardBlock_test.go
+++ node/blockAPI/shardBlock_test.go
@@ -92,6 +92,27 @@ func TestShardAPIBlockProcessor_GetBlockByNonceInvalidNonceShouldErr(t *testing.
 	assert.Error(t, err)
 }
 
+func TestShardAPIBlockProcessor_GetBlockByRoundInvalidRoundShouldErr(t *testing.T) {
+	t.Parallel()
+
+	shardID := uint32(3)
+	headerHash := []byte("d08089f2ab739520598fd7aeed08c427460fe94f286383047f3f61951afc4e00")
+
+	storerMock := mock.NewStorerMock()
+
+	shardAPIBlockProcessor := createMockShardAPIProcessor(
+		shardID,
+		headerHash,
+		storerMock,
+		true,
+		true,
+	)
+
+	blk, err := shardAPIBlockProcessor.GetBlockByRound(100, false)
+	assert.Nil(t, blk)
+	assert.Error(t, err)
+}
+
 func TestShardAPIBlockProcessor_GetBlockByHashFromNormalNode(t *testing.T) {
 	t.Parallel()
 
@@ -204,6 +225,72 @@ func TestShardAPIBlockProcessor_GetBlockByNonceFromHistoryNode(t *testing.T) {
 	}
 
 	blk, err := shardAPIBlockProcessor.GetBlockByNonce(1, false)
+	assert.Nil(t, err)
+	assert.Equal(t, expectedBlock, blk)
+}
+
+func TestShardAPIBlockProcessor_GetBlockByRoundFromHistoryNode(t *testing.T) {
+	t.Parallel()
+
+	nonce := uint64(1)
+	round := uint64(2)
+	epoch := uint32(1)
+	shardID := uint32(3)
+	miniblockHeader := []byte("miniBlockHash")
+	headerHash := []byte("d08089f2ab739520598fd7aeed08c427460fe94f286383047f3f61951afc4e00")
+
+	storerMock := mock.NewStorerMock()
+
+	shardAPIBlockProcessor := createMockShardAPIProcessor(
+		shardID,
+		headerHash,
+		storerMock,
+		true,
+		true,
+	)
+
+	header := &block.Header{
+		Nonce:   nonce,
+		Round:   round,
+		ShardID: shardID,
+		Epoch:   epoch,
+		MiniBlockHeaders: []block.MiniBlockHeader{
+			{Hash: miniblockHeader},
+		},
+		AccumulatedFees: big.NewInt(100),
+		DeveloperFees:   big.NewInt(50),
+	}
+	headerBytes, _ := json.Marshal(header)
+	_ = storerMock.Put(headerHash, headerBytes)
+
+	uint64Converter := shardAPIBlockProcessor.uint64ByteSliceConverter
+	roundBytes := uint64Converter.ToByteSlice(round)
+	nonceBytes := uint64Converter.ToByteSlice(nonce)
+	_ = storerMock.Put(roundBytes, nonceBytes)
+	_ = storerMock.Put(nonceBytes, headerHash)
+
+	expectedBlock := &api.Block{
+		Nonce: nonce,
+		Round: round,
+		Shard: shardID,
+		Epoch: epoch,
+		Hash:  hex.EncodeToString(headerHash),
+		MiniBlocks: []*api.MiniBlock{
+			{
+				Hash: hex.EncodeToString(miniblockHeader),
+				Type: block.TxBlock.String(),
+			},
+		},
+		AccumulatedFees: "100",
+		DeveloperFees:   "50",
+		Status:          BlockStatusOnChain,
+	}
+
+	blk, err := shardAPIBlockProcessor.GetBlockByRound(nonce, false)
+	assert.NotNil(t, err)
+	assert.Nil(t, blk)
+
+	blk, err = shardAPIBlockProcessor.GetBlockByRound(round, false)
 	assert.Nil(t, err)
 	assert.Equal(t, expectedBlock, blk)
 }
