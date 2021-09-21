eeedd09fd36103b6ac2207961af2d6d78bf150fa
Iulian Pascalau <iulian.pascalau@gmail.com>
2021-09-21 19:36:51 +0300

    - removed async operations on the app status handler facade

--- statusHandler/appStatusFacade.go
+++ statusHandler/appStatusFacade.go
@@ -31,63 +31,49 @@ func (asf *AppStatusFacade) IsInterfaceNil() bool {
 
 // Increment method - will increment the value for a key for every handler
 func (asf *AppStatusFacade) Increment(key string) {
-	go func() {
-		for _, ash := range asf.handlers {
-			ash.Increment(key)
-		}
-	}()
+	for _, ash := range asf.handlers {
+		ash.Increment(key)
+	}
 }
 
 // AddUint64 method - will increase the value for a key for every handler
 func (asf *AppStatusFacade) AddUint64(key string, value uint64) {
-	go func() {
-		for _, ash := range asf.handlers {
-			ash.AddUint64(key, value)
-		}
-	}()
+	for _, ash := range asf.handlers {
+		ash.AddUint64(key, value)
+	}
 }
 
 // Decrement method - will decrement the value for a key for every handler
 func (asf *AppStatusFacade) Decrement(key string) {
-	go func() {
-		for _, ash := range asf.handlers {
-			ash.Decrement(key)
-		}
-	}()
+	for _, ash := range asf.handlers {
+		ash.Decrement(key)
+	}
 }
 
 // SetInt64Value method - will update the value for a key for every handler
 func (asf *AppStatusFacade) SetInt64Value(key string, value int64) {
-	go func() {
-		for _, ash := range asf.handlers {
-			ash.SetInt64Value(key, value)
-		}
-	}()
+	for _, ash := range asf.handlers {
+		ash.SetInt64Value(key, value)
+	}
 }
 
 // SetUInt64Value method - will update the value for a key for every handler
 func (asf *AppStatusFacade) SetUInt64Value(key string, value uint64) {
-	go func() {
-		for _, ash := range asf.handlers {
-			ash.SetUInt64Value(key, value)
-		}
-	}()
+	for _, ash := range asf.handlers {
+		ash.SetUInt64Value(key, value)
+	}
 }
 
 // SetStringValue method - will update the value for a key for every handler
 func (asf *AppStatusFacade) SetStringValue(key string, value string) {
-	go func() {
-		for _, ash := range asf.handlers {
-			ash.SetStringValue(key, value)
-		}
-	}()
+	for _, ash := range asf.handlers {
+		ash.SetStringValue(key, value)
+	}
 }
 
 // Close method will close all the handlers
 func (asf *AppStatusFacade) Close() {
-	go func() {
-		for _, ash := range asf.handlers {
-			ash.Close()
-		}
-	}()
+	for _, ash := range asf.handlers {
+		ash.Close()
+	}
 }
