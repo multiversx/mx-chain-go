fec232e344264df6f90f1065311cc1406d7aded6
Robert Sasu <sasu.robert@gmail.com>
2021-10-05 12:46:18 +0300

    tests which show it just works

--- integrationTests/vm/txsFee/scCalls_test.go
+++ integrationTests/vm/txsFee/scCalls_test.go
@@ -11,11 +11,13 @@ import (
 	"testing"
 
 	arwenConfig "github.com/ElrondNetwork/arwen-wasm-vm/v1_4/config"
+	"github.com/ElrondNetwork/elrond-go-core/core"
 	"github.com/ElrondNetwork/elrond-go-core/data/block"
 	"github.com/ElrondNetwork/elrond-go/integrationTests/mock"
 	"github.com/ElrondNetwork/elrond-go/integrationTests/vm"
 	"github.com/ElrondNetwork/elrond-go/integrationTests/vm/txsFee/utils"
 	"github.com/ElrondNetwork/elrond-go/process"
+	"github.com/ElrondNetwork/elrond-go/testscommon/txDataBuilder"
 	"github.com/ElrondNetwork/elrond-go/vm/systemSmartContracts/defaults"
 	vmcommon "github.com/ElrondNetwork/elrond-vm-common"
 	"github.com/stretchr/testify/require"
@@ -288,5 +290,77 @@ func TestScCallAndGasChangeShouldWork(t *testing.T) {
 
 		indexerTx := testIndexer.GetIndexerPreparedTransaction(t)
 		require.Equal(t, uint64(748), indexerTx.GasUsed)
+	}
+}
+
+func TestESDTScCallAndGasChangeShouldWork(t *testing.T) {
+	testContext, err := vm.CreatePreparedTxProcessorWithVMs(vm.ArgEnableEpoch{})
+	require.Nil(t, err)
+	defer testContext.Close()
+
+	owner := []byte("12345678901234567890123456789011")
+	senderBalance := big.NewInt(1000000000)
+	gasPrice := uint64(10)
+	gasLimit := uint64(2000000)
+
+	_, _ = vm.CreateAccount(testContext.Accounts, owner, 0, senderBalance)
+	ownerAccount, _ := testContext.Accounts.LoadAccount(owner)
+	scAddress := utils.DoDeploySecond(t, testContext, "../esdt/testdata/forwarder-raw.wasm", ownerAccount, gasPrice, gasLimit, nil, big.NewInt(0))
+	utils.CleanAccumulatedIntermediateTransactions(t, testContext)
+
+	sndAddr := []byte("12345678901234567890123456789112")
+	senderBalance = big.NewInt(10000000)
+	gasPrice = uint64(10)
+	gasLimit = uint64(30000)
+
+	esdtBalance := big.NewInt(100000000)
+	token := []byte("miiutoken")
+	utils.CreateAccountWithESDTBalance(t, testContext.Accounts, sndAddr, senderBalance, token, 0, esdtBalance)
+
+	txData := txDataBuilder.NewBuilder()
+	valueToSendToSc := int64(1000)
+	txData.TransferESDT(string(token), valueToSendToSc).Str("forward_direct_esdt_via_transf_exec").Bytes(sndAddr)
+	for idx := uint64(0); idx < 10; idx++ {
+		tx := vm.CreateTransaction(idx, big.NewInt(0), sndAddr, scAddress, gasPrice, gasLimit, txData.ToBytes())
+
+		_, err = testContext.TxProcessor.ProcessTransaction(tx)
+		require.Nil(t, err)
+		require.Nil(t, testContext.GetLatestError())
+
+		_, err = testContext.Accounts.Commit()
+		require.Nil(t, err)
+
+		intermediateTxs := testContext.GetIntermediateTransactions(t)
+		testIndexer := vm.CreateTestIndexer(t, testContext.ShardCoordinator, testContext.EconomicsData)
+		testIndexer.SaveTransaction(tx, block.TxBlock, intermediateTxs)
+
+		indexerTx := testIndexer.GetIndexerPreparedTransaction(t)
+		require.Equal(t, uint64(25092), indexerTx.GasUsed)
+	}
+
+	mockGasSchedule := testContext.GasSchedule.(*mock.GasScheduleNotifierMock)
+	testGasSchedule := arwenConfig.MakeGasMapForTests()
+	newGasSchedule := defaults.FillGasMapInternal(testGasSchedule, 1)
+	newGasSchedule["BuiltInCost"][core.BuiltInFunctionESDTTransfer] = 2
+	newGasSchedule["ElrondAPICost"]["TransferValue"] = 2
+	mockGasSchedule.ChangeGasSchedule(newGasSchedule)
+
+	for idx := uint64(0); idx < 10; idx++ {
+		tx := vm.CreateTransaction(10+idx, big.NewInt(0), sndAddr, scAddress, gasPrice, gasLimit, txData.ToBytes())
+
+		_, err = testContext.TxProcessor.ProcessTransaction(tx)
+		require.Nil(t, err)
+		require.Nil(t, testContext.GetLatestError())
+
+		_, err = testContext.Accounts.Commit()
+		require.Nil(t, err)
+
+		intermediateTxs := testContext.GetIntermediateTransactions(t)
+		testIndexer := vm.CreateTestIndexer(t, testContext.ShardCoordinator, testContext.EconomicsData)
+		testIndexer.SaveTransaction(tx, block.TxBlock, intermediateTxs)
+
+		indexerTx := testIndexer.GetIndexerPreparedTransaction(t)
+		// 2 esdt transfers and one api call - + 3
+		require.Equal(t, uint64(25095), indexerTx.GasUsed)
 	}
 }
