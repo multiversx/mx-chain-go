b728cd9546b3203454371e22bece2dbf7c2f55e7
raduchis <radu.chis@elrond.com>
2021-10-08 15:23:08 +0300

    removed logs

--- vm/systemSmartContracts/staking.go
+++ vm/systemSmartContracts/staking.go
@@ -356,15 +356,6 @@ func (s *stakingSC) unJailV1(args *vmcommon.ContractCallInput) vmcommon.ReturnCo
 }
 
 func (s *stakingSC) unJail(args *vmcommon.ContractCallInput) vmcommon.ReturnCode {
-	defer func() {
-		waitingListBLSKeys, _ := s.getFirstElementsFromWaitingList(5)
-		if waitingListBLSKeys != nil {
-			for poz, bls := range waitingListBLSKeys.blsKeys {
-				log.Debug("Queue5", "poz", poz, "bls", bls)
-			}
-		}
-	}()
-
 	if !s.flagEnableStaking.IsSet() {
 		return s.unJailV1(args)
 	}
@@ -968,19 +959,7 @@ func (s *stakingSC) saveElementAndList(key []byte, element *ElementInList, waiti
 }
 
 func (s *stakingSC) removeFromWaitingList(blsKey []byte) error {
-	defer func() {
-		waitingListBLSKeys, _ := s.getFirstElementsFromWaitingList(5)
-		if waitingListBLSKeys != nil {
-			for poz, bls := range waitingListBLSKeys.blsKeys {
-				log.Debug("Queue5", "poz", poz, "bls", bls)
-			}
-		}
-	}()
-
 	inWaitingListKey := createWaitingListKey(blsKey)
-
-	log.Debug("removing from WaitingQueue", "bls", blsKey)
-
 	marshaledData := s.eei.GetStorage(inWaitingListKey)
 	if len(marshaledData) == 0 {
 		return nil
