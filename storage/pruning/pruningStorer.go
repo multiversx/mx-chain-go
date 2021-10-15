9a73ff757e05123bbd13af4c4603fc2b16d81e6d
bogdan-rosianu <bogdan.rosianu@yahoo.com>
2021-10-15 14:45:04 +0300

    minor improvements for pruning storer concurrent operations

--- storage/pruning/pruningStorer.go
+++ storage/pruning/pruningStorer.go
@@ -658,6 +658,9 @@ func (ps *PruningStorer) saveHeaderForEpochStartPrepare(header data.HeaderHandle
 
 // changeEpoch will handle creating a new persister and removing of the older ones
 func (ps *PruningStorer) changeEpoch(header data.HeaderHandler) error {
+	ps.lock.Lock()
+	defer ps.lock.Unlock()
+
 	epoch := header.GetEpoch()
 	log.Debug("PruningStorer - change epoch", "unit", ps.identifier, "epoch", epoch)
 	// if pruning is not enabled, don't create new persisters, but use the same one instead
@@ -666,9 +669,7 @@ func (ps *PruningStorer) changeEpoch(header data.HeaderHandler) error {
 		return nil
 	}
 
-	ps.lock.RLock()
 	_, ok := ps.persistersMapByEpoch[epoch]
-	ps.lock.RUnlock()
 	if ok {
 		err := ps.changeEpochWithExisting(epoch)
 		if err != nil {
@@ -697,20 +698,16 @@ func (ps *PruningStorer) changeEpoch(header data.HeaderHandler) error {
 
 	singleItemPersisters := []*persisterData{newPersister}
 
-	ps.lock.Lock()
 	ps.activePersisters = append(singleItemPersisters, ps.activePersisters...)
 	ps.persistersMapByEpoch[epoch] = newPersister
 
 	err = ps.activePersisters[0].getPersister().Init()
 	if err != nil {
-		ps.lock.Unlock()
 		return err
 	}
-	ps.lock.Unlock()
 
 	wasExtended := ps.extendSavedEpochsIfNeeded(header)
 	if wasExtended {
-		ps.lock.RLock()
 		if len(ps.activePersisters) > int(ps.numOfActivePersisters) {
 			log.Debug("PruningStorer - skip closing and destroying persisters due to a stuck shard -",
 				"current epoch", epoch,
@@ -718,7 +715,6 @@ func (ps *PruningStorer) changeEpoch(header data.HeaderHandler) error {
 				"default maximum num active persisters", ps.numOfActivePersisters,
 				"oldest epoch in storage", ps.activePersisters[len(ps.activePersisters)-1].epoch)
 		}
-		ps.lock.RUnlock()
 		return nil
 	}
 
@@ -730,6 +726,7 @@ func (ps *PruningStorer) changeEpoch(header data.HeaderHandler) error {
 	return nil
 }
 
+// should be called under mutex protection
 func (ps *PruningStorer) extendSavedEpochsIfNeeded(header data.HeaderHandler) bool {
 	epoch := header.GetEpoch()
 	metaBlock, mbOk := header.(*block.MetaBlock)
@@ -748,9 +745,7 @@ func (ps *PruningStorer) extendSavedEpochsIfNeeded(header data.HeaderHandler) bo
 		return false
 	}
 
-	ps.lock.RLock()
 	oldestEpochInCurrentSetting := ps.activePersisters[len(ps.activePersisters)-1].epoch
-	ps.lock.RUnlock()
 
 	oldestEpochToKeep := computeOldestEpoch(metaBlock)
 	shouldKeepOlderEpochsIfShardIsStuck := epoch-oldestEpochToKeep < maxNumEpochsToKeepIfAShardIsStuck
@@ -767,8 +762,8 @@ func (ps *PruningStorer) extendSavedEpochsIfNeeded(header data.HeaderHandler) bo
 	return false
 }
 
+// should be called under mutex protection
 func (ps *PruningStorer) changeEpochWithExisting(epoch uint32) error {
-	ps.lock.RLock()
 	numActivePersisters := ps.numOfActivePersisters
 
 	var err error
@@ -783,12 +778,10 @@ func (ps *PruningStorer) changeEpochWithExisting(epoch uint32) error {
 	for e := int64(epoch); e >= oldestEpochActive; e-- {
 		p, ok := ps.persistersMapByEpoch[uint32(e)]
 		if !ok {
-			ps.lock.RUnlock()
 			return nil
 		}
 		persisters = append(persisters, p)
 	}
-	ps.lock.RUnlock()
 
 	for _, p := range persisters {
 		if p.getIsClosed() {
@@ -801,25 +794,21 @@ func (ps *PruningStorer) changeEpochWithExisting(epoch uint32) error {
 		activePersisters = append(activePersisters, p)
 	}
 
-	ps.lock.Lock()
 	ps.activePersisters = activePersisters
-	ps.lock.Unlock()
 
 	return nil
 }
 
+// should be called under mutex protection
 func (ps *PruningStorer) extendActivePersisters(from uint32, to uint32) error {
 	persisters := make([]*persisterData, 0)
-	ps.lock.RLock()
 	for e := int(to); e >= int(from); e-- {
 		p, ok := ps.persistersMapByEpoch[uint32(e)]
 		if !ok {
-			ps.lock.RUnlock()
 			return nil
 		}
 		persisters = append(persisters, p)
 	}
-	ps.lock.RUnlock()
 
 	reOpenedPersisters := make([]*persisterData, 0)
 	for _, p := range persisters {
@@ -833,25 +822,23 @@ func (ps *PruningStorer) extendActivePersisters(from uint32, to uint32) error {
 		}
 	}
 
-	ps.lock.Lock()
 	ps.activePersisters = append(ps.activePersisters, reOpenedPersisters...)
-	ps.lock.Unlock()
 
 	return nil
 }
 
+// should be called under mutex protection
 func (ps *PruningStorer) closePersisters(epoch uint32) error {
 	// activePersisters outside the numOfActivePersisters border have to he closed for both scenarios: full archive or not
 	persistersToClose := make([]*persisterData, 0)
 
-	ps.lock.Lock()
 	if ps.numOfActivePersisters < uint32(len(ps.activePersisters)) {
-		for idx := int(ps.numOfActivePersisters); idx < len(ps.activePersisters); idx++ {
+		activePersistersCount := len(ps.activePersisters)
+		for idx := int(ps.numOfActivePersisters); idx < activePersistersCount; idx++ {
 			persisterToClose := ps.activePersisters[idx]
 			// remove it from the active persisters slice
 			ps.activePersisters = ps.activePersisters[:ps.numOfActivePersisters]
-			epochToClose := epoch - ps.numOfActivePersisters
-			ps.persistersMapByEpoch[epochToClose] = persisterToClose
+			ps.persistersMapByEpoch[persisterToClose.epoch] = persisterToClose
 			persistersToClose = append(persistersToClose, persisterToClose)
 		}
 	}
@@ -867,7 +854,6 @@ func (ps *PruningStorer) closePersisters(epoch uint32) error {
 			idxToRemove--
 		}
 	}
-	ps.lock.Unlock()
 
 	for _, pd := range persistersToClose {
 		err := pd.Close()
