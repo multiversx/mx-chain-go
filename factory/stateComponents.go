98fab4fc8ed343397424a8667b99262dfbfc16c5
BeniaminDrasovean <beniamin.drasovean@elrond.network>
2021-10-22 15:46:38 +0300

    close oldTrieStorageCreator on StateComponents close

--- factory/stateComponents.go
+++ factory/stateComponents.go
@@ -35,11 +35,12 @@ type stateComponentsFactory struct {
 
 // stateComponents struct holds the state components of the Elrond protocol
 type stateComponents struct {
-	peerAccounts        state.AccountsAdapter
-	accountsAdapter     state.AccountsAdapter
-	accountsAdapterAPI  state.AccountsAdapter
-	triesContainer      common.TriesHolder
-	trieStorageManagers map[string]common.StorageManager
+	peerAccounts          state.AccountsAdapter
+	accountsAdapter       state.AccountsAdapter
+	accountsAdapterAPI    state.AccountsAdapter
+	triesContainer        common.TriesHolder
+	trieStorageManagers   map[string]common.StorageManager
+	oldTrieStorageCreator trieFactory.TrieStorageCreator
 }
 
 // NewStateComponentsFactory will return a new instance of stateComponentsFactory
@@ -100,11 +101,12 @@ func (scf *stateComponentsFactory) Create() (*stateComponents, error) {
 	}
 
 	return &stateComponents{
-		peerAccounts:        peerAdapter,
-		accountsAdapter:     accountsAdapter,
-		accountsAdapterAPI:  accountsAdapterAPI,
-		triesContainer:      triesContainer,
-		trieStorageManagers: trieStorageManagers,
+		peerAccounts:          peerAdapter,
+		accountsAdapter:       accountsAdapter,
+		accountsAdapterAPI:    accountsAdapterAPI,
+		triesContainer:        triesContainer,
+		trieStorageManagers:   trieStorageManagers,
+		oldTrieStorageCreator: oldTrieStorageCreator,
 	}, nil
 }
 
@@ -217,6 +219,8 @@ func (pc *stateComponents) Close() error {
 			errString += fmt.Errorf("trieStorageManager close failed: %w ", err).Error()
 		}
 	}
+
+	_ = pc.oldTrieStorageCreator.Close()
 
 	if len(errString) != 0 {
 		return fmt.Errorf("state components close failed: %s", errString)
