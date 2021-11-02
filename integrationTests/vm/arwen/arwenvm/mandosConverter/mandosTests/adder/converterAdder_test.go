65f32c81d872b49889ff03a4f9c527033955e45f
andrei-diaconescu <andreidiaconescu_1999@yahoo.com>
2021-11-02 15:21:14 +0200

    example benchmarks: mex/adder/ecc

--- integrationTests/vm/arwen/arwenvm/mandosConverter/mandosTests/adder/converterAdder_test.go
+++ integrationTests/vm/arwen/arwenvm/mandosConverter/mandosTests/adder/converterAdder_test.go
@@ -1,43 +1,19 @@
 package testadder
 
 import (
-	"fmt"
 	"testing"
 
-	mge "github.com/ElrondNetwork/arwen-wasm-vm/v1_4/mandos-go/elrondgo-exporter"
-	"github.com/ElrondNetwork/elrond-go/integrationTests/vm"
 	mc "github.com/ElrondNetwork/elrond-go/integrationTests/vm/arwen/arwenvm/mandosConverter"
-	"github.com/stretchr/testify/require"
 )
 
 func TestMandosConverter_AdderWithExternalSteps(t *testing.T) {
-	mandosAccounts, _, mandosTransactions, _, err := mge.GetAccountsAndTransactionsFromMandos("./adder_with_external_steps.scen.json")
-	require.Nil(t, err)
-	testContext, err := vm.CreatePreparedTxProcessorWithVMs(vm.ArgEnableEpoch{})
-	require.Nil(t, err)
-	err = mc.CreateAccountsFromMandosAccs(testContext, mandosAccounts)
-	require.Nil(t, err)
-	mc.CheckAccounts(t, testContext.Accounts, mandosAccounts)
-	transactions := mc.CreateTransactionsFromMandosTxs(mandosTransactions)
-	mc.CheckTransactions(t, transactions, mandosTransactions)
+	mc.CheckConverter(t, "./adder_with_external_steps.scen.json")
 }
 
 func Benchmark_MandosConverter_SimpleAdderWithDeploy(b *testing.B) {
-	testContext, transactions, err := mc.SetStateFromMandosTest("./adder.scen.json")
-	if err != nil {
-		fmt.Println("Setting state went wrong: ", err)
-		return
-	}
-	defer testContext.Close()
-	mc.RunSingleTransactionBenchmark(b, testContext, transactions[0])
+	mc.BenchmarkMandosSpecificTx(b, "./adder.scen.json")
 }
 
 func Benchmark_MandosConverter_AdderWithExternalSteps(b *testing.B) {
-	testContext, transactions, err := mc.SetStateFromMandosTest("./adder_with_external_steps.scen.json")
-	if err != nil {
-		fmt.Println("Setting state went wrong: ", err)
-		return
-	}
-	defer testContext.Close()
-	mc.RunSingleTransactionBenchmark(b, testContext, transactions[0])
+	mc.BenchmarkMandosSpecificTx(b, "./adder_with_external_steps.scen.json")
 }
--- integrationTests/vm/arwen/arwenvm/mandosConverter/mandosTests/ecBenchmarks/converterEllipticCurves_test.go
+++ integrationTests/vm/arwen/arwenvm/mandosConverter/mandosTests/ecBenchmarks/converterEllipticCurves_test.go
@@ -1,32 +1,15 @@
 package Benchmark_TestEllipticCurveScalarMultP224
 
 import (
-	"fmt"
 	"testing"
 
-	mge "github.com/ElrondNetwork/arwen-wasm-vm/v1_4/mandos-go/elrondgo-exporter"
-	"github.com/ElrondNetwork/elrond-go/integrationTests/vm"
 	mc "github.com/ElrondNetwork/elrond-go/integrationTests/vm/arwen/arwenvm/mandosConverter"
-	"github.com/stretchr/testify/require"
 )
 
 func TestMandosConverter_EllipticCurves(t *testing.T) {
-	mandosAccounts, _, mandosTransactions, _, err := mge.GetAccountsAndTransactionsFromMandos("./elliptic_curves.scen.json")
-	require.Nil(t, err)
-	testContext, err := vm.CreatePreparedTxProcessorWithVMs(vm.ArgEnableEpoch{})
-	require.Nil(t, err)
-	err = mc.CreateAccountsFromMandosAccs(testContext, mandosAccounts)
-	require.Nil(t, err)
-	mc.CheckAccounts(t, testContext.Accounts, mandosAccounts)
-	transactions := mc.CreateTransactionsFromMandosTxs(mandosTransactions)
-	mc.CheckTransactions(t, transactions, mandosTransactions)
+	mc.CheckConverter(t, "./elliptic_curves.scen.json")
 }
 
 func Benchmark_MandosConverter_EllipticCurves(b *testing.B) {
-	testContext, transactions, err := mc.SetStateFromMandosTest("./elliptic_curves.scen.json")
-	if err != nil {
-		fmt.Println("Setting state went wrong: ", err)
-		return
-	}
-	mc.RunSingleTransactionBenchmark(b, testContext, transactions[0])
+	mc.BenchmarkMandosSpecificTx(b, "./elliptic_curves.scen.json")
 }
