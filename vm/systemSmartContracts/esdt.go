1def08c088129b67b5d922c6b92c2a010d2151aa
bogdan-rosianu <bogdan.rosianu@yahoo.com>
2021-11-22 16:53:54 +0200

    EN-11153: reverted a print used in tests

--- vm/systemSmartContracts/esdt.go
+++ vm/systemSmartContracts/esdt.go
@@ -6,7 +6,6 @@ import (
 	"encoding/hex"
 	"fmt"
 	"math/big"
-	"runtime/debug"
 	"strings"
 	"sync"
 
@@ -1858,9 +1857,6 @@ func (e *esdt) CanUseContract() bool {
 }
 
 func (e *esdt) getContractConfig(args *vmcommon.ContractCallInput) vmcommon.ReturnCode {
-	log.Warn("entered getContractConfig")
-	debug.PrintStack()
-
 	returnCode := e.checkArgumentsForGeneralViewFunc(args)
 	if returnCode != vmcommon.Ok {
 		return returnCode
