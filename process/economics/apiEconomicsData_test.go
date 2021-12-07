197e0c53e0630ef115306b5ffd7a04bb86a12e1f
Iulian Pascalau <iulian.pascalau@gmail.com>
2021-12-07 12:51:14 +0200

    - parallelized unit tests

--- process/economics/apiEconomicsData_test.go
+++ process/economics/apiEconomicsData_test.go
@@ -17,12 +17,16 @@ func TestNewAPIEconomicsData(t *testing.T) {
 	t.Parallel()
 
 	t.Run("nil economics data", func(t *testing.T) {
+		t.Parallel()
+
 		apiEconomicsData, err := economics.NewAPIEconomicsData(nil)
 
 		assert.True(t, check.IfNil(apiEconomicsData))
 		assert.Equal(t, process.ErrNilEconomicsData, err)
 	})
 	t.Run("should work", func(t *testing.T) {
+		t.Parallel()
+
 		args := createArgsForEconomicsData(1)
 		economicsData, _ := economics.NewEconomicsData(args)
 		apiEconomicsData, err := economics.NewAPIEconomicsData(economicsData)
@@ -47,6 +51,8 @@ func TestApiEconomicsData_CheckValidityTxValues(t *testing.T) {
 	apiEconomicsData, _ := economics.NewAPIEconomicsData(economicsData)
 
 	t.Run("maximum gas limit as defined should work", func(t *testing.T) {
+		t.Parallel()
+
 		tx := &transaction.Transaction{
 			GasPrice: minGasPrice + 1,
 			GasLimit: maxGasLimitPerBlock,
@@ -56,6 +62,8 @@ func TestApiEconomicsData_CheckValidityTxValues(t *testing.T) {
 		require.Nil(t, err)
 	})
 	t.Run("maximum gas limit + 1 as defined should error", func(t *testing.T) {
+		t.Parallel()
+
 		tx := &transaction.Transaction{
 			GasPrice: minGasPrice + 1,
 			GasLimit: maxGasLimitPerBlock + 1,
@@ -65,6 +73,8 @@ func TestApiEconomicsData_CheckValidityTxValues(t *testing.T) {
 		require.Equal(t, process.ErrMoreGasThanGasLimitPerMiniBlockForSafeCrossShard, err)
 	})
 	t.Run("maximum gas limit - 1 as defined should work", func(t *testing.T) {
+		t.Parallel()
+
 		tx := &transaction.Transaction{
 			GasPrice: minGasPrice + 1,
 			GasLimit: maxGasLimitPerBlock - 1,
