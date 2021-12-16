4d17d9bd86d7e45cb1c2de5dbfdc382b960e6fe4
ssd04 <dariussuirab@gmail.com>
2021-12-16 18:34:04 +0200

    unit tests for common functions in sharding

--- sharding/common_test.go
+++ sharding/common_test.go
@@ -1,1 +1,60 @@
 package sharding
+
+import (
+	"errors"
+	"strconv"
+	"testing"
+
+	"github.com/ElrondNetwork/elrond-go/sharding/nodesCoordinator"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSerializableValidatorsToValidators_NilPubKey(t *testing.T) {
+	serializableValidators := make(map[string][]*SerializableValidator)
+
+	serializableValidator := &SerializableValidator{
+		PubKey:  nil,
+		Chances: 0,
+		Index:   0,
+	}
+
+	serializableValidators[strconv.Itoa(0)] = []*SerializableValidator{serializableValidator}
+
+	_, err := SerializableValidatorsToValidators(serializableValidators)
+	assert.True(t, errors.Is(err, nodesCoordinator.ErrNilPubKey))
+}
+
+func TestSerializableValidatorsToValidators_ShouldFail(t *testing.T) {
+	serializableValidators := make(map[string][]*SerializableValidator)
+
+	serializableValidator := &SerializableValidator{
+		PubKey:  []byte{},
+		Chances: 0,
+		Index:   0,
+	}
+
+	serializableValidators["a"] = []*SerializableValidator{serializableValidator}
+
+	_, err := SerializableValidatorsToValidators(serializableValidators)
+	assert.Error(t, err)
+}
+
+func TestSerializableValidatorsToValidators_ShouldWork(t *testing.T) {
+	serializableValidators := make(map[string][]*SerializableValidator)
+
+	serializableValidator := &SerializableValidator{
+		PubKey:  []byte("dummy pk"),
+		Chances: 0,
+		Index:   0,
+	}
+
+	shardId := uint32(0)
+
+	serializableValidators[strconv.Itoa(0)] = []*SerializableValidator{serializableValidator}
+
+	validatorsPerShard, err := SerializableValidatorsToValidators(serializableValidators)
+	assert.Nil(t, err)
+
+	assert.Equal(t, 1, len(validatorsPerShard))
+	assert.Equal(t, []byte("dummy pk"), validatorsPerShard[shardId][0].PubKey())
+}
