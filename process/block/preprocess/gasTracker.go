b227ed7944a13f6984584b8dc0e5712c365b721f
AdoAdoAdo <anaridado@gmail.com>
2021-12-20 15:05:35 +0200

    process: gasTracker change consumed gas limit in receiver shard condition

--- process/block/preprocess/gasTracker.go
+++ process/block/preprocess/gasTracker.go
@@ -33,11 +33,10 @@ func (gt *gasTracker) computeGasConsumed(
 	if gt.shardCoordinator.SelfId() == senderShardId {
 		gasConsumedByTxInSelfShard = gasConsumedByTxInSenderShard
 
-		if gasConsumedByTxInReceiverShard > gt.economicsFee.MaxGasLimitPerMiniBlockForSafeCrossShard() {
+		if gasConsumedByTxInReceiverShard > gt.economicsFee.MaxGasLimitPerTx() {
 			return 0, process.ErrMaxGasLimitPerOneTxInReceiverShardIsReached
 		}
 
-		// TODO: should this be checked against gt.economicsFee.MaxGasLimitPerTx() ?
 		if gasInfo.gasConsumedByMiniBlockInReceiverShard+gasConsumedByTxInReceiverShard > gt.economicsFee.MaxGasLimitPerBlockForSafeCrossShard() {
 			return 0, process.ErrMaxGasLimitPerMiniBlockInReceiverShardIsReached
 		}
