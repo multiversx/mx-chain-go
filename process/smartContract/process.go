647f69162e5b0143c465a08769147b6c371e9630
robertsasu <sasu.robert@gmail.com>
2022-03-01 17:15:12 +0200

    try sign

--- process/smartContract/process.go
+++ process/smartContract/process.go
@@ -2880,6 +2880,8 @@ func (sc *scProcessor) EpochConfirmed(epoch uint32, _ uint64) {
 
 	sc.flagIsPayableBySC.SetValue(epoch >= sc.isPayableBySCEnableEpoch)
 	log.Debug("smartContract/process: enable epoch for payable by SC", "enabled", sc.flagIsPayableBySC.IsSet())
+
+	// try sign
 }
 
 // IsInterfaceNil returns true if there is no value under the interface
