5f4bfb8791ea6290885f6341c26a723e594cdf72
robertsasu <sasu.robert@gmail.com>
2022-03-04 14:32:30 +0200

    integrate latest VM

--- config/epochConfig.go
+++ config/epochConfig.go
@@ -74,6 +74,7 @@ type EnableEpochs struct {
 	TransformToMultiShardCreateEnableEpoch            uint32
 	ESDTRegisterAndSetAllRolesEnableEpoch             uint32
 	DoNotReturnOldBlockInBlockchainHookEnableEpoch    uint32
+	FailExecutionOnEveryAPIErrorEnableEpoch           uint32
 }
 
 // GasScheduleByEpochs represents a gas schedule toml entry that will be applied from the provided epoch
