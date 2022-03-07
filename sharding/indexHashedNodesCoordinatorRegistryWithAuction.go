8c1ed21e136b01a12893cc43a86ea7c69a5db230
Elrond/ <marius.ciucam@gmail.com>
2022-03-07 15:06:02 +0200

    FEAT: ihnc with auction

---
+++ sharding/indexHashedNodesCoordinatorRegistryWithAuction.go
@@ -0,0 +1,70 @@
+package sharding
+
+import "fmt"
+
+// EpochValidatorsWithAuction holds one epoch configuration for a nodes coordinator
+type EpochValidatorsWithAuction struct {
+	*EpochValidators
+	AuctionValidators []*SerializableValidator `json:"auctionValidators"`
+}
+
+// NodesCoordinatorRegistryWithAuction holds the data that can be used to initialize a nodes coordinator
+type NodesCoordinatorRegistryWithAuction struct {
+	EpochsConfig map[string]*EpochValidatorsWithAuction `json:"epochConfigs"`
+	CurrentEpoch uint32                                 `json:"currentEpoch"`
+}
+
+// NodesCoordinatorToRegistryWithAuction will export the nodesCoordinator data to the registry which contains auction list
+func (ihgs *indexHashedNodesCoordinator) NodesCoordinatorToRegistryWithAuction() *NodesCoordinatorRegistryWithAuction {
+	ihgs.mutNodesConfig.RLock()
+	defer ihgs.mutNodesConfig.RUnlock()
+
+	registry := &NodesCoordinatorRegistryWithAuction{
+		CurrentEpoch: ihgs.currentEpoch,
+		EpochsConfig: make(map[string]*EpochValidatorsWithAuction),
+	}
+
+	minEpoch := 0
+	lastEpoch := ihgs.getLastEpochConfig()
+	if lastEpoch >= nodesCoordinatorStoredEpochs {
+		minEpoch = int(lastEpoch) - nodesCoordinatorStoredEpochs + 1
+	}
+
+	for epoch := uint32(minEpoch); epoch <= lastEpoch; epoch++ {
+		epochNodesData, ok := ihgs.nodesConfig[epoch]
+		if !ok {
+			continue
+		}
+
+		registry.EpochsConfig[fmt.Sprint(epoch)] = epochNodesConfigToEpochValidatorsWithAuction(epochNodesData)
+	}
+
+	return registry
+}
+
+func epochNodesConfigToEpochValidatorsWithAuction(config *epochNodesConfig) *EpochValidatorsWithAuction {
+	result := &EpochValidatorsWithAuction{
+		EpochValidators: &EpochValidators{
+			EligibleValidators: make(map[string][]*SerializableValidator, len(config.eligibleMap)),
+			WaitingValidators:  make(map[string][]*SerializableValidator, len(config.waitingMap)),
+			LeavingValidators:  make(map[string][]*SerializableValidator, len(config.leavingMap)),
+		},
+		AuctionValidators: make([]*SerializableValidator, len(config.auctionList)),
+	}
+
+	for k, v := range config.eligibleMap {
+		result.EligibleValidators[fmt.Sprint(k)] = ValidatorArrayToSerializableValidatorArray(v)
+	}
+
+	for k, v := range config.waitingMap {
+		result.WaitingValidators[fmt.Sprint(k)] = ValidatorArrayToSerializableValidatorArray(v)
+	}
+
+	for k, v := range config.leavingMap {
+		result.LeavingValidators[fmt.Sprint(k)] = ValidatorArrayToSerializableValidatorArray(v)
+	}
+
+	result.AuctionValidators = ValidatorArrayToSerializableValidatorArray(config.auctionList)
+
+	return result
+}
