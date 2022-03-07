82b8b21df08f240f81f90d06c551de3263db6bcd
Sorin Stanculeanu <sorinstanculeanu26@gmail.com>
2022-03-07 18:49:50 +0200

    updated peerShardMapperStub

--- integrationTests/mock/peerShardMapperStub.go
+++ integrationTests/mock/peerShardMapperStub.go
@@ -6,12 +6,28 @@ import "github.com/ElrondNetwork/elrond-go-core/core"
 type PeerShardMapperStub struct {
 	GetLastKnownPeerIDCalled        func(pk []byte) (*core.PeerID, bool)
 	UpdatePeerIDPublicKeyPairCalled func(pid core.PeerID, pk []byte)
+	UpdatePeerIdShardIdCalled       func(pid core.PeerID, shardID uint32)
+	UpdatePeerIdSubTypeCalled       func(pid core.PeerID, peerSubType core.P2PPeerSubType)
 }
 
 // UpdatePeerIDPublicKeyPair -
 func (psms *PeerShardMapperStub) UpdatePeerIDPublicKeyPair(pid core.PeerID, pk []byte) {
 	if psms.UpdatePeerIDPublicKeyPairCalled != nil {
 		psms.UpdatePeerIDPublicKeyPairCalled(pid, pk)
+	}
+}
+
+// UpdatePeerIdShardId -
+func (psms *PeerShardMapperStub) UpdatePeerIdShardId(pid core.PeerID, shardID uint32) {
+	if psms.UpdatePeerIdShardIdCalled != nil {
+		psms.UpdatePeerIdShardIdCalled(pid, shardID)
+	}
+}
+
+// UpdatePeerIdSubType -
+func (psms *PeerShardMapperStub) UpdatePeerIdSubType(pid core.PeerID, peerSubType core.P2PPeerSubType) {
+	if psms.UpdatePeerIdSubTypeCalled != nil {
+		psms.UpdatePeerIdSubTypeCalled(pid, peerSubType)
 	}
 }
 
