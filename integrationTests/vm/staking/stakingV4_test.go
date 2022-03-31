6cb12757e7471179b4e2091175a13a86be5fce8c
Elrond/ <marius.ciucam@gmail.com>
2022-03-31 13:27:46 +0300

    FEAT: Refactor

--- integrationTests/vm/staking/stakingV4_test.go
+++ integrationTests/vm/staking/stakingV4_test.go
@@ -1,15 +1,38 @@
 package staking
 
 import (
-	"fmt"
+	"math/big"
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/ElrondNetwork/elrond-go-core/data/block"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNewTestMetaProcessor(t *testing.T) {
 	node := NewTestMetaProcessor(1, 1, 1, 1, 1)
-	header, err := node.MetaBlockProcessor.CreateNewHeader(0, 0)
-	require.Nil(t, err)
-	fmt.Println(header)
+	metaHdr := &block.MetaBlock{}
+	headerHandler, bodyHandler, err := node.MetaBlockProcessor.CreateBlock(metaHdr, func() bool { return true })
+	assert.Nil(t, err)
+
+	err = headerHandler.SetRound(uint64(1))
+	assert.Nil(t, err)
+
+	err = headerHandler.SetNonce(1)
+	assert.Nil(t, err)
+
+	err = headerHandler.SetPrevHash([]byte("hash"))
+	assert.Nil(t, err)
+
+	err = headerHandler.SetAccumulatedFees(big.NewInt(0))
+	assert.Nil(t, err)
+
+	_ = bodyHandler
+	/*
+		metaHeaderHandler, _ := headerHandler.(data.MetaHeaderHandler)
+		err = metaHeaderHandler.SetAccumulatedFeesInEpoch(big.NewInt(0))
+		assert.Nil(t, err)
+
+		err = node.MetaBlockProcessor.ProcessBlock(headerHandler, bodyHandler, func() time.Duration { return time.Second })
+		assert.Nil(t, err)
+	*/
 }
