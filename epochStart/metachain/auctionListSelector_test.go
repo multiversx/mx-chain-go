c3217c1e745977bd86c608b0638133d7cf86a6a7
Elrond/ <marius.ciucam@gmail.com>
2022-05-23 13:48:29 +0300

    FIX: After merges 2

--- epochStart/metachain/auctionListSelector_test.go
+++ epochStart/metachain/auctionListSelector_test.go
@@ -159,33 +159,28 @@ func TestCalcNormalizedRandomness(t *testing.T) {
 	t.Run("randomness longer than expected len", func(t *testing.T) {
 		t.Parallel()
 
-		randomness := []byte("rand")
-		expectedLen := 2
-
-		result := calcNormalizedRandomness(randomness, expectedLen)
-
+		result := calcNormalizedRandomness([]byte("rand"), 2)
 		require.Equal(t, []byte("ra"), result)
 	})
 
 	t.Run("randomness length equal to expected len", func(t *testing.T) {
 		t.Parallel()
 
-		randomness := []byte("rand")
-		expectedLen := 4
-
-		result := calcNormalizedRandomness(randomness, expectedLen)
-
+		result := calcNormalizedRandomness([]byte("rand"), 4)
 		require.Equal(t, []byte("rand"), result)
 	})
 
 	t.Run("randomness length less than expected len", func(t *testing.T) {
 		t.Parallel()
 
-		randomness := []byte("rand")
-		expectedLen := 6
-
-		result := calcNormalizedRandomness(randomness, expectedLen)
-
+		result := calcNormalizedRandomness([]byte("rand"), 6)
 		require.Equal(t, []byte("randra"), result)
+	})
+
+	t.Run("expected len is zero", func(t *testing.T) {
+		t.Parallel()
+
+		result := calcNormalizedRandomness([]byte("rand"), 0)
+		require.Empty(t, result)
 	})
 }
