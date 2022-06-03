c192c7d5f5459c3504eb77a4670565457878e90a
Iuga Mihai <mihai9625@gmail.com>
2022-06-03 11:30:33 +0300

    add comments

--- testscommon/dataFieldParserStub.go
+++ testscommon/dataFieldParserStub.go
@@ -2,10 +2,12 @@ package testscommon
 
 import datafield "github.com/ElrondNetwork/elrond-vm-common/parsers/dataField"
 
+// DataFieldParserStub -
 type DataFieldParserStub struct {
 	ParseCalled func(dataField []byte, sender, receiver []byte) *datafield.ResponseParseData
 }
 
+// Parse -
 func (df *DataFieldParserStub) Parse(dataField []byte, sender, receiver []byte) *datafield.ResponseParseData {
 	if df.ParseCalled != nil {
 		return df.ParseCalled(dataField, sender, receiver)
