b0dc968bc342a3fdd95daa7917d66028a55be46e
ssd04 <dariussuirab@gmail.com>
2022-06-28 14:07:08 +0300

    add coresponding txHash for each log event

--- outport/notifier/eventNotifier.go
+++ outport/notifier/eventNotifier.go
@@ -35,6 +35,7 @@ type Event struct {
 	Identifier string   `json:"identifier"`
 	Topics     [][]byte `json:"topics"`
 	Data       []byte   `json:"data"`
+	TxHash     string   `json:"txHash"`
 }
 
 // RevertBlock holds revert event data
@@ -55,6 +56,12 @@ type eventNotifier struct {
 	marshalizer     marshal.Marshalizer
 	hasher          hashing.Hasher
 	pubKeyConverter core.PubkeyConverter
+}
+
+// logEvent defines a log event associated with coresponding tx hash
+type logEvent struct {
+	eventHandler nodeData.EventHandler
+	txHash       string
 }
 
 // ArgsEventNotifier defines the arguments needed for event notifier creation
@@ -105,7 +112,7 @@ func (en *eventNotifier) SaveBlock(args *indexer.ArgsSaveBlockData) error {
 }
 
 func (en *eventNotifier) getLogEventsFromTransactionsPool(logs []*nodeData.LogData) []Event {
-	var logEvents []nodeData.EventHandler
+	var logEvents []logEvent
 	for _, logData := range logs {
 		if logData == nil {
 			continue
@@ -114,7 +121,14 @@ func (en *eventNotifier) getLogEventsFromTransactionsPool(logs []*nodeData.LogDa
 			continue
 		}
 
-		logEvents = append(logEvents, logData.LogHandler.GetLogEvents()...)
+		for _, eventHandler := range logData.LogHandler.GetLogEvents() {
+			le := logEvent{
+				eventHandler: eventHandler,
+				txHash:       logData.TxHash,
+			}
+
+			logEvents = append(logEvents, le)
+		}
 	}
 
 	if len(logEvents) == 0 {
@@ -122,10 +136,10 @@ func (en *eventNotifier) getLogEventsFromTransactionsPool(logs []*nodeData.LogDa
 	}
 
 	var events []Event
-	for _, eventHandler := range logEvents {
-		if !eventHandler.IsInterfaceNil() {
-			bech32Address := en.pubKeyConverter.Encode(eventHandler.GetAddress())
-			eventIdentifier := string(eventHandler.GetIdentifier())
+	for _, event := range logEvents {
+		if !event.eventHandler.IsInterfaceNil() {
+			bech32Address := en.pubKeyConverter.Encode(event.eventHandler.GetAddress())
+			eventIdentifier := string(event.eventHandler.GetIdentifier())
 
 			log.Debug("eventNotifier: received event from address",
 				"address", bech32Address,
@@ -135,8 +149,9 @@ func (en *eventNotifier) getLogEventsFromTransactionsPool(logs []*nodeData.LogDa
 			events = append(events, Event{
 				Address:    bech32Address,
 				Identifier: eventIdentifier,
-				Topics:     eventHandler.GetTopics(),
-				Data:       eventHandler.GetData(),
+				Topics:     event.eventHandler.GetTopics(),
+				Data:       event.eventHandler.GetData(),
+				TxHash:     event.txHash,
 			})
 		}
 	}
