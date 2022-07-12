7b07ca5f5b817dddce7c728b09a8d34359e98edb
robertsasu <sasu.robert@gmail.com>
2022-07-12 13:55:29 +0300

    adding a new test

--- integrationTests/vm/esdt/multi-transfer/esdtMultiTransferThroughForwarder/esdtMultiTransferThroughForwarder_test.go
+++ integrationTests/vm/esdt/multi-transfer/esdtMultiTransferThroughForwarder/esdtMultiTransferThroughForwarder_test.go
@@ -206,8 +206,11 @@ func TestESDTMultiTransferWithWrongArguments1(t *testing.T) {
 	forwarder := net.DeployNonpayableSC(owner, "../../testdata/execute.wasm")
 	vaultOtherShard := net.DeployNonpayableSC(net.NodesSharded[1][0].OwnAccount, "../../testdata/vaultV2.wasm")
 
-	// Issue and create SFT
+	// Create the fungible token
 	supply := int64(1000)
+	tokenID := multitransfer.IssueFungibleToken(t, net, senderNode, "FUNG1", supply)
+
+	// Issue and create SFT
 	sftID := multitransfer.IssueNft(net, senderNode, "SFT1", true)
 	multitransfer.CreateSFT(t, net, senderNode, sftID, 1, supply)
 
@@ -215,9 +218,9 @@ func TestESDTMultiTransferWithWrongArguments1(t *testing.T) {
 	txData := txDataBuilder.NewBuilder()
 	txData.Func(core.BuiltInFunctionMultiESDTNFTTransfer)
 	txData.Bytes(forwarder).Int(1)
-	txData.Str(sftID).Int(1).Int64(1)
-
+	txData.Str(sftID).Int(1).Int64(10).Str("doAsyncCall").Bytes(vaultOtherShard)
 	tx := net.CreateTxUint64(owner, owner.Address, 0, txData.ToBytes())
+	tx.Data = append(tx.Data, []byte("@@4d756c7469455344544e46545472616e73666572@06@0000000000000000050029139511fd210457a1affbb772a13c22d78e9ab028ab@01@475245454e2d383138393035@01@01@")...)
 	tx.GasLimit = net.MaxGasLimit / 2
 	_ = net.SignAndSendTx(owner, tx)
 	net.Steps(4)
@@ -225,117 +228,16 @@ func TestESDTMultiTransferWithWrongArguments1(t *testing.T) {
 	esdt.CheckAddressHasTokens(t, forwarder, net.Nodes, []byte(sftID), 1, supply)
 	esdt.CheckAddressHasTokens(t, forwarder, net.Nodes, []byte(tokenID), 0, supply)
 
-	// transfer to a user from another shard
-	transfers := []*multitransfer.EsdtTransfer{
-		{
-			TokenIdentifier: tokenID,
-			Nonce:           0,
-			Amount:          100,
-		}}
-	destAddress := net.NodesSharded[1][0].OwnAccount.Address
-	multiTransferThroughForwarder(
-		net,
-		senderNode.OwnAccount,
-		forwarder,
-		"multi_transfer_via_async",
-		transfers,
-		destAddress)
+	txData = txDataBuilder.NewBuilder()
+	txData.Func(core.BuiltInFunctionMultiESDTNFTTransfer)
+	txData.Bytes(forwarder).Int(1)
+	txData.Str(tokenID).Int(0).Int64(10).Str("doAsyncCall").Bytes(vaultOtherShard)
+	tx = net.CreateTxUint64(owner, owner.Address, 0, txData.ToBytes())
+	tx.Data = append(tx.Data, []byte("@@4d756c7469455344544e46545472616e73666572@06@0000000000000000050029139511fd210457a1affbb772a13c22d78e9ab028ab@01@475245454e2d383138393035@01@01@")...)
+	tx.GasLimit = net.MaxGasLimit / 2
+	_ = net.SignAndSendTx(owner, tx)
+	net.Steps(4)
 
-	esdt.CheckAddressHasTokens(t, forwarder, net.Nodes, []byte(tokenID), 0, 900)
-	esdt.CheckAddressHasTokens(t, destAddress, net.Nodes, []byte(tokenID), 0, 100)
-
-	// transfer to vault, same shard
-	multiTransferThroughForwarder(
-		net,
-		senderNode.OwnAccount,
-		forwarder,
-		"forward_sync_accept_funds_multi_transfer",
-		transfers,
-		vault)
-
-	esdt.CheckAddressHasTokens(t, forwarder, net.Nodes, []byte(tokenID), 0, 800)
-	esdt.CheckAddressHasTokens(t, vault, net.Nodes, []byte(tokenID), 0, 100)
-
-	// transfer fungible and non-fungible
-	// transfer to vault, same shard
-	transfers = []*multitransfer.EsdtTransfer{
-		{
-			TokenIdentifier: tokenID,
-			Nonce:           0,
-			Amount:          100,
-		},
-		{
-			TokenIdentifier: sftID,
-			Nonce:           1,
-			Amount:          100,
-		},
-	}
-	multiTransferThroughForwarder(
-		net,
-		senderNode.OwnAccount,
-		forwarder,
-		"forward_sync_accept_funds_multi_transfer",
-		transfers,
-		vault)
-
-	esdt.CheckAddressHasTokens(t, forwarder, net.Nodes, []byte(tokenID), 0, 700)
-	esdt.CheckAddressHasTokens(t, vault, net.Nodes, []byte(tokenID), 0, 200)
-
-	esdt.CheckAddressHasTokens(t, forwarder, net.Nodes, []byte(sftID), 1, 900)
-	esdt.CheckAddressHasTokens(t, vault, net.Nodes, []byte(sftID), 1, 100)
-
-	// transfer fungible and non-fungible
-	// transfer to vault, cross shard via transfer and execute
-	transfers = []*multitransfer.EsdtTransfer{
-		{
-			TokenIdentifier: tokenID,
-			Nonce:           0,
-			Amount:          100,
-		},
-		{
-			TokenIdentifier: sftID,
-			Nonce:           1,
-			Amount:          100,
-		},
-	}
-	multiTransferThroughForwarder(
-		net,
-		senderNode.OwnAccount,
-		forwarder,
-		"forward_transf_exec_accept_funds_multi_transfer",
-		transfers,
-		vaultOtherShard)
-
-	esdt.CheckAddressHasTokens(t, forwarder, net.Nodes, []byte(tokenID), 0, 600)
-	esdt.CheckAddressHasTokens(t, vaultOtherShard, net.Nodes, []byte(tokenID), 0, 100)
-
-	esdt.CheckAddressHasTokens(t, forwarder, net.Nodes, []byte(sftID), 1, 800)
-	esdt.CheckAddressHasTokens(t, vaultOtherShard, net.Nodes, []byte(sftID), 1, 100)
-
-	// transfer to vault, cross shard, via async call
-	transfers = []*multitransfer.EsdtTransfer{
-		{
-			TokenIdentifier: tokenID,
-			Nonce:           0,
-			Amount:          100,
-		},
-		{
-			TokenIdentifier: sftID,
-			Nonce:           1,
-			Amount:          100,
-		},
-	}
-	multiTransferThroughForwarder(
-		net,
-		senderNode.OwnAccount,
-		forwarder,
-		"multi_transfer_via_async",
-		transfers,
-		vaultOtherShard)
-
-	esdt.CheckAddressHasTokens(t, forwarder, net.Nodes, []byte(tokenID), 0, 500)
-	esdt.CheckAddressHasTokens(t, vaultOtherShard, net.Nodes, []byte(tokenID), 0, 200)
-
-	esdt.CheckAddressHasTokens(t, forwarder, net.Nodes, []byte(sftID), 1, 700)
-	esdt.CheckAddressHasTokens(t, vaultOtherShard, net.Nodes, []byte(sftID), 1, 200)
+	esdt.CheckAddressHasTokens(t, forwarder, net.Nodes, []byte(sftID), 1, supply)
+	esdt.CheckAddressHasTokens(t, forwarder, net.Nodes, []byte(tokenID), 0, supply)
 }
