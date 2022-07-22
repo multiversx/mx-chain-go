2f4a4ed08aca9a55cc58af9082c13ad56615d2b7
BeniaminDrasovean <beniamin.drasovean@elrond.network>
2022-07-22 12:42:07 +0300

    sort imports

--- trie/trieStorageManagerFactory_test.go
+++ trie/trieStorageManagerFactory_test.go
@@ -2,12 +2,12 @@ package trie_test
 
 import (
 	"fmt"
+	"testing"
+
 	"github.com/ElrondNetwork/elrond-go-core/core"
 	"github.com/ElrondNetwork/elrond-go/common"
 	"github.com/ElrondNetwork/elrond-go/testscommon"
 	trieMock "github.com/ElrondNetwork/elrond-go/testscommon/trie"
-	"testing"
-
 	"github.com/ElrondNetwork/elrond-go/trie"
 	"github.com/stretchr/testify/assert"
 )
