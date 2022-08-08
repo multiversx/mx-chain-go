1c2431de0da52f509eb4492345e00dde16c5291b
Bogdan Rosianu <bogdan.rosianu@yahoo.com>
2022-08-08 18:05:00 +0300

    fixes + refactoring

--- txcache/txListForSender_test.go
+++ txcache/txListForSender_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 
 	"github.com/ElrondNetwork/elrond-go-core/data/transaction"
-	"github.com/ElrondNetwork/elrond-go-storage/mock/txcachemocks"
+	"github.com/ElrondNetwork/elrond-go-storage/testscommon/txcachemocks"
 	"github.com/stretchr/testify/require"
 )
 
