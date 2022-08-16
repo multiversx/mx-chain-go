26d59b8971a01015d47d153ad1eec032a106db06
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2022-08-16 18:38:35 +0300

    Added some unit tests (plus some refactoring).

--- api/groups/urlParams_test.go
+++ api/groups/urlParams_test.go
@@ -1,22 +1,14 @@
 package groups
 
 import (
-	"net/http"
-	"net/url"
 	"testing"
 
-	"github.com/gin-gonic/gin"
+	"github.com/ElrondNetwork/elrond-go/testscommon"
 	"github.com/stretchr/testify/require"
 )
 
 func TestParseBoolUrlParam(t *testing.T) {
-	c := &gin.Context{
-		Request: &http.Request{
-			URL: &url.URL{
-				RawQuery: "a=true&b=false&c=foobar&d",
-			},
-		},
-	}
+	c := testscommon.CreateGinContextWithRawQuery("a=true&b=false&c=foobar&d")
 
 	value, err := parseBoolUrlParam(c, "a")
 	require.Nil(t, err)
@@ -40,13 +32,7 @@ func TestParseBoolUrlParam(t *testing.T) {
 }
 
 func TestParseUint32UrlParam(t *testing.T) {
-	c := &gin.Context{
-		Request: &http.Request{
-			URL: &url.URL{
-				RawQuery: "a=7&b=0&c=foobar&d=-1&e=4294967295&f=4294967296",
-			},
-		},
-	}
+	c := testscommon.CreateGinContextWithRawQuery("a=7&b=0&c=foobar&d=-1&e=4294967295&f=4294967296")
 
 	value, err := parseUint32UrlParam(c, "a")
 	require.Nil(t, err)
@@ -77,4 +63,20 @@ func TestParseUint32UrlParam(t *testing.T) {
 	require.NotNil(t, err)
 	require.False(t, value.HasValue)
 	require.Equal(t, uint32(0), value.Value)
+}
+
+func TestParseHexBytesUrlParam(t *testing.T) {
+	c := testscommon.CreateGinContextWithRawQuery("a=aaaa&b=test&c")
+
+	value, err := parseHexBytesUrlParam(c, "a")
+	require.Nil(t, err)
+	require.Equal(t, []byte{0xaa, 0xaa}, value)
+
+	value, err = parseHexBytesUrlParam(c, "b")
+	require.NotNil(t, err)
+	require.Nil(t, value)
+
+	value, err = parseHexBytesUrlParam(c, "c")
+	require.Nil(t, err)
+	require.Equal(t, []byte(nil), value)
 }
