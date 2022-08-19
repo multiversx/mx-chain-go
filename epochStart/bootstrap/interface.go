7a6f74efd3e37dc1d1fe03a74fbcdf1e694cb307
SebastianMarian <sebastian.marian@elrond.com>
2022-08-19 12:41:57 +0300

    * Fixed sorting of imports

--- epochStart/bootstrap/interface.go
+++ epochStart/bootstrap/interface.go
@@ -2,12 +2,12 @@ package bootstrap
 
 import (
 	"context"
-	"github.com/ElrondNetwork/elrond-go/epochStart"
 
 	"github.com/ElrondNetwork/elrond-go-core/core"
 	"github.com/ElrondNetwork/elrond-go-core/data"
 	"github.com/ElrondNetwork/elrond-go-core/data/block"
 	"github.com/ElrondNetwork/elrond-go/dataRetriever"
+	"github.com/ElrondNetwork/elrond-go/epochStart"
 	"github.com/ElrondNetwork/elrond-go/process"
 	"github.com/ElrondNetwork/elrond-go/sharding/nodesCoordinator"
 )
