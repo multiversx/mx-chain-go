d0b82511f70a2c1166f712df1ea959bcbc270ddf
Elrond/ <marius.ciucam@gmail.com>
2022-09-20 14:51:06 +0300

    FEAT: Remove covalent indexer from node

--- config/externalConfig.go
+++ config/externalConfig.go
@@ -4,7 +4,6 @@ package config
 type ExternalConfig struct {
 	ElasticSearchConnector ElasticSearchConfig
 	EventNotifierConnector EventNotifierConfig
-	CovalentConnector      CovalentConfig
 }
 
 // ElasticSearchConfig will hold the configuration for the elastic search
@@ -26,12 +25,4 @@ type EventNotifierConfig struct {
 	ProxyUrl         string
 	Username         string
 	Password         string
-}
-
-// CovalentConfig will hold the configurations for covalent indexer
-type CovalentConfig struct {
-	Enabled              bool
-	URL                  string
-	RouteSendData        string
-	RouteAcknowledgeData string
 }
