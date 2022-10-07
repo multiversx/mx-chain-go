a09e10fe68a92ca86ebc608ebdedb3079fdc260d
jules01 <iulian.pascalau@gmail.com>
2022-10-07 12:22:42 +0300

    - fixed preferred peers

--- node/nodeRunner.go
+++ node/nodeRunner.go
@@ -1213,11 +1213,6 @@ func (nr *nodeRunner) CreateManagedBootstrapComponents(
 func (nr *nodeRunner) CreateManagedNetworkComponents(
 	coreComponents mainFactory.CoreComponentsHolder,
 ) (mainFactory.NetworkComponentsHandler, error) {
-	decodedPreferredPeers, err := decodePreferredPeers(*nr.configs.PreferencesConfig, coreComponents.ValidatorPubKeyConverter())
-	if err != nil {
-		return nil, err
-	}
-
 	networkComponentsFactoryArgs := mainFactory.NetworkComponentsFactoryArgs{
 		P2pConfig:             *nr.configs.P2pConfig,
 		MainConfig:            *nr.configs.GeneralConfig,
@@ -1225,7 +1220,7 @@ func (nr *nodeRunner) CreateManagedNetworkComponents(
 		StatusHandler:         coreComponents.StatusHandler(),
 		Marshalizer:           coreComponents.InternalMarshalizer(),
 		Syncer:                coreComponents.SyncTimer(),
-		PreferredPeersSlices:  decodedPreferredPeers,
+		PreferredPeersSlices:  nr.configs.PreferencesConfig.Preferences.PreferredConnections,
 		BootstrapWaitTime:     common.TimeToWaitForP2PBootstrap,
 		NodeOperationMode:     p2p.NormalOperation,
 		ConnectionWatcherType: nr.configs.PreferencesConfig.Preferences.ConnectionWatcherType,
@@ -1491,20 +1486,6 @@ func enableGopsIfNeeded(gopsEnabled bool) {
 	}
 
 	log.Trace("gops", "enabled", gopsEnabled)
-}
-
-func decodePreferredPeers(prefConfig config.Preferences, validatorPubKeyConverter core.PubkeyConverter) ([]string, error) {
-	decodedPeers := make([]string, 0)
-	for _, connectionSlice := range prefConfig.Preferences.PreferredConnections {
-		peerBytes, err := validatorPubKeyConverter.Decode(connectionSlice)
-		if err != nil {
-			return nil, fmt.Errorf("cannot decode preferred peer(%s) : %w", connectionSlice, err)
-		}
-
-		decodedPeers = append(decodedPeers, string(peerBytes))
-	}
-
-	return decodedPeers, nil
 }
 
 func createWhiteListerVerifiedTxs(generalConfig *config.Config) (process.WhiteListHandler, error) {
