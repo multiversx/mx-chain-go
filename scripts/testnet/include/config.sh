58b9e55ede62c4d71bc53be2ede654eba36d7e0f
Iuga Mihai <mihai9625@gmail.com>
2022-10-21 16:00:06 +0300

    fixes and extra check

--- scripts/testnet/include/config.sh
+++ scripts/testnet/include/config.sh
@@ -33,12 +33,15 @@ copySeednodeConfig() {
 
   pushd $ELRONDDIR/cmd/keygenerator
 
-  go build
-  ./keygenerator --key-type p2p
+  if [[ ! -f "p2pKey.pem" ]]; then
+      go build
+      ./keygenerator --key-type p2p
+  fi
+
   cp p2pKey.pem $TESTNETDIR/seednode/config
   GENERATED_P2P_PUB_KEY=$(grep for  ./p2pKey.pem | head -1 | grep -oP '[^[:blank:]-]*' | tail -1)
-  export P2P_SEEDNODE_ADDRESS="/ip4/127.0.0.1/tcp/$PORT_SEEDNODE/p2p/$GENERATED_P2P_PUB_KEY"
-  rm p2pKey.pem
+  P2P_SEEDNODE_ADDRESS=$P2P_SEEDNODE_ADDRESS$GENERATED_P2P_PUB_KEY
+  export P2P_SEEDNODE_ADDRESS
 
   popd
 }
