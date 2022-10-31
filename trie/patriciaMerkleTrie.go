eebaf11e3f264469e7c29a8a68f2335ebb2ce1eb
ssd04 <dariussuirab@gmail.com>
2022-10-31 13:57:31 +0200

    refactor to have dfs and bfs iterator with base iterator

--- trie/patriciaMerkleTrie.go
+++ trie/patriciaMerkleTrie.go
@@ -409,7 +409,7 @@ func (tr *patriciaMerkleTrie) GetSerializedNodes(rootHash []byte, maxBuffToSend 
 		return nil, 0, err
 	}
 
-	it, err := NewIterator(newTr)
+	it, err := NewBFSIterator(newTr)
 	if err != nil {
 		return nil, 0, err
 	}
