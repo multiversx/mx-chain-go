61440528d3532e7efadd6320b85d5b008dde8aad
ssd04 <dariussuirab@gmail.com>
2022-11-21 12:02:36 +0200

    conflicts fix after merge

--- state/accountsDB.go
+++ state/accountsDB.go
@@ -519,11 +519,7 @@ func (adb *AccountsDB) loadDataTrie(accountHandler baseAccountHandler, mainTrie 
 
 	dataTrie, err := mainTrie.Recreate(accountHandler.GetRootHash())
 	if err != nil {
-<<<<<<< HEAD
-		return fmt.Errorf("%w for rootHash %v", err, accountHandler.GetRootHash())
-=======
 		return fmt.Errorf("trie was not found for hash, rootHash = %s, err = %w", hex.EncodeToString(accountHandler.GetRootHash()), err)
->>>>>>> rc/v1.4.0
 	}
 
 	accountHandler.SetDataTrie(dataTrie)
