f386cfe28347cac6b0a2e821e8b5160cdc527ca1
matei-p <bogdandanu@gmail.com>
2023-01-11 16:37:35 +0200

    merge fixes

--- node/nodeDebugFactory/interceptedDebugHandler_test.go
+++ node/nodeDebugFactory/interceptedDebugHandler_test.go
@@ -67,7 +67,7 @@ func TestCreateInterceptedDebugHandler_InvalidDebugConfigShouldErr(t *testing.T)
 	)
 
 	assert.NotNil(t, err)
-	assert.True(t, strings.Contains(err.Error(), "Must provide a positive size"))
+	assert.True(t, strings.Contains(err.Error(), "must provide a positive size when creating NewInterceptorResolver"))
 }
 
 func TestCreateInterceptedDebugHandler_SettingOnInterceptorsErrShouldErr(t *testing.T) {
