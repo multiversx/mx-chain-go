ff184c8adb6f3421817a3fc396305d7c5f1e4e9e
SebastianMarian <sebastian.marian@elrond.com>
2023-01-23 15:38:20 +0200

    * Fixed after self-review

--- process/block/preprocess/sovereignChainTransactions.go
+++ process/block/preprocess/sovereignChainTransactions.go
@@ -14,7 +14,7 @@ type sovereignChainTransactions struct {
 	*transactions
 }
 
-// NewSovereignChainTransactionPreprocessor creates a new transaction preprocessor object
+// NewSovereignChainTransactionPreprocessor creates a new sovereign chain transaction preprocessor object
 func NewSovereignChainTransactionPreprocessor(
 	transactions *transactions,
 ) (*sovereignChainTransactions, error) {
@@ -22,33 +22,33 @@ func NewSovereignChainTransactionPreprocessor(
 		return nil, process.ErrNilPreProcessor
 	}
 
-	sctp := &sovereignChainTransactions{
+	sct := &sovereignChainTransactions{
 		transactions,
 	}
 
-	sctp.scheduledTXContinueFunc = sctp.shouldContinueProcessingScheduledTx
+	sct.scheduledTXContinueFunc = sct.shouldContinueProcessingScheduledTx
 
-	return sctp, nil
+	return sct, nil
 }
 
-// ProcessBlockTransactions processes all the transaction from the block.Body, updates the state
-func (sctp *sovereignChainTransactions) ProcessBlockTransactions(
+// ProcessBlockTransactions processes all the transactions from me from the body given
+func (sct *sovereignChainTransactions) ProcessBlockTransactions(
 	header data.HeaderHandler,
 	body *block.Body,
 	haveTime func() bool,
 ) (block.MiniBlockSlice, error) {
-	calculatedMiniBlocks, _, err := sctp.processTxsFromMe(body, haveTime, header.GetPrevRandSeed())
+	calculatedMiniBlocks, _, err := sct.processTxsFromMe(body, haveTime, header.GetPrevRandSeed())
 	return calculatedMiniBlocks, err
 }
 
-// CreateAndProcessMiniBlocks creates miniBlocks from selected transactions
-func (sctp *sovereignChainTransactions) CreateAndProcessMiniBlocks(haveTime func() bool, randomness []byte) (block.MiniBlockSlice, error) {
+// CreateAndProcessMiniBlocks creates mini blocks from the selected transactions
+func (sct *sovereignChainTransactions) CreateAndProcessMiniBlocks(haveTime func() bool, randomness []byte) (block.MiniBlockSlice, error) {
 	startTime := time.Now()
 
 	//TODO: Check if this value of 2x is ok, as we have here normal + scheduled txs
-	gasBandwidth := sctp.economicsFee.MaxGasLimitPerBlock(sctp.shardCoordinator.SelfId()) * 2.0
+	gasBandwidth := sct.economicsFee.MaxGasLimitPerBlock(sct.shardCoordinator.SelfId()) * 2.0
 
-	sortedTxs, err := sctp.computeSortedTxs(sctp.shardCoordinator.SelfId(), sctp.shardCoordinator.SelfId(), randomness)
+	sortedTxs, err := sct.computeSortedTxs(sct.shardCoordinator.SelfId(), sct.shardCoordinator.SelfId(), randomness)
 	elapsedTime := time.Since(startTime)
 	if err != nil {
 		log.Debug("computeSortedTxs", "error", err.Error())
@@ -75,9 +75,9 @@ func (sctp *sovereignChainTransactions) CreateAndProcessMiniBlocks(haveTime func
 		"time [s]", elapsedTime,
 	)
 
-	selectedTxs, _, _ := sctp.addTxsWithinBandwidth(nil, sortedTxs, 0, gasBandwidth)
+	selectedTxs, _, _ := sct.addTxsWithinBandwidth(nil, sortedTxs, 0, gasBandwidth)
 
-	scheduledMiniBlocks, err := sctp.createScheduledMiniBlocksFromMeAsProposer(
+	scheduledMiniBlocks, err := sct.createScheduledMiniBlocksFromMeAsProposer(
 		haveTime,
 		haveAdditionalTimeFalse,
 		selectedTxs,
@@ -91,26 +91,26 @@ func (sctp *sovereignChainTransactions) CreateAndProcessMiniBlocks(haveTime func
 	return scheduledMiniBlocks, nil
 }
 
-func (sctp *sovereignChainTransactions) computeSortedTxs(
+func (sct *sovereignChainTransactions) computeSortedTxs(
 	sndShardId uint32,
 	dstShardId uint32,
 	randomness []byte,
 ) ([]*txcache.WrappedTransaction, error) {
 	strCache := process.ShardCacherIdentifier(sndShardId, dstShardId)
-	txShardPool := sctp.txPool.ShardDataStore(strCache)
+	txShardPool := sct.txPool.ShardDataStore(strCache)
 	if check.IfNil(txShardPool) {
 		return nil, process.ErrNilTxDataPool
 	}
 
 	sortedTransactionsProvider := createSortedTransactionsProvider(txShardPool)
 	sortedTxs := sortedTransactionsProvider.GetSortedTransactions()
-	sctp.sortTransactionsBySenderAndNonce(sortedTxs, randomness)
+	sct.sortTransactionsBySenderAndNonce(sortedTxs, randomness)
 
 	return sortedTxs, nil
 }
 
 // ProcessMiniBlock does nothing on sovereign chain
-func (sctp *sovereignChainTransactions) ProcessMiniBlock(
+func (sct *sovereignChainTransactions) ProcessMiniBlock(
 	_ *block.MiniBlock,
 	_ func() bool,
 	_ func() bool,
@@ -122,7 +122,7 @@ func (sctp *sovereignChainTransactions) ProcessMiniBlock(
 	return nil, 0, false, nil
 }
 
-func (sctp *sovereignChainTransactions) shouldContinueProcessingScheduledTx(
+func (sct *sovereignChainTransactions) shouldContinueProcessingScheduledTx(
 	_ func(uint32) bool,
 	wrappedTx *txcache.WrappedTransaction,
 	_ map[string]struct{},
@@ -147,7 +147,7 @@ func (sctp *sovereignChainTransactions) shouldContinueProcessingScheduledTx(
 		return nil, nil, false
 	}
 
-	addressHasEnoughBalance := sctp.hasAddressEnoughInitialBalance(tx)
+	addressHasEnoughBalance := sct.hasAddressEnoughInitialBalance(tx)
 	if !addressHasEnoughBalance {
 		mbInfo.schedulingInfo.numScheduledTxsWithInitialBalanceConsumed++
 		return nil, nil, false
