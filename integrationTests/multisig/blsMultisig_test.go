1fe3048def808a4640192453f32b463091a969d4
AdoAdoAdo <anaridado@gmail.com>
2023-02-06 17:44:51 +0200

    benchmarks multisigners KOSK & NoKOSK

--- integrationTests/multisig/blsMultisig_test.go
+++ integrationTests/multisig/blsMultisig_test.go
@@ -12,14 +12,35 @@ import (
 	llsig "github.com/multiversx/mx-chain-crypto-go/signing/mcl/multisig"
 	"github.com/multiversx/mx-chain-crypto-go/signing/multisig"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func createKeysAndMultiSignerBls(
+func createKeysAndMultiSignerBlsKOSK(
+	grSize uint16,
+	suite crypto.Suite,
+) ([][]byte, [][]byte, crypto.MultiSigner) {
+
+	kg, privKeys, pubKeys := createMultiSignerSetup(grSize, suite)
+	llSigner := &llsig.BlsMultiSignerKOSK{}
+	multiSigner, _ := multisig.NewBLSMultisig(llSigner, kg)
+
+	return privKeys, pubKeys, multiSigner
+}
+
+func createKeysAndMultiSignerBlsNoKOSK(
 	grSize uint16,
 	hasher hashing.Hasher,
 	suite crypto.Suite,
 ) ([][]byte, [][]byte, crypto.MultiSigner) {
+	kg, privKeys, pubKeys := createMultiSignerSetup(grSize, suite)
+	llSigner := &llsig.BlsMultiSigner{Hasher: hasher}
 
+	multiSigner, _ := multisig.NewBLSMultisig(llSigner, kg)
+
+	return privKeys, pubKeys, multiSigner
+}
+
+func createMultiSignerSetup(grSize uint16, suite crypto.Suite) (crypto.KeyGenerator, [][]byte, [][]byte) {
 	kg := signing.NewKeyGenerator(suite)
 	privKeys := make([][]byte, grSize)
 	pubKeys := make([][]byte, grSize)
@@ -29,11 +50,7 @@ func createKeysAndMultiSignerBls(
 		privKeys[i], _ = sk.ToByteArray()
 		pubKeys[i], _ = pk.ToByteArray()
 	}
-	llSigner := &llsig.BlsMultiSigner{Hasher: hasher}
-
-	multiSigner, _ := multisig.NewBLSMultisig(llSigner, kg)
-
-	return privKeys, pubKeys, multiSigner
+	return kg, privKeys, pubKeys
 }
 
 func createSignaturesShares(privKeys [][]byte, multiSigner crypto.MultiSigner, message []byte) [][]byte {
@@ -58,7 +75,7 @@ func TestMultiSig_Bls(t *testing.T) {
 	hasher, _ := blake2b.NewBlake2bWithSize(hashSize)
 	suite := mcl.NewSuiteBLS12()
 
-	privKeys, pubKeys, multiSigner := createKeysAndMultiSignerBls(numSigners, hasher, suite)
+	privKeys, pubKeys, multiSigner := createKeysAndMultiSignerBlsNoKOSK(numSigners, hasher, suite)
 
 	numOfTimesToRepeatTests := 100
 	for currentIdx := 0; currentIdx < numOfTimesToRepeatTests; currentIdx++ {
@@ -72,4 +89,120 @@ func TestMultiSig_Bls(t *testing.T) {
 		err = multiSigner.VerifyAggregatedSig(pubKeys, []byte(message), aggSig)
 		assert.Nil(t, err)
 	}
+}
+
+type multiSignerSetup struct {
+	privKeys          [][]byte
+	pubKeys           [][]byte
+	partialSignatures [][][]byte
+	messages          []string
+	aggSignatures     [][]byte
+}
+
+func createMultiSigSetupKOSK(numSigners uint16, numMessages int) (*multiSignerSetup, crypto.MultiSigner) {
+	var multiSigner crypto.MultiSigner
+	setup := &multiSignerSetup{}
+	suite := mcl.NewSuiteBLS12()
+	setup.privKeys, setup.pubKeys, multiSigner = createKeysAndMultiSignerBlsKOSK(numSigners, suite)
+	setup.messages, setup.partialSignatures = createMessagesAndPartialSignatures(numMessages, setup.privKeys, multiSigner)
+
+	return setup, multiSigner
+}
+
+func createMessagesAndPartialSignatures(numMessages int, privKeys [][]byte, multiSigner crypto.MultiSigner) ([]string, [][][]byte) {
+	partialSignatures := make([][][]byte, numMessages)
+	messages := make([]string, numMessages)
+
+	for i := 0; i < numMessages; i++ {
+		messages[i] = fmt.Sprintf("message%d", i)
+		signatures := createSignaturesShares(privKeys, multiSigner, []byte(messages[i]))
+		partialSignatures[i] = signatures
+	}
+
+	return messages, partialSignatures
+}
+
+func aggregateSignatures(
+	setup *multiSignerSetup,
+	multiSigner crypto.MultiSigner,
+) [][]byte {
+	aggSignatures := make([][]byte, len(setup.messages))
+	for i := 0; i < len(setup.messages); i++ {
+		aggSignatures[i], _ = multiSigner.AggregateSigs(setup.pubKeys, setup.partialSignatures[i])
+	}
+
+	return aggSignatures
+}
+
+const metaConsensusSize = uint16(400)
+const shardConsensusSize = uint16(63)
+func Benchmark_MultiSigForMetachain(b *testing.B) {
+	numSigners := metaConsensusSize
+	numMessages := 50
+	benchmarkMultiSigners(b, numSigners, numMessages)
+}
+
+func Benchmark_MultiSigForShard(b *testing.B) {
+	numSigners := shardConsensusSize
+	numMessages := 50
+	benchmarkMultiSigners(b, numSigners, numMessages)
+}
+
+func benchmarkMultiSigners(b *testing.B, numSigners uint16, numMessages int) {
+	setupKOSK, multiSignerKOSK := createMultiSigSetupKOSK(numSigners, numMessages)
+	multiSignerNoKOSK := createMultiSignerNoKOSK()
+
+	// signature shares, public keys and messages should be the same for both multi signers
+	setupNoKOSK := *setupKOSK
+	setupNoKOSK.aggSignatures = aggregateSignatures(&setupNoKOSK, multiSignerNoKOSK)
+	setupKOSK.aggSignatures = aggregateSignatures(setupKOSK, multiSignerKOSK)
+
+	b.Run("AggregateSigs KOSK", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_, _ = multiSignerKOSK.AggregateSigs(setupKOSK.pubKeys, setupKOSK.partialSignatures[i%numMessages])
+		}
+	})
+	b.Run("VerifyAggregatedSig KOSK", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			err := multiSignerKOSK.VerifyAggregatedSig(setupKOSK.pubKeys, []byte(setupKOSK.messages[i%numMessages]), setupKOSK.aggSignatures[i%numMessages])
+			require.Nil(b, err)
+		}
+	})
+	b.Run("Verify signature shares KOSK", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			for j := 0; j < int(numSigners); j++ {
+				err := multiSignerKOSK.VerifySignatureShare(setupKOSK.pubKeys[j], []byte(setupKOSK.messages[i%numMessages]), setupKOSK.partialSignatures[i%numMessages][j])
+				require.Nil(b, err)
+			}
+		}
+	})
+	b.Run("AggregateSigs NoKOSK", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_, _ = multiSignerNoKOSK.AggregateSigs(setupNoKOSK.pubKeys, setupNoKOSK.partialSignatures[i%numMessages])
+		}
+	})
+	b.Run("VerifyAggregatedSig NoKOSK", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			err := multiSignerNoKOSK.VerifyAggregatedSig(setupNoKOSK.pubKeys, []byte(setupNoKOSK.messages[i%numMessages]), setupNoKOSK.aggSignatures[i%numMessages])
+			require.Nil(b, err)
+		}
+	})
+	b.Run("Verify signature shares NoKOSK", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			for j := 0; j < int(numSigners); j++ {
+				err := multiSignerNoKOSK.VerifySignatureShare(setupNoKOSK.pubKeys[j], []byte(setupNoKOSK.messages[i%numMessages]), setupNoKOSK.partialSignatures[i%numMessages][j])
+				require.Nil(b, err)
+			}
+		}
+	})
+}
+
+func createMultiSignerNoKOSK() crypto.MultiSigner {
+	suite := mcl.NewSuiteBLS12()
+	kg := signing.NewKeyGenerator(suite)
+	hashSize := hashing.BlsHashSize
+	hasher, _ := blake2b.NewBlake2bWithSize(hashSize)
+	llSigner := &llsig.BlsMultiSigner{Hasher: hasher}
+	multiSignerNoKOSK, _ := multisig.NewBLSMultisig(llSigner, kg)
+	return multiSignerNoKOSK
 }
