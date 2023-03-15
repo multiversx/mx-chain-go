1111c0030d719452a5278f786a861c9eb183fbb8
Sorin Stanculeanu <sorinstanculeanu26@gmail.com>
2023-03-15 14:51:25 +0200

    update mx-chain-p2p-go and added more test logs

--- dataRetriever/topicSender/topicRequestSender.go
+++ dataRetriever/topicSender/topicRequestSender.go
@@ -193,6 +193,7 @@ func (trs *topicRequestSender) sendOnTopic(
 		if err != nil {
 			continue
 		}
+		log.Debug("testing- sending request to peer", "self", trs.messenger.ID().Pretty(), "peer", peer.Pretty(), "topic", topicToSendRequest)
 		trs.peersRatingHandler.DecreaseRating(peer)
 
 		logData = append(logData, peerType)
