2ad690e2e0fc0520f3986a3b6086f1b0da6a3696
jules01 <iulian.pascalau@gmail.com>
2023-03-23 18:14:46 +0200

    - changed log level

--- trie/node.go
+++ trie/node.go
@@ -120,7 +120,7 @@ func computeAndSetNodeHash(n node) ([]byte, error) {
 func getNodeFromDBAndDecode(n []byte, db common.DBWriteCacher, marshalizer marshal.Marshalizer, hasher hashing.Hasher) (node, error) {
 	encChild, err := db.Get(n)
 	if err != nil {
-		log.Trace(common.GetNodeFromDBErrorString, "error", err, "key", n, "stack trace", string(debug.Stack()))
+		log.Warn(common.GetNodeFromDBErrorString, "error", err, "key", n, "stack trace", string(debug.Stack()))
 		return nil, fmt.Errorf(common.GetNodeFromDBErrorString+" %w for key %v", err, hex.EncodeToString(n))
 	}
 
