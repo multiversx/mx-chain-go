005e2d4bdc452accb6c64cbab6cd6d0ea322b8e1
AdoAdoAdo <anaridado@gmail.com>
2023-04-04 17:43:33 +0300

    fix activation epoch in test

--- config/tomlConfig_test.go
+++ config/tomlConfig_test.go
@@ -690,7 +690,7 @@ func TestEnableEpochConfig(t *testing.T) {
     RuntimeMemStoreLimitEnableEpoch = 63
 
 	# SetGuardianEnableEpoch represents the epoch when guard account feature is enabled
-	SetGuardianEnableEpoch = 10
+	SetGuardianEnableEpoch = 64
 
     # MaxNodesChangeEnableEpoch holds configuration for changing the maximum number of nodes and the enabling epoch
     MaxNodesChangeEnableEpoch = [
@@ -787,7 +787,7 @@ func TestEnableEpochConfig(t *testing.T) {
 			AlwaysSaveTokenMetaDataEnableEpoch:          61,
 			RuntimeCodeSizeFixEnableEpoch:               62,
 			RuntimeMemStoreLimitEnableEpoch:             63,
-			SetGuardianEnableEpoch:                      10,
+			SetGuardianEnableEpoch:                      64,
 			BLSMultiSignerEnableEpoch: []MultiSignerConfig{
 				{
 					EnableEpoch: 0,
