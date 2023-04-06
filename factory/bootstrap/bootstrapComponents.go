2e1456a02acc1645e66d96e2723117f8db180ff0
Sorin Stanculeanu <sorinstanculeanu26@gmail.com>
2023-04-06 13:03:39 +0300

    more fixes after merge

--- factory/bootstrap/bootstrapComponents.go
+++ factory/bootstrap/bootstrapComponents.go
@@ -162,12 +162,15 @@ func (bcf *bootstrapComponentsFactory) Create() (*bootstrapComponents, error) {
 		return nil, err
 	}
 
-	unitOpener := createUnitOpener(
+	unitOpener, err := createUnitOpener(
 		bootstrapDataProvider,
 		latestStorageDataProvider,
 		storage.DefaultEpochString,
 		storage.DefaultShardString,
 	)
+	if err != nil {
+		return nil, err
+	}
 
 	dataSyncerFactory := bootstrap.NewScheduledDataSyncerFactory()
 
@@ -334,7 +337,7 @@ func createUnitOpener(
 	latestDataFromStorageProvider storage.LatestStorageDataProviderHandler,
 	defaultEpochString string,
 	defaultShardString string,
-) storage.UnitOpenerHandler {
+) (storage.UnitOpenerHandler, error) {
 	argsStorageUnitOpener := storageFactory.ArgsNewOpenStorageUnits{
 		BootstrapDataProvider:     bootstrapDataProvider,
 		LatestStorageDataProvider: latestDataFromStorageProvider,
