5fb4112ed28ddf4f437cde3e1a744b762682f2cf
jules01 <iulian.pascalau@gmail.com>
2023-05-24 16:52:21 +0300

    - added semi-integration test

--- integrationTests/vm/txsFee/relayedMoveBalance_test.go
+++ integrationTests/vm/txsFee/relayedMoveBalance_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/multiversx/mx-chain-go/integrationTests/vm/txsFee/utils"
 	"github.com/multiversx/mx-chain-go/process"
 	vmcommon "github.com/multiversx/mx-chain-vm-common-go"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -153,4 +154,33 @@ func TestRelayedMoveBalanceInvalidUserTxValueShouldConsumeGas(t *testing.T) {
 	// check accumulated fees
 	accumulatedFees := testContext.TxFeeHandler.GetAccumulatedFees()
 	require.Equal(t, big.NewInt(275), accumulatedFees)
+}
+
+func TestRelayedMoveBalanceInvalidNonce(t *testing.T) {
+	testContext, err := vm.CreatePreparedTxProcessorWithVMs(config.EnableEpochs{})
+	require.Nil(t, err)
+	defer testContext.Close()
+
+	relayerAddr := []byte("12345678901234567890123456789033")
+	sndAddr := []byte("12345678901234567890123456789012")
+	rcvAddr := []byte("12345678901234567890123456789022")
+
+	_, _ = vm.CreateAccount(testContext.Accounts, sndAddr, 0, big.NewInt(0))
+	userTx := vm.CreateTransaction(100, big.NewInt(150), sndAddr, rcvAddr, 1, 100, []byte("aaaa"))
+
+	_, _ = vm.CreateAccount(testContext.Accounts, relayerAddr, 0, big.NewInt(3000))
+
+	rtxData := utils.PrepareRelayerTxData(userTx)
+	rTxGasLimit := 1 + userTx.GasLimit + uint64(len(rtxData))
+	rtx := vm.CreateTransaction(0, big.NewInt(100), relayerAddr, sndAddr, 1, rTxGasLimit, rtxData)
+
+	retCode, _ := testContext.TxProcessor.ProcessTransaction(rtx)
+	require.Equal(t, vmcommon.UserError, retCode)
+
+	_, err = testContext.Accounts.Commit()
+	require.Nil(t, err)
+
+	senderAccount, _ := testContext.Accounts.GetExistingAccount(sndAddr)
+	require.NotNil(t, senderAccount)
+	assert.Equal(t, uint64(0), senderAccount.GetNonce())
 }
