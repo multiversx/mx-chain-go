5f5fecea19fb31f9040ea973a3c28f4e4ce8f388
Iuga Mihai <mihai9625@gmail.com>
2023-05-25 15:46:50 +0300

    fixes after review

--- outport/process/transactionsfee/dataHolders_test.go
+++ outport/process/transactionsfee/dataHolders_test.go
@@ -14,13 +14,13 @@ import (
 func TestTransactionsAndScrsHolder(t *testing.T) {
 	t.Parallel()
 
-	txHash := "txHash"
-	scrHash1 := "scrHash1"
-	scrHash2 := "scrHash2"
-	scrHash3 := "scrHash3"
+	txHash := []byte("txHash")
+	scrHash1 := []byte("scrHash1")
+	scrHash2 := []byte("scrHash2")
+	scrHash3 := []byte("scrHash3")
 	pool := &outportcore.TransactionPool{
 		Transactions: map[string]*outportcore.TxInfo{
-			hex.EncodeToString([]byte(txHash)): {
+			hex.EncodeToString(txHash): {
 				Transaction: &transaction.Transaction{
 					Nonce: 1,
 				},
@@ -30,26 +30,26 @@ func TestTransactionsAndScrsHolder(t *testing.T) {
 			},
 		},
 		SmartContractResults: map[string]*outportcore.SCRInfo{
-			hex.EncodeToString([]byte(scrHash1)): {
+			hex.EncodeToString(scrHash1): {
 				SmartContractResult: &smartContractResult.SmartContractResult{
 					Nonce:          2,
-					OriginalTxHash: []byte(txHash),
+					OriginalTxHash: txHash,
 				},
 				FeeInfo: &outportcore.FeeInfo{
 					Fee: big.NewInt(0),
 				},
 			},
 
-			hex.EncodeToString([]byte(scrHash2)): {
+			hex.EncodeToString(scrHash2): {
 				SmartContractResult: &smartContractResult.SmartContractResult{},
 				FeeInfo: &outportcore.FeeInfo{
 					Fee: big.NewInt(0),
 				},
 			},
-			hex.EncodeToString([]byte(scrHash3)): {
+			hex.EncodeToString(scrHash3): {
 				SmartContractResult: &smartContractResult.SmartContractResult{
 					Nonce:          3,
-					OriginalTxHash: []byte(txHash),
+					OriginalTxHash: txHash,
 				},
 				FeeInfo: &outportcore.FeeInfo{
 					Fee: big.NewInt(0),
@@ -59,7 +59,7 @@ func TestTransactionsAndScrsHolder(t *testing.T) {
 		Logs: []*outportcore.LogData{
 			{
 				Log:    &transaction.Log{Address: []byte("addr")},
-				TxHash: hex.EncodeToString([]byte(txHash)),
+				TxHash: hex.EncodeToString(txHash),
 			},
 			{
 				Log:    &transaction.Log{},
@@ -71,7 +71,7 @@ func TestTransactionsAndScrsHolder(t *testing.T) {
 	res := prepareTransactionsAndScrs(pool)
 	require.NotNil(t, res)
 	require.Equal(t, 1, len(res.txsWithResults))
-	require.Equal(t, 2, len(res.txsWithResults[hex.EncodeToString([]byte(txHash))].scrs))
-	require.NotNil(t, res.txsWithResults[hex.EncodeToString([]byte(txHash))].log)
+	require.Equal(t, 2, len(res.txsWithResults[hex.EncodeToString(txHash)].scrs))
+	require.NotNil(t, res.txsWithResults[hex.EncodeToString(txHash)].log)
 	require.Equal(t, 1, len(res.scrsNoTx))
 }
