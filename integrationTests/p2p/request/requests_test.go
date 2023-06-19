5136c6bc2f1b8b3fc5d2d368761003db0250bcdd
jules01 <iulian.pascalau@gmail.com>
2023-06-19 18:20:28 +0300

    - integration test changes

--- integrationTests/p2p/request/requests_test.go
+++ integrationTests/p2p/request/requests_test.go
@@ -1,8 +1,6 @@
 package request
 
 import (
-	"fmt"
-	"math/big"
 	"testing"
 	"time"
 
@@ -13,7 +11,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestRequestsInShardingEnvironment(t *testing.T) {
+func TestRequestsInShardingEnvironmentWithConnectNewNode(t *testing.T) {
 	if testing.Short() {
 		t.Skip("this is not a short test")
 	}
@@ -36,69 +34,108 @@ func TestRequestsInShardingEnvironment(t *testing.T) {
 		}
 	}()
 
-	// metachain nodes will contain a dummy metablock in their caches
-	metablock := &block.MetaBlock{
-		Nonce:                  1,
-		Round:                  1,
-		TimeStamp:              uint64(time.Now().Unix()),
-		RootHash:               []byte("root hash"),
-		PubKeysBitmap:          []byte{0},
-		PrevHash:               []byte("prev hash"),
-		Signature:              []byte("signature"),
-		RandSeed:               []byte("rand seed"),
-		PrevRandSeed:           []byte("prev rand seed"),
-		ValidatorStatsRootHash: []byte("validator root hash"),
-		ChainID:                integrationTests.ChainID,
-		SoftwareVersion:        []byte("version"),
-		AccumulatedFees:        big.NewInt(0),
-		AccumulatedFeesInEpoch: big.NewInt(0),
-		DeveloperFees:          big.NewInt(0),
-		DevFeesInEpoch:         big.NewInt(0),
+	//// metachain nodes will contain a dummy metablock in their caches
+	//metablock := &block.MetaBlock{
+	//	Nonce:                  1,
+	//	Round:                  1,
+	//	TimeStamp:              uint64(time.Now().Unix()),
+	//	RootHash:               []byte("root hash"),
+	//	PubKeysBitmap:          []byte{0},
+	//	PrevHash:               []byte("prev hash"),
+	//	Signature:              []byte("signature"),
+	//	RandSeed:               []byte("rand seed"),
+	//	PrevRandSeed:           []byte("prev rand seed"),
+	//	ValidatorStatsRootHash: []byte("validator root hash"),
+	//	ChainID:                integrationTests.ChainID,
+	//	SoftwareVersion:        []byte("version"),
+	//	AccumulatedFees:        big.NewInt(0),
+	//	AccumulatedFeesInEpoch: big.NewInt(0),
+	//	DeveloperFees:          big.NewInt(0),
+	//	DevFeesInEpoch:         big.NewInt(0),
+	//}
+	mb := &block.MiniBlock{
+		TxHashes:        [][]byte{[]byte("tx hash1")},
+		ReceiverShardID: 1,
+		SenderShardID:   0,
+		Type:            0,
+		Reserved:        nil,
 	}
-	hash, err := core.CalculateHash(integrationTests.TestMarshalizer, integrationTests.TestHasher, metablock)
+	hash, err := core.CalculateHash(integrationTests.TestMarshalizer, integrationTests.TestHasher, mb)
 	require.Nil(t, err)
 
 	for _, node := range nodes {
-		if node.ShardCoordinator.SelfId() != core.MetachainShardId {
+		if node.ShardCoordinator.SelfId() != 0 {
 			continue
 		}
 
-		node.DataPool.Headers().AddHeader(hash, metablock)
+		node.DataPool.MiniBlocks().Put(hash, mb, 0)
 	}
 
-	// the shard nodes will request this metablock
+	// the shard nodes will request this header
 	var lastIdxMissing int
-	var ok bool
 	for i := 0; i < 10; i++ {
 		requestHashOnShardNodes(nodes, hash)
-		lastIdxMissing, ok = checkAllNodesHaveBlock(nodes, hash)
-		if ok {
-			return
+		lastIdxMissing = checkAllNodesHaveBlock(nodes, hash)
+		if lastIdxMissing == -1 {
+			break
 		}
 
 		time.Sleep(time.Second)
 	}
+	assert.Equal(t, -1, lastIdxMissing)
 
-	assert.Fail(t, fmt.Sprintf("node iwth index %d did not received the metablock hash %x", lastIdxMissing, hash))
+	args := integrationTests.ArgTestProcessorNode{
+		MaxShards:            uint32(numOfShards),
+		NodeShardId:          1,
+		TxSignPrivKeyShardId: 0,
+		EpochsConfig:         integrationTests.GetDefaultEnableEpochsConfig(),
+	}
+	newNode := integrationTests.NewTestProcessorNode(args)
+	defer newNode.Close()
+
+	allNodes := []integrationTests.Connectable{newNode}
+	for _, n := range nodes {
+		allNodes = append(allNodes, n)
+	}
+	integrationTests.ConnectNodes(allNodes)
+
+	onlyNewNodeList := []*integrationTests.TestProcessorNode{newNode}
+	integrationTests.DisplayAndStartNodes(onlyNewNodeList)
+
+	// the shard nodes will request this metablock
+	for i := 0; i < 10; i++ {
+		requestHashOnShardNodes(onlyNewNodeList, hash)
+		lastIdxMissing = checkAllNodesHaveBlock(onlyNewNodeList, hash)
+		if lastIdxMissing == -1 {
+			break
+		}
+
+		time.Sleep(time.Second)
+	}
+	assert.Equal(t, -1, lastIdxMissing)
 }
 
 func requestHashOnShardNodes(nodes []*integrationTests.TestProcessorNode, hash []byte) {
 	for _, node := range nodes {
-		if node.ShardCoordinator.SelfId() == core.MetachainShardId {
+		if node.ShardCoordinator.SelfId() != 1 {
 			continue
 		}
 
-		node.RequestHandler.RequestMetaHeader(hash)
+		node.RequestHandler.RequestMiniBlock(0, hash)
 	}
 }
 
-func checkAllNodesHaveBlock(nodes []*integrationTests.TestProcessorNode, hash []byte) (int, bool) {
+func checkAllNodesHaveBlock(nodes []*integrationTests.TestProcessorNode, hash []byte) int {
 	for idx, n := range nodes {
-		_, err := n.DataPool.Headers().GetHeaderByHash(hash)
-		if err != nil {
-			return idx, false
+		if (n.ShardCoordinator.SelfId() != 0) && (n.ShardCoordinator.SelfId() != 1) {
+			continue
+		}
+
+		_, ok := n.DataPool.MiniBlocks().Get(hash)
+		if !ok {
+			return idx
 		}
 	}
 
-	return 0, true
+	return -1
 }
