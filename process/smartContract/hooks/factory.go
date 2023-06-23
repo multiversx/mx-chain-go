a965561474ed4c1401721925aee6a93e996e9276
radu chis <radu.chis@elrond.com>
2023-06-23 11:22:57 +0300

    added BlockChainHookFactoryHandler

--- process/smartContract/hooks/factory.go
+++ process/smartContract/hooks/factory.go
@@ -2,21 +2,55 @@ package hooks
 
 import (
 	"fmt"
-
 	"github.com/multiversx/mx-chain-go/common"
 	customErrors "github.com/multiversx/mx-chain-go/errors"
 	"github.com/multiversx/mx-chain-go/process"
 )
 
+// BlockChainHookFactoryHandler defines the blockchain hook factory handler
+type BlockChainHookFactoryHandler interface {
+	CreateBlockChainHook(args ArgBlockChainHook) (process.BlockChainHookHandler, error)
+}
+
+// blockChainHookFactory - factory for blockchain hook chain run type normal
+type blockChainHookFactory struct {
+}
+
+// NewBlockChainHookFactory creates a new instance of blockChainHookFactory
+func NewBlockChainHookFactory() BlockChainHookFactoryHandler {
+	return &blockChainHookFactory{}
+}
+
+// CreateBlockChainHook creates a blockchain hook based on the chain run type normal
+func (bhf *blockChainHookFactory) CreateBlockChainHook(args ArgBlockChainHook) (process.BlockChainHookHandler, error) {
+	return NewBlockChainHookImpl(args)
+}
+
+// SovereignBlockChainHookFactory - factory for blockchain hook chain run type sovereign
+type SovereignBlockChainHookFactory struct {
+	blockChainHookFactory BlockChainHookFactoryHandler
+}
+
+// NewSovereignBlockChainHookFactory creates a new instance of SovereignBlockChainHookFactory
+func NewSovereignBlockChainHookFactory(blockChainHookFactory BlockChainHookFactoryHandler) BlockChainHookFactoryHandler {
+	return &SovereignBlockChainHookFactory{
+		blockChainHookFactory: blockChainHookFactory,
+	}
+}
+
+// CreateBlockChainHook creates a blockchain hook based on the chain run type sovereign
+func (bhf *SovereignBlockChainHookFactory) CreateBlockChainHook(args ArgBlockChainHook) (process.BlockChainHookHandler, error) {
+	bh, _ := NewBlockChainHookImpl(args)
+	return NewSovereignBlockChainHook(bh)
+}
+
 // CreateBlockChainHook creates a blockchain hook based on the chain run type (normal/sovereign)
 func CreateBlockChainHook(chainRunType common.ChainRunType, args ArgBlockChainHook) (process.BlockChainHookHandler, error) {
-	bh, err := NewBlockChainHookImpl(args)
-
 	switch chainRunType {
 	case common.ChainRunTypeRegular:
-		return bh, err
+		return NewBlockChainHookFactory().CreateBlockChainHook(args)
 	case common.ChainRunTypeSovereign:
-		return NewSovereignBlockChainHook(bh)
+		return NewSovereignBlockChainHookFactory(NewBlockChainHookFactory()).CreateBlockChainHook(args)
 	default:
 		return nil, fmt.Errorf("%w type %v", customErrors.ErrUnimplementedChainRunType, chainRunType)
 	}
