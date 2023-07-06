b3d3958ae6ebeca4ba57f3c1952cf80025f482e9
jules01 <iulian.pascalau@gmail.com>
2023-07-06 14:33:59 +0300

    - fixed race condition in test

--- process/transaction/shardProcess_test.go
+++ process/transaction/shardProcess_test.go
@@ -3300,8 +3300,8 @@ func TestTxProcessor_AddNonExecutableLog(t *testing.T) {
 		SndAddr: relayer,
 		RcvAddr: sender,
 	}
-	originalTxHash, err := core.CalculateHash(args.Marshalizer, args.Hasher, originalTx)
-	assert.Nil(t, err)
+	originalTxHash, errCalculateHash := core.CalculateHash(args.Marshalizer, args.Hasher, originalTx)
+	assert.Nil(t, errCalculateHash)
 
 	t.Run("not a non-executable error should not record log", func(t *testing.T) {
 		t.Parallel()
@@ -3315,7 +3315,7 @@ func TestTxProcessor_AddNonExecutableLog(t *testing.T) {
 			},
 		}
 		txProc, _ := txproc.NewTxProcessor(argsLocal)
-		err = txProc.AddNonExecutableLog(errors.New("random error"), originalTxHash, originalTx)
+		err := txProc.AddNonExecutableLog(errors.New("random error"), originalTxHash, originalTx)
 		assert.Nil(t, err)
 	})
 	t.Run("is non executable tx error should record log", func(t *testing.T) {
@@ -3340,7 +3340,7 @@ func TestTxProcessor_AddNonExecutableLog(t *testing.T) {
 		}
 
 		txProc, _ := txproc.NewTxProcessor(argsLocal)
-		err = txProc.AddNonExecutableLog(process.ErrLowerNonceInTransaction, originalTxHash, originalTx)
+		err := txProc.AddNonExecutableLog(process.ErrLowerNonceInTransaction, originalTxHash, originalTx)
 		assert.Nil(t, err)
 
 		err = txProc.AddNonExecutableLog(process.ErrHigherNonceInTransaction, originalTxHash, originalTx)
