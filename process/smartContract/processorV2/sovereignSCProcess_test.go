bf0a6bc0f0ddde605bf37d9369c98820dcdb36ef
MariusC <marius.ciucam@gmail.com>
2023-07-10 15:16:02 +0300

    CLN: Test 2

--- process/smartContract/processorV2/sovereignSCProcess_test.go
+++ process/smartContract/processorV2/sovereignSCProcess_test.go
@@ -1,24 +1,23 @@
 package processorV2
 
 import (
-	"bytes"
 	"encoding/hex"
 	"math/big"
+	"math/rand"
 	"sync"
 	"testing"
 
 	"github.com/multiversx/mx-chain-core-go/core"
-	"github.com/multiversx/mx-chain-core-go/data"
 	"github.com/multiversx/mx-chain-core-go/data/esdt"
 	"github.com/multiversx/mx-chain-core-go/data/smartContractResult"
 	"github.com/multiversx/mx-chain-core-go/data/typeConverters/uint64ByteSlice"
-	"github.com/multiversx/mx-chain-core-go/hashing"
 	"github.com/multiversx/mx-chain-core-go/hashing/sha256"
 	"github.com/multiversx/mx-chain-core-go/marshal"
 	"github.com/multiversx/mx-chain-go/common"
 	"github.com/multiversx/mx-chain-go/common/forking"
 	"github.com/multiversx/mx-chain-go/config"
 	"github.com/multiversx/mx-chain-go/process"
+	"github.com/multiversx/mx-chain-go/process/coordinator"
 	"github.com/multiversx/mx-chain-go/process/mock"
 	"github.com/multiversx/mx-chain-go/process/smartContract"
 	"github.com/multiversx/mx-chain-go/process/smartContract/hooks"
@@ -28,9 +27,6 @@ import (
 	"github.com/multiversx/mx-chain-go/state/storagePruningManager"
 	"github.com/multiversx/mx-chain-go/state/storagePruningManager/evictionWaitingList"
 	"github.com/multiversx/mx-chain-go/state/syncer"
-	"github.com/multiversx/mx-chain-go/storage"
-	"github.com/multiversx/mx-chain-go/storage/database"
-	"github.com/multiversx/mx-chain-go/storage/storageunit"
 	"github.com/multiversx/mx-chain-go/storage/txcache"
 	"github.com/multiversx/mx-chain-go/testscommon"
 	"github.com/multiversx/mx-chain-go/testscommon/dataRetriever"
@@ -43,31 +39,71 @@ import (
 	"github.com/multiversx/mx-chain-go/trie"
 	vmcommon "github.com/multiversx/mx-chain-vm-common-go"
 	"github.com/multiversx/mx-chain-vm-common-go/builtInFunctions"
-	mock2 "github.com/multiversx/mx-chain-vm-common-go/mock"
+	mockVm "github.com/multiversx/mx-chain-vm-common-go/mock"
+	"github.com/multiversx/mx-chain-vm-common-go/parsers"
 	"github.com/stretchr/testify/require"
 )
 
-func createAccountsDB(
-	hasher hashing.Hasher,
-	marshaller marshal.Marshalizer,
-	enableEpochsHandler common.EnableEpochsHandler,
-) *state.AccountsDB {
+var (
+	esdtKeyPrefix = []byte(core.ProtectedKeyPrefix + core.ESDTKeyIdentifier)
+	sovHasher     = sha256.NewSha256()
+	sovMarshaller = &marshal.GogoProtoMarshalizer{}
+	sovPubKeyConv = createMockPubkeyConverter()
+	sovShardCoord = mock.NewMultiShardsCoordinatorMock(1)
+
+	sovEnableEpochsHandler = &enableEpochsHandlerMock.EnableEpochsHandlerStub{
+		IsSaveToSystemAccountFlagEnabledField:  true,
+		IsOptimizeNFTStoreFlagEnabledField:     true,
+		IsSendAlwaysFlagEnabledField:           true,
+		IsESDTNFTImprovementV1FlagEnabledField: true,
+	}
+)
+
+func createSovereignSmartContractProcessorArguments() scrCommon.ArgsNewSmartContractProcessor {
+	accAdapter := createAccountsDB()
+	esdtStorage := createESDTDataStorage(accAdapter)
+	builtInFuncContainer := createBuiltInFuncContainer(accAdapter, esdtStorage)
+	sovBlock := createSovBlockchainHook(accAdapter, builtInFuncContainer, esdtStorage)
+	txTypeHandler := createTxTypeHandler(builtInFuncContainer)
+
+	return scrCommon.ArgsNewSmartContractProcessor{
+		VmContainer:         &mock.VMContainerMock{},
+		ArgsParser:          smartContract.NewArgumentParser(),
+		Hasher:              &hashingMocks.HasherMock{},
+		Marshalizer:         &marshal.GogoProtoMarshalizer{},
+		AccountsDB:          accAdapter,
+		BlockChainHook:      sovBlock,
+		BuiltInFunctions:    builtInFuncContainer,
+		PubkeyConv:          sovPubKeyConv,
+		ShardCoordinator:    sovShardCoord,
+		ScrForwarder:        &mock.IntermediateTransactionHandlerMock{},
+		BadTxForwarder:      &mock.IntermediateTransactionHandlerMock{},
+		TxFeeHandler:        &mock.FeeAccumulatorStub{},
+		TxLogsProcessor:     &mock.TxLogsProcessorStub{},
+		EconomicsFee:        &economicsmocks.EconomicsHandlerStub{},
+		TxTypeHandler:       txTypeHandler,
+		GasHandler:          &testscommon.GasHandlerStub{},
+		EnableEpochsHandler: sovEnableEpochsHandler,
+		GasSchedule:         testscommon.NewGasScheduleNotifierMock(make(map[string]map[string]uint64)),
+		WasmVMChangeLocker:  &sync.RWMutex{},
+		VMOutputCacher:      txcache.NewDisabledCache(),
+	}
+}
+
+func createAccountsDB() *state.AccountsDB {
 	argsAccCreator := state.ArgsAccountCreation{
-		Hasher:              hasher,
-		Marshaller:          marshaller,
-		EnableEpochsHandler: &enableEpochsHandlerMock.EnableEpochsHandlerStub{},
+		Hasher:              sovHasher,
+		Marshaller:          sovMarshaller,
+		EnableEpochsHandler: sovEnableEpochsHandler,
 	}
 	accCreator, _ := factory.NewAccountCreator(argsAccCreator)
 
 	storageManagerArgs := storageMock.GetStorageManagerArgs()
-	storageManagerArgs.Marshalizer = marshaller
-	storageManagerArgs.Hasher = hasher
-	storageManagerArgs.MainStorer = createMemUnit()
-	storageManagerArgs.CheckpointsStorer = createMemUnit()
+	storageManagerArgs.Marshalizer = sovMarshaller
+	storageManagerArgs.Hasher = sovHasher
 
 	trieFactoryManager, _ := trie.CreateTrieStorageManager(storageManagerArgs, storageMock.GetStorageManagerOptions())
-
-	tr, _ := trie.NewTrie(trieFactoryManager, marshaller, hasher, enableEpochsHandler, 5)
+	tr, _ := trie.NewTrie(trieFactoryManager, sovMarshaller, sovHasher, sovEnableEpochsHandler, 5)
 	ewlArgs := evictionWaitingList.MemoryEvictionWaitingListArgs{
 		RootHashesSize: 100,
 		HashesSize:     10000,
@@ -77,8 +113,8 @@ func createAccountsDB(
 
 	args := state.ArgsAccountsDB{
 		Trie:                  tr,
-		Hasher:                hasher,
-		Marshaller:            marshaller,
+		Hasher:                sovHasher,
+		Marshaller:            sovMarshaller,
 		AccountFactory:        accCreator,
 		StoragePruningManager: spm,
 		ProcessingMode:        common.Normal,
@@ -90,85 +126,66 @@ func createAccountsDB(
 	return adb
 }
 
-func createMemUnit() storage.Storer {
-	capacity := uint32(10)
-	shards := uint32(1)
-	sizeInBytes := uint64(0)
-	cache, _ := storageunit.NewCache(storageunit.CacheConfig{Type: storageunit.LRUCache, Capacity: capacity, Shards: shards, SizeInBytes: sizeInBytes})
-	persist, _ := database.NewlruDB(100000)
-	unit, _ := storageunit.NewStorageUnit(cache, persist)
-
-	return unit
-}
-
-func createESDTDataStorage(marshaller marshal.Marshalizer, accountsDB state.AccountsAdapter) vmcommon.ESDTNFTStorageHandler {
+func createESDTDataStorage(accountsDB state.AccountsAdapter) vmcommon.ESDTNFTStorageHandler {
 	esdtDataStorage, _ := builtInFunctions.NewESDTDataStorage(builtInFunctions.ArgsNewESDTDataStorage{
 		Accounts:              accountsDB,
 		GlobalSettingsHandler: &testscommon.ESDTGlobalSettingsHandlerStub{},
-		Marshalizer:           marshaller,
-		EnableEpochsHandler: &enableEpochsHandlerMock.EnableEpochsHandlerStub{
-			IsSaveToSystemAccountFlagEnabledField:  true,
-			IsOptimizeNFTStoreFlagEnabledField:     true,
-			IsSendAlwaysFlagEnabledField:           true,
-			IsESDTNFTImprovementV1FlagEnabledField: true,
-		},
-		ShardCoordinator: mock.NewMultiShardsCoordinatorMock(1),
+		Marshalizer:           sovMarshaller,
+		EnableEpochsHandler:   sovEnableEpochsHandler,
+		ShardCoordinator:      sovShardCoord,
 	})
 
 	return esdtDataStorage
 }
 
-func createBuiltInFuncContainer(marshaller marshal.Marshalizer, accountsDB state.AccountsAdapter, esdtDataStorage vmcommon.ESDTNFTStorageHandler) vmcommon.BuiltInFunctionContainer {
+func createBuiltInFuncContainer(
+	accountsDB state.AccountsAdapter,
+	esdtDataStorage vmcommon.ESDTNFTStorageHandler,
+) vmcommon.BuiltInFunctionContainer {
 	builtInFuncContainer := builtInFunctions.NewBuiltInFunctionContainer()
 	esdtMultiTransfer, _ := builtInFunctions.NewESDTNFTMultiTransferFunc(
 		10,
-		marshaller,
-		&mock2.GlobalSettingsHandlerStub{},
+		sovMarshaller,
+		&mockVm.GlobalSettingsHandlerStub{},
 		accountsDB,
-		mock.NewMultiShardsCoordinatorMock(1),
+		sovShardCoord,
 		vmcommon.BaseOperationCost{},
-		&enableEpochsHandlerMock.EnableEpochsHandlerStub{},
-		&mock2.ESDTRoleHandlerStub{},
+		sovEnableEpochsHandler,
+		&mockVm.ESDTRoleHandlerStub{},
 		esdtDataStorage,
 	)
 
-	_ = esdtMultiTransfer.SetPayableChecker(&mock2.PayableHandlerStub{})
+	_ = esdtMultiTransfer.SetPayableChecker(&mockVm.PayableHandlerStub{})
 	_ = builtInFuncContainer.Add(core.BuiltInFunctionMultiESDTNFTTransfer, esdtMultiTransfer)
 
 	return builtInFuncContainer
 }
 
 func createSovBlockchainHook(
-	marshaller marshal.Marshalizer,
 	accountsDB state.AccountsAdapter,
+	builtInFuncContainer vmcommon.BuiltInFunctionContainer,
+	esdtStorage vmcommon.ESDTNFTStorageHandler,
 ) process.BlockChainHookHandler {
-	esdtStorage := createESDTDataStorage(marshaller, accountsDB)
-	builtInFuncContainer := createBuiltInFuncContainer(marshaller, accountsDB, esdtStorage)
-
 	args := hooks.ArgBlockChainHook{
-		Accounts:              accountsDB,
-		PubkeyConv:            createMockPubkeyConverter(),
-		StorageService:        genericMocks.NewChainStorerMock(0),
-		DataPool:              dataRetriever.NewPoolsHolderMock(),
-		BlockChain:            &testscommon.ChainHandlerMock{},
-		ShardCoordinator:      mock.NewMultiShardsCoordinatorMock(1),
-		Marshalizer:           marshaller,
-		Uint64Converter:       uint64ByteSlice.NewBigEndianConverter(),
-		BuiltInFunctions:      builtInFuncContainer,
-		NFTStorageHandler:     esdtStorage,
-		GlobalSettingsHandler: &testscommon.ESDTGlobalSettingsHandlerStub{},
-		CompiledSCPool:        &testscommon.TxCacherStub{},
-		ConfigSCStorage:       config.StorageConfig{},
-		EnableEpochs:          config.EnableEpochs{},
-		EpochNotifier:         forking.NewGenericEpochNotifier(),
-		EnableEpochsHandler:   &enableEpochsHandlerMock.EnableEpochsHandlerStub{},
-		WorkingDir:            "",
-		NilCompiledSCStore:    true,
-		GasSchedule: &testscommon.GasScheduleNotifierMock{
-			LatestGasScheduleCalled: func() map[string]map[string]uint64 {
-				return map[string]map[string]uint64{}
-			},
-		},
+		Accounts:                 accountsDB,
+		PubkeyConv:               sovPubKeyConv,
+		StorageService:           genericMocks.NewChainStorerMock(0),
+		DataPool:                 dataRetriever.NewPoolsHolderMock(),
+		BlockChain:               &testscommon.ChainHandlerMock{},
+		ShardCoordinator:         sovShardCoord,
+		Marshalizer:              sovMarshaller,
+		Uint64Converter:          uint64ByteSlice.NewBigEndianConverter(),
+		BuiltInFunctions:         builtInFuncContainer,
+		NFTStorageHandler:        esdtStorage,
+		GlobalSettingsHandler:    &testscommon.ESDTGlobalSettingsHandlerStub{},
+		CompiledSCPool:           &testscommon.TxCacherStub{},
+		ConfigSCStorage:          config.StorageConfig{},
+		EnableEpochs:             config.EnableEpochs{},
+		EpochNotifier:            forking.NewGenericEpochNotifier(),
+		EnableEpochsHandler:      sovEnableEpochsHandler,
+		WorkingDir:               "",
+		NilCompiledSCStore:       true,
+		GasSchedule:              testscommon.NewGasScheduleNotifierMock(make(map[string]map[string]uint64)),
 		Counter:                  &testscommon.BlockChainHookCounterStub{},
 		MissingTrieNodesNotifier: syncer.NewMissingTrieNodesNotifier(),
 	}
@@ -178,80 +195,43 @@ func createSovBlockchainHook(
 	return sovBlockchainHook
 }
 
-func createSovereignSmartContractProcessorArguments() scrCommon.ArgsNewSmartContractProcessor {
-	gasSchedule := make(map[string]map[string]uint64)
-	gasSchedule[common.BaseOpsAPICost] = make(map[string]uint64)
-	gasSchedule[common.BaseOpsAPICost][common.AsyncCallStepField] = 1000
-	gasSchedule[common.BaseOpsAPICost][common.AsyncCallbackGasLockField] = 3000
-	gasSchedule[common.BuiltInCost] = make(map[string]uint64)
-	gasSchedule[common.BuiltInCost][core.BuiltInFunctionESDTTransfer] = 2000
+func createTxTypeHandler(builtInFuncContainer vmcommon.BuiltInFunctionContainer) process.TxTypeHandler {
+	esdtParser, _ := parsers.NewESDTTransferParser(sovMarshaller)
+	txTypeHandler, _ := coordinator.NewTxTypeHandler(coordinator.ArgNewTxTypeHandler{
+		PubkeyConverter:     sovPubKeyConv,
+		ShardCoordinator:    sovShardCoord,
+		BuiltInFunctions:    builtInFuncContainer,
+		ArgumentParser:      parsers.NewCallArgsParser(),
+		ESDTTransferParser:  esdtParser,
+		EnableEpochsHandler: sovEnableEpochsHandler,
+	})
 
-	hasher := sha256.NewSha256()
-	marshaller := &marshal.GogoProtoMarshalizer{}
-	accAdapter := createAccountsDB(hasher, marshaller, &enableEpochsHandlerMock.EnableEpochsHandlerStub{})
-	sovBlock := createSovBlockchainHook(marshaller, accAdapter)
-
-	return scrCommon.ArgsNewSmartContractProcessor{
-		VmContainer:      &mock.VMContainerMock{},
-		ArgsParser:       smartContract.NewArgumentParser(),
-		Hasher:           &hashingMocks.HasherMock{},
-		Marshalizer:      &marshal.GogoProtoMarshalizer{},
-		AccountsDB:       accAdapter,
-		BlockChainHook:   sovBlock,
-		BuiltInFunctions: builtInFunctions.NewBuiltInFunctionContainer(),
-		PubkeyConv:       createMockPubkeyConverter(),
-		ShardCoordinator: mock.NewMultiShardsCoordinatorMock(1),
-		ScrForwarder:     &mock.IntermediateTransactionHandlerMock{},
-		BadTxForwarder:   &mock.IntermediateTransactionHandlerMock{},
-		TxFeeHandler:     &mock.FeeAccumulatorStub{},
-		TxLogsProcessor:  &mock.TxLogsProcessorStub{},
-		EconomicsFee: &economicsmocks.EconomicsHandlerStub{
-			DeveloperPercentageCalled: func() float64 {
-				return 0.0
-			},
-			ComputeTxFeeCalled: func(tx data.TransactionWithFeeHandler) *big.Int {
-				return core.SafeMul(tx.GetGasLimit(), tx.GetGasPrice())
-			},
-			ComputeFeeForProcessingCalled: func(tx data.TransactionWithFeeHandler, gasToUse uint64) *big.Int {
-				return core.SafeMul(tx.GetGasPrice(), gasToUse)
-			},
-		},
-		TxTypeHandler: &testscommon.TxTypeHandlerMock{},
-		GasHandler:    &testscommon.GasHandlerStub{},
-		EnableEpochsHandler: &enableEpochsHandlerMock.EnableEpochsHandlerStub{
-			IsSCDeployFlagEnabledField: true,
-		},
-		GasSchedule:        testscommon.NewGasScheduleNotifierMock(gasSchedule),
-		WasmVMChangeLocker: &sync.RWMutex{},
-		VMOutputCacher:     txcache.NewDisabledCache(),
-	}
+	return txTypeHandler
 }
 
-const baseESDTKeyPrefix = core.ProtectedKeyPrefix + core.ESDTKeyIdentifier
-
-var keyPrefix = []byte(baseESDTKeyPrefix)
-
-func computeESDTNFTTokenKey(esdtTokenKey []byte, nonce uint64) []byte {
-	return append(esdtTokenKey, big.NewInt(0).SetUint64(nonce).Bytes()...)
-}
-
-func testNFTTokenShouldExist(
-	tb testing.TB,
-	marshaller vmcommon.Marshalizer,
+func requireTokenExists(
+	t *testing.T,
 	account vmcommon.AccountHandler,
 	tokenName []byte,
 	nonce uint64,
 	expectedValue *big.Int,
 ) {
-	tokenId := append(keyPrefix, tokenName...)
+	tokenId := append(esdtKeyPrefix, tokenName...)
 	esdtNFTTokenKey := computeESDTNFTTokenKey(tokenId, nonce)
-	esdtData := &esdt.ESDigitalToken{Value: big.NewInt(0), Type: uint32(core.Fungible)}
-	marshaledData, _, _ := account.(vmcommon.UserAccountHandler).AccountDataHandler().RetrieveValue(esdtNFTTokenKey)
-	_ = marshaller.Unmarshal(esdtData, marshaledData)
-	require.Equal(tb, expectedValue, esdtData.Value)
+	esdtData := &esdt.ESDigitalToken{}
+	marshaledData, _, err := account.(vmcommon.UserAccountHandler).AccountDataHandler().RetrieveValue(esdtNFTTokenKey)
+	require.Nil(t, err)
+
+	err = sovMarshaller.Unmarshal(esdtData, marshaledData)
+	require.Nil(t, err)
+	require.Equal(t, expectedValue, esdtData.Value)
 }
 
-func createNFTMetaData(marshaller marshal.Marshalizer, value *big.Int, nonce uint64, creator []byte) ([]byte, error) {
+func computeESDTNFTTokenKey(esdtTokenKey []byte, nonce uint64) []byte {
+	return append(esdtTokenKey, big.NewInt(0).SetUint64(nonce).Bytes()...)
+}
+
+func createNFTMetaData(value *big.Int, nonce uint64, creator []byte) []byte {
 	esdtData := &esdt.ESDigitalToken{
 		Type:  uint32(core.NonFungible),
 		Value: value,
@@ -265,64 +245,53 @@ func createNFTMetaData(marshaller marshal.Marshalizer, value *big.Int, nonce uin
 		},
 	}
 
-	return marshaller.Marshal(esdtData)
+	nftMetaData, _ := sovMarshaller.Marshal(esdtData)
+	return nftMetaData
 }
 
-func TestSovereignSCProcessor_ProcessSmartContractResultExecuteSCIfMetaAndBuiltIn(t *testing.T) {
+func generateRandomBytes(len int) []byte {
+	randomBytes := make([]byte, len)
+	_, _ = rand.Read(randomBytes)
+	return randomBytes
+}
+
+func TestSovereignSCProcessor_ProcessSmartContractResultIncomingSCR(t *testing.T) {
 	t.Parallel()
 
-	scAddress := []byte("000000000001234567890123456789012")
-	dstScAddress := createAccount(scAddress)
-	dstScAddress.SetCode([]byte("code"))
-	shardCoordinator := mock.NewMultiShardsCoordinatorMock(1)
-	shardCoordinator.ComputeIdCalled = func(address []byte) uint32 {
-		if bytes.Equal(scAddress, address) {
-			return shardCoordinator.SelfId()
-		}
-		return 0
-	}
-	shardCoordinator.CurrentShard = core.MetachainShardId
-
 	arguments := createSovereignSmartContractProcessorArguments()
-	arguments.ShardCoordinator = shardCoordinator
-	arguments.VmContainer = &mock.VMContainerMock{
-		GetCalled: func(key []byte) (handler vmcommon.VMExecutionHandler, e error) {
-			return &mock.VMExecutionHandlerStub{
-				RunSmartContractCallCalled: func(input *vmcommon.ContractCallInput) (output *vmcommon.VMOutput, e error) {
-					return &vmcommon.VMOutput{ReturnCode: vmcommon.Ok}, nil
-				},
-			}, nil
-		},
-	}
-	arguments.TxTypeHandler = &testscommon.TxTypeHandlerMock{
-		ComputeTransactionTypeCalled: func(tx data.TransactionHandler) (process.TransactionType, process.TransactionType) {
-			return process.BuiltInFunctionCall, process.BuiltInFunctionCall
-		},
-	}
-	enableEpochsHandlerStub := &enableEpochsHandlerMock.EnableEpochsHandlerStub{}
-	arguments.EnableEpochsHandler = enableEpochsHandlerStub
-
 	sc, _ := NewSmartContractProcessorV2(arguments)
 	sovProc, _ := NewSovereignSCRProcessor(sc)
 
-	metaData, _ := createNFTMetaData(arguments.Marshalizer, big.NewInt(100), 4, scAddress)
-	dataScr := []byte("MultiESDTNFTTransfer@02@746f6b656e31@04@")
-	dataScr = append(dataScr, []byte(hex.EncodeToString(metaData))...)
-	dataScr = append(dataScr, []byte("@746f6b656e32@@32")...)
+	scAddress := generateRandomBytes(32)
+
+	token1 := []byte("token1")
+	nftTransferNonce := big.NewInt(4)
+	nftTransferValue := big.NewInt(100)
+	nftMetaData := createNFTMetaData(nftTransferValue, nftTransferNonce.Uint64(), scAddress)
+	transferNFT :=
+		hex.EncodeToString(token1) + "@" + // id
+			hex.EncodeToString(nftTransferNonce.Bytes()) + "@" + // nonce != 0
+			hex.EncodeToString(nftMetaData) + "@" // meta data
+
+	token2 := []byte("token2")
+	esdtTransferNonce := big.NewInt(0)
+	esdtTransferVal := big.NewInt(50)
+	transferESDT :=
+		hex.EncodeToString(token2) + "@" + // id
+			hex.EncodeToString(esdtTransferNonce.Bytes()) + "@" + // nonce = 0
+			hex.EncodeToString(esdtTransferVal.Bytes()) // value
 
 	scr := smartContractResult.SmartContractResult{
 		SndAddr: core.ESDTSCAddress,
 		RcvAddr: scAddress,
-		Data:    dataScr,
+		Data:    []byte("MultiESDTNFTTransfer@02@" + transferNFT + transferESDT),
 		Value:   big.NewInt(0),
 	}
 	_, err := sovProc.ProcessSmartContractResult(&scr)
 	require.Nil(t, err)
 
-	acc, _ := sovProc.accounts.LoadAccount(scAddress)
-
-	_, _ = arguments.AccountsDB.Commit()
-
-	testNFTTokenShouldExist(t, arguments.Marshalizer, acc, []byte("token1"), 4, big.NewInt(100))
-	testNFTTokenShouldExist(t, arguments.Marshalizer, acc, []byte("token2"), 0, big.NewInt(50))
+	acc, err := sovProc.accounts.LoadAccount(scAddress)
+	require.Nil(t, err)
+	requireTokenExists(t, acc, token1, nftTransferNonce.Uint64(), nftTransferValue)
+	requireTokenExists(t, acc, token2, esdtTransferNonce.Uint64(), esdtTransferVal)
 }
