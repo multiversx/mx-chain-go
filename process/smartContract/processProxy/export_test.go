b1ed14c18dced6170d7b619778cb51850b554a32
radu chis <radu.chis@multiversx.com>
2023-07-28 14:35:36 +0300

    removed scProcesorFactories

--- process/smartContract/processProxy/export_test.go
+++
@@ -1,9 +0,0 @@
-package processProxy
-
-import (
-	"github.com/multiversx/mx-chain-go/process/smartContract/scrCommon"
-)
-
-func CreateMockSmartContractProcessorArguments() scrCommon.ArgsNewSmartContractProcessor {
-	return createMockSmartContractProcessorArguments()
-}
--- process/smartContract/processorV2/export_test.go
+++
@@ -1,7 +0,0 @@
-package processorV2
-
-import "github.com/multiversx/mx-chain-go/process/smartContract/scrCommon"
-
-func CreateMockSmartContractProcessorArguments() scrCommon.ArgsNewSmartContractProcessor {
-	return createMockSmartContractProcessorArguments()
-}
