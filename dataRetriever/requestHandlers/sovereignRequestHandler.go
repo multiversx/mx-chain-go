f35c2d236c20c28338b98c08e891d846ebc844f0
MariusC <marius.ciucam@gmail.com>
2023-08-29 11:28:44 +0300

    First commit

--- dataRetriever/requestHandlers/sovereignRequestHandler.go
+++ dataRetriever/requestHandlers/sovereignRequestHandler.go
@@ -21,10 +21,12 @@ func NewSovereignResolverRequestHandler(resolverRequestHandler *resolverRequestH
 
 // RequestExtendedShardHeaderByNonce method asks for extended shard header from the connected peers by nonce
 func (srrh *sovereignResolverRequestHandler) RequestExtendedShardHeaderByNonce(_ uint64) {
+	log.Error("RequestExtendedShardHeaderByNonce")
 	//TODO: This method should be implemented for sovereign chain
 }
 
 // RequestExtendedShardHeader method asks for extended shard header from the connected peers by nonce
 func (srrh *sovereignResolverRequestHandler) RequestExtendedShardHeader(_ []byte) {
+	log.Error("RequestExtendedShardHeader")
 	//TODO: This method should be implemented for sovereign chain
 }
