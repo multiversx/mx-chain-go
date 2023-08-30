84eec472d81e92a8fe23be1203bc1042be065fc5
MariusC <marius.ciucam@gmail.com>
2023-08-30 11:55:13 +0300

    FEAT: Some unworking version

--- dataRetriever/requestHandlers/sovereignRequestHandler.go
+++ dataRetriever/requestHandlers/sovereignRequestHandler.go
@@ -1,6 +1,12 @@
 package requestHandlers
 
-import "github.com/multiversx/mx-chain-go/process"
+import (
+	"fmt"
+
+	"github.com/multiversx/mx-chain-go/dataRetriever"
+	"github.com/multiversx/mx-chain-go/process"
+	"github.com/multiversx/mx-chain-go/process/factory"
+)
 
 type sovereignResolverRequestHandler struct {
 	*resolverRequestHandler
@@ -20,9 +26,67 @@ func NewSovereignResolverRequestHandler(resolverRequestHandler *resolverRequestH
 }
 
 // RequestExtendedShardHeaderByNonce method asks for extended shard header from the connected peers by nonce
-func (srrh *sovereignResolverRequestHandler) RequestExtendedShardHeaderByNonce(_ uint64) {
-	log.Error("RequestExtendedShardHeaderByNonce")
+func (srrh *sovereignResolverRequestHandler) RequestExtendedShardHeaderByNonce(nonce uint64) {
+	log.Error("RequestExtendedShardHeaderByNonce", "nonce", nonce)
+
+	suffix := fmt.Sprintf("%s_%d", uniqueHeadersSuffix, srrh.shardID)
+	key := []byte(fmt.Sprintf("%d-%d", srrh.shardID, nonce))
+	if !srrh.testIfRequestIsNeeded(key, suffix) {
+		return
+	}
+
+	log.Debug("requesting shard header by nonce from network",
+		"shard", srrh.shardID,
+		"nonce", nonce,
+	)
+
+	requester, err := srrh.getShardHeaderRequester(srrh.shardID)
+	if err != nil {
+		log.Error("RequestShardHeaderByNonce.getShardHeaderRequester",
+			"error", err.Error(),
+			"shard", srrh.shardID,
+		)
+		return
+	}
+
+	headerRequester, ok := requester.(NonceRequester)
+	if !ok {
+		log.Warn("wrong assertion type when creating header requester")
+		return
+	}
+
+	srrh.whiteList.Add([][]byte{key})
+
+	epoch := srrh.getEpoch()
+	err = headerRequester.RequestDataFromNonce(nonce, epoch)
+	if err != nil {
+		log.Debug("RequestShardHeaderByNonce.RequestDataFromNonce",
+			"error", err.Error(),
+			"epoch", epoch,
+			"nonce", nonce,
+		)
+		return
+	}
+
+	srrh.addRequestedItems([][]byte{key}, suffix)
+
 	//TODO: This method should be implemented for sovereign chain
+}
+
+func (srrh *sovereignResolverRequestHandler) getShardHeaderRequester(shardID uint32) (dataRetriever.Requester, error) {
+
+	headerRequester, err := srrh.requestersFinder.CrossShardRequester(factory.ShardBlocksTopic, shardID) // CrossShardRequester(factory.ShardBlocksTopic, shardID)
+	if err != nil {
+		err = fmt.Errorf("%w, topic: %s, current shard ID: %d, cross shard ID: %d",
+			err, factory.ShardBlocksTopic, srrh.shardID, shardID)
+
+		log.Warn("available requesters in container",
+			"requesters", srrh.requestersFinder.RequesterKeys(),
+		)
+		return nil, err
+	}
+
+	return headerRequester, nil
 }
 
 // RequestExtendedShardHeader method asks for extended shard header from the connected peers by nonce
