50392510359684f48349c9af39066d7a59a30f21
MariusC <marius.ciucam@gmail.com>
2023-08-31 13:03:59 +0300

    FEAT: Save incoming events on extended header

--- cmd/sovereignnode/incomingHeader/extendedHeaderProcessor.go
+++ cmd/sovereignnode/incomingHeader/extendedHeaderProcessor.go
@@ -4,6 +4,8 @@ import (
 	"github.com/multiversx/mx-chain-core-go/core"
 	"github.com/multiversx/mx-chain-core-go/data"
 	"github.com/multiversx/mx-chain-core-go/data/block"
+	"github.com/multiversx/mx-chain-core-go/data/sovereign"
+	"github.com/multiversx/mx-chain-core-go/data/transaction"
 	"github.com/multiversx/mx-chain-core-go/hashing"
 	"github.com/multiversx/mx-chain-core-go/marshal"
 )
@@ -14,11 +16,36 @@ type extendedHeaderProcessor struct {
 	hasher      hashing.Hasher
 }
 
-func createExtendedHeader(headerV2 *block.HeaderV2, scrs []*scrInfo) *block.ShardHeaderExtended {
+func createExtendedHeader(incomingHeader sovereign.IncomingHeaderHandler, scrs []*scrInfo) (*block.ShardHeaderExtended, error) {
+	headerV2, castOk := incomingHeader.GetHeaderHandler().(*block.HeaderV2)
+	if !castOk {
+		return nil, errInvalidHeaderType
+	}
+	events, err := getEvents(incomingHeader.GetIncomingEventHandlers())
+	if err != nil {
+		return nil, err
+	}
+
 	return &block.ShardHeaderExtended{
 		Header:             headerV2,
 		IncomingMiniBlocks: createIncomingMb(scrs),
+		IncomingEvents:     events,
+	}, nil
+}
+
+func getEvents(events []data.EventHandler) ([]*transaction.Event, error) {
+	ret := make([]*transaction.Event, len(events))
+
+	for idx, eventHandler := range events {
+		event, castOk := eventHandler.(*transaction.Event)
+		if !castOk {
+			return nil, errInvalidEventType
+		}
+
+		ret[idx] = event
 	}
+
+	return ret, nil
 }
 
 func createIncomingMb(scrs []*scrInfo) []*block.MiniBlock {
