244d859183cf338134600c80778d96f6a7f72438
MariusC <marius.ciucam@gmail.com>
2023-09-01 11:13:08 +0300

    FEAT: Add first implementation for sovereignShardRequestersContainerFactory

--- dataRetriever/factory/requestersContainer/sovereignShardRequesterContainerFactory.go
+++ dataRetriever/factory/requestersContainer/sovereignShardRequesterContainerFactory.go
@@ -1,21 +1,67 @@
 package requesterscontainer
 
-import "github.com/multiversx/mx-chain-go/dataRetriever"
+import (
+	"github.com/multiversx/mx-chain-go/dataRetriever"
+	"github.com/multiversx/mx-chain-go/dataRetriever/requestHandlers/requesters"
+	"github.com/multiversx/mx-chain-go/process/factory"
+)
 
 // TODO: Implement this in MX-14516
 
 type sovereignShardRequestersContainerFactory struct {
-	*baseRequestersContainerFactory
+	*shardRequestersContainerFactory
 }
 
 // NewSovereignShardRequestersContainerFactory creates a new container filled with topic requesters for sovereign shards
-func NewSovereignShardRequestersContainerFactory(_ FactoryArgs) (*sovereignShardRequestersContainerFactory, error) {
-	return &sovereignShardRequestersContainerFactory{}, nil
+func NewSovereignShardRequestersContainerFactory(args FactoryArgs) (*sovereignShardRequestersContainerFactory, error) {
+	shardRequester, err := NewShardRequestersContainerFactory(args)
+	if err != nil {
+		return nil, err
+	}
+
+	return &sovereignShardRequestersContainerFactory{
+		shardRequestersContainerFactory: shardRequester,
+	}, nil
 }
 
 // Create returns a requesters container that will hold all sovereign requesters
 func (srcf *sovereignShardRequestersContainerFactory) Create() (dataRetriever.RequestersContainer, error) {
+	_, err := srcf.shardRequestersContainerFactory.Create()
+	if err != nil {
+		return nil, err
+	}
+
+	err = srcf.generateExtendedShardHeaderRequesters()
+	if err != nil {
+		return nil, err
+	}
+
 	return nil, nil
+}
+
+func (srcf *sovereignShardRequestersContainerFactory) generateExtendedShardHeaderRequesters() error {
+	shardC := srcf.shardCoordinator
+
+	shardID := srcf.shardCoordinator.SelfId()
+	identifierHdr := factory.ExtendedHeaderProofTopic + shardC.CommunicationIdentifier(shardID)
+	requestSender, err := srcf.createOneRequestSenderWithSpecifiedNumRequests(identifierHdr, EmptyExcludePeersOnTopic, shardID, 0, srcf.numIntraShardPeers)
+	if err != nil {
+		return err
+	}
+
+	arg := requesters.ArgHeaderRequester{
+		ArgBaseRequester: requesters.ArgBaseRequester{
+			RequestSender: requestSender,
+			Marshaller:    srcf.marshaller,
+		},
+		NonceConverter: srcf.uint64ByteSliceConverter,
+	}
+	requester, err := requesters.NewHeaderRequester(arg)
+	if err != nil {
+		return err
+	}
+
+	return srcf.container.Add(identifierHdr, requester)
 }
 
 // IsInterfaceNil returns true if there is no value under the interface
