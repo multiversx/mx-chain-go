f201c6b9d50f21059282756f6b942de2349f84d7
MariusC <marius.ciucam@gmail.com>
2023-09-07 16:08:37 +0300

    FIX: Add missing comm

--- dataRetriever/factory/requestersContainer/interface.go
+++ dataRetriever/factory/requestersContainer/interface.go
@@ -4,6 +4,7 @@ import "github.com/multiversx/mx-chain-go/dataRetriever"
 
 // TODO: We should implement this for meta as well and use when nodeRunner will be split in shardRunner+metaRunner
 
+// RequesterContainerFactoryCreator should create requester container factories depending on the chain run type
 type RequesterContainerFactoryCreator interface {
 	CreateRequesterContainerFactory(args FactoryArgs) (dataRetriever.RequestersContainerFactory, error)
 	IsInterfaceNil() bool
