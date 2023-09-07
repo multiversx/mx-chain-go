0f676cea5c8db800bfb8008c51b6a1b3cab0271e
Sorin Stanculeanu <sorinstanculeanu26@gmail.com>
2023-09-07 11:11:45 +0300

    added missing import

--- process/coordinator/printDoubleTransactionsDetector_test.go
+++ process/coordinator/printDoubleTransactionsDetector_test.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"testing"
 
+	"github.com/multiversx/mx-chain-core-go/core"
 	"github.com/multiversx/mx-chain-core-go/core/check"
 	"github.com/multiversx/mx-chain-core-go/data/block"
 	"github.com/multiversx/mx-chain-go/common"
