f6b983ddeb2e988755a20aa7c2bf1bccf1f9e21b
MariusC <marius.ciucam@gmail.com>
2023-09-19 17:22:17 +0300

    FIX: Add missing comments

--- testscommon/sovereignInterceptedExtendedHeaderDataStub.go
+++ testscommon/sovereignInterceptedExtendedHeaderDataStub.go
@@ -2,12 +2,14 @@ package testscommon
 
 import "github.com/multiversx/mx-chain-core-go/data"
 
+// SovereignInterceptedExtendedHeaderDataStub -
 type SovereignInterceptedExtendedHeaderDataStub struct {
 	*InterceptedDataStub
 	HashCalled              func() []byte
 	GetExtendedHeaderCalled func() data.ShardHeaderExtendedHandler
 }
 
+// Hash -
 func (s *SovereignInterceptedExtendedHeaderDataStub) Hash() []byte {
 	if s.HashCalled != nil {
 		return s.HashCalled()
@@ -16,6 +18,7 @@ func (s *SovereignInterceptedExtendedHeaderDataStub) Hash() []byte {
 	return nil
 }
 
+// GetExtendedHeader -
 func (s *SovereignInterceptedExtendedHeaderDataStub) GetExtendedHeader() data.ShardHeaderExtendedHandler {
 	if s.GetExtendedHeaderCalled != nil {
 		return s.GetExtendedHeaderCalled()
