d5748350bb1c1b3a6faf27a6e6e73dda85fa29cc
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2023-10-05 18:10:56 +0300

    Fix a new set of tests.

--- integrationTests/vm/txsFee/multiShard/relayedScDeploy_test.go
+++ integrationTests/vm/txsFee/multiShard/relayedScDeploy_test.go
@@ -31,7 +31,7 @@ func TestRelayedSCDeployShouldWork(t *testing.T) {
 	require.Equal(t, uint32(1), testContextRelayer.ShardCoordinator.ComputeId(sndAddr))
 
 	gasPrice := uint64(10)
-	gasLimit := uint64(1000)
+	gasLimit := uint64(1960)
 
 	_, _ = vm.CreateAccount(testContextRelayer.Accounts, relayerAddr, 0, big.NewInt(50000))
 
@@ -51,12 +51,12 @@ func TestRelayedSCDeployShouldWork(t *testing.T) {
 	_, err = testContextRelayer.Accounts.Commit()
 	require.Nil(t, err)
 
-	expectedBalanceRelayer := big.NewInt(26930)
+	expectedBalanceRelayer := big.NewInt(2530)
 	utils.TestAccount(t, testContextRelayer.Accounts, relayerAddr, 1, expectedBalanceRelayer)
 
 	// check accumulated fees
 	accumulatedFees := testContextRelayer.TxFeeHandler.GetAccumulatedFees()
-	require.Equal(t, big.NewInt(13070), accumulatedFees)
+	require.Equal(t, big.NewInt(27870), accumulatedFees)
 
 	// execute on inner tx destination
 	retCode, err = testContextInner.TxProcessor.ProcessTransaction(rtx)
@@ -71,7 +71,7 @@ func TestRelayedSCDeployShouldWork(t *testing.T) {
 
 	// check accumulated fees
 	accumulatedFees = testContextInner.TxFeeHandler.GetAccumulatedFees()
-	require.Equal(t, big.NewInt(8490), accumulatedFees)
+	require.Equal(t, big.NewInt(19600), accumulatedFees)
 
 	txs := testContextInner.GetIntermediateTransactions(t)
 
