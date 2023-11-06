30d80d73aaea2c9ee5a6c6974ae0394421111f63
MariusC <marius.ciucam@gmail.com>
2023-11-06 14:58:21 +0200

    CLN: Start to remove subround pointer from extra sig comp

--- consensus/spos/bls/interface.go
+++ consensus/spos/bls/interface.go
@@ -1,9 +1,12 @@
 package bls
 
-import "github.com/multiversx/mx-chain-go/consensus"
+import (
+	"github.com/multiversx/mx-chain-core-go/data"
+	"github.com/multiversx/mx-chain-go/consensus"
+)
 
 type SubRoundExtraDataSignatureHandler interface {
-	CreateSignatureShare(selfIndex uint16) ([]byte, error)
+	CreateSignatureShare(header data.HeaderHandler, selfIndex uint16, selfPubKey []byte) ([]byte, error)
 	AddSigShareToConsensusMessage(sigShare []byte, cnsMsg *consensus.Message)
 	StoreSignatureShare(index uint16, cnsMsg *consensus.Message) error
 	Identifier() string
