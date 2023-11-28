5809526a1262a71bb635ecec44889f8626e21472
Adrian Dobrita <anaridado@gmail.com>
2023-11-28 12:31:47 +0200

    fix alignment

--- config/tomlConfig_test.go
+++ config/tomlConfig_test.go
@@ -839,7 +839,7 @@ func TestEnableEpochConfig(t *testing.T) {
     # FixGasRemainingForSaveKeyValueBuiltinFunctionEnableEpoch represents the epoch when the fix for the remaining gas in the SaveKeyValue builtin function is enabled
     FixGasRemainingForSaveKeyValueBuiltinFunctionEnableEpoch = 91
 
-	# CurrentRandomnessOnSortingEnableEpoch represents the epoch when the current randomness on sorting is enabled
+    # CurrentRandomnessOnSortingEnableEpoch represents the epoch when the current randomness on sorting is enabled
     CurrentRandomnessOnSortingEnableEpoch = 92
 
     # MaxNodesChangeEnableEpoch holds configuration for changing the maximum number of nodes and the enabling epoch
