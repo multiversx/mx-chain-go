f407a827d9d6fdf268a3d1b18d9f4e0303a4471e
axenteoctavian <axente.octavian@gmail.com>
2024-01-10 11:45:30 +0200

    fixes after review

--- node/nodeHelper.go
+++ node/nodeHelper.go
@@ -2,10 +2,10 @@ package node
 
 import (
 	"errors"
-	"github.com/multiversx/mx-chain-core-go/core/check"
 	"time"
 
 	"github.com/multiversx/mx-chain-core-go/core"
+	"github.com/multiversx/mx-chain-core-go/core/check"
 	"github.com/multiversx/mx-chain-go/common"
 	"github.com/multiversx/mx-chain-go/config"
 	globalErrors "github.com/multiversx/mx-chain-go/errors"
