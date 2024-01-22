52538cfba5506382a489918cab6a40493f8403eb
ssd04 <dariussuirab@gmail.com>
2024-01-22 21:20:36 +0200

    update integration test

--- integrationTests/state/stateTrie/removeCodeLeaf_test.go
+++ integrationTests/state/stateTrie/removeCodeLeaf_test.go
@@ -15,7 +15,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestAccountsDB_RevertDataStepByStepWithCommitsAccountDataWithMigratedCode(t *testing.T) {
+func TestAccountsDB_MigrateCodeLeaf(t *testing.T) {
 	t.Parallel()
 
 	t.Run("account with migrated code leaf activation, should save code data to storage directly", func(t *testing.T) {
@@ -50,8 +50,6 @@ func TestAccountsDB_RevertDataStepByStepWithCommitsAccountDataWithMigratedCode(t
 		snapshotMod := adb.JournalLen()
 
 		stateMock, err = adb.LoadAccount(adr)
-		require.Nil(t, err)
-		err = adb.SaveAccount(stateMock)
 		require.Nil(t, err)
 
 		codeData := []byte("codeData")
@@ -136,9 +134,6 @@ func TestAccountsDB_RevertDataStepByStepWithCommitsAccountDataWithMigratedCode(t
 
 		snapshotMod := adb.JournalLen()
 
-		err = adb.SaveAccount(stateMock1)
-		require.Nil(t, err)
-
 		testMigrateCodeLeaf(t, testContext, stateMock1.AddressBytes())
 
 		stateMock1, err = adb.LoadAccount(adr1)
@@ -146,6 +141,9 @@ func TestAccountsDB_RevertDataStepByStepWithCommitsAccountDataWithMigratedCode(t
 
 		accVersion := stateMock1.(state.UserAccountHandler).GetVersion()
 		require.Equal(t, uint8(core.WithoutCodeLeaf), accVersion)
+
+		accVersion2 := stateMock2.(state.UserAccountHandler).GetVersion()
+		require.Equal(t, uint8(core.NotSpecified), accVersion2)
 
 		// check code data saved to storage
 		val, err := trieStorage.Get(codeDataHash)
@@ -158,6 +156,10 @@ func TestAccountsDB_RevertDataStepByStepWithCommitsAccountDataWithMigratedCode(t
 		stateMock1, err = adb.LoadAccount(adr1)
 		require.Nil(t, err)
 
+		stateMock2, err = adb.LoadAccount(adr2)
+		require.Nil(t, err)
+
+		// revert will set old version back
 		accVersion = stateMock1.(state.UserAccountHandler).GetVersion()
 		require.Equal(t, uint8(core.NotSpecified), accVersion)
 
@@ -165,6 +167,106 @@ func TestAccountsDB_RevertDataStepByStepWithCommitsAccountDataWithMigratedCode(t
 		val, err = trieStorage.Get(codeDataHash)
 		require.Nil(t, err)
 		require.Equal(t, codeData, val)
+	})
+}
+
+func TestAccountsDB_MigrateCodeLeaf_UpdatesAfterMigration(t *testing.T) {
+	t.Parallel()
+
+	t.Run("account with migrated code leaf, update code after migration", func(t *testing.T) {
+		t.Parallel()
+
+		enableEpochs := config.EnableEpochs{
+			MigrateCodeLeafEnableEpoch: 0,
+		}
+
+		shardID := uint32(1)
+		shardCoordinator, _ := sharding.NewMultiShardCoordinator(3, shardID)
+		gasScheduleNotifier := vm.CreateMockGasScheduleNotifier()
+
+		storer := integrationTests.CreateMemUnit()
+		trieStorage, _ := integrationTests.CreateTrieStorageManager(storer)
+
+		testContext, err := vm.CreatePreparedTxProcessorWithVMsWithShardCoordinatorDBAndGas(enableEpochs, shardCoordinator, storer, gasScheduleNotifier)
+		require.Nil(t, err)
+		defer testContext.Close()
+
+		adr := integrationTests.CreateRandomAddress()
+		adr[31] = 1 // set address for shard 1
+
+		adb := testContext.Accounts
+
+		stateMock, err := adb.LoadAccount(adr)
+		require.Nil(t, err)
+
+		err = adb.SaveAccount(stateMock)
+		require.Nil(t, err)
+
+		stateMock, err = adb.LoadAccount(adr)
+		require.Nil(t, err)
+
+		codeData := []byte("codeData")
+		codeDataHash := integrationTests.TestHasher.Compute(string(codeData))
+		stateMock.(state.UserAccountHandler).SetCodeHash(codeDataHash)
+		stateMock.(state.UserAccountHandler).SetCode(codeData)
+
+		err = adb.SaveAccount(stateMock)
+		require.Nil(t, err)
+
+		testMigrateCodeLeaf(t, testContext, stateMock.AddressBytes())
+
+		stateMock, err = adb.LoadAccount(adr)
+		require.Nil(t, err)
+
+		accVersion := stateMock.(state.UserAccountHandler).GetVersion()
+		require.Equal(t, uint8(core.WithoutCodeLeaf), accVersion)
+
+		val, err := trieStorage.Get(codeDataHash)
+		require.Nil(t, err)
+		require.Equal(t, codeData, val)
+
+		snapshotMod := adb.JournalLen()
+
+		newCodeData := []byte("newCodeData")
+		newCodeDataHash := integrationTests.TestHasher.Compute(string(newCodeData))
+		stateMock.(state.UserAccountHandler).SetCodeHash(newCodeDataHash)
+		stateMock.(state.UserAccountHandler).SetCode(newCodeData)
+
+		err = adb.SaveAccount(stateMock)
+		require.Nil(t, err)
+
+		val, err = trieStorage.Get(newCodeDataHash)
+		require.Nil(t, err)
+		require.Equal(t, newCodeData, val)
+
+		stateMock, err = adb.LoadAccount(adr)
+		require.Nil(t, err)
+		require.Equal(t, newCodeDataHash, stateMock.(state.UserAccountHandler).GetCodeHash())
+
+		// old code data not deleted from storage
+		val, err = trieStorage.Get(codeDataHash)
+		require.Nil(t, err)
+		require.Equal(t, codeData, val)
+
+		// revert should reference old code from storage
+		err = adb.RevertToSnapshot(snapshotMod)
+		require.Nil(t, err)
+
+		stateMock, err = adb.LoadAccount(adr)
+		require.Nil(t, err)
+		require.Equal(t, codeDataHash, stateMock.(state.UserAccountHandler).GetCodeHash())
+
+		// remove account will not delete code from storage
+		err = adb.RemoveAccount(adr)
+		require.Nil(t, err)
+
+		val, err = trieStorage.Get(codeDataHash)
+		require.Nil(t, err)
+		require.Equal(t, codeData, val)
+
+		val, err = trieStorage.Get(newCodeDataHash)
+		require.Nil(t, err)
+		require.Equal(t, newCodeData, val)
 	})
 }
 
