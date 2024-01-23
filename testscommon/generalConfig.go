5fbacee0a736ab12b52a34d185052f13d2b34b2f
MariusC <marius.ciucam@gmail.com>
2024-01-23 11:53:16 +0200

    FIX: Unit tests

--- testscommon/generalConfig.go
+++ testscommon/generalConfig.go
@@ -424,6 +424,14 @@ func GetGeneralConfig() config.Config {
 					},
 				},
 			},
+			OutgoingSubscribedEvents: config.OutgoingSubscribedEvents{
+				SubscribedEvents: []config.SubscribedEvent{
+					{
+						Identifier: "bridgeOps",
+						Addresses:  []string{"erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th"},
+					},
+				},
+			},
 		},
 	}
 }
