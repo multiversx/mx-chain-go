1732dcba8ababc6f82ed9779ab6d8e753fa802f5
Iulian Pascalau <iulian.pascalau@gmail.com>
2024-02-13 08:57:53 +0200

    - fixed chain simulator config

--- node/chainSimulator/configs/configs.go
+++ node/chainSimulator/configs/configs.go
@@ -218,6 +218,7 @@ func generateValidatorsKeyAndUpdateFiles(
 
 	nodes.ConsensusGroupSize = 1
 	nodes.MetaChainConsensusGroupSize = 1
+	nodes.Hysteresis = 0
 
 	nodes.MinNodesPerShard = args.MinNodesPerShard
 	nodes.MetaChainMinNodes = args.MetaChainMinNodes
