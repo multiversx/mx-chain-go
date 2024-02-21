e8dd458f39467635779fc4bae5ec821ebefdf524
Iulian Pascalau <iulian.pascalau@gmail.com>
2024-02-21 10:08:53 +0200

    - refactored unit tests

--- epochStart/metachain/systemSCs_test.go
+++ epochStart/metachain/systemSCs_test.go
@@ -47,9 +47,12 @@ import (
 	"github.com/multiversx/mx-chain-go/testscommon/cryptoMocks"
 	dataRetrieverMock "github.com/multiversx/mx-chain-go/testscommon/dataRetriever"
 	"github.com/multiversx/mx-chain-go/testscommon/enableEpochsHandlerMock"
+	"github.com/multiversx/mx-chain-go/testscommon/epochNotifier"
 	"github.com/multiversx/mx-chain-go/testscommon/genesisMocks"
+	"github.com/multiversx/mx-chain-go/testscommon/marshallerMock"
 	"github.com/multiversx/mx-chain-go/testscommon/shardingMocks"
 	"github.com/multiversx/mx-chain-go/testscommon/stakingcommon"
+	stateMock "github.com/multiversx/mx-chain-go/testscommon/state"
 	statusHandlerMock "github.com/multiversx/mx-chain-go/testscommon/statusHandler"
 	storageMock "github.com/multiversx/mx-chain-go/testscommon/storage"
 	"github.com/multiversx/mx-chain-go/trie"
@@ -99,9 +102,27 @@ func createPhysicalUnit(t *testing.T) (storage.Storer, string) {
 	return unit, dir
 }
 
-type enableEpochHandlerWithEpochConfirm interface {
-	common.EnableEpochsHandler
-	core.EpochSubscriberHandler
+func createMockArgsForSystemSCProcessor() ArgsNewEpochStartSystemSCProcessing {
+	return ArgsNewEpochStartSystemSCProcessing{
+		SystemVM:                     &mock.VMExecutionHandlerStub{},
+		UserAccountsDB:               &stateMock.AccountsStub{},
+		PeerAccountsDB:               &stateMock.AccountsStub{},
+		Marshalizer:                  &marshallerMock.MarshalizerStub{},
+		StartRating:                  0,
+		ValidatorInfoCreator:         &testscommon.ValidatorStatisticsProcessorStub{},
+		ChanceComputer:               &mock.ChanceComputerStub{},
+		ShardCoordinator:             &testscommon.ShardsCoordinatorMock{},
+		EndOfEpochCallerAddress:      vm.EndOfEpochAddress,
+		StakingSCAddress:             vm.StakingSCAddress,
+		ESDTOwnerAddressBytes:        vm.ESDTSCAddress,
+		GenesisNodesConfig:           &genesisMocks.NodesSetupStub{},
+		EpochNotifier:                &epochNotifier.EpochNotifierStub{},
+		NodesConfigProvider:          &shardingMocks.NodesCoordinatorStub{},
+		StakingDataProvider:          &stakingcommon.StakingDataProviderStub{},
+		AuctionListSelector:          &stakingcommon.AuctionListSelectorStub{},
+		MaxNodesChangeConfigProvider: &testscommon.MaxNodesChangeConfigProviderStub{},
+		EnableEpochsHandler:          &enableEpochsHandlerMock.EnableEpochsHandlerStub{},
+	}
 }
 
 func TestNewSystemSCProcessor(t *testing.T) {
@@ -961,118 +982,127 @@ func createFullArgumentsForSystemSCProcessing(enableEpochsConfig config.EnableEp
 func TestSystemSCProcessor_ProcessSystemSmartContractInitDelegationMgr(t *testing.T) {
 	t.Parallel()
 
-	t.Run("flag not active - activation epoch is in the future", func(t *testing.T) {
-		args, _ := createFullArgumentsForSystemSCProcessing(config.EnableEpochs{
-			DelegationSmartContractEnableEpoch: 39,
-			StakingV2EnableEpoch:               1000,
-		}, testscommon.CreateMemUnit())
-		s, _ := NewSystemSCProcessor(args)
-		handler := s.enableEpochsHandler.(enableEpochHandlerWithEpochConfirm)
-		handler.EpochConfirmed(37, 0)
+	t.Run("flag not active", func(t *testing.T) {
+		args := createMockArgsForSystemSCProcessor()
+		args.EnableEpochsHandler = &enableEpochsHandlerMock.EnableEpochsHandlerStub{
+			IsFlagEnabledCalled: func(flag core.EnableEpochFlag) bool {
+				if flag == common.GovernanceFlagInSpecificEpochOnly ||
+					flag == common.StakingV4Step1Flag ||
+					flag == common.StakingV4Step2Flag ||
+					flag == common.SwitchHysteresisForMinNodesFlagInSpecificEpochOnly ||
+					flag == common.StakingV2OwnerFlagInSpecificEpochOnly ||
+					flag == common.CorrectLastUnJailedFlagInSpecificEpochOnly ||
+					flag == common.DelegationSmartContractFlagInSpecificEpochOnly ||
+					flag == common.CorrectLastUnJailedFlag ||
+					flag == common.SwitchJailWaitingFlag ||
+					flag == common.StakingV2Flag ||
+					flag == common.ESDTFlagInSpecificEpochOnly {
+
+					return false
+				}
+
+				return true
+			},
+		}
+		args.SystemVM = &mock.VMExecutionHandlerStub{
+			RunSmartContractCreateCalled: func(input *vmcommon.ContractCreateInput) (*vmcommon.VMOutput, error) {
+				assert.Fail(t, "should have not called")
+
+				return nil, fmt.Errorf("should have not called")
+			},
+		}
+		processor, _ := NewSystemSCProcessor(args)
+		require.NotNil(t, processor)
 
 		validatorsInfo := state.NewShardValidatorsInfoMap()
-		err := s.ProcessSystemSmartContract(validatorsInfo, &block.Header{})
+		err := processor.ProcessSystemSmartContract(validatorsInfo, &block.Header{})
 		assert.Nil(t, err)
-
-		acc, err := s.userAccountsDB.GetExistingAccount(vm.DelegationManagerSCAddress)
-		assert.NotNil(t, err)
-		assert.True(t, check.IfNil(acc))
 	})
-	t.Run("flag active in that specific epoch", func(t *testing.T) {
-		args, _ := createFullArgumentsForSystemSCProcessing(config.EnableEpochs{
-			DelegationSmartContractEnableEpoch: 37,
-			StakingV2EnableEpoch:               1000,
-		}, testscommon.CreateMemUnit())
-		s, _ := NewSystemSCProcessor(args)
-		handler := s.enableEpochsHandler.(enableEpochHandlerWithEpochConfirm)
-		handler.EpochConfirmed(37, 0)
+	t.Run("flag active", func(t *testing.T) {
+		args := createMockArgsForSystemSCProcessor()
+		args.EnableEpochsHandler = &enableEpochsHandlerMock.EnableEpochsHandlerStub{
+			IsFlagEnabledCalled: func(flag core.EnableEpochFlag) bool {
+				return flag == common.DelegationSmartContractFlagInSpecificEpochOnly
+			},
+		}
+		runSmartContractCreateCalled := false
+		args.SystemVM = &mock.VMExecutionHandlerStub{
+			RunSmartContractCreateCalled: func(input *vmcommon.ContractCreateInput) (*vmcommon.VMOutput, error) {
+				runSmartContractCreateCalled = true
+
+				return &vmcommon.VMOutput{}, nil
+			},
+		}
+		processor, _ := NewSystemSCProcessor(args)
+		require.NotNil(t, processor)
 
 		validatorsInfo := state.NewShardValidatorsInfoMap()
-		err := s.ProcessSystemSmartContract(validatorsInfo, &block.Header{})
+		err := processor.ProcessSystemSmartContract(validatorsInfo, &block.Header{})
 		assert.Nil(t, err)
-
-		acc, err := s.userAccountsDB.GetExistingAccount(vm.DelegationManagerSCAddress)
-		assert.Nil(t, err)
-
-		userAcc, _ := acc.(state.UserAccountHandler)
-		assert.Equal(t, userAcc.GetOwnerAddress(), vm.DelegationManagerSCAddress)
-		assert.NotNil(t, userAcc.GetCodeMetadata())
-	})
-	t.Run("flag not active - activation epoch is in the past", func(t *testing.T) {
-		args, _ := createFullArgumentsForSystemSCProcessing(config.EnableEpochs{
-			DelegationSmartContractEnableEpoch: 35,
-			StakingV2EnableEpoch:               1000,
-		}, testscommon.CreateMemUnit())
-		s, _ := NewSystemSCProcessor(args)
-		handler := s.enableEpochsHandler.(enableEpochHandlerWithEpochConfirm)
-		handler.EpochConfirmed(37, 0)
-
-		validatorsInfo := state.NewShardValidatorsInfoMap()
-		err := s.ProcessSystemSmartContract(validatorsInfo, &block.Header{})
-		assert.Nil(t, err)
-
-		acc, err := s.userAccountsDB.GetExistingAccount(vm.DelegationManagerSCAddress)
-		assert.NotNil(t, err)
-		assert.True(t, check.IfNil(acc))
+		assert.True(t, runSmartContractCreateCalled)
 	})
 }
 
 func TestSystemSCProcessor_ProcessSystemSmartContractInitGovernance(t *testing.T) {
 	t.Parallel()
+	t.Run("flag not active", func(t *testing.T) {
+		args := createMockArgsForSystemSCProcessor()
+		args.EnableEpochsHandler = &enableEpochsHandlerMock.EnableEpochsHandlerStub{
+			IsFlagEnabledCalled: func(flag core.EnableEpochFlag) bool {
+				if flag == common.GovernanceFlagInSpecificEpochOnly ||
+					flag == common.StakingV4Step1Flag ||
+					flag == common.StakingV4Step2Flag ||
+					flag == common.SwitchHysteresisForMinNodesFlagInSpecificEpochOnly ||
+					flag == common.StakingV2OwnerFlagInSpecificEpochOnly ||
+					flag == common.CorrectLastUnJailedFlagInSpecificEpochOnly ||
+					flag == common.DelegationSmartContractFlagInSpecificEpochOnly ||
+					flag == common.CorrectLastUnJailedFlag ||
+					flag == common.SwitchJailWaitingFlag ||
+					flag == common.StakingV2Flag ||
+					flag == common.ESDTFlagInSpecificEpochOnly {
 
-	t.Run("flag not active - activation epoch is in the future", func(t *testing.T) {
-		args, _ := createFullArgumentsForSystemSCProcessing(config.EnableEpochs{
-			GovernanceEnableEpoch: 39,
-			StakingV2EnableEpoch:  1000,
-		}, testscommon.CreateMemUnit())
-		s, _ := NewSystemSCProcessor(args)
-		handler := s.enableEpochsHandler.(enableEpochHandlerWithEpochConfirm)
-		handler.EpochConfirmed(37, 0)
+					return false
+				}
+
+				return true
+			},
+		}
+		args.SystemVM = &mock.VMExecutionHandlerStub{
+			RunSmartContractCallCalled: func(input *vmcommon.ContractCallInput) (*vmcommon.VMOutput, error) {
+				assert.Fail(t, "should have not called")
+
+				return nil, fmt.Errorf("should have not called")
+			},
+		}
+		processor, _ := NewSystemSCProcessor(args)
+		require.NotNil(t, processor)
 
 		validatorsInfo := state.NewShardValidatorsInfoMap()
-		err := s.ProcessSystemSmartContract(validatorsInfo, &block.Header{})
+		err := processor.ProcessSystemSmartContract(validatorsInfo, &block.Header{})
 		assert.Nil(t, err)
-
-		acc, err := s.userAccountsDB.GetExistingAccount(vm.GovernanceSCAddress)
-		assert.NotNil(t, err)
-		assert.True(t, check.IfNil(acc))
 	})
-	t.Run("flag active in that specific epoch", func(t *testing.T) {
-		args, _ := createFullArgumentsForSystemSCProcessing(config.EnableEpochs{
-			GovernanceEnableEpoch: 37,
-			StakingV2EnableEpoch:  1000,
-		}, testscommon.CreateMemUnit())
-		s, _ := NewSystemSCProcessor(args)
-		handler := s.enableEpochsHandler.(enableEpochHandlerWithEpochConfirm)
-		handler.EpochConfirmed(37, 0)
+	t.Run("flag active", func(t *testing.T) {
+		args := createMockArgsForSystemSCProcessor()
+		args.EnableEpochsHandler = &enableEpochsHandlerMock.EnableEpochsHandlerStub{
+			IsFlagEnabledCalled: func(flag core.EnableEpochFlag) bool {
+				return flag == common.GovernanceFlagInSpecificEpochOnly
+			},
+		}
+		runSmartContractCreateCalled := false
+		args.SystemVM = &mock.VMExecutionHandlerStub{
+			RunSmartContractCallCalled: func(input *vmcommon.ContractCallInput) (*vmcommon.VMOutput, error) {
+				runSmartContractCreateCalled = true
+
+				return &vmcommon.VMOutput{}, nil
+			},
+		}
+		processor, _ := NewSystemSCProcessor(args)
+		require.NotNil(t, processor)
 
 		validatorsInfo := state.NewShardValidatorsInfoMap()
-		err := s.ProcessSystemSmartContract(validatorsInfo, &block.Header{})
+		err := processor.ProcessSystemSmartContract(validatorsInfo, &block.Header{})
 		assert.Nil(t, err)
-
-		acc, err := s.userAccountsDB.GetExistingAccount(vm.GovernanceSCAddress)
-		assert.Nil(t, err)
-
-		userAcc, _ := acc.(state.UserAccountHandler)
-		assert.Empty(t, userAcc.GetOwnerAddress())
-		assert.Empty(t, userAcc.GetCodeMetadata())
-	})
-	t.Run("flag not active - activation epoch is in the past", func(t *testing.T) {
-		args, _ := createFullArgumentsForSystemSCProcessing(config.EnableEpochs{
-			GovernanceEnableEpoch: 35,
-			StakingV2EnableEpoch:  1000,
-		}, testscommon.CreateMemUnit())
-		s, _ := NewSystemSCProcessor(args)
-		handler := s.enableEpochsHandler.(enableEpochHandlerWithEpochConfirm)
-		handler.EpochConfirmed(37, 0) // epoch 37
-
-		validatorsInfo := state.NewShardValidatorsInfoMap()
-		err := s.ProcessSystemSmartContract(validatorsInfo, &block.Header{})
-		assert.Nil(t, err)
-
-		acc, err := s.userAccountsDB.GetExistingAccount(vm.GovernanceSCAddress)
-		assert.NotNil(t, err)
-		assert.True(t, check.IfNil(acc))
+		assert.True(t, runSmartContractCreateCalled)
 	})
 }
 
