2fec868173dc8a4713db0797dba39b2b48bd5b0e
Alexander Cristurean <alexander.cristurean@yahoo.com>
2024-02-27 17:57:48 +0200

    added script to create local-testnet with docker.

---
+++ scripts/docker-testnet/start.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -e
+
+export DOCKERTESTNETDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+MULTIVERSXTESTNETSCRIPTSDIR="$(dirname "$DOCKERTESTNETDIR")/testnet"
+
+source "$DOCKERTESTNETDIR/variables.sh"
+source "$DOCKERTESTNETDIR/helpers.sh"
+source "$MULTIVERSXTESTNETSCRIPTSDIR/include/config.sh"
+source "$MULTIVERSXTESTNETSCRIPTSDIR/include/build.sh"
+
+prepareFolders
+
+buildConfigGenerator
+
+generateConfig
+
+copyConfig
+
+copySeednodeConfig
+updateSeednodeConfig
+
+copyNodeConfig
+updateNodeConfig
+
+startSeedNode
+startObservers
+startValidators
+
+if [ $USE_PROXY -eq 1 ]; then
+  prepareFolders_Proxy
+  copyProxyConfig
+  updateProxyConfigDocker
+  startProxyDocker
+fi
+
