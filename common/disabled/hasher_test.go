d76c28ea0c59c6df6f4f2169d13c87a856625bd5
axenteoctavian <axente.octavian@gmail.com>
2024-03-12 11:48:47 +0200

    fixes after review

--- common/disabled/hasher_test.go
+++ common/disabled/hasher_test.go
@@ -14,28 +14,10 @@ func TestHasher_MethodsShouldNotPanic(t *testing.T) {
 	require.False(t, check.IfNil(h))
 
 	require.NotPanics(t, func() {
-		_ = h.Compute("string")
-		_ = h.Size()
-		_ = h.IsInterfaceNil()
+		bytes := h.Compute("string")
+		require.NotNil(t, bytes)
+
+		size := h.Size()
+		require.NotNil(t, size)
 	})
-}
-
-func TestHasher_Compute(t *testing.T) {
-	t.Parallel()
-
-	h := NewDisabledHasher()
-	require.False(t, check.IfNil(h))
-
-	bytes := h.Compute("string")
-	require.NotNil(t, bytes)
-}
-
-func TestHasher_Size(t *testing.T) {
-	t.Parallel()
-
-	h := NewDisabledHasher()
-	require.False(t, check.IfNil(h))
-
-	size := h.Size()
-	require.NotNil(t, size)
 }
