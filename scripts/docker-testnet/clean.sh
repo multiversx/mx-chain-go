24c1ac41b0c374b0ab58a82e9d2e210f2f9a6878
Alexander Cristurean <alexander.cristurean@yahoo.com>
2024-03-18 10:33:43 +0200

    fixes after review.

--- scripts/docker-testnet/clean.sh
+++ scripts/docker-testnet/clean.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
+set -eux
+
 # Delete the entire testnet folder, which includes configuration, executables and logs.
 
 export MULTIVERSXTESTNETSCRIPTSDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 source "$MULTIVERSXTESTNETSCRIPTSDIR/variables.sh"
 
-echo "Stopping all containers..."
-docker stop $(docker ps -a -q)
+# Get the IDs of containers attached to the network
+CONTAINER_IDS=$(docker network inspect -f '{{range .Containers}}{{.Name}} {{end}}' "$DOCKER_NETWORK_NAME")
 
-echo "Removing all containers..."
-docker container prune -f
+# Stop each container
+echo "Removing containers..."
+for CONTAINER_ID in $CONTAINER_IDS; do
+    docker stop "$CONTAINER_ID"
+    docker rm "$CONTAINER_ID"
+done
 
 echo "Removing network..."
 docker network rm ${DOCKER_NETWORK_NAME}
