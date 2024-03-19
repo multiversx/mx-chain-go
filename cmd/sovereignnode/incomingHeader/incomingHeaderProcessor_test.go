83b7aec00e86771c8a70c9ef55baf9a8c6be875c
axenteoctavian <axente.octavian@gmail.com>
2024-03-19 12:50:18 +0200

    fixes after review

--- cmd/sovereignnode/incomingHeader/incomingHeaderProcessor_test.go
+++ cmd/sovereignnode/incomingHeader/incomingHeaderProcessor_test.go
@@ -838,6 +838,9 @@ func TestIncomingHeaderHandler_AddHeader(t *testing.T) {
 
 	err = handler.AddHeader([]byte("hash"), incomingHeader)
 	require.Nil(t, err)
+	require.Equal(t, 1, checkValidityCt)
+	require.Equal(t, 1, deserializeEventDataCt)
+	require.Equal(t, 2, deserializeTokenDataCt)
 	require.True(t, wasAddedInHeaderPool)
 	require.True(t, wasAddedInTxPool)
 	require.True(t, wasOutGoingOpConfirmed)
