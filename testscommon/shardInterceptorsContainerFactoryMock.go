a8e606595eeb6f5110eb8c1b029b8a44121504f8
axenteoctavian <axente.octavian@gmail.com>
2024-04-22 14:38:46 +0300

    fixes after review

--- testscommon/shardInterceptorsContainerFactoryMock.go
+++ testscommon/shardInterceptorsContainerFactoryMock.go
@@ -4,10 +4,12 @@ import (
 	"github.com/multiversx/mx-chain-go/process"
 )
 
+// ShardInterceptorsContainerFactoryMock -
 type ShardInterceptorsContainerFactoryMock struct {
 	CreateCalled func() (process.InterceptorsContainer, process.InterceptorsContainer, error)
 }
 
+// Create -
 func (s *ShardInterceptorsContainerFactoryMock) Create() (process.InterceptorsContainer, process.InterceptorsContainer, error) {
 	if s.CreateCalled != nil {
 		return s.Create()
@@ -15,6 +17,7 @@ func (s *ShardInterceptorsContainerFactoryMock) Create() (process.InterceptorsCo
 	return &InterceptorsContainerStub{}, &InterceptorsContainerStub{}, nil
 }
 
+// IsInterfaceNil - checks if underlying pointer is nil
 func (s *ShardInterceptorsContainerFactoryMock) IsInterfaceNil() bool {
 	return s == nil
 }
