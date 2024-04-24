2fd415b69edcce6bbae084170fd4d514a64a0bc4
axenteoctavian <axente.octavian@gmail.com>
2024-04-24 18:12:48 +0300

    issue sft test

--- cmd/sovereignnode/chainSimulator/tests/esdt/issueSft_test.go
+++ cmd/sovereignnode/chainSimulator/tests/esdt/issueSft_test.go
@@ -6,8 +6,10 @@ import (
 	"testing"
 	"time"
 
+	"github.com/multiversx/mx-chain-go/config"
 	"github.com/multiversx/mx-chain-go/node/chainSimulator"
 	"github.com/multiversx/mx-chain-go/node/chainSimulator/components/api"
+	sovereignChainSimulator "github.com/multiversx/mx-chain-go/sovereignnode/chainSimulator"
 	"github.com/multiversx/mx-chain-go/sovereignnode/chainSimulator/utils"
 
 	"github.com/multiversx/mx-chain-core-go/core"
@@ -24,49 +26,49 @@ func TestEsdt_IssueSft(t *testing.T) {
 		t.Skip("this is not a short test")
 	}
 
-	//epochConfig, economicsConfig, sovereignExtraConfig, err := sovereignChainSimulator.LoadSovereignConfigs(sovereignConfigPath)
-	//require.Nil(t, err)
+	epochConfig, economicsConfig, sovereignExtraConfig, err := sovereignChainSimulator.LoadSovereignConfigs(sovereignConfigPath)
+	require.Nil(t, err)
 
-	//cs, err := sovereignChainSimulator.NewSovereignChainSimulator(sovereignChainSimulator.ArgsSovereignChainSimulator{
-	//	SovereignExtraConfig: *sovereignExtraConfig,
-	//	ChainSimulatorArgs: chainSimulator.ArgsChainSimulator{
-	//		BypassTxSignatureCheck: false,
-	//		TempDir:                t.TempDir(),
-	//		PathToInitialConfig:    defaultPathToInitialConfig,
-	//		NumOfShards:            1,
-	//		GenesisTimestamp:       time.Now().Unix(),
-	//		RoundDurationInMillis:  uint64(6000),
-	//		RoundsPerEpoch:         core.OptionalUint64{},
-	//		ApiInterface:           api.NewNoApiInterface(),
-	//		MinNodesPerShard:       2,
-	//		ConsensusGroupSize:     2,
-	//		AlterConfigsFunction: func(cfg *config.Configs) {
-	//			cfg.EconomicsConfig = economicsConfig
-	//			cfg.EpochConfig = epochConfig
-	//			cfg.GeneralConfig.SovereignConfig = *sovereignExtraConfig
-	//			cfg.GeneralConfig.VirtualMachine.Execution.WasmVMVersions = []config.WasmVMVersionByEpoch{{StartEpoch: 0, Version: "v1.5"}}
-	//			cfg.GeneralConfig.VirtualMachine.Querying.WasmVMVersions = []config.WasmVMVersionByEpoch{{StartEpoch: 0, Version: "v1.5"}}
-	//		},
-	//	},
-	//})
-	cs, err := chainSimulator.NewChainSimulator(chainSimulator.ArgsChainSimulator{
-		BypassTxSignatureCheck: false,
-		TempDir:                t.TempDir(),
-		PathToInitialConfig:    defaultPathToInitialConfig,
-		NumOfShards:            3,
-		GenesisTimestamp:       time.Now().Unix(),
-		RoundDurationInMillis:  uint64(6000),
-		RoundsPerEpoch: core.OptionalUint64{
-			HasValue: true,
-			Value:    30,
+	cs, err := sovereignChainSimulator.NewSovereignChainSimulator(sovereignChainSimulator.ArgsSovereignChainSimulator{
+		SovereignExtraConfig: *sovereignExtraConfig,
+		ChainSimulatorArgs: chainSimulator.ArgsChainSimulator{
+			BypassTxSignatureCheck: false,
+			TempDir:                t.TempDir(),
+			PathToInitialConfig:    defaultPathToInitialConfig,
+			NumOfShards:            1,
+			GenesisTimestamp:       time.Now().Unix(),
+			RoundDurationInMillis:  uint64(6000),
+			RoundsPerEpoch:         core.OptionalUint64{},
+			ApiInterface:           api.NewNoApiInterface(),
+			MinNodesPerShard:       2,
+			ConsensusGroupSize:     2,
+			AlterConfigsFunction: func(cfg *config.Configs) {
+				cfg.EconomicsConfig = economicsConfig
+				cfg.EpochConfig = epochConfig
+				cfg.GeneralConfig.SovereignConfig = *sovereignExtraConfig
+				cfg.GeneralConfig.VirtualMachine.Execution.WasmVMVersions = []config.WasmVMVersionByEpoch{{StartEpoch: 0, Version: "v1.5"}}
+				cfg.GeneralConfig.VirtualMachine.Querying.WasmVMVersions = []config.WasmVMVersionByEpoch{{StartEpoch: 0, Version: "v1.5"}}
+			},
 		},
-		InitialEpoch:                3,
-		ApiInterface:                api.NewNoApiInterface(),
-		MinNodesPerShard:            1,
-		MetaChainMinNodes:           1,
-		ConsensusGroupSize:          1,
-		MetaChainConsensusGroupSize: 1,
 	})
+	//cs, err := chainSimulator.NewChainSimulator(chainSimulator.ArgsChainSimulator{
+	//	BypassTxSignatureCheck: false,
+	//	TempDir:                t.TempDir(),
+	//	PathToInitialConfig:    defaultPathToInitialConfig,
+	//	NumOfShards:            3,
+	//	GenesisTimestamp:       time.Now().Unix(),
+	//	RoundDurationInMillis:  uint64(6000),
+	//	RoundsPerEpoch: core.OptionalUint64{
+	//		HasValue: true,
+	//		Value:    30,
+	//	},
+	//	InitialEpoch:                3,
+	//	ApiInterface:                api.NewNoApiInterface(),
+	//	MinNodesPerShard:            1,
+	//	MetaChainMinNodes:           1,
+	//	ConsensusGroupSize:          1,
+	//	MetaChainConsensusGroupSize: 1,
+	//})
 	require.Nil(t, err)
 	require.NotNil(t, cs)
 
