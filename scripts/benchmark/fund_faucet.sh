4356428a5eae89eed42d464f468f31f8154898dc
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2024-05-21 15:30:20 +0300

    Remove some files.

--- scripts/benchmark/fund_faucet.sh
+++
@@ -1,2 +0,0 @@
-mxpy tx new --proxy=http://localhost:7950 --pem=~/MultiversX/testnet/filegen/output/walletKey.pem --recall-nonce --gas-limit=50000 --value=100000000000000000000 \
-    --receiver=erd1sjs26q5pmngu7qjnpkcqgstdppvqul7vdqa0cru5ae5axkq37czqndz3vp --send
