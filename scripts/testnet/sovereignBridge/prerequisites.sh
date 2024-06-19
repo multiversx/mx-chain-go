83742a7806d11a21f79ed57dc2cf0fc5dab2a117
axenteoctavian <axente.octavian@gmail.com>
2024-06-19 14:52:11 +0300

    install docker changes

--- scripts/testnet/sovereignBridge/prerequisites.sh
+++ scripts/testnet/sovereignBridge/prerequisites.sh
@@ -7,22 +7,25 @@ pipx install multiversx-sdk-cli --force
 pipx ensurepath
 
 # install docker
-sudo apt-get update
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt install -y docker-ce
-# docker post install
-sudo groupadd docker
-sudo usermod -aG docker $USER
-newgrp docker
+if ! command -v docker &> /dev/null
+then
+  sudo apt-get update
+  sudo apt-get install ca-certificates curl
+  sudo install -m 0755 -d /etc/apt/keyrings
+  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  sudo chmod a+r /etc/apt/keyrings/docker.asc
+  # Add the repository to Apt sources:
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+  sudo apt install -y docker-ce
+  # docker post install
+  sudo groupadd docker
+  sudo usermod -aG docker $USER
+  newgrp docker
+fi
 
 mkdir -p $(eval echo "${CONTRACTS_DIRECTORY}")
 version=$(basename `curl -s https://github.com/multiversx/mx-sovereign-sc/releases/latest -I | grep location | awk -F"https:/" '{print $2}' | tr -d "\r"`)
