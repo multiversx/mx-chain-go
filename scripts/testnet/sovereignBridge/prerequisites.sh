0e68979c0b092ba48877e94876a4c64de5507464
axenteoctavian <axente.octavian@gmail.com>
2024-06-19 16:26:30 +0300

    new bridge prerequisites

--- scripts/testnet/sovereignBridge/prerequisites.sh
+++ scripts/testnet/sovereignBridge/prerequisites.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+echo "Installing software prerequisites..."
 sudo apt update
 sudo apt install -y python3-pip pipx screen ca-certificates curl wget
 pip install multiversx-sdk
 pipx install multiversx-sdk-cli --force
 pipx ensurepath
+
+echo "Downloading cross-chain contracts..."
+# load config paths
+source config/configs.cfg
+
+mkdir -p $(eval echo "${CONTRACTS_DIRECTORY}")
+version=$(basename `curl -s https://github.com/multiversx/mx-sovereign-sc/releases/latest -I | grep location | awk -F"https:/" '{print $2}' | tr -d "\r"`)
+wget -O $(eval echo ${ESDT_SAFE_WASM}) https://github.com/multiversx/mx-sovereign-sc/releases/download/${version}/esdt-safe.wasm
+wget -O $(eval echo ${FEE_MARKET_WASM}) https://github.com/multiversx/mx-sovereign-sc/releases/download/${version}/fee-market.wasm
+wget -O $(eval echo ${HEADER_VERIFIER_WASM}) https://github.com/multiversx/mx-sovereign-sc/releases/download/${version}/header-verifier.wasm
 
 # install docker
 if ! command -v docker &> /dev/null
@@ -26,13 +37,3 @@ then
   sudo usermod -aG docker $USER
   newgrp docker
 fi
-
-echo "Downloading cross-chain contracts..."
-# load config paths
-source config/configs.cfg
-
-mkdir -p $(eval echo "${CONTRACTS_DIRECTORY}")
-version=$(basename `curl -s https://github.com/multiversx/mx-sovereign-sc/releases/latest -I | grep location | awk -F"https:/" '{print $2}' | tr -d "\r"`)
-wget -O $(eval echo ${ESDT_SAFE_WASM}) https://github.com/multiversx/mx-sovereign-sc/releases/download/${version}/esdt-safe.wasm
-wget -O $(eval echo ${FEE_MARKET_WASM}) https://github.com/multiversx/mx-sovereign-sc/releases/download/${version}/fee-market.wasm
-wget -O $(eval echo ${HEADER_VERIFIER_WASM}) https://github.com/multiversx/mx-sovereign-sc/releases/download/${version}/header-verifier.wasm
