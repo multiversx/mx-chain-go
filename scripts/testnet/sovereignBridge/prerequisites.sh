1ad3b0632126cfde902ead9998928eda0b6aa170
axenteoctavian <axente.octavian@gmail.com>
2024-06-19 14:34:21 +0300

    bridge prerequisites update

--- scripts/testnet/sovereignBridge/prerequisites.sh
+++ scripts/testnet/sovereignBridge/prerequisites.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-apt update
-apt install -y python3-pip pipx screen ca-certificates curl wget
+sudo apt update
+sudo apt install -y python3-pip pipx screen ca-certificates curl wget
 pip install multiversx-sdk
 pipx install multiversx-sdk-cli --force
 pipx ensurepath
@@ -11,8 +11,8 @@ install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" > /etc/apt/sources.list.d/docker.list
-apt update
-apt install -y docker-ce
+sudo apt update
+sudo apt install -y docker-ce
 
 mkdir -p $(eval echo "${CONTRACTS_DIRECTORY}")
 version=$(basename `curl -s https://github.com/multiversx/mx-sovereign-sc/releases/latest -I | grep location | awk -F"https:/" '{print $2}' | tr -d "\r"`)
