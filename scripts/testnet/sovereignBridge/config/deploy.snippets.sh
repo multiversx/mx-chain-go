76b80914c5c672744ca9b43af18f6445c73ae7db
axenteoctavian <axente.octavian@gmail.com>
2024-06-20 09:24:45 +0300

    deploy scripts update

--- scripts/testnet/sovereignBridge/config/deploy.snippets.sh
+++ scripts/testnet/sovereignBridge/config/deploy.snippets.sh
@@ -1,4 +1,4 @@
-# This function will:
+# This function will deploy full sovereign setup:
 # - deploy all main chain contracts and update sovereign configs
 # - deploy sovereign nodes with all services
 deploySovereignWithCrossChainContracts() {
@@ -7,7 +7,7 @@ deploySovereignWithCrossChainContracts() {
     sovereignDeploy
 }
 
-# This function will:
+# This function will deploy main chain services:
 # - deploy all main chain contracts
 # - update sovereign configs
 # - prepare a main chain observer for sovereign nodes
@@ -31,7 +31,7 @@ deployMainChainContractsAndSetupObserver() {
     prepareObserver
 }
 
-# This function will:
+# This function will deploy sovereign:
 # - update some parameter in notifier
 # - run the sovereign nodes config
 # - deploy header verifier contract on main chain
@@ -50,6 +50,8 @@ sovereignDeploy() {
 
     sovereignStart
 
+    deployObserver
+
     setHeaderVerifierAddressInEsdtSafe
 
     getFundsInAddressSovereign
@@ -61,33 +63,41 @@ sovereignDeploy() {
     unpauseEsdtSafeContractSovereign
 }
 
-# This function will:
+# This function will start sovereign:
 # - update and start bridge service
 # - start sovereign nodes
-# - deploy the main chain observer
 sovereignStart() {
     updateAndStartBridgeService
 
     $TESTNET_DIR/sovereignStart.sh
-
-    deployObserver
 }
 
-# This function will:
+# This function will restart sovereign:
 # - stop sovereign nodes and services
 # - deploy sovereign nodes with all services
 sovereignRestart() {
+    stopSovereign
+
+    sovereignStart
+}
+
+# This function will reset sovereign:
+# - stop sovereign nodes and services
+# - deploy sovereign nodes with all services
+sovereignReset() {
     stopAndCleanSovereign
 
     sovereignDeploy
 }
 
-# This function will:
+
+# This function will upgrade and reset sovereign:
 # - stop sovereign and clean nodes
 # - pull the latest changes for all the repositories
 # - download the new version of the contracts and update them on main chain
+# - update sovereign configs
 # - deploy sovereign nodes with all services
-sovereignUpgradeAndRestart() {
+sovereignUpgradeAndReset() {
     stopAndCleanSovereign
 
     gitPullAllChanges || return
@@ -98,26 +108,32 @@ sovereignUpgradeAndRestart() {
 
     upgradeFeeMarketContract
 
+    setGenesisContract
+
+    updateSovereignConfig
+
+    prepareObserver
+
     sovereignDeploy
 }
 
-# This function will:
+# This function will stop sovereign:
 # - stop sovereign nodes
 # - stop the bridge service
-# - stop the main chain observer
 stopSovereign() {
     $TESTNET_DIR/stop.sh
 
     screen -S sovereignBridgeService -X kill
-
-    stopObserver
 }
 
-# This function will:
+# This function will stop and clean sovereign:
 # - stop sovereign nodes and services
 # - clean the sovereign configuration
+# - stop the main chain observer
 stopAndCleanSovereign() {
     stopSovereign
 
     $TESTNET_DIR/clean.sh
+
+    stopObserver
 }
