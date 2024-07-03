20a1b18770156c785b74669ca650c3a94292f3cd
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2024-07-03 22:13:50 +0300

    WrappedTransaction.computeFee() delegates to the Node's economicsData logic.

--- txcache/wrappedTransaction.go
+++ txcache/wrappedTransaction.go
@@ -2,71 +2,30 @@ package txcache
 
 import (
 	"bytes"
+	"math/big"
 
 	"github.com/multiversx/mx-chain-core-go/data"
 )
 
-const processFeeFactor = float64(0.8) // 80%
-
 // WrappedTransaction contains a transaction, its hash and extra information
 type WrappedTransaction struct {
-	Tx                   data.TransactionHandler
-	TxHash               []byte
-	SenderShardID        uint32
-	ReceiverShardID      uint32
-	Size                 int64
-	TxFeeScoreNormalized uint64
+	Tx              data.TransactionHandler
+	TxHash          []byte
+	SenderShardID   uint32
+	ReceiverShardID uint32
+	Size            int64
+	TxFee           float64
 }
 
 func (wrappedTx *WrappedTransaction) sameAs(another *WrappedTransaction) bool {
 	return bytes.Equal(wrappedTx.TxHash, another.TxHash)
 }
 
-// estimateTxGas returns an approximation for the necessary computation units (gas units)
-func estimateTxGas(tx *WrappedTransaction) uint64 {
-	gasLimit := tx.Tx.GetGasLimit()
-	return gasLimit
-}
-
-// estimateTxFeeScore returns a normalized approximation for the cost of a transaction
-func estimateTxFeeScore(tx *WrappedTransaction, txGasHandler TxGasHandler, txFeeHelper feeHelper) uint64 {
-	moveGas, processGas := txGasHandler.SplitTxGasInCategories(tx.Tx)
-
-	normalizedMoveGas := moveGas >> txFeeHelper.gasLimitShift()
-	normalizedProcessGas := processGas >> txFeeHelper.gasLimitShift()
-
-	normalizedGasPriceMove := txGasHandler.GasPriceForMove(tx.Tx) >> txFeeHelper.gasPriceShift()
-	normalizedGasPriceProcess := normalizeGasPriceProcessing(tx, txGasHandler, txFeeHelper)
-
-	normalizedFeeMove := normalizedMoveGas * normalizedGasPriceMove
-	normalizedFeeProcess := normalizedProcessGas * normalizedGasPriceProcess
-
-	adjustmentFactor := computeProcessingGasPriceAdjustment(tx, txGasHandler, txFeeHelper)
-
-	tx.TxFeeScoreNormalized = normalizedFeeMove + normalizedFeeProcess*adjustmentFactor
-
-	return tx.TxFeeScoreNormalized
-}
-
-func normalizeGasPriceProcessing(tx *WrappedTransaction, txGasHandler TxGasHandler, txFeeHelper feeHelper) uint64 {
-	return txGasHandler.GasPriceForProcessing(tx.Tx) >> txFeeHelper.gasPriceShift()
-}
-
-func computeProcessingGasPriceAdjustment(
-	tx *WrappedTransaction,
-	txGasHandler TxGasHandler,
-	txFeeHelper feeHelper,
-) uint64 {
-	minPriceFactor := txFeeHelper.minGasPriceFactor()
-
-	if minPriceFactor <= 2 {
-		return 1
-	}
-
-	actualPriceFactor := float64(1)
-	if txGasHandler.MinGasPriceForProcessing() != 0 {
-		actualPriceFactor = float64(txGasHandler.GasPriceForProcessing(tx.Tx)) / float64(txGasHandler.MinGasPriceForProcessing())
-	}
-
-	return uint64(float64(txFeeHelper.minGasPriceFactor()) * processFeeFactor / actualPriceFactor)
+// computeFee computes the transaction fee.
+// The returned fee is also held on the transaction object.
+func (wrappedTx *WrappedTransaction) computeFee(txGasHandler TxGasHandler) float64 {
+	fee := txGasHandler.ComputeTxFee(wrappedTx.Tx)
+	feeAsFloat, _ := new(big.Float).SetInt(fee).Float64()
+	wrappedTx.TxFee = feeAsFloat
+	return feeAsFloat
 }
