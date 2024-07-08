9884434626360e6e8502ae29bf4b577b11232ee7
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2024-07-08 14:14:23 +0300

    Additional tests.

--- txcache/noncesTracker_test.go
+++ txcache/noncesTracker_test.go
@@ -61,3 +61,116 @@ func TestNoncesTracker_computeExpectedSumOfSquaresOfNonces(t *testing.T) {
 	sum = tracker.computeExpectedSumOfSquaresOfNoncesTimesSix(oneTrillion, 65535)
 	require.Equal(t, uint64(445375860), sum)
 }
+
+func TestNoncesTracker_isSpotlessSequence(t *testing.T) {
+	t.Run("empty sequence", func(t *testing.T) {
+		tracker := newNoncesTracker()
+
+		// A little bit of ambiguity (a sequence holding the nonce zero only behaves like an empty sequence):
+		require.True(t, tracker.isSpotlessSequence(0, 0))
+		require.True(t, tracker.isSpotlessSequence(0, 1))
+
+		require.False(t, tracker.isSpotlessSequence(0, 2))
+		require.False(t, tracker.isSpotlessSequence(7, 3))
+	})
+
+	t.Run("1-item sequence", func(t *testing.T) {
+		tracker := newNoncesTracker()
+		tracker.addNonce(0)
+
+		// A little bit of ambiguity (a sequence holding the nonce zero only behaves like an empty sequence):
+		require.True(t, tracker.isSpotlessSequence(0, 1))
+		require.True(t, tracker.isSpotlessSequence(0, 0))
+
+		require.False(t, tracker.isSpotlessSequence(0, 2))
+		require.False(t, tracker.isSpotlessSequence(7, 3))
+
+		tracker.removeNonce(0)
+		tracker.addNonce(5)
+		require.True(t, tracker.isSpotlessSequence(5, 1))
+		require.False(t, tracker.isSpotlessSequence(5, 2))
+		require.False(t, tracker.isSpotlessSequence(7, 1))
+		require.False(t, tracker.isSpotlessSequence(7, 2))
+
+		tracker.removeNonce(5)
+		tracker.addNonce(42)
+		require.True(t, tracker.isSpotlessSequence(42, 1))
+		require.False(t, tracker.isSpotlessSequence(42, 2))
+		require.False(t, tracker.isSpotlessSequence(7, 1))
+		require.False(t, tracker.isSpotlessSequence(7, 2))
+	})
+
+	t.Run("with spotless addition and removal", func(t *testing.T) {
+		t.Parallel()
+
+		tracker := newNoncesTracker()
+		numTotalTxsSender := uint64(100)
+		firstNonce := uint64(oneBillion)
+		lastNonce := firstNonce + numTotalTxsSender - 1
+		numCurrentTxs := uint64(0)
+
+		// We add nonces in increasing order:
+		for nonce := firstNonce; nonce < firstNonce+numTotalTxsSender; nonce++ {
+			tracker.addNonce(nonce)
+			numCurrentTxs++
+
+			isSpotless := tracker.isSpotlessSequence(firstNonce, numCurrentTxs)
+			if !isSpotless {
+				require.Fail(t, "nonce sequence is not spotless (after add)", "nonce: %d", nonce)
+			}
+		}
+
+		// We remove nonces in decreasing order:
+		for nonce := lastNonce; nonce >= firstNonce; nonce-- {
+			tracker.removeNonce(nonce)
+			numCurrentTxs--
+
+			isSpotless := tracker.isSpotlessSequence(firstNonce, numCurrentTxs)
+			if !isSpotless {
+				require.Fail(t, "nonce sequence is not spotless (after remove)", "nonce: %d", nonce)
+			}
+		}
+	})
+
+	t.Run("with initial gap", func(t *testing.T) {
+		tracker := newNoncesTracker()
+
+		tracker.addNonce(5)
+		tracker.addNonce(6)
+		tracker.addNonce(7)
+
+		require.False(t, tracker.isSpotlessSequence(2, 3))
+	})
+
+	t.Run("with initial duplicate", func(t *testing.T) {
+		tracker := newNoncesTracker()
+
+		tracker.addNonce(5)
+		tracker.addNonce(5)
+		tracker.addNonce(6)
+
+		require.False(t, tracker.isSpotlessSequence(2, 3))
+	})
+
+	t.Run("with middle gap", func(t *testing.T) {
+		tracker := newNoncesTracker()
+
+		tracker.addNonce(5)
+		tracker.addNonce(6)
+		tracker.addNonce(8)
+
+		require.False(t, tracker.isSpotlessSequence(5, 3))
+	})
+
+	t.Run("with middle duplicate", func(t *testing.T) {
+		tracker := newNoncesTracker()
+
+		tracker.addNonce(5)
+		tracker.addNonce(6)
+		tracker.addNonce(6)
+		tracker.addNonce(8)
+
+		require.False(t, tracker.isSpotlessSequence(5, 4))
+		require.False(t, tracker.isSpotlessSequence(5, 3))
+	})
+}
