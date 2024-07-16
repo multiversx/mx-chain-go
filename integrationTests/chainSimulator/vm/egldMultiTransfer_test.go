0a13356221e5f81b2696679be489d0c900a7ece7
ssd04 <dariussuirab@gmail.com>
2024-07-16 16:16:23 +0300

    added more scenarios

--- integrationTests/chainSimulator/vm/egldMultiTransfer_test.go
+++ integrationTests/chainSimulator/vm/egldMultiTransfer_test.go
@@ -2,6 +2,7 @@ package vm
 
 import (
 	"encoding/hex"
+	"fmt"
 	"math/big"
 	"strings"
 	"testing"
@@ -190,6 +191,263 @@ func TestChainSimulator_EGLD_MultiTransfer(t *testing.T) {
 
 	beforeBalance1, _ := big.NewInt(0).SetString(beforeBalanceStr1, 10)
 	expectedBalance1 := big.NewInt(0).Add(beforeBalance1, egldValue)
+
+	require.Equal(t, expectedBalance1.String(), account1.Balance)
+}
+
+func TestChainSimulator_EGLD_MultiTransfer_Insufficient_Funds(t *testing.T) {
+	if testing.Short() {
+		t.Skip("this is not a short test")
+	}
+
+	startTime := time.Now().Unix()
+	roundDurationInMillis := uint64(6000)
+	roundsPerEpoch := core.OptionalUint64{
+		HasValue: true,
+		Value:    20,
+	}
+
+	activationEpoch := uint32(4)
+
+	baseIssuingCost := "1000"
+
+	numOfShards := uint32(3)
+	cs, err := chainSimulator.NewChainSimulator(chainSimulator.ArgsChainSimulator{
+		BypassTxSignatureCheck:   true,
+		TempDir:                  t.TempDir(),
+		PathToInitialConfig:      defaultPathToInitialConfig,
+		NumOfShards:              numOfShards,
+		GenesisTimestamp:         startTime,
+		RoundDurationInMillis:    roundDurationInMillis,
+		RoundsPerEpoch:           roundsPerEpoch,
+		ApiInterface:             api.NewNoApiInterface(),
+		MinNodesPerShard:         3,
+		MetaChainMinNodes:        3,
+		NumNodesWaitingListMeta:  0,
+		NumNodesWaitingListShard: 0,
+		AlterConfigsFunction: func(cfg *config.Configs) {
+			cfg.EpochConfig.EnableEpochs.EGLDInMultiTransferEnableEpoch = activationEpoch
+			cfg.SystemSCConfig.ESDTSystemSCConfig.BaseIssuingCost = baseIssuingCost
+		},
+	})
+	require.Nil(t, err)
+	require.NotNil(t, cs)
+
+	defer cs.Close()
+
+	addrs := createAddresses(t, cs, false)
+
+	err = cs.GenerateBlocksUntilEpochIsReached(int32(activationEpoch))
+	require.Nil(t, err)
+
+	// issue NFT
+	nftTicker := []byte("NFTTICKER")
+	tx := issueNonFungibleTx(0, addrs[0].Bytes, nftTicker, baseIssuingCost)
+
+	txResult, err := cs.SendTxAndGenerateBlockTilTxIsExecuted(tx, maxNumOfBlockToGenerateWhenExecutingTx)
+	require.Nil(t, err)
+	require.NotNil(t, txResult)
+	require.Equal(t, "success", txResult.Status.String())
+
+	nftTokenID := txResult.Logs.Events[0].Topics[0]
+
+	roles := [][]byte{
+		[]byte(core.ESDTRoleNFTCreate),
+		[]byte(core.ESDTRoleTransfer),
+	}
+	setAddressEsdtRoles(t, cs, addrs[0], nftTokenID, roles)
+
+	log.Info("Issued NFT token id", "tokenID", string(nftTokenID))
+
+	nftMetaData := txsFee.GetDefaultMetaData()
+	nftMetaData.Nonce = []byte(hex.EncodeToString(big.NewInt(1).Bytes()))
+
+	tx = nftCreateTx(1, addrs[0].Bytes, nftTokenID, nftMetaData)
+
+	txResult, err = cs.SendTxAndGenerateBlockTilTxIsExecuted(tx, maxNumOfBlockToGenerateWhenExecutingTx)
+	require.Nil(t, err)
+	require.NotNil(t, txResult)
+
+	require.Equal(t, "success", txResult.Status.String())
+
+	err = cs.GenerateBlocks(10)
+	require.Nil(t, err)
+
+	account0, err := cs.GetAccount(addrs[0])
+	require.Nil(t, err)
+
+	beforeBalanceStr0 := account0.Balance
+
+	egldValue, _ := big.NewInt(0).SetString(beforeBalanceStr0, 10)
+	egldValue = egldValue.Add(egldValue, big.NewInt(13))
+	tx = multiESDTNFTTransferWithEGLDTx(2, addrs[0].Bytes, addrs[1].Bytes, [][]byte{nftTokenID}, egldValue)
+
+	txResult, err = cs.SendTxAndGenerateBlockTilTxIsExecuted(tx, maxNumOfBlockToGenerateWhenExecutingTx)
+	require.Nil(t, err)
+	require.NotNil(t, txResult)
+
+	fmt.Println(txResult)
+	fmt.Println(string(txResult.Logs.Events[0].Topics[0]))
+	fmt.Println(string(txResult.Logs.Events[0].Topics[1]))
+
+	require.NotEqual(t, "success", txResult.Status.String())
+
+	eventLog := string(txResult.Logs.Events[0].Topics[1])
+	require.Equal(t, "insufficient funds for token EGLD-000000", eventLog)
+}
+
+func TestChainSimulator_Multiple_EGLD_Transfers(t *testing.T) {
+	if testing.Short() {
+		t.Skip("this is not a short test")
+	}
+
+	startTime := time.Now().Unix()
+	roundDurationInMillis := uint64(6000)
+	roundsPerEpoch := core.OptionalUint64{
+		HasValue: true,
+		Value:    20,
+	}
+
+	activationEpoch := uint32(4)
+
+	baseIssuingCost := "1000"
+
+	numOfShards := uint32(3)
+	cs, err := chainSimulator.NewChainSimulator(chainSimulator.ArgsChainSimulator{
+		BypassTxSignatureCheck:   true,
+		TempDir:                  t.TempDir(),
+		PathToInitialConfig:      defaultPathToInitialConfig,
+		NumOfShards:              numOfShards,
+		GenesisTimestamp:         startTime,
+		RoundDurationInMillis:    roundDurationInMillis,
+		RoundsPerEpoch:           roundsPerEpoch,
+		ApiInterface:             api.NewNoApiInterface(),
+		MinNodesPerShard:         3,
+		MetaChainMinNodes:        3,
+		NumNodesWaitingListMeta:  0,
+		NumNodesWaitingListShard: 0,
+		AlterConfigsFunction: func(cfg *config.Configs) {
+			cfg.EpochConfig.EnableEpochs.EGLDInMultiTransferEnableEpoch = activationEpoch
+			cfg.SystemSCConfig.ESDTSystemSCConfig.BaseIssuingCost = baseIssuingCost
+		},
+	})
+	require.Nil(t, err)
+	require.NotNil(t, cs)
+
+	defer cs.Close()
+
+	addrs := createAddresses(t, cs, false)
+
+	err = cs.GenerateBlocksUntilEpochIsReached(int32(activationEpoch))
+	require.Nil(t, err)
+
+	// issue NFT
+	nftTicker := []byte("NFTTICKER")
+	tx := issueNonFungibleTx(0, addrs[0].Bytes, nftTicker, baseIssuingCost)
+
+	txResult, err := cs.SendTxAndGenerateBlockTilTxIsExecuted(tx, maxNumOfBlockToGenerateWhenExecutingTx)
+	require.Nil(t, err)
+	require.NotNil(t, txResult)
+	require.Equal(t, "success", txResult.Status.String())
+
+	nftTokenID := txResult.Logs.Events[0].Topics[0]
+
+	roles := [][]byte{
+		[]byte(core.ESDTRoleNFTCreate),
+		[]byte(core.ESDTRoleTransfer),
+	}
+	setAddressEsdtRoles(t, cs, addrs[0], nftTokenID, roles)
+
+	log.Info("Issued NFT token id", "tokenID", string(nftTokenID))
+
+	nftMetaData := txsFee.GetDefaultMetaData()
+	nftMetaData.Nonce = []byte(hex.EncodeToString(big.NewInt(1).Bytes()))
+
+	tx = nftCreateTx(1, addrs[0].Bytes, nftTokenID, nftMetaData)
+
+	txResult, err = cs.SendTxAndGenerateBlockTilTxIsExecuted(tx, maxNumOfBlockToGenerateWhenExecutingTx)
+	require.Nil(t, err)
+	require.NotNil(t, txResult)
+
+	require.Equal(t, "success", txResult.Status.String())
+
+	err = cs.GenerateBlocks(10)
+	require.Nil(t, err)
+
+	account0, err := cs.GetAccount(addrs[0])
+	require.Nil(t, err)
+
+	beforeBalanceStr0 := account0.Balance
+
+	account1, err := cs.GetAccount(addrs[1])
+	require.Nil(t, err)
+
+	beforeBalanceStr1 := account1.Balance
+
+	// multi nft transfer with multiple EGLD-000000 tokens
+	numTransfers := 3
+	encodedReceiver := hex.EncodeToString(addrs[1].Bytes)
+	egldValue := oneEGLD.Mul(oneEGLD, big.NewInt(3))
+
+	txDataField := []byte(strings.Join(
+		[]string{
+			core.BuiltInFunctionMultiESDTNFTTransfer,
+			encodedReceiver,
+			hex.EncodeToString(big.NewInt(int64(numTransfers)).Bytes()),
+			hex.EncodeToString([]byte("EGLD-000000")),
+			"00",
+			hex.EncodeToString(egldValue.Bytes()),
+			hex.EncodeToString(nftTokenID),
+			hex.EncodeToString(big.NewInt(1).Bytes()),
+			hex.EncodeToString(big.NewInt(int64(1)).Bytes()),
+			hex.EncodeToString([]byte("EGLD-000000")),
+			"00",
+			hex.EncodeToString(egldValue.Bytes()),
+		}, "@"),
+	)
+
+	tx = &transaction.Transaction{
+		Nonce:     2,
+		SndAddr:   addrs[0].Bytes,
+		RcvAddr:   addrs[0].Bytes,
+		GasLimit:  10_000_000,
+		GasPrice:  minGasPrice,
+		Data:      txDataField,
+		Value:     big.NewInt(0),
+		Version:   1,
+		Signature: []byte("dummySig"),
+		ChainID:   []byte(configs.ChainID),
+	}
+
+	txResult, err = cs.SendTxAndGenerateBlockTilTxIsExecuted(tx, maxNumOfBlockToGenerateWhenExecutingTx)
+	require.Nil(t, err)
+	require.NotNil(t, txResult)
+
+	fmt.Println(txResult)
+	fmt.Println(string(txResult.Logs.Events[0].Topics[0]))
+	fmt.Println(string(txResult.Logs.Events[0].Topics[1]))
+
+	require.Equal(t, "success", txResult.Status.String())
+
+	// check accounts balance
+	account0, err = cs.GetAccount(addrs[0])
+	require.Nil(t, err)
+
+	beforeBalance0, _ := big.NewInt(0).SetString(beforeBalanceStr0, 10)
+
+	expectedBalance0 := big.NewInt(0).Sub(beforeBalance0, egldValue)
+	expectedBalance0 = big.NewInt(0).Sub(expectedBalance0, egldValue)
+	txsFee, _ := big.NewInt(0).SetString(txResult.Fee, 10)
+	expectedBalanceWithFee0 := big.NewInt(0).Sub(expectedBalance0, txsFee)
+
+	require.Equal(t, expectedBalanceWithFee0.String(), account0.Balance)
+
+	account1, err = cs.GetAccount(addrs[1])
+	require.Nil(t, err)
+
+	beforeBalance1, _ := big.NewInt(0).SetString(beforeBalanceStr1, 10)
+	expectedBalance1 := big.NewInt(0).Add(beforeBalance1, egldValue)
+	expectedBalance1 = big.NewInt(0).Add(expectedBalance1, egldValue)
 
 	require.Equal(t, expectedBalance1.String(), account1.Balance)
 }
