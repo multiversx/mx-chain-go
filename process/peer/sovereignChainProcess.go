774f0beb24b57ebb3aba62de7f07bead96d2179a
MariusC <marius.ciucam@gmail.com>
2024-08-01 18:21:12 +0300

    FEAT: Revert back to nil update peer state

--- process/peer/sovereignChainProcess.go
+++ process/peer/sovereignChainProcess.go
@@ -1,8 +1,6 @@
 package peer
 
 import (
-	"math/big"
-
 	"github.com/multiversx/mx-chain-core-go/data"
 	"github.com/multiversx/mx-chain-go/process"
 )
@@ -27,56 +25,8 @@ func NewSovereignChainValidatorStatisticsProcessor(validatorStatistics *validato
 }
 
 func (vs *sovereignChainValidatorStatistics) updateShardDataPeerState(
-	header data.CommonHeaderHandler,
-	cacheMap map[string]data.CommonHeaderHandler,
+	_ data.CommonHeaderHandler,
+	_ map[string]data.CommonHeaderHandler,
 ) error {
 	return nil
-	if header.GetNonce() == vs.genesisNonce {
-		return nil
-	}
-
-	epoch := computeEpoch(header)
-
-	log.Debug("updateShardDataPeerState - registering shard leader fees",
-		"shard header round", header.GetRound(),
-		"accumulatedFees", header.GetAccumulatedFees().String(),
-		"developerFees", header.GetDeveloperFees().String(),
-	)
-
-	prevShardData, shardInfoErr := vs.searchInMap(header.GetPrevHash(), cacheMap)
-	if shardInfoErr != nil {
-		return shardInfoErr
-	}
-
-	prevShardConsensus, err := vs.nodesCoordinator.ComputeConsensusGroup(
-		prevShardData.GetPrevRandSeed(),
-		prevShardData.GetRound(),
-		prevShardData.GetShardID(),
-		epoch,
-	)
-	if err != nil {
-		return shardInfoErr
-	}
-
-	shardInfoErr = vs.updateValidatorInfoOnSuccessfulBlock(
-		prevShardConsensus,
-		prevShardData.GetPubKeysBitmap(),
-		big.NewInt(0).Sub(prevShardData.GetAccumulatedFees(), prevShardData.GetDeveloperFees()),
-		prevShardData.GetShardID(),
-	)
-	if shardInfoErr != nil {
-		return shardInfoErr
-	}
-
-	if header.GetNonce() == vs.genesisNonce+1 {
-		return nil
-	}
-
-	return vs.checkForMissedBlocks(
-		header.GetRound(),
-		prevShardData.GetRound(),
-		prevShardData.GetRandSeed(),
-		header.GetShardID(),
-		epoch,
-	)
 }
