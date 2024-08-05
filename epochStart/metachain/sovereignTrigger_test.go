c0237a6bf6e864a4ea4019b5dd5a4660733a507b
MariusC <marius.ciucam@gmail.com>
2024-08-05 13:43:18 +0300

    FEAT: Refactor test for RevertStateToBlock with interfaces

--- epochStart/metachain/sovereignTrigger_test.go
+++ epochStart/metachain/sovereignTrigger_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/multiversx/mx-chain-core-go/data"
 	"github.com/multiversx/mx-chain-core-go/data/block"
 	"github.com/multiversx/mx-chain-go/epochStart"
 	"github.com/stretchr/testify/require"
@@ -41,4 +42,29 @@ func TestSovereignTrigger_SetProcessed(t *testing.T) {
 	_ = sovHdr.SetStartOfEpochHeader()
 	sovTrigger.SetProcessed(sovHdr, &block.Body{})
 	require.Equal(t, uint32(4), sovTrigger.Epoch())
+}
+
+func TestSovereignTrigger_RevertBehindEpochStartBlock(t *testing.T) {
+	t.Parallel()
+
+	triggerFactory := func(arguments *ArgsNewMetaEpochStartTrigger) epochStart.TriggerHandler {
+		sovEpochStartTrigger, _ := NewSovereignTrigger(arguments)
+		return sovEpochStartTrigger
+	}
+	metaHdrFactory := func(round uint64, epoch uint32, isEpochStart bool) data.MetaHeaderHandler {
+		sovHdr := &block.SovereignChainHeader{
+			Header: &block.Header{
+				Round: round,
+				Epoch: epoch,
+			},
+		}
+
+		if isEpochStart {
+			sovHdr.IsStartOfEpoch = true
+		}
+
+		return sovHdr
+	}
+
+	testTriggerRevertBehindEpochStartBlock(t, triggerFactory, metaHdrFactory)
 }
