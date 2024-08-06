62319912669859403fc1e1fe7e8d1a9510c0faf9
MariusC <marius.ciucam@gmail.com>
2024-08-06 12:38:02 +0300

    DOC: Add task

--- process/block/shardblock.go
+++ process/block/shardblock.go
@@ -1081,7 +1081,7 @@ func (sp *shardProcessor) commonHeaderAndBodyCommit(
 	}
 
 	sp.blockChain.SetCurrentBlockHeaderHash(headerHash)
-	// TODO: Here, also index reward txs for sovereign
+	// TODO: MX-15708 Here, also index reward txs for sovereign and analyse other missing meta fields
 	sp.indexBlockIfNeeded(body, headerHash, header, lastBlockHeader)
 	sp.recordBlockInHistory(headerHash, header, body)
 
