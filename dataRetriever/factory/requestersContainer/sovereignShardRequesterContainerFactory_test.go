4d5804f4d35e6c538da72c5b1375a90ac6d8c6ba
MariusC <marius.ciucam@gmail.com>
2024-08-13 13:10:32 +0300

    FEAT: Sovereign requesters

--- dataRetriever/factory/requestersContainer/sovereignShardRequesterContainerFactory_test.go
+++ dataRetriever/factory/requestersContainer/sovereignShardRequesterContainerFactory_test.go
@@ -1,8 +1,12 @@
 package requesterscontainer_test
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 
+	"github.com/multiversx/mx-chain-core-go/core"
+	"github.com/multiversx/mx-chain-go/dataRetriever"
 	"github.com/multiversx/mx-chain-go/dataRetriever/factory/requestersContainer"
 	"github.com/multiversx/mx-chain-go/errors"
 	"github.com/multiversx/mx-chain-go/process/factory"
@@ -50,8 +54,38 @@ func TestSovereignShardRequestersContainerFactory_Create(t *testing.T) {
 	_, err = container.Get(extendedHeaderKey)
 	require.Nil(t, err)
 
-	numShards := int(shardCoord.NumberOfShards())
-	require.Equal(t, getNumRequesters(numShards)+1, container.Len()) // only one added container for extended header
+	numRequesterSCRs := 1
+	numRequesterTxs := 1
+	numRequesterRewardTxs := 0
+	numRequesterHeaders := 1
+	numRequesterMiniBlocks := 1
+	numRequesterMetaBlockHeaders := 0
+	numRequesterTrieNodes := 2
+	numRequesterPeerAuth := 1
+	numRequesterValidatorInfo := 1
+	numRequesterExtendedHeader := 1
+	numRequesters := numRequesterTxs + numRequesterHeaders + numRequesterMiniBlocks + numRequesterMetaBlockHeaders +
+		numRequesterSCRs + numRequesterRewardTxs + numRequesterTrieNodes + numRequesterPeerAuth + numRequesterValidatorInfo + numRequesterExtendedHeader
+
+	require.Equal(t, numRequesters, container.Len()) // only one added container for extended header
+
+	topicDelim := "_"
+	topicDelimCt := 0
+	iterateFunc := func(key string, requester dataRetriever.Requester) bool {
+		require.False(t, strings.Contains(strings.ToLower(key), "meta"))
+
+		if strings.Contains(key, topicDelim) {
+			keyTokens := strings.Split(key, topicDelim)
+			require.Len(t, keyTokens, 2)
+			require.Equal(t, fmt.Sprintf("%d", core.SovereignChainShardId), keyTokens[1])
+			topicDelimCt++
+		}
+
+		return true
+	}
+
+	container.Iterate(iterateFunc)
+	require.Equal(t, numRequesters-2, topicDelimCt) // without peerAuthentication + validatorInfo topics
 }
 
 func TestSovereignShardRequestersContainerFactory_NumPeers(t *testing.T) {
