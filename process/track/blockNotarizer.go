3524e1a45cc8a05e39fce433fe772e0a6517bf32
axenteoctavian <axente.octavian@gmail.com>
2024-08-16 13:45:01 +0300

    set log debug

--- process/track/blockNotarizer.go
+++ process/track/blockNotarizer.go
@@ -9,6 +9,7 @@ import (
 	"github.com/multiversx/mx-chain-core-go/data"
 	"github.com/multiversx/mx-chain-core-go/hashing"
 	"github.com/multiversx/mx-chain-core-go/marshal"
+
 	"github.com/multiversx/mx-chain-go/process"
 	"github.com/multiversx/mx-chain-go/sharding"
 )
@@ -129,7 +130,7 @@ func (bn *blockNotarizer) DisplayNotarizedHeaders(shardID uint32, message string
 		"nb", len(notarizedHeaders))
 
 	for _, hdrInfo := range notarizedHeaders {
-		log.Trace("notarized header info",
+		log.Debug("notarized header info",
 			"round", hdrInfo.Header.GetRound(),
 			"nonce", hdrInfo.Header.GetNonce(),
 			"hash", hdrInfo.Hash)
