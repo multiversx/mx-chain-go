e47ef515ee8db63a87a21e5e9f6ba1797b2444c3
Alexander Cristurean <alexander.cristurean@yahoo.com>
2024-08-28 16:51:46 +0300

    scaffold for state changes.

--- outport/host/driver.go
+++ outport/host/driver.go
@@ -9,24 +9,22 @@ import (
 	"github.com/multiversx/mx-chain-core-go/data/outport"
 	"github.com/multiversx/mx-chain-core-go/marshal"
 
-	"github.com/multiversx/mx-chain-go/state"
+	"github.com/multiversx/mx-chain-go/state/stateChanges"
 )
 
 // ArgsHostDriver holds the arguments needed for creating a new hostDriver
 type ArgsHostDriver struct {
-	Marshaller            marshal.Marshalizer
-	SenderHost            SenderHost
-	Log                   core.Logger
-	StateChangesCollector state.StateChangesCollector
+	Marshaller marshal.Marshalizer
+	SenderHost SenderHost
+	Log        core.Logger
 }
 
 type hostDriver struct {
-	marshaller            marshal.Marshalizer
-	senderHost            SenderHost
-	isClosed              atomic.Flag
-	log                   core.Logger
-	payloadProc           payloadProcessorHandler
-	stateChangesCollector state.StateChangesCollector
+	marshaller  marshal.Marshalizer
+	senderHost  SenderHost
+	isClosed    atomic.Flag
+	log         core.Logger
+	payloadProc payloadProcessorHandler
 }
 
 // NewHostDriver will create a new instance of hostDriver
@@ -40,9 +38,6 @@ func NewHostDriver(args ArgsHostDriver) (*hostDriver, error) {
 	if check.IfNil(args.Log) {
 		return nil, core.ErrNilLogger
 	}
-	if check.IfNil(args.StateChangesCollector) {
-		return nil, ErrNilStateChangesCollector
-	}
 
 	payloadProc, err := newPayloadProcessor(args.Log)
 	if err != nil {
@@ -55,12 +50,11 @@ func NewHostDriver(args ArgsHostDriver) (*hostDriver, error) {
 	}
 
 	return &hostDriver{
-		marshaller:            args.Marshaller,
-		senderHost:            args.SenderHost,
-		log:                   args.Log,
-		isClosed:              atomic.Flag{},
-		payloadProc:           payloadProc,
-		stateChangesCollector: args.StateChangesCollector,
+		marshaller:  args.Marshaller,
+		senderHost:  args.SenderHost,
+		log:         args.Log,
+		isClosed:    atomic.Flag{},
+		payloadProc: payloadProc,
 	}, nil
 }
 
@@ -87,10 +81,6 @@ func (o *hostDriver) SaveValidatorsPubKeys(validatorsPubKeys *outport.Validators
 // SaveValidatorsRating will handle the saving of the validators' rating
 func (o *hostDriver) SaveValidatorsRating(validatorsRating *outport.ValidatorsRating) error {
 	return o.handleAction(validatorsRating, outport.TopicSaveValidatorsRating)
-}
-
-func (o *hostDriver) SaveStateChanges() error {
-	return o.handleAction(o.stateChangesCollector.RetrieveStateChanges(), "SaveStateChanges")
 }
 
 // SaveAccounts will handle the accounts' saving
@@ -145,4 +135,13 @@ func (o *hostDriver) Close() error {
 // IsInterfaceNil returns true if there is no value under the interface
 func (o *hostDriver) IsInterfaceNil() bool {
 	return o == nil
+}
+
+func (o *hostDriver) SendDummy(data []byte) error {
+	return o.handleAction(data, "SendDummy")
+}
+
+func (o *hostDriver) SaveStateChanges(stateChanges []stateChanges.StateChange) error {
+	//TODO: add this as a constant
+	return o.handleAction(stateChanges, "SaveStateChanges")
 }
