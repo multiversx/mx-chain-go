4c377155fd7f91419f4425ccdaeb5e6e25fb1138
danielradu <daniel.radu@student.tuiasi.ro>
2024-09-02 09:12:16 +0300

    small fix after review

--- sharding/oneShardCoordinator_test.go
+++ sharding/oneShardCoordinator_test.go
@@ -12,46 +12,22 @@ func TestOneShardCoordinator_NumberOfShardsShouldWork(t *testing.T) {
 
 	oneShardCoordinator := OneShardCoordinator{}
 
-	t.Run("NumberOfShardsShouldWork", func(t *testing.T) {
-		t.Parallel()
+	returnedVal := oneShardCoordinator.NumberOfShards()
+	require.Equal(t, uint32(1), returnedVal)
 
-		returnedVal := oneShardCoordinator.NumberOfShards()
-		require.Equal(t, uint32(1), returnedVal)
-	})
+	returnedVal = oneShardCoordinator.ComputeId([]byte{})
+	require.Equal(t, uint32(0), returnedVal)
 
-	t.Run("ComputeIdShouldWork", func(t *testing.T) {
-		t.Parallel()
+	returnedVal = oneShardCoordinator.SelfId()
+	require.Equal(t, uint32(0), returnedVal)
 
-		returnedVal := oneShardCoordinator.ComputeId([]byte{})
-		require.Equal(t, uint32(0), returnedVal)
-	})
+	isShameShard := oneShardCoordinator.SameShard(nil, nil)
+	require.True(t, isShameShard)
 
-	t.Run("SelfIdShouldWork", func(t *testing.T) {
-		t.Parallel()
+	communicationID := oneShardCoordinator.CommunicationIdentifier(0)
+	require.Equal(t, core.CommunicationIdentifierBetweenShards(0, 0), communicationID)
 
-		returnedVal := oneShardCoordinator.SelfId()
-		require.Equal(t, uint32(0), returnedVal)
-	})
-
-	t.Run("SameShardShouldWork", func(t *testing.T) {
-		t.Parallel()
-
-		returnedVal := oneShardCoordinator.SameShard(nil, nil)
-		require.True(t, returnedVal)
-	})
-
-	t.Run("CommunicationIdentifierShouldWork", func(t *testing.T) {
-		t.Parallel()
-
-		returnedVal := oneShardCoordinator.CommunicationIdentifier(0)
-		require.Equal(t, core.CommunicationIdentifierBetweenShards(0, 0), returnedVal)
-	})
-
-	t.Run("IsInterfaceNilShouldWork", func(t *testing.T) {
-		t.Parallel()
-
-		returnedVal := oneShardCoordinator.IsInterfaceNil()
-		require.False(t, returnedVal)
-	})
+	isInterfaceNile := oneShardCoordinator.IsInterfaceNil()
+	require.False(t, isInterfaceNile)
 
 }
