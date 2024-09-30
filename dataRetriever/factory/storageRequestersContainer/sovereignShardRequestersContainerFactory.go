dc47d293fdd1289e9cc50a1a93003c8109b16f53
MariusC <marius.ciucam@gmail.com>
2024-09-30 15:41:47 +0300

    FEAT: Extended header requester + unit test

--- dataRetriever/factory/storageRequestersContainer/sovereignShardRequestersContainerFactory.go
+++ dataRetriever/factory/storageRequestersContainer/sovereignShardRequestersContainerFactory.go
@@ -2,13 +2,27 @@ package storagerequesterscontainer
 
 import (
 	"github.com/multiversx/mx-chain-core-go/core"
+	"github.com/multiversx/mx-chain-core-go/core/check"
 	"github.com/multiversx/mx-chain-go/common"
 	"github.com/multiversx/mx-chain-go/dataRetriever"
+	storagerequesters "github.com/multiversx/mx-chain-go/dataRetriever/storageRequesters"
+	errorsMx "github.com/multiversx/mx-chain-go/errors"
 	"github.com/multiversx/mx-chain-go/process/factory"
 )
 
 type sovereignShardRequestersContainerFactory struct {
 	*shardRequestersContainerFactory
+}
+
+// NewSovereignShardRequestersContainerFactory creates a sovereign container filled with topic requesters
+func NewSovereignShardRequestersContainerFactory(baseContainer *shardRequestersContainerFactory) (*sovereignShardRequestersContainerFactory, error) {
+	if check.IfNil(baseContainer) {
+		return nil, errorsMx.ErrNilShardRequesterContainerFactory
+	}
+
+	return &sovereignShardRequestersContainerFactory{
+		shardRequestersContainerFactory: baseContainer,
+	}, nil
 }
 
 // Create returns a requester container that will hold all requesters in the system
@@ -50,6 +64,11 @@ func (srcf *sovereignShardRequestersContainerFactory) Create() (dataRetriever.Re
 		return nil, err
 	}
 
+	err = srcf.generateExtendedShardHeaderRequesters()
+	if err != nil {
+		return nil, err
+	}
+
 	return srcf.container, nil
 }
 
@@ -86,4 +105,42 @@ func (srcf *sovereignShardRequestersContainerFactory) generateMiniBlocksRequeste
 	keys[0] = identifierMiniBlocks
 
 	return srcf.container.AddMultiple(keys, requestersSlice)
+}
+
+func (srcf *sovereignShardRequestersContainerFactory) generateExtendedShardHeaderRequesters() error {
+	shardC := srcf.shardCoordinator
+	identifierHdr := factory.ExtendedHeaderProofTopic + shardC.CommunicationIdentifier(shardC.SelfId())
+
+	hdrStorer, err := srcf.store.GetStorer(dataRetriever.ExtendedShardHeadersUnit)
+	if err != nil {
+		return err
+	}
+
+	hdrNonceHashDataUnit := dataRetriever.ExtendedShardHeadersNonceHashDataUnit + dataRetriever.UnitType(shardC.SelfId())
+	hdrNonceStore, err := srcf.store.GetStorer(hdrNonceHashDataUnit)
+	if err != nil {
+		return err
+	}
+
+	arg := storagerequesters.ArgHeaderRequester{
+		Messenger:                srcf.messenger,
+		ResponseTopicName:        identifierHdr,
+		NonceConverter:           srcf.uint64ByteSliceConverter,
+		HdrStorage:               hdrStorer,
+		HeadersNoncesStorage:     hdrNonceStore,
+		ManualEpochStartNotifier: srcf.manualEpochStartNotifier,
+		ChanGracefullyClose:      srcf.chanGracefullyClose,
+		DelayBeforeGracefulClose: defaultBeforeGracefulClose,
+	}
+	requester, err := storagerequesters.NewHeaderRequester(arg)
+	if err != nil {
+		return err
+	}
+
+	return srcf.container.Add(identifierHdr, requester)
+}
+
+// IsInterfaceNil returns true if there is no value under the interface
+func (srcf *sovereignShardRequestersContainerFactory) IsInterfaceNil() bool {
+	return srcf == nil
 }
