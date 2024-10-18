efcd1d5de688b72a3937cdc480085d8ffae14fe8
MariusC <marius.ciucam@gmail.com>
2024-10-18 15:41:51 +0300

    FEAT: RunTypeComponentsHolder for incoming header processor

--- process/block/sovereign/incomingHeader/incomingHeaderHandlerFactory.go
+++ process/block/sovereign/incomingHeader/incomingHeaderHandlerFactory.go
@@ -1,14 +1,26 @@
 package incomingHeader
 
 import (
+	"github.com/multiversx/mx-chain-core-go/core/check"
 	"github.com/multiversx/mx-chain-go/config"
 	"github.com/multiversx/mx-chain-go/dataRetriever"
+	sovereignBlock "github.com/multiversx/mx-chain-go/dataRetriever/dataPool/sovereign"
+	errorsMx "github.com/multiversx/mx-chain-go/errors"
 	mainFactory "github.com/multiversx/mx-chain-go/factory"
 	"github.com/multiversx/mx-chain-go/process"
+	"github.com/multiversx/mx-chain-go/process/block/sovereign"
 
 	hasherFactory "github.com/multiversx/mx-chain-core-go/hashing/factory"
 	marshallerFactory "github.com/multiversx/mx-chain-core-go/marshal/factory"
 )
+
+// RunTypeComponentsHolder defines run type components needed to create an incoming header processor
+type RunTypeComponentsHolder interface {
+	OutGoingOperationsPoolHandler() sovereignBlock.OutGoingOperationsPool
+	DataCodecHandler() sovereign.DataCodecHandler
+	TopicsCheckerHandler() sovereign.TopicsCheckerHandler
+	IsInterfaceNil() bool
+}
 
 // CreateIncomingHeaderProcessor creates the incoming header processor
 func CreateIncomingHeaderProcessor(
@@ -17,6 +29,10 @@ func CreateIncomingHeaderProcessor(
 	mainChainNotarizationStartRound uint64,
 	runTypeComponents mainFactory.RunTypeComponentsHolder,
 ) (process.IncomingHeaderSubscriber, error) {
+	if check.IfNil(runTypeComponents) {
+		return nil, errorsMx.ErrNilRunTypeComponents
+	}
+
 	marshaller, err := marshallerFactory.NewMarshalizer(config.WebSocketConfig.MarshallerType)
 	if err != nil {
 		return nil, err
