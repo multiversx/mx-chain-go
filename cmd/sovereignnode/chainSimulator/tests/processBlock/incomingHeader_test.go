c83b8bf97c7dee976db03aa4c5f288361a8a9897
MariusC <marius.ciucam@gmail.com>
2024-10-22 12:57:19 +0300

    FEAT: Add some ugly chain sim tests, will be good when everything works

--- cmd/sovereignnode/chainSimulator/tests/processBlock/incomingHeader_test.go
+++ cmd/sovereignnode/chainSimulator/tests/processBlock/incomingHeader_test.go
@@ -8,11 +8,15 @@ import (
 	"time"
 
 	"github.com/multiversx/mx-chain-core-go/core"
+	"github.com/multiversx/mx-chain-core-go/data"
 	coreAPI "github.com/multiversx/mx-chain-core-go/data/api"
 	"github.com/multiversx/mx-chain-core-go/data/block"
 	"github.com/multiversx/mx-chain-core-go/data/sovereign"
 	"github.com/multiversx/mx-chain-core-go/data/transaction"
+	"github.com/multiversx/mx-chain-go/config"
+	process2 "github.com/multiversx/mx-chain-go/process"
 	"github.com/multiversx/mx-chain-go/process/block/sovereign/incomingHeader"
+	logger "github.com/multiversx/mx-chain-logger-go"
 	"github.com/stretchr/testify/require"
 
 	chainSim "github.com/multiversx/mx-chain-go/integrationTests/chainSimulator"
@@ -89,6 +93,143 @@ func TestSovereignChainSimulator_IncomingHeader(t *testing.T) {
 	require.NotNil(t, esdts)
 	require.True(t, esdts[token] != nil)
 	require.Equal(t, amountToTransfer, esdts[token].Value.String())
+}
+
+type sovChainBlockTracer interface {
+	process2.BlockTracker
+	ComputeLongestExtendedShardChainFromLastNotarized() ([]data.HeaderHandler, [][]byte, error)
+	IsGenesisLastCrossNotarizedHeader() bool
+}
+
+var log = logger.GetOrCreate("dsa")
+
+func TestSovereignChainSimulator_AddIncomingHeaderExpectCorrectGenesisBlock(t *testing.T) {
+	if testing.Short() {
+		t.Skip("this is not a short test")
+	}
+
+	startRound := uint64(101)
+	cs, err := sovereignChainSimulator.NewSovereignChainSimulator(sovereignChainSimulator.ArgsSovereignChainSimulator{
+		SovereignConfigPath: sovereignConfigPath,
+		ArgsChainSimulator: &chainSimulator.ArgsChainSimulator{
+			BypassTxSignatureCheck: false,
+			TempDir:                t.TempDir(),
+			PathToInitialConfig:    defaultPathToInitialConfig,
+			GenesisTimestamp:       time.Now().Unix(),
+			RoundDurationInMillis:  uint64(6000),
+			RoundsPerEpoch:         core.OptionalUint64{},
+			ApiInterface:           api.NewNoApiInterface(),
+			MinNodesPerShard:       2,
+			AlterConfigsFunction: func(cfg *config.Configs) {
+				cfg.GeneralConfig.SovereignConfig.MainChainNotarization.MainChainNotarizationStartRound = startRound
+			},
+		},
+	})
+	require.Nil(t, err)
+	require.NotNil(t, cs)
+
+	defer cs.Close()
+
+	nodeHandler := cs.GetNodeHandler(core.SovereignChainShardId)
+
+	headerNonce := startRound - 5
+	prevHeader := createHeaderV2(headerNonce, generateRandomHash(), generateRandomHash())
+
+	sovBlockTracker, castOk := nodeHandler.GetProcessComponents().BlockTracker().(sovChainBlockTracer)
+	require.True(t, castOk)
+
+	for currRound := startRound - 4; currRound < startRound+5; currRound++ {
+
+		incomingHeader, incomingHeaderHash := createIncomingHeader(nodeHandler, &headerNonce, prevHeader, []*transaction.Event{})
+		err = nodeHandler.GetIncomingHeaderSubscriber().AddHeader(incomingHeaderHash, incomingHeader)
+		require.Nil(t, err)
+
+		lastCrossNotarizedHeader, _, err := sovBlockTracker.GetLastCrossNotarizedHeader(core.MainChainShardId)
+		require.Nil(t, err)
+
+		err = cs.GenerateBlocks(1)
+		require.Nil(t, err)
+
+		currentSovBlock := nodeHandler.GetChainHandler().GetCurrentBlockHeader().(data.SovereignChainHeaderHandler)
+
+		log.Error("currRound", "currRound", currRound)
+
+		if currRound <= 99 {
+			require.Zero(t, lastCrossNotarizedHeader.GetRound())
+			require.True(t, sovBlockTracker.IsGenesisLastCrossNotarizedHeader())
+			require.Empty(t, currentSovBlock.GetExtendedShardHeaderHashes())
+		} else if currRound == 100 {
+			logger.SetLogLevel("*:DEBUG")
+		} else if currRound == 101 {
+			require.Equal(t, startRound-1, lastCrossNotarizedHeader.GetRound())
+			require.False(t, sovBlockTracker.IsGenesisLastCrossNotarizedHeader())
+
+			log.Error("DSADSADSADSAD")
+
+			//require.Equal(t, [][]byte{incomingHeaderHash}, currentSovBlock.GetExtendedShardHeaderHashes())
+		} else {
+			require.Equal(t, currRound-2, lastCrossNotarizedHeader.GetRound())
+			require.False(t, sovBlockTracker.IsGenesisLastCrossNotarizedHeader())
+
+			//longestChain, _, err := sovBlockTracker.ComputeLongestExtendedShardChainFromLastNotarized()
+			//require.Nil(t, err)
+			//require.Len(t, longestChain, 1)
+			//require.Equal(t, currRound-1, longestChain[0].GetRound())
+		}
+
+		prevHeader = incomingHeader.Header
+
+	}
+}
+
+func TestSovereignChainSimulator_IncomingHeader2(t *testing.T) {
+	if testing.Short() {
+		t.Skip("this is not a short test")
+	}
+
+	cs, err := sovereignChainSimulator.NewSovereignChainSimulator(sovereignChainSimulator.ArgsSovereignChainSimulator{
+		SovereignConfigPath: sovereignConfigPath,
+		ArgsChainSimulator: &chainSimulator.ArgsChainSimulator{
+			BypassTxSignatureCheck: false,
+			TempDir:                t.TempDir(),
+			PathToInitialConfig:    defaultPathToInitialConfig,
+			GenesisTimestamp:       time.Now().Unix(),
+			RoundDurationInMillis:  uint64(6000),
+			RoundsPerEpoch:         core.OptionalUint64{},
+			ApiInterface:           api.NewNoApiInterface(),
+			MinNodesPerShard:       2,
+			AlterConfigsFunction: func(cfg *config.Configs) {
+				cfg.GeneralConfig.SovereignConfig.MainChainNotarization.MainChainNotarizationStartRound = 123456
+			},
+		},
+	})
+	require.Nil(t, err)
+	require.NotNil(t, cs)
+
+	defer cs.Close()
+
+	nodeHandler := cs.GetNodeHandler(core.SovereignChainShardId)
+
+	headerNonce := uint64(123456) - 4
+	prevHeader := createHeaderV2(headerNonce, generateRandomHash(), generateRandomHash())
+
+	cs.GenerateBlocks(3)
+
+	logger.SetLogLevel("*:DEBUG")
+	for i := 0; i <= 12; i++ {
+		log.Error("CONTOOOOOOOOOOOOR", "CONTOOOOOOOOOOOOR", i)
+
+		if i%3 == 0 {
+			incomingHeader, headerHash := createIncomingHeader(nodeHandler, &headerNonce, prevHeader, []*transaction.Event{})
+			err = nodeHandler.GetIncomingHeaderSubscriber().AddHeader(headerHash, incomingHeader)
+			require.Nil(t, err)
+
+			prevHeader = incomingHeader.Header
+		}
+
+		err = cs.GenerateBlocks(1)
+		require.Nil(t, err)
+	}
 }
 
 func createIncomingHeader(nodeHandler process.NodeHandler, headerNonce *uint64, prevHeader *block.HeaderV2, txsEvent []*transaction.Event) (*sovereign.IncomingHeader, []byte) {
