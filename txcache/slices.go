d6c48207aa51aee19caa09e1bef086486dffdd48
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2024-11-05 20:43:56 +0200

    Cleanup, some readme, refactoring.

--- txcache/slices.go
+++
@@ -1,7 +0,0 @@
-package txcache
-
-func reverseSlice[T any](s []T) {
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
-		s[i], s[j] = s[j], s[i]
-	}
-}
--- txcache/transactionsMinHeap.go
+++ txcache/transactionsMinHeap.go
@@ -5,7 +5,7 @@ type TransactionsMinHeap []*TransactionsHeapItem
 func (minHeap TransactionsMinHeap) Len() int { return len(minHeap) }
 
 func (minHeap TransactionsMinHeap) Less(i, j int) bool {
-	return minHeap[j].transaction.isTransactionMoreDesirableByProtocol(minHeap[i].transaction)
+	return minHeap[j].transaction.isTransactionMoreDesirableToNetwork(minHeap[i].transaction)
 }
 
 func (minHeap TransactionsMinHeap) Swap(i, j int) {
