7c5283632280f7182af9fe63fda7bc8a1ef38dda
MariusC <marius.ciucam@gmail.com>
2024-11-11 15:45:38 +0200

    FEAT: Unit tests intercepted sov header + mbs

--- process/block/interceptedBlocks/export_test.go
+++ process/block/interceptedBlocks/export_test.go
@@ -1,6 +1,16 @@
 package interceptedBlocks
 
+import (
+	"github.com/multiversx/mx-chain-core-go/data"
+	"github.com/multiversx/mx-chain-go/sharding"
+)
+
 // IsMetaHeaderOutOfRange -
 func (imh *InterceptedMetaHeader) IsMetaHeaderOutOfRange() bool {
 	return imh.isMetaHeaderEpochOutOfRange()
+}
+
+// CheckMiniBlocksHeaders -
+func (isbh *interceptedSovereignBlockHeader) CheckMiniBlocksHeaders(mbHeaders []data.MiniBlockHeaderHandler, coordinator sharding.Coordinator) error {
+	return isbh.checkMiniBlocksHeaders(mbHeaders, coordinator)
 }
