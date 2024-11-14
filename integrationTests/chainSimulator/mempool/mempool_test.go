0cb7187f64765bbb70d64f352858b1a3c33feb5c
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2024-11-14 16:17:38 +0200

    Sketch CS test: TestMempoolWithChainSimulator_Eviction.

---
+++ integrationTests/chainSimulator/mempool/mempool_test.go
@@ -0,0 +1,152 @@
+package relayedTx
+
+import (
+	"math/big"
+	"testing"
+	"time"
+
+	"github.com/multiversx/mx-chain-core-go/core"
+	"github.com/multiversx/mx-chain-core-go/data/transaction"
+	"github.com/multiversx/mx-chain-go/config"
+	testsChainSimulator "github.com/multiversx/mx-chain-go/integrationTests/chainSimulator"
+	"github.com/multiversx/mx-chain-go/node/chainSimulator"
+	"github.com/multiversx/mx-chain-go/node/chainSimulator/components/api"
+	"github.com/multiversx/mx-chain-go/node/chainSimulator/configs"
+	"github.com/multiversx/mx-chain-go/node/chainSimulator/dtos"
+	"github.com/multiversx/mx-chain-go/storage"
+	logger "github.com/multiversx/mx-chain-logger-go"
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	oneEGLD = big.NewInt(1000000000000000000)
+	log     = logger.GetOrCreate("testing")
+)
+
+func TestMempoolWithChainSimulator_Eviction(t *testing.T) {
+	if testing.Short() {
+		t.Skip("this is not a short test")
+	}
+
+	cs := startChainSimulator(t, func(cfg *config.Configs) {})
+	node := cs.GetNodeHandler(0)
+	mempool := node.GetDataComponents().Datapool().Transactions()
+
+	defer cs.Close()
+
+	initialBalance := big.NewInt(0).Mul(oneEGLD, big.NewInt(10))
+
+	numSenders := 10000
+	senders := make([]dtos.WalletAddress, numSenders)
+	sendersNonces := make([]uint64, numSenders)
+
+	for i := 0; i < numSenders; i++ {
+		sender, err := cs.GenerateAndMintWalletAddress(0, initialBalance)
+		require.NoError(t, err)
+
+		senders[i] = sender
+	}
+
+	receiver, err := cs.GenerateAndMintWalletAddress(0, big.NewInt(0))
+	require.NoError(t, err)
+
+	err = cs.GenerateBlocks(1)
+	require.Nil(t, err)
+
+	numTransactionsPerSender := 30
+	transactions := make([]*transaction.Transaction, 0, numSenders*numTransactionsPerSender)
+
+	for i := 0; i < numSenders; i++ {
+		for j := 0; j < numTransactionsPerSender; j++ {
+			tx := &transaction.Transaction{
+				Nonce:     sendersNonces[i],
+				Value:     oneEGLD,
+				SndAddr:   senders[i].Bytes,
+				RcvAddr:   receiver.Bytes,
+				Data:      []byte{},
+				GasLimit:  50000,
+				GasPrice:  1_000_000_000,
+				ChainID:   []byte(configs.ChainID),
+				Version:   2,
+				Signature: []byte("signature"),
+			}
+
+			sendersNonces[i]++
+			transactions = append(transactions, tx)
+		}
+	}
+
+	numSent, err := node.GetFacadeHandler().SendBulkTransactions(transactions)
+	require.NoError(t, err)
+	require.Equal(t, 300000, int(numSent))
+
+	time.Sleep(500 * time.Millisecond)
+	require.Equal(t, 300000, int(mempool.GetCounts().GetTotal()))
+
+	// Send one more transaction (fill up the mempool)
+	node.GetFacadeHandler().SendBulkTransactions([]*transaction.Transaction{
+		{
+			Nonce:     42,
+			Value:     oneEGLD,
+			SndAddr:   senders[7].Bytes,
+			RcvAddr:   receiver.Bytes,
+			Data:      []byte{},
+			GasLimit:  50000,
+			GasPrice:  1_000_000_000,
+			ChainID:   []byte(configs.ChainID),
+			Version:   2,
+			Signature: []byte("signature"),
+		},
+	})
+
+	time.Sleep(42 * time.Millisecond)
+	require.Equal(t, 300001, int(mempool.GetCounts().GetTotal()))
+
+	// Send one more transaction to trigger eviction
+	node.GetFacadeHandler().SendBulkTransactions([]*transaction.Transaction{
+		{
+			Nonce:     42,
+			Value:     oneEGLD,
+			SndAddr:   senders[7].Bytes,
+			RcvAddr:   receiver.Bytes,
+			Data:      []byte{},
+			GasLimit:  50000,
+			GasPrice:  1_000_000_000,
+			ChainID:   []byte(configs.ChainID),
+			Version:   2,
+			Signature: []byte("signature"),
+		},
+	})
+
+	time.Sleep(500 * time.Millisecond)
+	require.Equal(t, 300000+1+1-int(storage.TxPoolSourceMeNumItemsToPreemptivelyEvict), int(mempool.GetCounts().GetTotal()))
+}
+
+func startChainSimulator(t *testing.T, alterConfigsFunction func(cfg *config.Configs),
+) testsChainSimulator.ChainSimulator {
+	simulator, err := chainSimulator.NewChainSimulator(chainSimulator.ArgsChainSimulator{
+		BypassTxSignatureCheck: true,
+		TempDir:                t.TempDir(),
+		PathToInitialConfig:    "../../../cmd/node/config/",
+		NumOfShards:            1,
+		GenesisTimestamp:       time.Now().Unix(),
+		RoundDurationInMillis:  uint64(4000),
+		RoundsPerEpoch: core.OptionalUint64{
+			HasValue: true,
+			Value:    10,
+		},
+		ApiInterface:             api.NewNoApiInterface(),
+		MinNodesPerShard:         1,
+		MetaChainMinNodes:        1,
+		NumNodesWaitingListMeta:  0,
+		NumNodesWaitingListShard: 0,
+		AlterConfigsFunction:     alterConfigsFunction,
+	})
+	require.NoError(t, err)
+	require.NotNil(t, simulator)
+
+	err = simulator.GenerateBlocksUntilEpochIsReached(1)
+	require.NoError(t, err)
+
+	return simulator
+}
