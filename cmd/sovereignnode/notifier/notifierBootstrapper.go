5e680c8643cd7c3e8fd736330b9a969da8375ea2
MariusC <marius.ciucam@gmail.com>
2024-11-20 15:12:10 +0200

    FEAT: Refactor notifier bootstrap to work with confidence threshold to Register

--- cmd/sovereignnode/notifier/notifierBootstrapper.go
+++ cmd/sovereignnode/notifier/notifierBootstrapper.go
@@ -6,7 +6,6 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/multiversx/mx-chain-core-go/core"
 	"github.com/multiversx/mx-chain-core-go/core/check"
 	logger "github.com/multiversx/mx-chain-logger-go"
 	notifierProcess "github.com/multiversx/mx-chain-sovereign-notifier-go/process"
@@ -14,6 +13,8 @@ import (
 	"github.com/multiversx/mx-chain-go/errors"
 	"github.com/multiversx/mx-chain-go/process"
 )
+
+const roundsThreshold = process.MaxRoundsWithoutNewBlockReceived + 1
 
 var log = logger.GetOrCreate("notifier-bootstrap")
 
@@ -33,11 +34,9 @@ type notifierBootstrapper struct {
 	forkDetector          process.ForkDetector
 	sigStopNode           chan os.Signal
 
-	nodeSyncedChan chan bool
-	cancelFunc     func()
-	roundDuration  uint64
-
-	confidenceFactor uint32
+	syncedRoundsChan chan int32
+	cancelFunc       func()
+	roundDuration    uint64
 }
 
 // NewNotifierBootstrapper creates a ws receiver connection registration bootstrapper
@@ -50,7 +49,7 @@ func NewNotifierBootstrapper(args ArgsNotifierBootstrapper) (*notifierBootstrapp
 		incomingHeaderHandler: args.IncomingHeaderHandler,
 		sovereignNotifier:     args.SovereignNotifier,
 		forkDetector:          args.ForkDetector,
-		nodeSyncedChan:        make(chan bool, 1),
+		syncedRoundsChan:      make(chan int32, 1),
 		cancelFunc:            nil,
 		roundDuration:         args.RoundDuration,
 		sigStopNode:           args.SigStopNode,
@@ -83,17 +82,20 @@ func checkArgs(args ArgsNotifierBootstrapper) error {
 
 func (nb *notifierBootstrapper) receivedSyncState(isNodeSynchronized bool) {
 	if isNodeSynchronized && nb.forkDetector.GetHighestFinalBlockNonce() != 0 {
-		select {
-		case nb.nodeSyncedChan <- true:
-		default:
-		}
+		nb.writeRoundsDeltaOnChan(1)
 	} else if !isNodeSynchronized {
-		nb.confidenceFactor--
-		nb.confidenceFactor = core.MaxUint32(nb.confidenceFactor, 0)
+		nb.writeRoundsDeltaOnChan(-1)
 	}
 }
 
-// Start will start waiting on a go routine to be notified via nodeSyncedChan when the sovereign node is synced.
+func (nb *notifierBootstrapper) writeRoundsDeltaOnChan(delta int32) {
+	select {
+	case nb.syncedRoundsChan <- delta:
+	default:
+	}
+}
+
+// Start will start waiting on a go routine to be notified via syncedRoundsChan when the sovereign node is synced.
 // Meanwhile, it will print the current node state in log. When node is fully synced, it will register the incoming header
 // processor to the websocket listener and exit the waiting loop.
 func (nb *notifierBootstrapper) Start() {
@@ -103,19 +105,21 @@ func (nb *notifierBootstrapper) Start() {
 }
 
 func (nb *notifierBootstrapper) checkNodeState(ctx context.Context) {
-	timeToWaitReSync := (process.MaxRoundsWithoutNewBlockReceived + 1) * nb.roundDuration
+	timeToWaitReSync := uint64(roundsThreshold) * nb.roundDuration
 	ticker := time.NewTicker(time.Duration(timeToWaitReSync) * time.Millisecond)
 	defer ticker.Stop()
+
+	var syncedRounds uint32
 
 	for {
 		select {
 		case <-ctx.Done():
 			log.Debug("notifierBootstrapper.checkNodeState: worker's go routine is stopping...")
 			return
-		case <-nb.nodeSyncedChan:
-			nb.confidenceFactor++
-			if nb.confidenceFactor < process.MaxRoundsWithoutNewBlockReceived+1 {
-				log.Error("NOT YET", "confidence factory", nb.confidenceFactor)
+		case delta := <-nb.syncedRoundsChan:
+			syncedRounds = updateSyncedRounds(syncedRounds, delta)
+			if syncedRounds < uint32(roundsThreshold) {
+				log.Debug("notifierBootstrapper.checkNodeState", "syncedRounds", syncedRounds)
 				continue
 			}
 
@@ -124,7 +128,7 @@ func (nb *notifierBootstrapper) checkNodeState(ctx context.Context) {
 				log.Error("notifierBootstrapper: sovereignNotifier.RegisterHandler", "err", err)
 				nb.sigStopNode <- syscall.SIGTERM
 			} else {
-				log.Debug("notifierBootstrapper.checkNodeState", "is node synced", true)
+				log.Info("notifierBootstrapper.checkNodeState", "is node synced", true)
 			}
 
 			return
@@ -134,15 +138,37 @@ func (nb *notifierBootstrapper) checkNodeState(ctx context.Context) {
 	}
 }
 
+func updateSyncedRounds(syncedRounds uint32, delta int32) uint32 {
+	if delta > 0 {
+		syncedRounds += uint32(delta)
+	} else if syncedRounds > 0 {
+		syncedRounds--
+	}
+
+	return syncedRounds
+}
+
 // Close cancels current context and empties channel reads
 func (nb *notifierBootstrapper) Close() error {
 	if nb.cancelFunc != nil {
 		nb.cancelFunc()
 	}
 
-	nrReads := core.EmptyChannel(nb.nodeSyncedChan)
-	log.Debug("notifierBootstrapper: emptied channel", "nodeSyncedChan nrReads", nrReads)
+	nrReads := emptyChannel(nb.syncedRoundsChan)
+	log.Debug("notifierBootstrapper: emptied channel", "syncedRoundsChan nrReads", nrReads)
 	return nil
+}
+
+func emptyChannel(ch chan int32) int {
+	readsCnt := 0
+	for {
+		select {
+		case <-ch:
+			readsCnt++
+		default:
+			return readsCnt
+		}
+	}
 }
 
 // IsInterfaceNil checks if the underlying pointer is nil
