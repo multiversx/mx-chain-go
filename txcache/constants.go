6f5d549aee9747a075e6e24a0e10f31c8aa687dd
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2024-11-29 16:25:17 +0200

    Fix linter issues.

--- txcache/constants.go
+++ txcache/constants.go
@@ -1,6 +1,5 @@
 package txcache
 
 const diagnosisMaxTransactionsToDisplay = 10000
-const diagnosisSelectionGasRequested = 10_000_000_000
 const initialCapacityOfSelectionSlice = 30000
 const selectionLoopDurationCheckInterval = 10
