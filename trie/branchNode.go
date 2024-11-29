6c93eb3a12db39fc4ccc3909e0c06dfd0b5eb734
BeniaminDrasovean <beniamin.drasovean@elrond.network>
2024-11-29 15:17:44 +0200

    add EndGoRoutineProcessing call

--- trie/branchNode.go
+++ trie/branchNode.go
@@ -504,6 +504,7 @@ func (bn *branchNode) updateNode(
 		go func(childPos int) {
 			updateFunc(data[childPos], childPos, goRoutinesManager, modifiedHashes, hasBeenModified, db)
 
+			goRoutinesManager.EndGoRoutineProcessing()
 			waitGroup.Done()
 		}(childPos)
 	}
