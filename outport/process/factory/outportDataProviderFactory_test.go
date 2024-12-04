fe5798622d7e78548d9b38ed29a5adea42d74e07
MariusC <marius.ciucam@gmail.com>
2024-12-04 17:07:58 +0200

    FEAT: Common tests for sovereignOutportDataProviderFactory

--- outport/process/factory/outportDataProviderFactory_test.go
+++ outport/process/factory/outportDataProviderFactory_test.go
@@ -6,45 +6,51 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	"github.com/multiversx/mx-chain-go/outport"
 	"github.com/multiversx/mx-chain-go/outport/process/alteredaccounts"
 )
 
-func TestCreateOutportDataProviderDisabled(t *testing.T) {
+type outportFactory interface {
+	CreateOutportDataProvider(arg ArgOutportDataProviderFactory) (outport.DataProviderOutport, error)
+	IsInterfaceNil() bool
+}
+
+func TestCreateOutportDataProvider(t *testing.T) {
 	t.Parallel()
 
+	factory := NewOutportDataProviderFactory()
+	testCreateOutportDataProviderDisabled(t, factory)
+	testCreateOutportDataProviderError(t, factory)
+	testCreateOutportDataProvider(t, factory, "*process.outportDataProvider")
+}
+
+func testCreateOutportDataProviderDisabled(t *testing.T, factory outportFactory) {
 	arg := createArgOutportDataProviderFactory()
 	arg.HasDrivers = false
 
-	factory := NewOutportDataProviderFactory()
 	provider, err := factory.CreateOutportDataProvider(arg)
 	require.Nil(t, err)
 	require.NotNil(t, provider)
 	require.Equal(t, "*disabled.disabledOutportDataProvider", fmt.Sprintf("%T", provider))
 }
 
-func TestCreateOutportDataProviderError(t *testing.T) {
-	t.Parallel()
-
+func testCreateOutportDataProviderError(t *testing.T, factory outportFactory) {
 	arg := createArgOutportDataProviderFactory()
 	arg.HasDrivers = true
 	arg.AddressConverter = nil
 
-	factory := NewOutportDataProviderFactory()
 	provider, err := factory.CreateOutportDataProvider(arg)
 	require.Nil(t, provider)
 	require.Equal(t, alteredaccounts.ErrNilPubKeyConverter, err)
 }
 
-func TestCreateOutportDataProvider(t *testing.T) {
-	t.Parallel()
-
+func testCreateOutportDataProvider(t *testing.T, factory outportFactory, expectedType string) {
 	arg := createArgOutportDataProviderFactory()
 	arg.HasDrivers = true
 
-	factory := NewOutportDataProviderFactory()
 	provider, err := factory.CreateOutportDataProvider(arg)
 	require.Nil(t, err)
 	require.NotNil(t, provider)
-	require.Equal(t, "*process.outportDataProvider", fmt.Sprintf("%T", provider))
+	require.Equal(t, expectedType, fmt.Sprintf("%T", provider))
 	require.False(t, factory.IsInterfaceNil())
 }
