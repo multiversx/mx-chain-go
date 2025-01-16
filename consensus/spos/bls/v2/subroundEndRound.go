d8c9b0a26cceacd15c26ea34db8b8a6502ac62a3
ssd04 <dariussuirab@gmail.com>
2025-01-16 15:50:07 +0200

    do not return false if already existing proof

--- consensus/spos/bls/v2/subroundEndRound.go
+++ consensus/spos/bls/v2/subroundEndRound.go
@@ -264,7 +264,6 @@ func (sr *subroundEndRound) doEndRoundJobByNode() bool {
 		err = sr.EquivalentProofsPool().AddProof(proof)
 		if err != nil {
 			sr.Log.Debug("doEndRoundJobByNode.AddProof", "error", err)
-			return false
 		}
 	}
 
