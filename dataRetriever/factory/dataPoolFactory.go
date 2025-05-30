9edee2fb4db744a06effb05ee9d3056301c01501
Daniel Radu <daniel.radu@student.tuiasi.ro>
2025-05-30 12:50:45 +0300

    fixes after merging constants-config into upperbounds-config

--- dataRetriever/factory/dataPoolFactory.go
+++ dataRetriever/factory/dataPoolFactory.go
@@ -69,7 +69,7 @@ func NewDataPoolFromConfig(args ArgsDataPool) (dataRetriever.PoolsHolder, error)
 		Marshalizer:                    args.Marshalizer,
 		NumberOfShards:                 args.ShardCoordinator.NumberOfShards(),
 		SelfShardID:                    args.ShardCoordinator.SelfId(),
-		MaxNumBytesPerSenderUpperBound: mainConfig.TransactionsPool.MaxNumBytesPerSenderUpperBound,
+		MaxNumBytesPerSenderUpperBound: mainConfig.MempoolSelection.MaxNumBytesPerSenderUpperBound,
 	})
 	if err != nil {
 		return nil, fmt.Errorf("%w while creating the cache for the transactions", err)
