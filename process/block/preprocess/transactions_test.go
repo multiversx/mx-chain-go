1d6c7fd6293b18b2c102ab060315504772e5eb4b
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2025-06-11 11:41:27 +0300

    Fix import.

--- process/block/preprocess/transactions_test.go
+++ process/block/preprocess/transactions_test.go
@@ -11,7 +11,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ethereum/go-ethereum/log"
 	"github.com/multiversx/mx-chain-core-go/core"
 	"github.com/multiversx/mx-chain-core-go/data"
 	"github.com/multiversx/mx-chain-core-go/data/block"
