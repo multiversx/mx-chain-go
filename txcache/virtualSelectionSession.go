0b383b489a7d2de976bea965541711847fec53a5
Daniel Radu <daniel.radu31.dr.dr@gmail.com>
2025-06-19 15:58:07 +0300

    linter fix

--- txcache/virtualSelectionSession.go
+++ txcache/virtualSelectionSession.go
@@ -1,15 +1,11 @@
 package txcache
 
-import "math/big"
-
 type virtualSelectionSession struct {
 	session                  SelectionSession
 	virtualAccountsByAddress map[string]*virtualAccountRecord
 }
 
 type virtualAccountRecord struct {
-	initialNonce   uint64
-	initialBalance *big.Int
 }
 
 func newVirtualSelectionSession(session SelectionSession) *virtualSelectionSession {
--- txcache/virtualSelectionSession_test.go
+++ txcache/virtualSelectionSession_test.go
@@ -7,14 +7,8 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// this test will be removed
 func Test_newVirtualSelectionSession(t *testing.T) {
 	session := txcachemocks.NewSelectionSessionMock()
 	vss := newVirtualSelectionSession(session)
 	require.NotNil(t, vss)
-
-	vss.virtualAccountsByAddress["addr1"] = &virtualAccountRecord{
-		initialNonce:   0,
-		initialBalance: nil,
-	}
 }
