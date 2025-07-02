4f7732e0f1d9b8b7d6ab868c3930deb77a6a6c54
Daniel Radu <daniel.radu31.dr.dr@gmail.com>
2025-07-02 14:23:24 +0300

    methods and unit tests

--- txcache/virtualSelectionSession.go
+++ txcache/virtualSelectionSession.go
@@ -4,6 +4,7 @@ import (
 	"math/big"
 
 	"github.com/multiversx/mx-chain-core-go/core"
+	"github.com/multiversx/mx-chain-core-go/data"
 )
 
 type virtualSelectionSession struct {
@@ -19,8 +20,9 @@ type virtualAccountRecord struct {
 
 func newVirtualAccountRecord(initialNonce core.OptionalUint64, initialBalance *big.Int) *virtualAccountRecord {
 	return &virtualAccountRecord{
-		initialNonce:   initialNonce,
-		initialBalance: initialBalance,
+		initialNonce:    initialNonce,
+		initialBalance:  initialBalance,
+		consumedBalance: big.NewInt(0),
 	}
 }
 
@@ -62,4 +64,39 @@ func (virtualSession *virtualSelectionSession) getNonce(address []byte) (uint64,
 	}
 
 	return account.initialNonce.Value, nil
+}
+
+func (virtualSession *virtualSelectionSession) detectWillFeeExceedBalance(tx *WrappedTransaction) bool {
+	fee := tx.Fee
+	if fee == nil {
+		return false
+	}
+
+	// Here, we are not interested into an eventual transfer of value (we only check if there's enough balance to pay the transaction fee).
+	feePayer := tx.FeePayer
+	feePayerRecord, err := virtualSession.getVirtualRecord(feePayer)
+	if err != nil {
+		log.Debug("virtualSelectionSession.detectWillFeeExceedBalance",
+			"err", err)
+		return false
+	}
+
+	futureConsumedBalance := new(big.Int).Add(feePayerRecord.consumedBalance, fee)
+	feePayerBalance := feePayerRecord.initialBalance
+
+	willFeeExceedBalance := futureConsumedBalance.Cmp(feePayerBalance) > 0
+	if willFeeExceedBalance {
+		logSelect.Trace("virtualSelectionSession.detectWillFeeExceedBalance",
+			"tx", tx.TxHash,
+			"feePayer", feePayer,
+			"initialBalance", feePayerRecord.initialBalance,
+			"consumedBalance", feePayerRecord.consumedBalance,
+		)
+	}
+
+	return willFeeExceedBalance
+}
+
+func (virtualSession *virtualSelectionSession) isIncorrectlyGuarded(tx data.TransactionHandler) bool {
+	return virtualSession.session.IsIncorrectlyGuarded(tx)
 }
--- txcache/virtualSelectionSession_test.go
+++ txcache/virtualSelectionSession_test.go
@@ -1,14 +1,175 @@
 package txcache
 
 import (
+	"errors"
+	"math/big"
 	"testing"
 
+	"github.com/multiversx/mx-chain-core-go/core"
+	"github.com/multiversx/mx-chain-go/state"
+	stateMock "github.com/multiversx/mx-chain-go/testscommon/state"
 	"github.com/multiversx/mx-chain-go/testscommon/txcachemocks"
 	"github.com/stretchr/testify/require"
 )
 
 func Test_newVirtualSelectionSession(t *testing.T) {
+	t.Parallel()
+
 	session := txcachemocks.NewSelectionSessionMock()
 	virtualSession := newVirtualSelectionSession(session)
 	require.NotNil(t, virtualSession)
+}
+
+func Test_getVirtualRecord(t *testing.T) {
+	t.Parallel()
+
+	t.Run("should return virtual record", func(t *testing.T) {
+		t.Parallel()
+
+		sessionMock := txcachemocks.SelectionSessionMock{}
+		virtualSession := newVirtualSelectionSession(&sessionMock)
+
+		expectedRecord := virtualAccountRecord{
+			initialNonce: core.OptionalUint64{
+				Value:    3,
+				HasValue: true,
+			},
+			initialBalance:  big.NewInt(2),
+			consumedBalance: big.NewInt(3),
+		}
+		virtualSession.virtualAccountsByAddress = map[string]*virtualAccountRecord{
+			"alice": &expectedRecord,
+		}
+
+		actualRecord, err := virtualSession.getVirtualRecord([]byte("alice"))
+		require.NoError(t, err)
+		require.Equal(t, &expectedRecord, actualRecord)
+	})
+
+	t.Run("should return account from real session", func(t *testing.T) {
+		t.Parallel()
+
+		sessionMock := txcachemocks.SelectionSessionMock{
+			GetAccountStateCalled: func(address []byte) (state.UserAccountHandler, error) {
+				return &stateMock.StateUserAccountHandlerStub{
+					GetBalanceCalled: func() *big.Int {
+						return big.NewInt(2)
+					},
+					GetNonceCalled: func() uint64 {
+						return 2
+					},
+				}, nil
+			},
+		}
+		virtualSession := newVirtualSelectionSession(&sessionMock)
+
+		expectedRecord := virtualAccountRecord{
+			initialNonce: core.OptionalUint64{
+				Value:    2,
+				HasValue: true,
+			},
+			initialBalance:  big.NewInt(2),
+			consumedBalance: big.NewInt(0),
+		}
+		actualRecord, err := virtualSession.getVirtualRecord([]byte("alice"))
+
+		require.NoError(t, err)
+		require.Equal(t, expectedRecord.initialNonce, actualRecord.initialNonce)
+		require.Equal(t, expectedRecord.initialBalance, actualRecord.initialBalance)
+		require.Equal(t, expectedRecord.consumedBalance, actualRecord.consumedBalance)
+	})
+
+	t.Run("should err", func(t *testing.T) {
+		t.Parallel()
+
+		expErr := errors.New("error")
+		sessionMock := txcachemocks.SelectionSessionMock{
+			GetAccountStateCalled: func(address []byte) (state.UserAccountHandler, error) {
+				return nil, expErr
+			},
+		}
+		virtualSession := newVirtualSelectionSession(&sessionMock)
+
+		actualRecord, err := virtualSession.getVirtualRecord([]byte("alice"))
+		require.Nil(t, actualRecord)
+		require.Equal(t, expErr, err)
+
+	})
+}
+
+func Test_getNonce(t *testing.T) {
+	t.Parallel()
+
+	t.Run("should return nonce from real session", func(t *testing.T) {
+		t.Parallel()
+
+		sessionMock := txcachemocks.SelectionSessionMock{
+			GetAccountStateCalled: func(address []byte) (state.UserAccountHandler, error) {
+				return &stateMock.StateUserAccountHandlerStub{
+					GetBalanceCalled: func() *big.Int {
+						return big.NewInt(2)
+					},
+					GetNonceCalled: func() uint64 {
+						return 2
+					},
+				}, nil
+			},
+		}
+		virtualSession := newVirtualSelectionSession(&sessionMock)
+		actualNonce, err := virtualSession.getNonce([]byte("alice"))
+
+		require.NoError(t, err)
+		require.Equal(t, uint64(2), actualNonce)
+	})
+
+	t.Run("should return nonce from account record", func(t *testing.T) {
+		t.Parallel()
+
+		sessionMock := txcachemocks.SelectionSessionMock{
+			GetAccountStateCalled: func(address []byte) (state.UserAccountHandler, error) {
+				return &stateMock.StateUserAccountHandlerStub{
+					GetBalanceCalled: func() *big.Int {
+						return big.NewInt(2)
+					},
+					GetNonceCalled: func() uint64 {
+						return 2
+					},
+				}, nil
+			},
+		}
+
+		virtualSession := newVirtualSelectionSession(&sessionMock)
+
+		expectedRecord := virtualAccountRecord{
+			initialNonce: core.OptionalUint64{
+				Value:    3,
+				HasValue: true,
+			},
+			initialBalance:  big.NewInt(2),
+			consumedBalance: big.NewInt(3),
+		}
+		virtualSession.virtualAccountsByAddress = map[string]*virtualAccountRecord{
+			"alice": &expectedRecord,
+		}
+
+		actualNonce, err := virtualSession.getNonce([]byte("alice"))
+
+		require.NoError(t, err)
+		require.Equal(t, uint64(3), actualNonce)
+	})
+
+	t.Run("should err", func(t *testing.T) {
+		t.Parallel()
+
+		expErr := errors.New("error")
+		sessionMock := txcachemocks.SelectionSessionMock{
+			GetAccountStateCalled: func(address []byte) (state.UserAccountHandler, error) {
+				return nil, expErr
+			},
+		}
+		virtualSession := newVirtualSelectionSession(&sessionMock)
+
+		_, err := virtualSession.getNonce([]byte("alice"))
+		require.Equal(t, expErr, err)
+	})
 }
