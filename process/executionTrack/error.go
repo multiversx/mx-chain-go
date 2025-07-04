fbdd36043181ad3bb35466e578a058683567d901
miiu <mihai9625@gmail.com>
2025-07-04 15:13:00 +0300

    remove unused err

--- process/executionTrack/error.go
+++ process/executionTrack/error.go
@@ -5,7 +5,4 @@ import "errors"
 var (
 	// ErrNilExecutionResult signals  that a nil execution results has been provided
 	ErrNilExecutionResult = errors.New("nil execution result")
-
-	// ErrCannotFindExecutionResult signals that an execution result cannot be found
-	ErrCannotFindExecutionResult = errors.New("cannot find execution result")
 )
