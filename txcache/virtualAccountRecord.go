0c23f316e5162f3b12533a41c55b191d7183b0a0
Daniel Radu <daniel.radu31.dr.dr@gmail.com>
2025-07-18 15:12:45 +0300

    added comments

--- txcache/virtualAccountRecord.go
+++ txcache/virtualAccountRecord.go
@@ -24,10 +24,16 @@ func (virtualRecord *virtualAccountRecord) updateVirtualRecord(breadcrumb *accou
 	_ = virtualRecord.consumedBalance.Add(virtualRecord.consumedBalance, breadcrumb.consumedBalance)
 
 	if !breadcrumb.lastNonce.HasValue {
+		// in a certain tracked block, we can have breadcrumbs for accounts that are only used as relayers and never as senders
+		// those accounts don't have a set nonce, here we treat those cases
 		return
 	}
 
 	if !virtualRecord.initialNonce.HasValue {
+		// a virtual record is created accumulating information from at least one breadcrumb
+		// we could have a breadcrumb for the same account in two different tracked blocks
+		// a breadcrumb without nonce (if first occurred in record creation) leads to a virtual record without nonce
+		// here we treat the virtual records which don't have a nonce yet.
 		virtualRecord.setInitialNonce(breadcrumb)
 		return
 	}
@@ -43,6 +49,12 @@ func (virtualRecord *virtualAccountRecord) setInitialNonce(breadcrumb *accountBr
 }
 
 func (virtualRecord *virtualAccountRecord) updateInitialNonce(breadcrumb *accountBreadcrumb) {
+	// this solves the next scenario:
+	// two different breadcrumbs for the same sender
+	// in the first tracked block, the last tx of that sender has nonce X -> virtual record, for the moment, has nonce X
+	// in the second tracked block, the last tx of that sender has nonce X + N
+	// the virtual record has to be updated with the maximum between current value (X) and (X+N) value
+	// increasing by 1 helps knowing that we can select txs starting from this nonce of the virtual record
 	virtualRecord.initialNonce = core.OptionalUint64{
 		Value:    max(breadcrumb.lastNonce.Value, virtualRecord.initialNonce.Value) + 1,
 		HasValue: true,
