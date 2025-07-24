d15d433fd26411c6e64aca967a4ab75283d7144a
Mihaela Radian <mihaela.radian@multiversx.com>
2025-07-24 10:30:29 +0300

    alter comment for getGasHigherFactorAccepted

--- process/economics/gasConfigHandler.go
+++ process/economics/gasConfigHandler.go
@@ -114,8 +114,7 @@ func (handler *gasConfigHandler) getMaxGasLimitPerMiniBlock(epoch uint32) uint64
 	return gc.maxGasLimitPerMiniBlock
 }
 
-//TODO change description for this function
-// getMaxGasLimitPerMiniBlock returns max gas limit per mini block in a specific epoch
+// getGasHigherFactorAccepted returns the maximum gas higher factor accepted for the given epoch
 func (handler *gasConfigHandler) getGasHigherFactorAccepted(epoch uint32) uint64 {
 	gc := handler.getGasConfigForEpoch(epoch)
 	return gc.maxGasHigherFactorAccepted
