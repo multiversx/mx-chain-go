81ddc3a8a4823217dd7f593b223a5d06be79063e
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2025-08-29 14:20:46 +0300

    Adjust logging etc.

--- txcache/selectionTracker.go
+++ txcache/selectionTracker.go
@@ -61,10 +61,11 @@ func (st *selectionTracker) OnProposedBlock(
 	defer st.mutTracker.Unlock()
 
 	log.Debug("selectionTracker.OnProposedBlock",
-		"blockHash", blockHash,
 		"nonce", nonce,
+		"blockHash", blockHash,
 		"rootHash", rootHash,
-		"prevHash", prevHash)
+		"prevHash", prevHash,
+	)
 
 	// TODO brainstorm if this could be moved after getChainOfTrackedBlocks
 	txs, err := st.getTransactionsInBlock(blockBody)
@@ -113,10 +114,17 @@ func (st *selectionTracker) OnExecutedBlock(blockHeader data.HeaderHandler) erro
 	rootHash := blockHeader.GetRootHash()
 	prevHash := blockHeader.GetPrevHash()
 
+	log.Debug("selectionTracker.OnExecutedBlock",
+		"nonce", nonce,
+		"rootHash", rootHash,
+		"prevHash", prevHash,
+	)
+
 	tempTrackedBlock, err := newTrackedBlock(nonce, nil, rootHash, prevHash, nil)
 	if err != nil {
 		return err
 	}
+
 	st.mutTracker.Lock()
 	defer st.mutTracker.Unlock()
 
