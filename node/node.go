c01f3679580ac419397b65375edcf3b5092114a4
BeniaminDrasovean <beniamin.drasovean@elrond.network>
2025-10-01 12:51:08 +0300

    update iterateKeysMainTrie const

--- node/node.go
+++ node/node.go
@@ -51,7 +51,7 @@ const (
 	// esdtTickerNumChars represents the number of hex-encoded characters of a ticker
 	esdtTickerNumChars = 6
 
-	iterateKeysMainTrie = "mainTrie"
+	iterateKeysMainTrie = "maintrie"
 )
 
 var log = logger.GetOrCreate("node")
