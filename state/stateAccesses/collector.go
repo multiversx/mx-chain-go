a7db17699ecd6ed80fa2777e0db27891629b9c82
BeniaminDrasovean <beniamin.drasovean@elrond.network>
2025-10-02 15:48:03 +0300

    add missing test and update go mod

--- state/stateAccesses/collector.go
+++ state/stateAccesses/collector.go
@@ -175,10 +175,9 @@ func logCollectedStateAccesses(message string, stateAccessesForTx map[string]*da
 func stateAccessToString(stateAccess *data.StateAccess) string {
 	dataTrieChanges := make([]string, len(stateAccess.GetDataTrieChanges()))
 	for i, dataTrieChange := range stateAccess.GetDataTrieChanges() {
-		dataTrieChanges[i] = fmt.Sprintf("key: %v, val: %v, type: %v, operation %v, version %v", hex.EncodeToString(dataTrieChange.Key), hex.EncodeToString(dataTrieChange.Val), dataTrieChange.Type, dataTrieChange.Operation, dataTrieChange.Version)
+		dataTrieChanges[i] = fmt.Sprintf("key: %v, val: %v, type: %v, operation: %v, version: %v", hex.EncodeToString(dataTrieChange.Key), hex.EncodeToString(dataTrieChange.Val), dataTrieChange.Type, dataTrieChange.Operation, dataTrieChange.Version)
 	}
-	return fmt.Sprintf("type: %v, operation: %v, mainTrieKey: %v, mainTrieVal: %v, index: %v, dataTrieChanges: %v, accountChanges %v",
-		stateAccess.GetType(),
+	return fmt.Sprintf("type: %v, operation: %v, mainTrieKey: %v, mainTrieVal: %v, index: %v, dataTrieChanges: %v, accountChanges: %v", stateAccess.GetType(),
 		stateAccess.GetOperation(),
 		hex.EncodeToString(stateAccess.GetMainTrieKey()),
 		hex.EncodeToString(stateAccess.GetMainTrieVal()),
