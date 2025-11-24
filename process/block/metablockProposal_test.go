f508ef27aea749368dc5f6999609b26a16f23910
Daniel Radu <daniel.radu31.dr.dr@gmail.com>
2025-11-24 14:50:53 +0200

    unit tests

--- process/block/metablockProposal_test.go
+++ process/block/metablockProposal_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/multiversx/mx-chain-core-go/core"
 	"github.com/multiversx/mx-chain-core-go/data"
 	"github.com/multiversx/mx-chain-core-go/data/block"
+	"github.com/multiversx/mx-chain-go/testscommon/epochNotifier"
 	"github.com/stretchr/testify/require"
 
 	"github.com/multiversx/mx-chain-go/state"
@@ -3735,6 +3736,153 @@ func TestMetaProcessor_collectExecutionResultsEpochStartProposal(t *testing.T) {
 		require.Equal(t, metaExecResult.ExecutedTxCount, uint64(2))
 		require.Equal(t, metaExecResult.ReceiptsHash, []byte("receiptHash"))
 		require.Equal(t, metaExecResult.GetValidatorStatsRootHash(), []byte("valStatRootHash"))
+	})
+}
+
+func TestMetaProcessor_ProcessBlockProposal(t *testing.T) {
+	t.Parallel()
+
+	t.Run("should return ErrNilBlockHeader because of nil argument", func(t *testing.T) {
+		t.Parallel()
+
+		coreComponents, dataComponents, boostrapComponents, statusComponents := createMockComponentHolders()
+		arguments := createMockMetaArguments(coreComponents, dataComponents, boostrapComponents, statusComponents)
+
+		mp, err := blproc.NewMetaProcessor(arguments)
+		require.Nil(t, err)
+
+		_, err = mp.ProcessBlockProposal(nil, &block.Body{})
+		require.Equal(t, process.ErrNilBlockHeader, err)
+	})
+
+	t.Run("should return ErrNilBlockBody because of nil argument", func(t *testing.T) {
+		t.Parallel()
+
+		coreComponents, dataComponents, boostrapComponents, statusComponents := createMockComponentHolders()
+		arguments := createMockMetaArguments(coreComponents, dataComponents, boostrapComponents, statusComponents)
+
+		mp, err := blproc.NewMetaProcessor(arguments)
+		require.Nil(t, err)
+
+		_, err = mp.ProcessBlockProposal(&block.MetaBlockV3{}, nil)
+		require.Equal(t, process.ErrNilBlockBody, err)
+	})
+
+	t.Run("should return ErrInvalidHeader because of nil argument", func(t *testing.T) {
+		t.Parallel()
+
+		coreComponents, dataComponents, boostrapComponents, statusComponents := createMockComponentHolders()
+		arguments := createMockMetaArguments(coreComponents, dataComponents, boostrapComponents, statusComponents)
+
+		mp, err := blproc.NewMetaProcessor(arguments)
+		require.Nil(t, err)
+
+		_, err = mp.ProcessBlockProposal(&block.MetaBlock{}, &block.Body{})
+		require.Equal(t, process.ErrInvalidHeader, err)
+	})
+
+	t.Run("should return ErrWrongTypeAssertion in case of wrong header", func(t *testing.T) {
+		t.Parallel()
+
+		checkEpochCounter := 0
+		coreComponents, dataComponents, boostrapComponents, statusComponents := createMockComponentHolders()
+		coreComponents.EpochNotifierField = &epochNotifier.EpochNotifierStub{
+			CheckEpochCalled: func(header data.HeaderHandler) {
+				checkEpochCounter += 1
+			},
+		}
+
+		checkRoundCounter := 0
+		coreComponents.RoundNotifierField = &epochNotifier.RoundNotifierStub{
+			CheckRoundCalled: func(header data.HeaderHandler) {
+				checkRoundCounter += 1
+			},
+		}
+		arguments := createMockMetaArguments(coreComponents, dataComponents, boostrapComponents, statusComponents)
+
+		mp, err := blproc.NewMetaProcessor(arguments)
+		require.Nil(t, err)
+
+		_, err = mp.ProcessBlockProposal(&block.HeaderV3{
+			Round: 2,
+			Epoch: 2,
+		}, &block.Body{})
+		require.Equal(t, process.ErrWrongTypeAssertion, err)
+		require.Equal(t, 1, checkEpochCounter)
+	})
+
+	t.Run("should return ErrAccountStateDirty in case of dirty state", func(t *testing.T) {
+		t.Parallel()
+
+		coreComponents, dataComponents, boostrapComponents, statusComponents := createMockComponentHolders()
+		arguments := createMockMetaArguments(coreComponents, dataComponents, boostrapComponents, statusComponents)
+
+		accountsDb := make(map[state.AccountsDbIdentifier]state.AccountsAdapter)
+		accounts := &testscommonState.AccountsStub{
+			CommitCalled: func() ([]byte, error) {
+				return nil, nil
+			},
+			RootHashCalled: func() ([]byte, error) {
+				return nil, nil
+			},
+			RecreateTrieIfNeededCalled: func(options common.RootHashHolder) error {
+				return nil
+			},
+			JournalLenCalled: func() int {
+				return 1
+			},
+		}
+
+		accountsDb[state.UserAccountsState] = accounts
+		accountsDb[state.PeerAccountsState] = accounts
+
+		arguments.AccountsDB = accountsDb
+		mp, err := blproc.NewMetaProcessor(arguments)
+		require.Nil(t, err)
+
+		_, err = mp.ProcessBlockProposal(&block.MetaBlockV3{}, &block.Body{})
+		require.Equal(t, process.ErrAccountStateDirty, err)
+	})
+
+	t.Run("if checking context fails, the error should be propagated", func(t *testing.T) {
+		t.Parallel()
+
+		previousHash := []byte("hash")
+		coreComponents, dataComponents, boostrapComponents, statusComponents := createMockComponentHolders()
+		dataComponents.BlockChain = &testscommon.ChainHandlerStub{
+			GetLastExecutedBlockInfoCalled: func() (uint64, []byte, []byte) {
+				return 0, previousHash, nil
+			},
+		}
+		arguments := createMockMetaArguments(coreComponents, dataComponents, boostrapComponents, statusComponents)
+
+		mp, err := blproc.NewMetaProcessor(arguments)
+		require.Nil(t, err)
+
+		_, err = mp.ProcessBlockProposal(&block.MetaBlockV3{
+			PrevHash: []byte("wrongHash"),
+		}, &block.Body{})
+		require.Equal(t, process.ErrBlockHashDoesNotMatch, err)
+	})
+
+	t.Run("if creating block fails, the error should be propagated", func(t *testing.T) {
+		t.Parallel()
+
+		coreComponents, dataComponents, boostrapComponents, statusComponents := createMockComponentHolders()
+		arguments := createMockMetaArguments(coreComponents, dataComponents, boostrapComponents, statusComponents)
+
+		arguments.TxCoordinator = &testscommon.TransactionCoordinatorMock{
+			AddIntermediateTransactionsCalled: func(mapSCRs map[block.Type][]data.TransactionHandler, key []byte) error {
+				return expectedErr
+			},
+		}
+		mp, err := blproc.NewMetaProcessor(arguments)
+		require.Nil(t, err)
+
+		_, err = mp.ProcessBlockProposal(&block.MetaBlockV3{
+			Nonce: 1,
+		}, &block.Body{})
+		require.Equal(t, expectedErr, err)
 	})
 }
 
