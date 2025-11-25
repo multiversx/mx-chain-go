19d363717245e2963a9197bb639ecd1383bcb4ab
Sorin Stanculeanu <sorinstanculeanu26@gmail.com>
2025-11-25 12:34:16 +0200

    updated gas config and fixed failing tests

--- integrationTests/chainSimulator/simulate/simulate_test.go
+++ integrationTests/chainSimulator/simulate/simulate_test.go
@@ -266,6 +266,7 @@ func TestRelayedV3(t *testing.T) {
 	err = cs.ForceChangeOfEpoch()
 	require.NoError(t, err)
 
+	tx.GasLimit = 0 // reset GasLimit so it will be completed according to the new block limits for the updated epoch
 	cost, err = cs.GetNodeHandler(0).GetFacadeHandler().ComputeTransactionGasLimit(tx)
 	require.NoError(t, err)
 	require.Equal(t, uint64(855001), cost.GasUnits)
