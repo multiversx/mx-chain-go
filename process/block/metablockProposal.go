5a4aabf4b3c9c55c2e9dc515fe4417d7a3636591
ssd04 <dariussuirab@gmail.com>
2025-12-09 11:43:00 +0200

    fix debug log with err

--- process/block/metablockProposal.go
+++ process/block/metablockProposal.go
@@ -1061,8 +1061,9 @@ func (mp *metaProcessor) getShardHeadersFromMetaHeader(
 			shardInfoHandler.GetShardID(),
 		)
 		if err != nil {
-			log.Debug("hasExecutionResultsForProposedEpochChange: could not find header",
+			log.Debug("getShardHeadersFromMetaHeader: could not find header",
 				"hash", shardInfoHandler.GetHeaderHash(),
+				"error", err,
 			)
 			return nil, process.ErrMissingHeader
 		}
