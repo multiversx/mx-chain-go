39f9afa0fcaf6a920166df345b944fd9d1c01ede
Adrian Dobrita <anaridado@gmail.com>
2026-02-12 16:27:03 +0200

    avoid CPU churn on context validation error

--- process/asyncExecution/executionManager/executionManager.go
+++ process/asyncExecution/executionManager/executionManager.go
@@ -8,8 +8,6 @@ import (
 	"github.com/multiversx/mx-chain-core-go/marshal"
 	logger "github.com/multiversx/mx-chain-logger-go"
 
-	"github.com/multiversx/mx-chain-go/storage"
-
 	"github.com/multiversx/mx-chain-go/dataRetriever"
 	"github.com/multiversx/mx-chain-go/process"
 	"github.com/multiversx/mx-chain-go/process/asyncExecution/cache"
