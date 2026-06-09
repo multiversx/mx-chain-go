682e1ff1a867a7ad0e127792854f0a1a0c61f662
Adrian Dobrita <anaridado@gmail.com>
2026-06-09 16:22:44 +0300

    add meta sync watchdog for epoch start block

--- integrationTests/testSyncNode.go
+++ integrationTests/testSyncNode.go
@@ -246,6 +246,7 @@ func (tpn *TestProcessorNode) createMetaChainBootstrapper() (TestBootstrapper, e
 		EpochBootstrapper:           tpn.EpochStartTrigger,
 		ValidatorAccountsDB:         tpn.PeerState,
 		ValidatorStatisticsDBSyncer: &mock.AccountsDBSyncerStub{},
+		Watchdog:                    &testscommon.WatchdogMock{},
 	}
 
 	bootstrap, err := sync.NewMetaBootstrap(argsMetaBootstrapper)
