f60f0138a5e6536be4086fc4ed90f8c402b0cc5e
Sorin Stanculeanu <sorinstanculeanu26@gmail.com>
2026-07-03 16:19:56 +0300

    fix linter

--- process/block/interceptedBlocks/common.go
+++ process/block/interceptedBlocks/common.go
@@ -10,7 +10,6 @@ import (
 	"github.com/multiversx/mx-chain-go/sharding"
 )
 
-const maxLenMiniBlockReservedField = 10
 const maxLenMiniBlockHeaderReservedField = 32
 
 func checkForDuplicateHashes(hashes [][]byte) error {
